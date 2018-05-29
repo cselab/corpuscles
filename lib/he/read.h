3bf98447a71695085a0244d7b3d1eb5cf2a2abdf
Sergey Litvinov <slitvinov@gmail.com>
2018-05-29 17:36:03 +0200

    define an interface

--- lib/he/read.h
+++ lib/he/read.h
@@ -2,7 +2,21 @@
 
 typedef struct T T;
 
-int he_read(const char *path, T**);
-int he_fin(T*);
+int he_read_ini(const char *path, T**);
+int he_read_fin(T*);
+
+int he_read_nv(T*);
+int he_read_nt(T*);
+int he_read_ne(T*);
+int he_read_nh(T*);
+
+int he_read_next(T*, int**);
+int he_read_flip(T*, int**);
+
+int he_read_ver(T*, int**);
+int he_read_tri(T*, int**);
+int he_read_edg(T*, int**);
+
+int he_read_validate(T*);
 
 #undef T
