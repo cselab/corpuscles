bcf30f562a8ea0e09efe472eb36715b8e647068f
Sergey Litvinov <slitvinov@gmail.com>
2018-05-29 22:29:12 +0200

    set api

--- lib/he/he.h
+++ lib/he/he.h
@@ -6,7 +6,14 @@ typedef struct HeRead HeRead;
 int he_ini(HeRead*, T**);
 int he_fin(T*);
 
-int he_nxt(
-int he_next(
+int he_nxt(T*);
+int he_flp(T*);
+int he_ver(T*);
+int he_tri(T*);
+int he_edg(T*);
+
+int he_hdg_ver(T*);
+int he_hdg_edg(T*);
+int he_hdg_tri(T*);
 
 #undef T
