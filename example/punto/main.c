2a01a2d24d3e0425fab63dfa3eb4465ec592d95e
Sergey Litvinov <slitvinov@gmail.com>
2018-05-29 17:00:12 +0200

    missing main

---
+++ example/punto/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <he/err.h>
+#include <he/punto.h>
+#include <he/macro.h>
+
+#define N (3)
+
+static double XX[N];
+static double YY[N];
+static double ZZ[N];
+static double *queue[] = {XX, YY, ZZ, NULL};
+
+int main(__UNUSED int argc, const char *argv[]) {
+    const char *i, *o;
+    int n;
+    if (argv[1] == NULL)
+        ERR("needs FILE.in");
+    i = argv[1]; argv++;
+
+    if (argv[1] == NULL)
+        ERR("needs FILE.out");
+    o = argv[1]; argv++;
+    
+    punto_read(i, /**/ &n, queue);
+    MSG("n = %d", n);
+    MSG("writing: '%s'", o);
+    punto_write(n, queue, o);
+}
