ba6618d7556818c24bbe3462cc0b1d19d3088668
Sergey Litvinov <slitvinov@gmail.com>
2018-05-29 16:39:48 +0200

    add make file fragments

---
+++ make/dir.mk
@@ -0,0 +1,13 @@
+# run make in all directory in `D`
+
+PREFIX = $(HOME)
+S = $(PWD)
+
+install:
+	@install0 () ( cd "$$d" && $(MAKE) PREFIX=$(PREFIX) S=$S); \
+	for d in $D; \
+	do install0; \
+	echo dir "'$$d'"; \
+	done
+
+.PHONY: install
