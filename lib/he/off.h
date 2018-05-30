8a195b1b361acefb5008f3f3b6f45af5cca83226
Sergey Litvinov <slitvinov@gmail.com>
2018-05-30 10:43:08 +0200

    initial import of off.h

---
+++ lib/he/off.h
@@ -0,0 +1,11 @@
+#define T HeOff
+
+typedef struct T T;
+
+int he_off_ini(const char *path, T**);
+int he_off_fin(T*);
+
+int he_off_nt(T*);
+int he_off_nv(T*);
+int he_off_xyz(T*, real**);
+int he_off_tri(T*, int**);
