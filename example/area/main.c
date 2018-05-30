8065bce5e58a0cdb03a77b0aaea8a5bb4bcc43d5
Sergey Litvinov <slitvinov@gmail.com>
2018-05-30 23:46:59 +0200

    minor

--- example/area/main.c
+++ example/area/main.c
@@ -15,7 +15,6 @@ void get3(int i, int j, int k, /**/ real a[3], real b[3], real c[3]) {
 void main0() {
     int m, i, j, k;
     real A, a[3], b[3], c[3];
-
     A = 0;
     for (m = 0; m < NT; m++) {
         i = T0[m]; j = T1[m]; k = T2[m];
@@ -23,7 +22,6 @@ void main0() {
         A += tri_area(a, b, c);
     }
     printf("%g\n", A);
-    
 }
 
 int main() {
