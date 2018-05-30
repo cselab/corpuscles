f6a7b892af6893558b74aa2765c0b2c834ec7b43
Sergey Litvinov <slitvinov@gmail.com>
2018-05-30 16:19:53 +0200

    more or less woriking version of edg

--- example/edg/main.c
+++ example/edg/main.c
@@ -2,14 +2,40 @@
 
 #include <he/err.h>
 #include <he/edg.h>
+#include <he/util.h>
 
 static HeEdg *edg;
 
 static void ini(int n) { he_edg_ini(n, &edg); }
 static void fin() { he_edg_fin(edg); }
+static int eq(const char *a, const char *b) { return util_eq(a, b); }
+static void set(int i, int j, int v) {
+    MSG("set %d %d %d", i, j, v);
+    he_edg_set(edg, i, j, v);
+}
+static void get(int i, int j) {
+    int v;
+    v = he_edg_get(edg, i, j);
+    MSG("get %d %d %d", i, j, v);
+}
 
 int main() {
-    int n;
-    ini(n = 10);
+    const char *path = "/dev/stdin";
+    FILE *f;
+    char cmd[2048], line[2048];
+    int i, j, v, c, n;
+    f = fopen(path, "r");
+    if (util_fgets(line, f) == NULL)
+        ER("fail to read file '%s'", path);
+    if (sscanf(line, "%d", &n) != 1)
+        ER("wrong first line: '%s'", line);
+    ini(n);
+    while (util_fgets(line, f) != NULL) {
+        c = sscanf(line, "%s %d %d %d", cmd, &i, &j, &v);
+        if (eq(cmd, "set") && c == 4) set(i, j, v);
+        else if (eq(cmd, "get") && c == 3) get(i, j);
+        else ER("wrong line: '%s'", line);
+    }
+    fclose(f);
     fin();
 }
--- lib/edg.c
+++ lib/edg.c
@@ -4,9 +4,12 @@
 #include "he/edg.h"
 
 #define T HeEdg
+#define E(fmt, ...) ERR(HE_INDEX, fmt, ##__VA_ARGS__);
+
 typedef struct Node Node;
 struct Node {
-    int v;
+    int j;
+    int v; /* value */
     Node  *nxt;
 };
 struct T {
@@ -33,7 +36,6 @@ static void free_node(Node *p) {
         p = n;
     }
 }
-
 int he_edg_fin(T *q) {
     int n, i;
     n = q->n;
@@ -42,4 +44,45 @@ int he_edg_fin(T *q) {
     FREE(q->node);
     FREE(q);
     return HE_OK;
+}
+
+int he_edg_set(T *q, int i, int j, int v) {
+    int n;
+    Node *nxt, *prv, *new;
+    n = q->n;
+    if (0 > i || i >= n || 0 > j || j >= n)    
+        E("[%d %d], n = %d", i, j, n);
+    
+    prv = q->node[i];
+    while (prv != NULL) {
+        if (prv->j == j) {
+            MSG("reset [%d %d] to %d", i, j, v);
+            return HE_OK;
+        }
+        nxt = prv->nxt;
+        if (nxt == NULL) break;
+        prv = nxt;
+    }
+
+    MALLOC(1, &new);
+    new->j = j;
+    new->v = v;
+    new->nxt = NULL;
+    if (prv == NULL) q->node[i] = new;
+    else prv->nxt = new;
+    return HE_OK;
+}
+
+int he_edg_get(T *q, int i, int j) {
+    int n;
+    Node *node;
+    n = q->n;
+    if (0 > i || i >= n || 0 > j || j >= n)
+        E("[%d %d], n = %d", i, j, n);
+    node = q->node[i];
+    while (node != NULL) {
+        if (node->j == j) return node->v;
+        node = node->nxt;
+    }
+    return -1;
 }
