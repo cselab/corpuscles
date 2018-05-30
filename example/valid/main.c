96d71e602394a24a3ea21da84a8b521fe9bcec08
Sergey Litvinov <slitvinov@gmail.com>
2018-05-30 20:36:40 +0200

    validate dih

--- example/valid/main.c
+++ example/valid/main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+#include <real.h>
 #include <he/err.h>
 #include <he/x.h>
 
 int main() {
     int v, v0, e, e0, t, t0;
     int h, h0, f, cnt;
-    ini("/dev/stdin");
+    int i, j, k, l;
+    int i0, j0, i1, j1;
+    int n, nn, nnf;
 
+    ini("/dev/stdin");
     for (v = 0; v < NV; v++) { /* from vert and back */
         v0 = ver(hdg_ver(v));
         if (v != v0) ER("v=%d != v0=%d", v, v0);
@@ -40,6 +44,25 @@ int main() {
         h0 = flp(f);
         if (h != h0) ER("%d != %d", h, h0);
     }
+
+    for (h = 0; h < NH; h++) { if (bnd(h)) continue;
+        e = edg(h);
+        n = nxt(h); nn = nxt(nxt(h)); nnf = nxt(nxt(flp(h)));
+        i = D0[e]; j = D1[e]; k = D2[e]; l = D3[e];
+
+        i0 = ver(nn); i1 = ver(nnf);
+        if (i != i0 && i != i1)
+            ER("%d is not among [%d, %d]", i, i0, i1);
+        if (l != i0 && l != i1)
+            ER("%d is not among [%d, %d]", l, i0, i1);
+
+        j0 = ver(h);  j1 = ver(n);
+        if (j != j0 && j != j1)
+            ER("%d is not among [%d, %d]", j, j0, j1);
+        if (k != j0 && k != j1)
+            ER("%d is not among [%d, %d]", k, j0, j1);
+    }
+
     fin();
     MSG("ok");
     return(HE_OK);
--- lib/x.c
+++ lib/x.c
@@ -14,51 +14,6 @@ int *T0, *T1, *T2;
 int *D0, *D1, *D2, *D3;
 real *XX, *YY, *ZZ;
 
-int ini(const char *path) {
-    int i, *tri, *tri0;
-    real *ver, *ver0;
-    HeOff *off;
-    HeRead *read;
-
-    he_off_ini(path, &off);
-
-    NV = he_off_nv(off);
-    NT = he_off_nt(off);
-
-    he_off_tri(off, &tri0);
-    he_off_ver(off, &ver0);
-    he_read_tri_ini(NV, NT, tri0, &read);
-
-    he_ini(read, &he);
-    NV = he_nv(he); NT = he_nt(he);
-    NE = he_ne(he); NH = he_nh(he);
-
-    MALLOC(NV, &XX); MALLOC(NV, &YY); MALLOC(NV, &ZZ);
-    MALLOC(NT, &T0); MALLOC(NT, &T1); MALLOC(NT, &T2);
-    MALLOC(NE, &D0); MALLOC(NE, &D1); MALLOC(NE, &D2); MALLOC(NE, &D2);
-
-    for (ver = ver0, i = 0; i < NV; i++) {
-        XX[i] = *ver++; YY[i] = *ver++; ZZ[i] = *ver++;
-    }
-
-    for (tri = tri0, i = 0; i < NT; i++) {
-        T0[i] = *tri++; T1[i] = *tri++; T2[i] = *tri++;
-    }
-
-    he_read_fin(read);
-    he_off_fin(off);
-
-    return HE_OK;
-}
-
-int  fin()      {
-    FREE(XX); FREE(YY); FREE(ZZ);
-    FREE(T0); FREE(T1); FREE(T2);
-    FREE(D0); FREE(D1); FREE(D2); FREE(D2);
-
-    he_fin(he);
-    return HE_OK;
-}
 int  nxt(int h) { return he_nxt(he, h); }
 int  flp(int h) { return he_flp(he, h); }
 
@@ -70,3 +25,60 @@ int  hdg_ver(int v) { return he_hdg_ver(he, v); }
 int  hdg_edg(int e) { return he_hdg_edg(he, e); }
 int  hdg_tri(int t) { return he_hdg_tri(he, t); }
 int  bnd(int h)     { return he_bnd(he, h); }
+
+int ini(const char *path) {
+    int i, j, k, l, h, e, n, nn, nnf;
+    int *tri, *tri0;
+    real *xyz, *xyz0;
+    HeOff *off;
+    HeRead *read;
+
+    he_off_ini(path, &off);
+
+    NV = he_off_nv(off);
+    NT = he_off_nt(off);
+
+    he_off_tri(off, &tri0);
+    he_off_ver(off, &xyz0);
+    he_read_tri_ini(NV, NT, tri0, &read);
+
+    he_ini(read, &he);
+    NV = he_nv(he); NT = he_nt(he);
+    NE = he_ne(he); NH = he_nh(he);
+
+    MALLOC(NV, &XX); MALLOC(NV, &YY); MALLOC(NV, &ZZ);
+    MALLOC(NT, &T0); MALLOC(NT, &T1); MALLOC(NT, &T2);
+    MALLOC(NE, &D0); MALLOC(NE, &D1); MALLOC(NE, &D2); MALLOC(NE, &D3);
+
+    for (xyz = xyz0, i = 0; i < NV; i++) {
+        XX[i] = *xyz++; YY[i] = *xyz++; ZZ[i] = *xyz++;
+    }
+
+    for (tri = tri0, i = 0; i < NT; i++) {
+        T0[i] = *tri++; T1[i] = *tri++; T2[i] = *tri++;
+    }
+
+    for (e = 0; e < NE; e++) { /* i[jk]l */
+        h = hdg_edg(e); n = nxt(h); nn = nxt(nxt(h));
+        j = ver(h); k = ver(n); i = ver(nn);
+        if (!bnd(h)) {
+            nnf = nxt(nxt(flp(h)));
+            l = ver(nnf);
+        } else l = -1;
+        D0[e] = i; D1[e] = j; D2[e] = k; D3[e] = l;
+    }
+
+    he_read_fin(read);
+    he_off_fin(off);
+
+    return HE_OK;
+}
+
+int  fin()      {
+    FREE(XX); FREE(YY); FREE(ZZ);
+    FREE(T0); FREE(T1); FREE(T2);
+    FREE(D0); FREE(D1); FREE(D2); FREE(D3);
+
+    he_fin(he);
+    return HE_OK;
+}
