8db29872e5ccd602cebe85f201976beebaec2fa3
Sergey Litvinov <slitvinov@gmail.com>
2018-05-30 15:21:31 +0200

    add free_node

--- lib/edg.c
+++ lib/edg.c
@@ -11,13 +11,36 @@ struct Node {
 };
 struct T {
     int n;
-    Node *node;
+    Node **node;
 };
 
 int he_edg_ini(int n, T **pq) {
     T *q;
+    int i;
     MALLOC(1, &q);
-
+    MALLOC(n, &q->node);
+    for (i = 0; i < n; i++)
+        q->node[i] = NULL;
     *pq = q;
+    return HE_OK;
+}
+
+
+static void free_node(Node *p) {
+    Node *n;
+    while (p != NULL) {
+        n = p->nxt;
+        FREE(p);
+        p = n;
+    }
+}
+
+int he_edg_fin(T *q) {
+    int n, i;
+    n = q->n;
+    for (i = 0; i < n; i++)
+        free_node(q->node[i]);
+    FREE(q->node);
+    FREE(q);
     return HE_OK;
 }
