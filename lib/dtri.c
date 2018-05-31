ddcfa9468f1755f9e1a5060cbdce1dcd5a1b7e4c
Sergey Litvinov <slitvinov@gmail.com>
2018-05-31 13:08:02 +0200

    upd analit

---
+++ lib/dtri.c
@@ -0,0 +1,12 @@
+#include <math.h>
+#include <stdio.h>
+
+#include "real.h"
+#include "he/err.h"
+#include "he/vec.h"
+#include "he/tri.h"
+#include "he/dtri.h"
+
+int dtri_ctan(real[3], real b[3], real[3], /**/
+              real[3], real[3], real[3]) {
+}
---
+++ lib/he/dtri.h
@@ -0,0 +1,2 @@
+int dtri_ctan(real[3], real b[3], real[3], /**/
+              real[3], real[3], real[3]);
