c9cbe6d6d07c31c8ea248c5c88e35dd86de3046b
Sergey Litvinov <slitvinov@gmail.com>
2018-05-31 21:47:56 +0200

    define vectors

--- example/ten/main.c
+++ example/ten/main.c
@@ -8,10 +8,11 @@
 #include <he/ten.h>
 
 int main() {
+    real a[3] = {1, 2, 3}, b[3] = {10, 20, 30};
     Ten T, R, P;
-    ten_one(&T);
+    ten_dyadic(a, b, &T);
     ten_one(&R);
-    ten_plus(T, T, /**/ &P);
+    ten_plus(T, T, &P);
     ten_printf(P, "%-6.2f");
     return 0;
 }
