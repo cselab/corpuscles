1802f2c181d31129dc5388ebcf7c9d72221b81de
Sergey Litvinov <slitvinov@gmail.com>
2018-06-01 17:19:16 +0200

    add dtri_ctan

---
+++ example/dtri/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include <real.h>
+#include <he/err.h>
+#include <he/vec.h>
+#include <he/dtri.h>
+#include <he/macro.h>
+#include <he/util.h>
+
+#define FMT_IN   XE_REAL_IN
+static const char **argv;
+
+void vec(/**/ real a[3]) { vec_argv(&argv, a); }
+
+int eq(const char *a, const char *b) { return util_eq(a, b); }
+int main(__UNUSED int argc, const char **v) {
+    const char *op;
+    real a[3], b[3], c[3], da[3], db[3], dc[3];
+    argv = v;
+    argv++;
+    if (*argv == NULL) ER("mssing OP");
+
+    op = *argv++;
+    if (eq(op, "angle")) {
+        vec(a); vec(b); vec(c);
+        dtri_angle(a, b, c, da, db, dc);
+        vec_printf(da, "%g");
+        vec_printf(db, "%g");
+        vec_printf(dc, "%g");
+    } else if (eq(op, "ctan")) {
+        vec(a); vec(b); vec(c);
+        dtri_ctan(a, b, c, da, db, dc);
+        vec_printf(da, "%g");
+        vec_printf(db, "%g");
+        vec_printf(dc, "%g");
+    } else        
+        ER("unknown operation '%s'", op);
+    return 0;
+}
