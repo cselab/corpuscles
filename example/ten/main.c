e945f5fa69323d5df4fa33bd17a89bc9306dae87
Sergey Litvinov <slitvinov@gmail.com>
2018-06-01 15:13:12 +0200

    add ten test

--- example/ten/main.c
+++ example/ten/main.c
@@ -9,22 +9,33 @@
 #include <he/macro.h>
 #include <he/util.h>
 
+static const char **argv;
+void vec(/**/ real a[3]) { vec_argv(&argv, a); }
+void ten(/**/ Ten *T) { ten_argv(&argv, T); }
+
 int eq(const char *a, const char *b) { return util_eq(a, b); }
-int main(__UNUSED int c, const char *v[]) {
+int main(__UNUSED int c, const char **v) {
+    argv = v;
+    
+    const char *op;
     real a[3], b[3];
     Ten T, R, P, Q;
+    
+    argv++;
+    if (*argv == NULL) ER("mssing OP");
 
-    v++; vec_argv(&v, a);
-    vec_printf(a, "%-.6g");
-    return 0;
-    
-    v++;
-    if (v[0] == NULL) ER("mssing OP");
-    
-    if (eq(v[0], "dyadic")) {
-        v++; vec_argv(&v, a);
+    op = *argv++;
+    if (eq(op, "dyadic")) {
+        vec(a);
+        vec(b);
+        ten_dyadic(a, b, &T);
+        ten_printf(T, "%g");
+    } else if (eq(op, "plus")) {
+        ten(&T);
+        ten(&R);
+        ten_plus(T, R, &P);
+        ten_printf(P, "%g");
     } else
         ER("unknown operation '%s'", v[0]);
-    
     return 0;
 }
