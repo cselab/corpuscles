e347e9981a0ba6d53da428cfc758b9043123a98a
Sergey Litvinov <slitvinov@gmail.com>
2018-06-01 16:57:11 +0200

    add angle0

--- lib/dtri.c
+++ lib/dtri.c
@@ -7,10 +7,11 @@
 #include "he/tri.h"
 #include "he/dtri.h"
 
-/*
-int dtri_ctan(real  a[3], real  b[3], real  c[3],
-              real da[3], real db[3], real dc[3]) {
-    return HE_OK;
+static void angle0(real a[3], real b[3], real n[3],
+                   real da[3]) {
+    real v0, v[3], nv[3];
+    vec_minus(b, a,  v);
+    vec_cross(n, v, nv);
+    v0 = vec_dot(v, v); /* TODO: */
+    vec_scalar(nv, 1/v0,  da);
 }
-
-*/
