0943cfca098fbf8e24297c91d5c376e94aa4ec7f
Xin Bian <xin.z.bian@gmail.com>
2018-06-01 15:11:41 +0200

    added energy of kantor and nelson 1987

---
+++ example/eng/kantor_nelson/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <math.h>
+
+#include <real.h>
+#include <he/err.h>
+#include <he/x.h>
+#include <he/vec.h>
+#include <he/tri.h>
+#include <he/memory.h>
+
+void get4(int i, int j, int k, int l, /**/
+          real a[3], real b[3], real c[3], real d[3]) {
+    vec_get(i, XX, YY, ZZ, a);
+    vec_get(j, XX, YY, ZZ, b);
+    vec_get(k, XX, YY, ZZ, c);
+    vec_get(l, XX, YY, ZZ, d);    
+}
+
+void energy_kantor_nelson() {
+    enum {X, Y, Z};
+    int v, e;
+    int i, j, k, l;
+    real r0, ang, eng, a[3], b[3], c[3], d[3], r[3];
+    real *E;
+    real coef;
+
+    coef = 2.0*sqrt(3.0);
+      
+    MALLOC(NV, &E);
+    for (v = 0; v < NV; v++) E[v] = 0;
+    for (e = 0; e < NE; e++) {
+        i = D0[e]; j = D1[e]; k = D2[e]; l = D3[e];
+        get4(i, j, k, l, /**/ a, b, c, d);
+        ang = tri_dih(a, b, c, d);
+        eng = coef*(1 - cos(ang));
+        eng /= 4;
+        E[i] += eng; E[j] += eng; E[k] += eng; E[l] += eng;
+    }
+    for (v = 0; v < NV; v++) {
+        vec_get(v, XX, YY, ZZ, r);
+        r0 = vec_cylindrical_r(r);
+        printf("%g %g\n", r0, E[v]);
+    }
+    FREE(E);
+}
+
+int main() {
+    ini("/dev/stdin");
+    energy_kantor_nelson();
+    fin();
+    return HE_OK;
+}
