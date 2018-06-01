020304b9878bd6315de2f5717293daad31df440b
Sergey Litvinov <slitvinov@gmail.com>
2018-06-01 11:36:40 +0200

    add test data initial import of ten_argv

--- example/ten/main.c
+++ example/ten/main.c
@@ -9,10 +9,14 @@
 
 int main() {
     real a[3] = {1, 2, 3}, b[3] = {10, 20, 30};
-    Ten T, R, P;
+    Ten T, R, P, Q;
     ten_dyadic(a, b, &T);
     ten_one(&R);
     ten_plus(T, T, &P);
     ten_printf(P, "%-6.2f");
+
+    ten_fscanf(stdin, &Q);
+    ten_printf(Q, "%-6.2f");
+    
     return 0;
 }
--- lib/he/ten.h
+++ lib/he/ten.h
@@ -19,4 +19,7 @@ void ten_one(/**/ Ten*);
 int ten_sprintf(Ten, const char *fmt, /**/ char *s);
 int ten_fprintf(Ten, FILE*, const char *fmt);
 int ten_printf(Ten, const char *fmt);
+
 int ten_fscanf(FILE*, /**/ Ten*);
+/* read from argument list */
+int ten_argv(const char **pargv[], /**/ Ten*);
