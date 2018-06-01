85dabd1520f8aee1be5ff4f7c5420737f86557a8
Sergey Litvinov <slitvinov@gmail.com>
2018-06-01 09:28:24 +0200

    reverse $?

--- make/dir.mk
+++ make/dir.mk
@@ -7,7 +7,7 @@ dir:
 	@install0 () ( cd "$$d" && $(MAKE) PREFIX=$(PREFIX) S=$S $(MAKECMDGOALS) ); \
 	for d in $D; \
 	do install0; \
-	if test $$? -eq 0; \
+	if test $$? -ne 0; \
 	then printf >&2 "[dir.mk] make failed in '$$d'\n"; \
 	     exit 2; \
 	fi; \
