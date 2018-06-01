6612a6474216e41e3175237efe084062809d461f
Sergey Litvinov <slitvinov@gmail.com>
2018-06-01 17:32:28 +0200

    add HE_NUM

--- lib/dtri.c
+++ lib/dtri.c
@@ -13,7 +13,8 @@ static int angle0(real a[3], real b[3], real n[3],
     real v0, v[3], nv[3];
     vec_minus(b, a,  v);
     vec_cross(n, v, nv);
-    v0 = vec_dot(v, v); /* TODO: */
+    v0 = vec_dot(v, v);
+    if (v0 == 0) ERR(HE_NUM, "s = v0");    
     vec_scalar(nv, 1/v0,  da);
     return HE_OK;
 }
@@ -33,6 +34,7 @@ int dtri_cot(real a[3], real b[3], real c[3],   real da[3], real db[3], real dc[
     ang = tri_angle(a, b, c);
     s = sin(ang);
     s = -1/(s*s);
+    if (s == 0) ERR(HE_NUM, "s = 0");
     dtri_angle(a, b, c,   da0, db0, dc0);
     vec_scalar(da0, s,   da);
     vec_scalar(db0, s,   db);
