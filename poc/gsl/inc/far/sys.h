1f19504535e1272f941406ad5b82f3fa008c9103
Sergey Litvinov <slitvinov@gmail.com>
2018-06-07 19:02:43 +0200

    add far/sys

--- poc/gsl/inc/far/sys.h
+++ poc/gsl/inc/far/sys.h
@@ -1,33 +1,33 @@
 enum {DIM = 3};
 typedef struct Param Param;
-struct Param { double b, s, r; };
+struct Param { double al, om, la, be; };
 
 static Param param;
 static double q[DIM];
 
 static void ini(void) {
-    enum {X, Y, Z};
-    double a[DIM] = {-4.79874e+00, -3.87759e+00, 2.42234e+01};
-    double b[DIM] = {-4.84818e+00, -5.81392e+00, 2.08459e+01};
-    param.b = 8/3.0; param.s = 10; param.r = 28;
-    // q[0] = q[1] = q[2] = 1.0;
+    double pi = 3.141592653589793;
+    param.al = 0.01;
+    param.om = 2*pi;
+    param.la = 0.1;
+    param.be = 0.1;
     
-    q[X] = (a[X] + b[X])/2;
-    q[Y] = (a[Y] + b[Y])/2;
-    q[Z] = (a[Z] + b[Z])/2;
+    q[0] = 0.0; q[1] = q[2] = 0.01;
 }
 
+static double sq(double x) { return x*x; }
 static int f(__UNUSED double t, const double q[], double f[], __UNUSED void *vparam) {
-    double b, s, r;
+    double al, om, la, be;
     double x, y, z, dx, dy, dz;
     assert(DIM == 3);
 
-    b = param.b; s = param.s; r = param.r;
+    al = param.al; om = param.om; la = param.la; be = param.be;
+    
     x = q[0]; y = q[1]; z = q[2];
 
-    dx = s*(y-x);
-    dy = (-x*z)-y+r*x;
-    dz = x*y-b*z;
+    dx = al*x + om*y + al*sq(x) + 2*om*x*y + sq(z);
+    dy = -om*x + al*y - om*sq(x) + 2*al*x*y;
+    dz = -la*z - (la + be)*x*z;
 
     f[0] = dx; f[1] = dy; f[2] = dz;
 
--- poc/gsl/main.c
+++ poc/gsl/main.c
@@ -4,17 +4,15 @@
 #include <gsl/gsl_errno.h>
 #include <gsl/gsl_odeiv2.h>
 
+#include <he/err.h>
 #include <he/macro.h>
-
 #include "sys.h"
 
-int main (void) {
-    Param param;
+int main(void) {
     gsl_odeiv2_system sys = {f, NULL, DIM, &param};
     gsl_odeiv2_driver *driver;
     int i;
     double t, t1;
-    double y[DIM] = { 1.0, 1.0, 1.0 };
     double ti;
     int n, status;
     double istep, rel, abs;
@@ -22,21 +20,22 @@ int main (void) {
     istep = 1e-6; /* initial step size */
     rel = 1e-6;
     abs = 0.0;
-    param.b = 8/3.0; param.s = 10; param.r = 28;
-    n = 20000;
-    t = 0.0, t1 = 200.0;
-
+    n = 200000;
+    t = 0.0, t1 = 2000.0;
+    ini();
+    
     driver = gsl_odeiv2_driver_alloc_y_new (&sys, gsl_odeiv2_step_rk8pd,
                                        istep, rel, abs);
     for (i = 1; i <= n; i++) {
         ti = i * t1 / n;
-        status = gsl_odeiv2_driver_apply(driver, &t, ti, y);
+        status = gsl_odeiv2_driver_apply(driver, &t, ti, q);
         if (status != GSL_SUCCESS) {
             printf ("error, return value=%d\n", status);
             break;
         }
-        printf ("%.5e %.5e %.5e %.5e\n",
-                t, y[0], y[1], y[2]);
+        printf("%.5e ", t);
+        print(stdout, "%.5e");
+        putc('\n', stdout);
     }
     gsl_odeiv2_driver_free(driver);
     return 0;
