beb3c2a27d7d9f8662983b963d5b646581961f4c
Sergey Litvinov <slitvinov@gmail.com>
2018-06-07 11:58:45 +0200

    minor

--- poc/gsl/main.c
+++ poc/gsl/main.c
@@ -3,19 +3,28 @@
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_odeiv2.h>
 
-int func (double t, const double y[], double f[], void *params) {
-    double mu = *(double *)params;
+#include <he/macro.h>
+
+int func (__UNUSED double t, const double y[], double f[], void *params) {
+    double mu;
+    mu = *(double*)params;
     f[0] = y[1];
     f[1] = -y[0] - mu*y[1]*(y[0]*y[0] - 1);
     return GSL_SUCCESS;
 }
 
-int jac (double t, const double y[], double *dfdy,
-         double dfdt[], void *params) {
-    double mu = *(double *)params;
-    gsl_matrix_view dfdy_mat
-        = gsl_matrix_view_array (dfdy, 2, 2);
-    gsl_matrix * m = &dfdy_mat.matrix;
+int jac (__UNUSED double t, const double y[], double *dfdy, double dfdt[], void *params) {
+    double mu; 
+    gsl_matrix_view dfdy_mat;
+    gsl_matrix *m;
+
+    dfdy_mat = gsl_matrix_view_array (dfdy, 2, 2);    
+    m =  &dfdy_mat.matrix;
+    mu = *(double*)params;
+    
+    gsl_matrix_set (m, 0, 0, 0.0);
+    gsl_matrix_set (m, 0, 1, 1.0);
+    mu = *(double*)params;
     
     gsl_matrix_set (m, 0, 0, 0.0);
     gsl_matrix_set (m, 0, 1, 1.0);
