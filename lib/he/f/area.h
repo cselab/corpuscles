9c98b53913647eac3a6d68097b9e11a4ba001002
Sergey Litvinov <slitvinov@gmail.com>
2018-06-12 21:05:52 +0200

    add getters

---
+++ lib/he/f/area.h
@@ -0,0 +1,15 @@
+#define T HeFArea
+
+typedef struct He He;
+
+int he_f_area_ini(real a0, real k, He*, T**);
+int he_f_area_fin(T*);
+
+int he_f_area_compute(T*, He*,
+                      real *x, real *y, real *z, /**/
+                      real *fx, real *fy, real *fz);
+
+int he_f_area_a(T*, /**/ real  **a);
+int he_f_area_da(T*, /**/ real **da);
+
+#undef T
