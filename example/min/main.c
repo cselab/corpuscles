ba5a235522b838c93b26277ae1c17961cfaee9ff
Sergey Litvinov <slitvinov@gmail.com>
2018-06-12 18:19:12 +0200

    minor

---
+++ example/min/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+#include <real.h>
+
+#include <he/err.h>
+#include <he/x.h>
+#include <alg/x.h>
+
+int main() {
+    ini("/dev/stdin");
+    min_ini();
+
+    fin();
+    return 0;
+}
