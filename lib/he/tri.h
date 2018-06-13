77a6918d5942f43a282ee0de37d7d856d6131da6
Sergey Litvinov <slitvinov@gmail.com>
2018-06-13 09:40:56 +0200

    minor

--- lib/he/tri.h
+++ lib/he/tri.h
@@ -4,4 +4,3 @@ real tri_dih(real[3], real[3], real[3], real[3]);
 void tri_normal(real[3], real[3], real[3], /**/ real[3]);
 real tri_angle(real[3], real b[3], real[3]);
 real tri_cot(real[3], real b[3], real[3]);
-
