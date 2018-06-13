58ecec9ef68a01ab24cf93855af73abdfa6f53d7
Sergey Litvinov <slitvinov@gmail.com>
2018-06-13 19:09:33 +0200

    always pointer for Ten

--- lib/he/ten.h
+++ lib/he/ten.h
@@ -1,24 +1,24 @@
 typedef struct Ten { real t[3*3]; } Ten;
 
-void ten_dyadic(real[3], real[3], /**/ Ten*);
+void ten_dyadic(const real[3], const real[3], /**/ Ten*);
 
-void ten_plus(Ten, Ten, /**/ Ten*);
-void ten_minus(Ten, Ten, /**/ Ten*);
-void ten_scalar(Ten, real, /**/ Ten*);
-void ten_vec(Ten, real[3], /**/ real[3]);
+void ten_plus(const Ten*, const Ten*, /**/ Ten*);
+void ten_minus(const Ten*, const Ten*, /**/ Ten*);
+void ten_scalar(const Ten*, real, /**/ Ten*);
+void ten_vec(const Ten*, const real[3], /**/ real[3]);
 
 /* y += x */
-void ten_add(Ten x, /*io*/ Ten *y);
+void ten_add(const Ten *x, /*io*/ Ten *y);
 
 /* y += a*x */
-void ten_axpy(real a, Ten x, /*io*/ Ten *y);
-void ten_copy(Ten, /**/ Ten*);
+void ten_axpy(real a, const Ten *x, /*io*/ Ten *y);
+void ten_copy(const Ten*, /**/ Ten*);
 void ten_zero(/**/ Ten*);
 void ten_one(/**/ Ten*);
 
-int ten_sprintf(Ten, const char *fmt, /**/ char *s);
-int ten_fprintf(Ten, FILE*, const char *fmt);
-int ten_printf(Ten, const char *fmt);
+int ten_sprintf(const Ten*, const char *fmt, /**/ char *s);
+int ten_fprintf(const Ten*, FILE*, const char *fmt);
+int ten_printf(const Ten*, const char *fmt);
 
 int ten_fscanf(FILE*, /**/ Ten*);
 
--- lib/ten.c
+++ lib/ten.c
@@ -10,7 +10,7 @@ enum {XX, XY, XZ,   YX, YY, YZ,   ZX, ZY, ZZ};
 enum {SIZE = MAX_STRING_SIZE};
 #define FMT_IN   XE_REAL_IN
 
-void ten_dyadic(real a[3], real b[3], /**/ Ten *T) {
+void ten_dyadic(const real a[3], const real b[3], /**/ Ten *T) {
     real *t;
     t = T->t;
     t[XX] = a[X]*b[X]; t[XY] = a[X]*b[Y]; t[XZ] = a[X]*b[Z];
@@ -18,57 +18,63 @@ void ten_dyadic(real a[3], real b[3], /**/ Ten *T) {
     t[ZX] = a[Z]*b[X]; t[ZY] = a[Z]*b[Y]; t[ZZ] = a[Z]*b[Z];
 }
 
-void ten_plus(Ten P, Ten R, /**/ Ten *T) {
-    real *p, *r, *t;
-    p = P.t; r = R.t; t = T->t;
+void ten_plus(const Ten *P, const Ten *R, /**/ Ten *T) {
+    const real *p, *r;
+    real *t;
+    p = P->t; r = R->t; t = T->t;
     t[XX] = p[XX] + r[XX]; t[XY] = p[XY] + r[XY]; t[XZ] = p[XZ] + r[XZ];
     t[YX] = p[YX] + r[YX]; t[YY] = p[YY] + r[YY]; t[YZ] = p[YZ] + r[YZ];
     t[ZX] = p[ZX] + r[ZX]; t[ZY] = p[ZY] + r[ZY]; t[ZZ] = p[ZZ] + r[ZZ];
 }
 
-void ten_minus(Ten P, Ten R, /**/ Ten *T) {
-    real *p, *r, *t;
-    p = P.t; r = R.t; t = T->t;
+void ten_minus(const Ten *P, const Ten *R, /**/ Ten *T) {
+    const real *p, *r;
+    real *t;
+    p = P->t; r = R->t; t = T->t;
     t[XX] = p[XX] - r[XX]; t[XY] = p[XY] - r[XY]; t[XZ] = p[XZ] - r[XZ];
     t[YX] = p[YX] - r[YX]; t[YY] = p[YY] - r[YY]; t[YZ] = p[YZ] - r[YZ];
     t[ZX] = p[ZX] - r[ZX]; t[ZY] = p[ZY] - r[ZY]; t[ZZ] = p[ZZ] - r[ZZ];
 }
 
-void ten_scalar(Ten P, real s, /**/ Ten *T) {
-   real *p, *t;
-   p = P.t; t = T->t;
-   t[XX] = s*p[XX]; t[XY] = s*p[XY]; t[XZ] = s*p[XZ];
-   t[YX] = s*p[YX]; t[YY] = s*p[YY]; t[YZ] = s*p[YZ];
-   t[ZX] = s*p[ZX]; t[ZY] = s*p[ZY]; t[ZZ] = s*p[ZZ];
+void ten_scalar(const Ten *P, real s, /**/ Ten *T) {
+    const real *p;
+    real *t;
+    p = P->t; t = T->t;
+    t[XX] = s*p[XX]; t[XY] = s*p[XY]; t[XZ] = s*p[XZ];
+    t[YX] = s*p[YX]; t[YY] = s*p[YY]; t[YZ] = s*p[YZ];
+    t[ZX] = s*p[ZX]; t[ZY] = s*p[ZY]; t[ZZ] = s*p[ZZ];
 }
 
-void ten_vec(Ten T, real a[3], /**/ real b[3]) {
-    real *t;
-    t = T.t;
+void ten_vec(const Ten *T, const real a[3], /**/ real b[3]) {
+    const real *t;
+    t = T->t;
     b[X] = t[XX]*a[X] + t[XY]*a[Y] + t[XZ]*a[Z];
     b[Y] = t[YX]*a[X] + t[YY]*a[Y] + t[YZ]*a[Z];
     b[Z] = t[ZX]*a[X] + t[ZY]*a[Y] + t[ZZ]*a[Z];
 }
 
-void ten_add(Ten R, Ten *T) {
-   real *r, *t;
-   r = R.t; t = T->t;
-   t[XX] += r[XX]; t[XY] += r[XY]; t[XZ] += r[XZ];
-   t[YX] += r[YX]; t[YY] += r[YY]; t[YZ] += r[YZ];
-   t[ZX] += r[ZX]; t[ZY] += r[ZY]; t[ZZ] += r[ZZ];
+void ten_add(const Ten *R, /*io*/ Ten *T) {
+    const real *r;
+    real *t;
+    r = R->t; t = T->t;
+    t[XX] += r[XX]; t[XY] += r[XY]; t[XZ] += r[XZ];
+    t[YX] += r[YX]; t[YY] += r[YY]; t[YZ] += r[YZ];
+    t[ZX] += r[ZX]; t[ZY] += r[ZY]; t[ZZ] += r[ZZ];
 }
 
-void ten_axpy(real s, Ten P, /*io*/ Ten *T) {
-    real *p, *t;
-    p = P.t; t = T->t;
+void ten_axpy(real s, const Ten *P, /*io*/ Ten *T) {
+    const real *p;
+    real *t;
+    p = P->t; t = T->t;
     t[XX] += s*p[XX]; t[XY] += s*p[XY]; t[XZ] += s*p[XZ];
     t[YX] += s*p[YX]; t[YY] += s*p[YY]; t[YZ] += s*p[YZ];
     t[ZX] += s*p[ZX]; t[ZY] += s*p[ZY]; t[ZZ] += s*p[ZZ];
 }
 
-void ten_copy(Ten P, /**/ Ten *T) {
-    real *p, *t;
-    p = P.t; t = T->t;
+void ten_copy(const Ten *P, /**/ Ten *T) {
+    const real *p;
+    real *t;
+    p = P->t; t = T->t;
     t[XX] = p[XX]; t[XY] = p[XY]; t[XZ] = p[XZ];
     t[YX] = p[YX]; t[YY] = p[YY]; t[YZ] = p[YZ];
     t[ZX] = p[ZX]; t[ZY] = p[ZY]; t[ZZ] = p[ZZ];
@@ -90,11 +96,11 @@ void ten_one(Ten *T) {
     t[ZX] = 0; t[ZY] = 0; t[ZZ] = 1;
 }
 
-int ten_sprintf(Ten T, const char *fmt0, /**/ char *s) {
+int ten_sprintf(const Ten *T, const char *fmt0, /**/ char *s) {
     int r;
-    real *t;
+    const real *t;
     char fmt[SIZE];
-    t = T.t;
+    t = T->t;
     r = snprintf(fmt, SIZE, "%s %s %s\n" "%s %s %s\n" "%s %s %s\n",
                  fmt0, fmt0, fmt0, fmt0, fmt0, fmt0, fmt0, fmt0, fmt0);
     if (r < 0)
@@ -106,7 +112,7 @@ int ten_sprintf(Ten T, const char *fmt0, /**/ char *s) {
     return HE_OK;
 }
 
-int ten_fprintf(Ten T, FILE *stream, const char *fmt0) {
+int ten_fprintf(const Ten *T, FILE *stream, const char *fmt0) {
     char s[SIZE];
     if (ten_sprintf(T, fmt0, s) != HE_OK)
         ERR(HE_IO, "ten_sprintf failed");
@@ -115,7 +121,7 @@ int ten_fprintf(Ten T, FILE *stream, const char *fmt0) {
     return HE_OK;
 }
 
-int ten_printf(Ten T, const char *fmt0) {
+int ten_printf(const Ten *T, const char *fmt0) {
     if (ten_fprintf(T, stdout, fmt0) != HE_OK)
         ERR(HE_IO, "ten_fprintf failed");
     return HE_OK;
