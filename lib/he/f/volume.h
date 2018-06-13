bb64d34b64c156e6878f3bfa7b5e2b2854a1b641
Sergey Litvinov <slitvinov@gmail.com>
2018-06-13 15:12:35 +0200

    initial import of f/volume

---
+++ lib/he/f/volume.h
@@ -0,0 +1,18 @@
+#define T HeFArea
+
+typedef struct He He;
+typedef struct T T;
+
+/* E = sum_{tri} K * (area_t - area0)^2 */
+int he_f_area_ini(real a0, real k, He*, T**);
+int he_f_area_fin(T*);
+
+int he_f_area_force(T*, He*,
+                    real *x, real *y, real *z, /**/
+                    real *fx, real *fy, real *fz);
+real he_f_area_energy(T*, He*, real *x, real *y, real *z);
+
+int he_f_area_a(T*, /**/ real  **a);
+int he_f_area_da(T*, /**/ real **da);
+
+#undef T
