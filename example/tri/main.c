e2621c8f130070c219bf3a1c7fd7fbead1d85a56
Sergey Litvinov <slitvinov@gmail.com>
2018-06-13 09:42:38 +0200

    sort functions

---
+++ example/tri/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include <real.h>
+#include <he/err.h>
+#include <he/vec.h>
+#include <he/tri.h>
+#include <he/macro.h>
+#include <he/util.h>
+
+static const char **argv;
+
+void vec(/**/ real a[3]) { vec_argv(&argv, a); }
+
+int eq(const char *a, const char *b) { return util_eq(a, b); }
+int main(__UNUSED int argc, const char **v) {
+    const char *op;
+    real a[3], b[3], c[3], da[3], db[3], dc[3];
+    argv = v;
+    argv++;
+    if (*argv == NULL) ER("mssing OP");
+
+    op = *argv++;
+    if (eq(op, "angle")) {
+        vec(a); vec(b); vec(c);
+        dtri_angle(a, b, c, da, db, dc);
+        vec_printf(da, "%g");
+        vec_printf(db, "%g");
+        vec_printf(dc, "%g");
+    } else if (eq(op, "cot")) {
+        vec(a); vec(b); vec(c);
+        dtri_cot(a, b, c, da, db, dc);
+        vec_printf(da, "%g");
+        vec_printf(db, "%g");
+        vec_printf(dc, "%g");
+    } else if (eq(op, "area")) {
+        vec(a); vec(b); vec(c);
+        dtri_area(a, b, c, da, db, dc);
+        vec_printf(da, "%g");
+        vec_printf(db, "%g");
+        vec_printf(dc, "%g");        
+    } else        
+        ER("unknown operation '%s'", op);
+    return 0;
+}
--- lib/he/tri.h
+++ lib/he/tri.h
@@ -1,6 +1,6 @@
-real tri_area(real[3], real[3], real[3]);
-real tri_volume(real[3], real[3], real[3]);
-real tri_dih(real[3], real[3], real[3], real[3]);
-void tri_normal(real[3], real[3], real[3], /**/ real[3]);
 real tri_angle(real[3], real b[3], real[3]);
+real tri_area(real[3], real[3], real[3]);
 real tri_cot(real[3], real b[3], real[3]);
+real tri_dih(real[3], real[3], real[3], real[3]);
+real tri_volume(real[3], real[3], real[3]);
+void tri_normal(real[3], real[3], real[3], /**/ real[3]);
