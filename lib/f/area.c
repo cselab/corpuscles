5230a97a218c6344969758df2a62f9b95632dad8
Sergey Litvinov <slitvinov@gmail.com>
2018-06-13 09:59:22 +0200

    compute force

--- lib/f/area.c
+++ lib/f/area.c
@@ -6,6 +6,7 @@
 #include "he/he.h"
 #include "he/vec.h"
 #include "he/tri.h"
+#include "he/dtri.h"
 
 #include "he/f/area.h"
 
@@ -50,15 +51,19 @@ int he_f_area_da(T *q, /**/ real  **pa) {
     return HE_OK;
 }
 
-static void get(int t, He *he,
-                real x[], real y[], real z[], /**/
-                real a[3], real b[3], real c[3]) {
+static void get_ijk(int t, He *he, /**/ int *pi, int *pj, int *pk) {
     int h, n, nn, i, j, k;
     h = he_hdg_tri(he, t);
     n = he_nxt(he, h);
     nn = he_nxt(he, n);
-
     i = he_ver(he, h); j = he_ver(he, n); k = he_ver(he, nn);
+    *pi = i; *pj = j; *pk = k;
+}
+static void get(int t, He *he,
+                real *x, real *y, real *z, /**/
+                real a[3], real b[3], real c[3]) {
+    int i, j, k;
+    get_ijk(t, he, &i, &j, &k);
     vec_get(i, x, y, z, /**/ a);
     vec_get(j, x, y, z, /**/ b);
     vec_get(k, x, y, z, /**/ c);
@@ -71,17 +76,25 @@ static void compute_area(real a0, He *he, real *x, real *y, real *z, /**/ real *
     for (t = 0; t < n; t++) {
         get(t, he, x, y, z, /**/ a, b, c);
         area[t]  = one   = tri_area(a, b, c);
-        darea[t] = delta = one - a0;
+        darea[t] = delta = a0 - one;
     }
 }
 
-void compute_force(real K, real *da, He *he, real *x, real *y, real *z, /**/ real *fx, real *fy, real *fz) {
-    int n, t;
-    real a[3], b[3], c[3];
+void compute_force(real K, real *darea, He *he, real *x, real *y, real *z, /**/ real *fx, real *fy, real *fz) {
+    int n, t, i, j, k;
+    real a[3], b[3], c[3], da[3], db[3], dc[3], coeff;
     n = he_nt(he);    
     for (t = 0; t < n; t++) {
-        get(t, he, x, y, z, /**/ a, b, c);
-        
+        get_ijk(t, he, /**/ &i, &j, &k);
+        vec_get(i, x, y, z, /**/ a);
+        vec_get(j, x, y, z, /**/ b);
+        vec_get(k, x, y, z, /**/ c);
+
+        dtri_area(a, b, c, /**/ da, db, dc);
+        coeff = 2*K*darea[t];
+        vec_scalar_append(da, coeff, i, /**/ fx, fy, fz);
+        vec_scalar_append(db, coeff, j, /**/ fx, fy, fz);
+        vec_scalar_append(dc, coeff, k, /**/ fx, fy, fz);        
     }
 }
     
