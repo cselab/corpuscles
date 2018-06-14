1a6aba207cc18ebf40b384ccb4e13445e600abbc
Sergey Litvinov <slitvinov@gmail.com>
2018-06-14 09:44:17 +0200

    rename edg/hash

--- example/hash/main.c
+++ example/hash/main.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 #include <he/err.h>
-#include <he/edg.h>
+#include <he/hash.h>
 #include <he/util.h>
 
-static HeEdg *edg;
+static HeHash *hash;
 
-static void ini(int n) { he_edg_ini(n, &edg); }
-static void fin() { he_edg_fin(edg); }
+static void ini(int n) { he_hash_ini(n, &hash); }
+static void fin() { he_hash_fin(hash); }
 static int eq(const char *a, const char *b) { return util_eq(a, b); }
 static void set(int i, int j, int v) {
     printf("set %d %d %d\n", i, j, v);
-    he_edg_set(edg, i, j, v);
+    he_hash_set(hash, i, j, v);
 }
 static void get(int i, int j) {
     int v;
-    v = he_edg_get(edg, i, j);
+    v = he_hash_get(hash, i, j);
     printf("get %d %d %d\n", i, j, v);
 }
 
