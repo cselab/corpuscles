0ea74efa114256d2f3961bc4158fab4ed6d990c1
Sergey Litvinov <slitvinov@gmail.com>
2018-06-14 09:39:26 +0200

    add dedg_abs

--- lib/dedg.c
+++ lib/dedg.c
@@ -7,5 +7,10 @@
 #include "he/dedg.h"
 
 int dedg_abs(real a[3], real b[3], /**/ real da[3], real db[3]) {
+    real u[3], e[3];
+    vec_minus(b, a,   u);
+    vec_norm(u,   e);
+    vec_copy(e,     db);
+    vec_negative(e, da);
     return HE_OK;
 }
