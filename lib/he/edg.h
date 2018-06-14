7f55de42ee15bdb93b2a7ef0417aed5e1cb363da
Sergey Litvinov <slitvinov@gmail.com>
2018-06-14 09:34:11 +0200

    initial import of edg/dedg

---
+++ lib/he/edg.h
@@ -0,0 +1,7 @@
+real tri_angle(real[3], real b[3], real[3]);
+real tri_area(real[3], real[3], real[3]);
+real tri_cot(real[3], real b[3], real[3]);
+real tri_dih(real[3], real[3], real[3], real[3]);
+int  tri_edg(real[3], real[3], real[3], /**/ real[3], real[3], real[3]);
+real tri_volume(real[3], real[3], real[3]);
+int  tri_normal(real[3], real[3], real[3], /**/ real[3]);
--- lib/make/obj.mk
+++ lib/make/obj.mk
@@ -1,3 +1,3 @@
-O_S = dtri.s.o f/area.s.o f/harmonic.s.o f/volume.s.o off.s.o punto.s.o real.s.o ten.s.o tri.s.o vec.s.o x.s.o
-O_D = dtri.d.o f/area.d.o f/harmonic.d.o f/volume.d.o off.d.o punto.d.o real.d.o ten.d.o tri.d.o vec.d.o x.d.o
+O_S = edg.s.o dedg.s.o dtri.s.o f/area.s.o f/harmonic.s.o f/volume.s.o off.s.o punto.s.o real.s.o ten.s.o tri.s.o vec.s.o x.s.o
+O_D = edg.d.o dedg.d.o dtri.d.o f/area.d.o f/harmonic.d.o f/volume.d.o off.d.o punto.d.o real.d.o ten.d.o tri.d.o vec.d.o x.d.o
 O = hash.o err.o he.o memory.o read.o util.o
