0b1729dcb3ab6e81756869d4835bae4ebee3fd0c
Sergey Litvinov <slitvinov@gmail.com>
2018-06-15 11:26:22 +0200

    initial import of ddih

---
+++ lib/ddih.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#include "real.h"
+#include "he/err.h"
+#include "he/vec.h"
+#include "he/tri.h"
+
+#include "he/dih.h"
+
+int ddih_angle(real  a[3], real  b[3], real  c[3], real  d[3], /**/
+               real da[3], real db[3], real dc[3], real dd[3]) {
+    return HE_OK;
+}
---
+++ lib/he/ddih.h
@@ -0,0 +1,3 @@
+/* bc is an edge */
+int ddih_angle(real[3], real  b[3], real  c[3], real[3], /**/
+               real[3], real [3], real[3], real[3]);
