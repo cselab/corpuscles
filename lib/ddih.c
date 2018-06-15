c481d99f0b3dc93d71590a39bb3b549cbd323843
Sergey Litvinov <slitvinov@gmail.com>
2018-06-15 11:29:36 +0200

    inital version of ddih

--- lib/ddih.c
+++ lib/ddih.c
@@ -9,5 +9,29 @@
 
 int ddih_angle(real  a[3], real  b[3], real  c[3], real  d[3], /**/
                real da[3], real db[3], real dc[3], real dd[3]) {
+    real n[3], k[3], bc[3];
+    real e, An, Ak, cn, bn, bk, ck;
+
+    tri_normal(a, b, c,   n);
+    tri_normal(c, b, d,   k);
+
+    vec_minus(c, b,   bc);
+    e = vec_abs(bc);
+
+    An = tri_area(a, b, c);
+    Ak = tri_area(c, b, d);
+
+    vec_scalar(n, e/(2*An),   da);
+    vec_scalar(k, e/(2*Ak),   dd);
+
+    cn = tri_cot(b, c, a);
+    bn = tri_cot(a, b, c);
+
+    bk = tri_cot(c, b, d);
+    ck = tri_cot(d, c, b);
+
+    vec_linear_combination(-bn/e, n, -bk/e, k,    dc);
+    vec_linear_combination(-cn/e, n, -ck/e, k,    db);
+
     return HE_OK;
 }
