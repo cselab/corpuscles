dfc6b2dc6b11d43fa187a4120906340e5a028063
Sergey Litvinov <slitvinov@gmail.com>
2018-06-16 11:25:51 +0200

    initial import of area; add make/hdr.mk

---
+++ lib/area.c
@@ -0,0 +1,11 @@
+#include <math.h>
+#include <stdio.h>
+
+#include "real.h"
+#include "he/err.h"
+#include "he/tri.h"
+#include "he/tri.h"
+
+real he_area(He *he, const real *x, const real *y, const real *z) {
+    return 0.0;
+}
---
+++ lib/he/area.h
@@ -0,0 +1,1 @@
+real he_area(He *he, const real*, const real*, const real*);
