6d4dcd2230e5b277235fd2b1ec9dff2ca61d915b
Sergey Litvinov <slitvinov@gmail.com>
2018-06-18 12:21:21 +0200

    wip: gompper force

--- lib/f/gompper.c
+++ lib/f/gompper.c
@@ -5,12 +5,21 @@
 #include "he/err.h"
 #include "he/he.h"
 #include "he/vec.h"
+#include "he/tri.h"
 #include "he/dih.h"
 #include "he/ddih.h"
 
 #include "he/f/gompper.h"
 
 #define T HeFGompper
+
+#define  nxt(h)     he_nxt(he, (h))
+#define  flp(h)     he_flp(he, (h))
+#define  ver(h)     he_ver(he, (h))
+#define  hdg_ver(v) he_hdg_ver(he, (v))
+#define  hdg_edg(e) he_hdg_edg(he, (e))
+#define  bnd(h)     he_bnd(he, (h))
+
 
 struct T {
     int n;
@@ -23,14 +32,15 @@ struct T {
 
 int he_f_gompper_ini(real K, He *he, T **pq) {
     T *q;
-    int n;
+    int n, nh;
     MALLOC(1, &q);
+
+    nh = he_nh(he);
+    MALLOC(nh, &q->l2);
+    MALLOC(nh, &q->t);
+
     n = he_nv(he);
-
     MALLOC(n, &q->area);
-    MALLOC(n, &q->l2);
-    MALLOC(n, &q->t);
-
     MALLOC(n, &q->lx);
     MALLOC(n, &q->ly);
     MALLOC(n, &q->lz);
@@ -65,12 +75,6 @@ int he_f_gompper_laplace(T *q, /**/ real **px, real **py, real **pz ) {
 
 enum {BULK, BND};
 static int get_ijkl(int e, He *he, /**/ int *pi, int *pj, int *pk, int *pl) {
-#    define  nxt(h)     he_nxt(he, h)
-#    define  flp(h)     he_flp(he, h)
-#    define  ver(h)     he_ver(he, h)
-#    define  hdg_ver(v) he_hdg_ver(he, v)
-#    define  hdg_edg(e) he_hdg_edg(he, e)
-#    define  bnd(h)     he_bnd(he, h)
     int h, n, nn, nnf, i, j, k, l;
     h = he_hdg_edg(he, e);
     if (bnd(h)) return BND;
@@ -116,6 +120,64 @@ static void compute_force(real K,
     }
 }
 
+static void get_edg(int i, int j, const real *x, const real *y, const real *z, /**/ real r[3]) {
+    real a[3], b[3];
+    vec_get(i, x, y, z, a);
+    vec_get(j, x, y, z, b);
+    vec_minus(a, b, r);
+}
+static void get3(int i, int j, int k,
+                 const real *x, const real *y, const real *z,
+                 /**/ real a[3], real b[3], real c[3]) {
+    vec_get(i, x, y, z, a);
+    vec_get(j, x, y, z, b);
+    vec_get(k, x, y, z, c);
+}
+
+static void zero(int n, real *a) {
+    int i;
+    for (i = 0; i < n; i++) a[i] = 0;
+}
+static void compute_l2(He *he, const real *x, const real *y, const real *z, /**/ real *H) {
+    int h, n;
+    int i, j;
+    real r[3];
+    n = he_nh(he);
+    for (h = 0; h < n; h++) {
+        n = nxt(h);
+        i = ver(h); j = ver(n);
+        get_edg(i, j, x, y, z, /**/ r);
+        H[h] = vec_dot(r, r);
+    }
+}
+
+static void compute_t(He *he, const real *x, const real *y, const real *z, /**/ real *H) {
+    int nh, h, n, nn;
+    int i, j, k;
+    real a[3], b[3], c[3], cot;
+    nh = he_nh(he);
+    zero(nh, H);
+    for (h = 0; h < nh; h++) {
+        n = nxt(h); nn = nxt(n);
+        i = ver(h); j = ver(n); k = ver(nn);
+        get3(i, j, k, x, y, z, /**/ a, b, c);
+        cot = tri_cot(b, c, a);
+        H[h] += cot;
+        if (!bnd(h)) H[flp(h)] += cot;
+    }
+}
+
+static void compute_area(He *he, const real *l2, const real *t, /**/ real *V) {
+    int nv, nh, h, i;
+    nv = he_nv(he);
+    zero(nv, V);
+    nh = he_nh(he);
+    for (h = 0; h < nh; h++) {
+        i = ver(h);
+        V[i] += t[h]*l2[h]/8;
+    }
+}
+
 int he_f_gompper_force(T *q, He *he,
                       const real *x, const real *y, const real *z, /**/
                       real *fx, real *fy, real *fz) {
@@ -125,6 +187,10 @@ int he_f_gompper_force(T *q, He *he,
     K  = q->K;
     if (he_nv(he) != n)
         ERR(HE_INDEX, "he_nv(he)=%d != n = %d", he_nv(he), n);
+    compute_l2(he, x, y, z, /**/ q->l2);
+    compute_t(he, x, y, z, /**/ q->t);
+    compute_area(he, q->l2, q->t, /**/ q->area);
+
     compute_force(K, he, x, y, z, /**/ fx, fy, fz);
     return HE_OK;
 }
