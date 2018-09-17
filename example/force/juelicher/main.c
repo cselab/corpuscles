613d6416da75863458cd7324e9b3a8e3a01dc960
Xin Bian <xin.z.bian@gmail.com>
2018-09-17 13:51:29 +0200

    added force of Juelicher model

--- example/force/juelicher/main.c
+++ example/force/juelicher/main.c
@@ -7,6 +7,7 @@
 #include <he/vec.h>
 #include <he/tri.h>
 #include <he/memory.h>
+#include <he/punto.h>
 
 void get3(int i, int j, int k, /**/ real a[3], real b[3], real c[3]) {
 
@@ -24,6 +25,17 @@ void get4(int i, int j, int k, int l, /**/
   vec_get(k, XX, YY, ZZ, c);
   vec_get(l, XX, YY, ZZ, d);
   
+}
+
+static void vabs(int n, real *x, real *y, real *z, /**/ real *r) {
+  /*Given n vectors with x, y, z components,
+    calculate the absolute value/Euclidean length for each vector.*/
+  
+    int i;
+    
+    for (i = 0; i < n; i++)
+        r[i] = sqrt(x[i]*x[i] + y[i]*y[i] + z[i]*z[i]);
+    
 }
 
 static void write(real *fx, real *fy, real *fz,
@@ -44,73 +56,86 @@ void force_juelicher() {
     /*This routine calculates bending force
       according to Juelicher, J. Phys. II France, 1996
     
-    It traverses each edge/dihedral to calculate bond length and dihedral angle.
-    It traverses each vertex to calculate area and its derivatives.
+    1st loop; 
+    it traverses each edge, which has a dihedral angle,
+    to calculate bond length, dihedral angle and their product.
+    2nd loop;
+    it traverses each triangle to calculate its area and split it
+    1/3 into each of its vertex.
+    3rd lopp;
+    it traverses each edge again, to calculate derivatives.
+    4th lopp;
+    it traverses each triangle again, to calculate derivatives.
     
     Version: Xin Bian, 14 September 2018 @CSE Lab, ETH Zurich*/
   
   enum {X, Y, Z};
-  int v, e, h, t;
+
+  real H0;
+  int e, he, t;
   int i, j, k, l;
-  real a[3], b[3], c[3], d[3], u[3], coord[3];
-  real *curva_mean, *energy, *area;
+  real a[3], b[3], c[3], d[3];
+  real u[3], v[3], w[3], g[3], h[3], f[3];
 
-  real *lentheta; 
+  real mm[3], nn[3];
+  real m[3], n[3];
+  real mndot;
+  real mnmn[3], nmnm[3];
+  real temp_vec[3];
+  real theta_der[3];
+
+  real *lentheta, *area;
+  real len0, theta0, lentheta0, area0;
+  real aream, arean;
+  real coef, coef1;
   real *fx, *fy, *fz;
-  real temp;
 
-  real xjk[3];
   
-  real cur, len, len2, area0;
-  real theta, rxy, phi;
-  real area_tot_tri, area_tot_split;
-  real energy_tot;
-  
-  MALLOC(NV, &curva_mean);
-  MALLOC(NV, &energy);
+  MALLOC(NV, &lentheta);
   MALLOC(NV, &area);
 
-  MALLOC(NV, lentheta);
   MALLOC(NV, &fx);
   MALLOC(NV, &fy);
   MALLOC(NV, &fz);
 
+  H0 = 0;
   
-  for (v = 0; v < NV; v++) {
-    curva_mean[v] = 0;
-    energy[v]     = 0;
-    area[v]       = 0;
+  for (i = 0; i < NV; i++) {
 
-    ltheta = 0;
-    fx[v]  = 0;
-    fy[v]  = 0;
-    fz[v]  = 0;
+    lentheta[i] = 0;
+    area[i]     = 0;
+
+    fx[i]  = 0;
+    fy[i]  = 0;
+    fz[i]  = 0;
+    
   }
-  
+
+
+  //1st loop;
   for (e = 0; e < NE; e++) {
     
-    h = hdg_edg(e);
+    he = hdg_edg(e);
     
-    if ( bnd(h) ) continue;      
+    if ( bnd(he) ) continue;      
     
     i = D0[e]; j = D1[e]; k = D2[e]; l = D3[e];
     
     get4(i, j, k, l, /**/ a, b, c, d);
     
-    theta = tri_dih(a, b, c, d);
+    theta0 = tri_dih(a, b, c, d);
     
-    vec_minus(b, c, u);
-    len2 = vec_dot(u, u);
-    len  = sqrt(len2);
-    
-    temp = len*theta;
-    lentheta[j] += temp;
-    lentheta[k] += temp;
+    vec_minus(c, b, u);
+    len0 = vec_dot(u, u);
+    len0 = sqrt(len0);
+
+    lentheta0    = len0*theta0;
+    lentheta[j] += lentheta0;
+    lentheta[k] += lentheta0;
     
   }
 
-  area_tot_tri = 0;
-
+  //2nd loop;
   for (t = 0; t < NT; t++) {
       
     i = T0[t]; j = T1[t]; k = T2[t];
@@ -122,76 +147,156 @@ void force_juelicher() {
     area[j] += area0/3;
     area[k] += area0/3;
 
-    area_tot_tri += area0;
-    
   }
 
-  area_tot_split = 0;
-  energy_tot     = 0;
   
-
+  //3rd loop;
   for (e = 0; e < NE; e++) {
     
-    h = hdg_edg(e);
+    he = hdg_edg(e);
     
-    if ( bnd(h) ) continue;      
+    if ( bnd(he) ) continue;      
     
     i = D0[e]; j = D1[e]; k = D2[e]; l = D3[e];
     
     get4(i, j, k, l, /**/ a, b, c, d);
 
-    theta = tri_dih(a, b, c, d);
+    theta0 = tri_dih(a, b, c, d);
 
-    vec_minus(b, c, xjk);
+    aream = tri_area(a, b, c);
+    arean = tri_area(d, c, b);
+
+    vec_minus(c, b, u);
+    len0 = vec_dot(u, u);
+    len0 = sqrt(len0);
+
+    coef = -(lentheta[j]/area[j]/4.0 - H0) * theta0;
+    vec_scalar_append(u, -coef, j, fx, fy, fz);
+    vec_scalar_append(u, coef, k, fx, fy, fz);
     
-    coef = theta * lentheta[j]/A[j]/4.0;
-    vec_scalar_append(xjk, coef, j, fx, fy, fz);
+    coef = -(lentheta[k]/area[k]/4.0 - H0) * theta0;
+    vec_scalar_append(u, -coef, j, fx, fy, fz);
+    vec_scalar_append(u, coef, k, fx, fy, fz);
     
-    coef = -theta * lentheta[k]/A[k]/4.0;
+
+    vec_minus(a, b, v);
+    vec_minus(c, a, w);
     
-    vec_scalar_append(xjk, coef, k, fx, fy, fz);
+    vec_minus(b, d, f);
+    vec_minus(b, c, g);
+    vec_minus(d, c, h);
+
+    vec_cross(u, v, mm);
+    vec_cross(g, h, nn);
+
+    vec_norm(mm, m);
+    vec_norm(nn, n);
     
+    mndot = vec_dot(m,n);
+
+    vec_linear_combination(1.0, m, -mndot, n, mnmn);
+    vec_norm(mnmn, temp_vec);
+    vec_copy(temp_vec, mnmn);
+    
+    vec_linear_combination(1.0, n, -mndot, m, nmnm);
+    vec_norm(nmnm, temp_vec);
+    vec_copy(temp_vec, nmnm);
+
+    vec_cross(g, nmnm, theta_der);
+    
+    coef =  -(lentheta[j]/area[j]/4.0 - H0) * len0 ;
+
+    coef1 = -coef / aream / 2.0;
+    vec_scalar_append(theta_der, coef1, i, fx, fy, fz);
+
+    vec_cross(h, mnmn, theta_der);
+    coef1 = -coef / arean / 2.0;
+    vec_scalar_append(theta_der, coef1, j, fx, fy, fz);
+    
+    vec_cross(w, nmnm, theta_der);
+    coef1 = -coef / aream / 2.0;
+    vec_scalar_append(theta_der, coef1, j, fx, fy, fz);
+    
+    vec_cross(f, mnmn, theta_der);
+    coef1 = -coef / arean / 2.0;
+    vec_scalar_append(theta_der, coef1, k, fx, fy, fz);
+    
+    vec_cross(v, nmnm, theta_der);
+    coef1 = -coef / aream / 2.0;
+    vec_scalar_append(theta_der, coef1, k, fx, fy, fz);
+
+    vec_cross(u, mnmn, theta_der);
+    coef1 = -coef / arean / 2.0;
+    vec_scalar_append(theta_der, coef1, l, fx, fy, fz);
+
+
+    coef =  -(lentheta[k]/area[k]/4.0 - H0) * len0 ;
+
+    coef1 = -coef / aream / 2.0;
+    vec_scalar_append(theta_der, coef1, i, fx, fy, fz);
+
+    vec_cross(h, mnmn, theta_der);
+    coef1 = -coef / arean / 2.0;
+    vec_scalar_append(theta_der, coef1, j, fx, fy, fz);
+    
+    vec_cross(w, nmnm, theta_der);
+    coef1 = -coef / aream / 2.0;
+    vec_scalar_append(theta_der, coef1, j, fx, fy, fz);
+    
+    vec_cross(f, mnmn, theta_der);
+    coef1 = -coef / arean / 2.0;
+    vec_scalar_append(theta_der, coef1, k, fx, fy, fz);
+    
+    vec_cross(v, nmnm, theta_der);
+    coef1 = -coef / aream / 2.0;
+    vec_scalar_append(theta_der, coef1, k, fx, fy, fz);
+
+    vec_cross(u, mnmn, theta_der);
+    coef1 = -coef / arean / 2.0;
+    vec_scalar_append(theta_der, coef1, l, fx, fy, fz);
+  
   }
 
 
+  //4th loop
   for (t = 0; t < NT; t++) {
       
     i = T0[t]; j = T1[t]; k = T2[t];
       
     get3(i, j, k, a, b, c);
 
+    area0 = tri_area(a, b, c);
+
+    vec_minus(b, a, u);
+    vec_minus(c, a, v);
+    vec_minus(b, c, w);
+
+    vec_cross(u, v, n);
+
+    coef = 1.0/area0/4.0/3.0;
+
+    coef1 = (lentheta[i]*lentheta[i]/8.0/area[i]/area[i] - 2.0*H0*H0) * coef;
+    vec_cross(w, n, f);
+    vec_scalar_append(f, coef1, i, fx, fy, fz);
+    
+    coef1 = (lentheta[j]*lentheta[j]/8.0/area[j]/area[j] - 2.0*H0*H0) * coef;
+    vec_cross(v, n, f);
+    vec_scalar_append(f, coef1, j, fx, fy, fz);
+    
+    coef1 = (lentheta[k]*lentheta[k]/8.0/area[k]/area[k] - 2.0*H0*H0) * coef;    
+    vec_cross(n, u, f);
+    vec_scalar_append(f, coef1, k, fx, fy, fz);
 
   }
   
-  for (v = 0; v < NV; v++) {
-      
-    curva_mean[v] /= area[v];
-    energy[v] = 2 * curva_mean[v]*curva_mean[v]*area[v];
-
-    /*for verification*/
-    area_tot_split += area[v];
-    energy_tot     += energy[v];
-    
-    vec_get(v, XX, YY, ZZ, coord);
-    rxy = vec_cylindrical_r(coord);
-    
-    phi = TH[v];
-    if ( phi > pi / 2) {
-      phi = pi - phi;
-    }
-    
-    
-  }
-
-
   write(/*i*/ fx, fy, fz, area);
 
-  FREE(curva_mean);
-  FREE(energy);
-  FREE(area);
   
   FREE(lentheta);
-  FREE(fx); FREE(fy); FREE(fz);
+  FREE(area);
+  FREE(fx);
+  FREE(fy);
+  FREE(fz);
     
 }
 
