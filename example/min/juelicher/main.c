a8515dc9c0c7a6621d39e05232cc4d1a891f5430
Sergey Litvinov <slitvinov@gmail.com>
2018-09-23 22:30:38 +0200

    indent

--- example/min/juelicher/main.c
+++ example/min/juelicher/main.c
@@ -47,147 +47,136 @@ static void get3(const real *xx, const real *yy, const real *zz,
                  int i, int j, int k,
                  /**/ real a[3], real b[3], real c[3]) {
 
-  vec_get(i, xx, yy, zz, a);
-  vec_get(j, xx, yy, zz, b);
-  vec_get(k, xx, yy, zz, c);
+    vec_get(i, xx, yy, zz, a);
+    vec_get(j, xx, yy, zz, b);
+    vec_get(k, xx, yy, zz, c);
 }
 
 static void get4(const real *xx, const real *yy, const real *zz,
                  int i, int j, int k, int l, /**/
                  real a[3], real b[3], real c[3], real d[3]) {
-  vec_get(i, xx, yy, zz, a);
-  vec_get(j, xx, yy, zz, b);
-  vec_get(k, xx, yy, zz, c);
-  vec_get(l, xx, yy, zz, d);
+    vec_get(i, xx, yy, zz, a);
+    vec_get(j, xx, yy, zz, b);
+    vec_get(k, xx, yy, zz, c);
+    vec_get(l, xx, yy, zz, d);
 }
 
 static real energy(const real *xx, const real *yy, const real *zz) {
-  enum {X, Y, Z};
-  int v, e, h, t;
-  int i, j, k, l;
-  real a[3], b[3], c[3], d[3], u[3];
-  real cur, len, area0;
-  real theta;
-  real en, e0;
-  real C0, H0;
+    enum {X, Y, Z};
+    int v, e, h, t;
+    int i, j, k, l;
+    real a[3], b[3], c[3], d[3], u[3];
+    real cur, len, area0;
+    real theta;
+    real en, e0;
+    real C0, H0;
 
-  C0=0;
-  H0=C0/2.0;
-  zero(NV, curva_mean); zero(NV, AREA);
+    C0=0;
+    H0=C0/2.0;
+    zero(NV, curva_mean); zero(NV, AREA);
 
-  for (e = 0; e < NE; e++) {
-    h = hdg_edg(e);
-    if ( bnd(h) ) continue;
-    i = D0[e]; j = D1[e]; k = D2[e]; l = D3[e];
-    get4(xx, yy, zz, i, j, k, l, /**/ a, b, c, d);
-    theta = tri_dih(a, b, c, d);
-    vec_minus(b, c, u);
-    len = vec_abs(u);
-    cur = len*theta/4;
-    curva_mean[j] += cur;
-    curva_mean[k] += cur;
+    for (e = 0; e < NE; e++) {
+        h = hdg_edg(e);
+        if ( bnd(h) ) continue;
+        i = D0[e]; j = D1[e]; k = D2[e]; l = D3[e];
+        get4(xx, yy, zz, i, j, k, l, /**/ a, b, c, d);
+        theta = tri_dih(a, b, c, d);
+        vec_minus(b, c, u);
+        len = vec_abs(u);
+        cur = len*theta/4;
+        curva_mean[j] += cur;
+        curva_mean[k] += cur;
 
-  }
+    }
 
-  for (t = 0; t < NT; t++) {
-    i = T0[t]; j = T1[t]; k = T2[t];
-    get3(xx, yy, zz, i, j, k, a, b, c);
-    area0 = tri_area(a, b, c);
-    AREA[i] += area0/3;
-    AREA[j] += area0/3;
-    AREA[k] += area0/3;
+    for (t = 0; t < NT; t++) {
+        i = T0[t]; j = T1[t]; k = T2[t];
+        get3(xx, yy, zz, i, j, k, a, b, c);
+        area0 = tri_area(a, b, c);
+        AREA[i] += area0/3;
+        AREA[j] += area0/3;
+        AREA[k] += area0/3;
 
-  }
+    }
 
-  en  = 0;
-  for (v = 0; v < NV; v++) {
-    curva_mean[v] /= AREA[v];
-    e0 = 2 * (curva_mean[v]-H0)*(curva_mean[v]-H0)*AREA[v];
-    en  += e0;
-  }
-  return Kb*en;
+    en  = 0;
+    for (v = 0; v < NV; v++) {
+        curva_mean[v] /= AREA[v];
+        e0 = 2 * (curva_mean[v]-H0)*(curva_mean[v]-H0)*AREA[v];
+        en  += e0;
+    }
+    return Kb*en;
 }
 
 static void force(const real *xx, const real *yy, const real *zz,
                   /**/ real *fx, real *fy, real *fz) {
-  enum {X, Y, Z};
+    enum {X, Y, Z};
 
-  int e, t;
-  int i, j, k, l;
-  real a[3], b[3], c[3], d[3];
-  real da[3], db[3], dc[3], dd[3];
-  real u[3];
+    int e, t;
+    int i, j, k, l;
+    real a[3], b[3], c[3], d[3];
+    real da[3], db[3], dc[3], dd[3];
+    real u[3];
 
-  real lentheta0, area0, theta0, len0;
-  real coef;
+    real lentheta0, area0, theta0, len0;
+    real coef, c0, c1;
 
-  zero(NV, lentheta); zero(NV, AREA);
+    zero(NV, lentheta); zero(NV, AREA);
 
-  for (e = 0; e < NE; e++) {
-    i = D0[e]; j = D1[e]; k = D2[e]; l = D3[e];
-    get4(xx, yy, zz, i, j, k, l, /**/ a, b, c, d);
-    vec_minus(c, b, u);
-    lentheta0    = vec_abs(u) * tri_dih(a, b, c, d);
-    lentheta[j] += lentheta0;
-    lentheta[k] += lentheta0;
-  }
+    for (e = 0; e < NE; e++) {
+        i = D0[e]; j = D1[e]; k = D2[e]; l = D3[e];
+        get4(xx, yy, zz, i, j, k, l, /**/ a, b, c, d);
+        vec_minus(c, b, u);
+        lentheta0    = vec_abs(u) * tri_dih(a, b, c, d);
+        lentheta[j] += lentheta0;
+        lentheta[k] += lentheta0;
+    }
 
-  for (t = 0; t < NT; t++) {
-    i = T0[t]; j = T1[t]; k = T2[t];
-    get3(xx, yy, zz, i, j, k, a, b, c);
-    area0 = tri_area(a, b, c);
-    AREA[i] += area0/3;
-    AREA[j] += area0/3;
-    AREA[k] += area0/3;
-  }
+    for (t = 0; t < NT; t++) {
+        i = T0[t]; j = T1[t]; k = T2[t];
+        get3(xx, yy, zz, i, j, k, a, b, c);
+        area0 = tri_area(a, b, c)/3;
+        AREA[i] += area0; AREA[j] += area0; AREA[k] += area0;
+    }
 
+    for (e = 0; e < NE; e++) {
+        i = D0[e]; j = D1[e]; k = D2[e]; l = D3[e];
+        get4(xx, yy, zz, i, j, k, l, /**/ a, b, c, d);
+        theta0 = tri_dih(a, b, c, d);
+        dedg_abs(c, b, /**/ dc, db);
+        coef = -(lentheta[j]/AREA[j] + lentheta[k]/AREA[k])/4.0*theta0*Kb;
+        vec_scalar_append(db, coef, j, fx, fy, fz);
+        vec_scalar_append(dc, coef, k, fx, fy, fz);
+    }
 
-  for (e = 0; e < NE; e++) {
-    i = D0[e]; j = D1[e]; k = D2[e]; l = D3[e];
-    get4(xx, yy, zz, i, j, k, l, /**/ a, b, c, d);
-    theta0 = tri_dih(a, b, c, d);
+    for (e = 0; e < NE; e++) {
+        i = D0[e]; j = D1[e]; k = D2[e]; l = D3[e];
+        get4(xx, yy, zz, i, j, k, l, /**/ a, b, c, d);
+        ddih_angle(a, b, c, d, /**/ da, db, dc, dd);
+        vec_minus(c, b, u);
+        len0 = vec_abs(u);
+        coef = -(lentheta[j]/AREA[j] + lentheta[k]/AREA[k])/4.0*len0*Kb;
 
-    dedg_abs(c, b, /**/ dc, db);
+        vec_scalar_append(da, coef, i, fx, fy, fz);
+        vec_scalar_append(db, coef, j, fx, fy, fz);
+        vec_scalar_append(dc, coef, k, fx, fy, fz);
+        vec_scalar_append(dd, coef, l, fx, fy, fz);
+    }
 
-    coef = -(lentheta[j]/AREA[j]/4.0) * theta0 * Kb;
-    vec_scalar_append(db, coef, j, fx, fy, fz);
-    vec_scalar_append(dc, coef, k, fx, fy, fz);
-
-    coef = -(lentheta[k]/AREA[k]/4.0) * theta0 * Kb;
-    vec_scalar_append(db, coef, j, fx, fy, fz);
-    vec_scalar_append(dc, coef, k, fx, fy, fz);
-  }
-
-
-  for (e = 0; e < NE; e++) {
-    i = D0[e]; j = D1[e]; k = D2[e]; l = D3[e];
-    get4(xx, yy, zz, i, j, k, l, /**/ a, b, c, d);
-    ddih_angle(a, b, c, d, /**/ da, db, dc, dd);
-    vec_minus(c, b, u);
-    len0 = vec_dot(u, u);
-    len0 = sqrt(len0);
-
-    coef =
-        -(lentheta[j]/AREA[j]/4.0) * len0 * Kb
-        -(lentheta[k]/AREA[k]/4.0) * len0 * Kb;
-
-    vec_scalar_append(da, coef, i, fx, fy, fz);
-    vec_scalar_append(db, coef, j, fx, fy, fz);
-    vec_scalar_append(dc, coef, k, fx, fy, fz);
-    vec_scalar_append(dd, coef, l, fx, fy, fz);
-  }
-
-  for (t = 0; t < NT; t++) {
-    i = T0[t]; j = T1[t]; k = T2[t];
-    get3(xx, yy, zz, i, j, k, a, b, c);
-    dtri_area(a, b, c, /**/ da, db, dc);
-    coef = (lentheta[i]*lentheta[i]/8.0/AREA[i]/AREA[i]);
-    vec_scalar_append(da, Kb*coef/3.0, i, fx, fy, fz);
-    coef = (lentheta[j]*lentheta[j]/8.0/AREA[j]/AREA[j]);
-    vec_scalar_append(db, Kb*coef/3.0, j, fx, fy, fz);
-    coef = (lentheta[k]*lentheta[k]/8.0/AREA[k]/AREA[k]);
-    vec_scalar_append(dc, Kb*coef/3.0, k, fx, fy, fz);
-  }
+    for (t = 0; t < NT; t++) {
+        i = T0[t]; j = T1[t]; k = T2[t];
+        get3(xx, yy, zz, i, j, k, a, b, c);
+        dtri_area(a, b, c, /**/ da, db, dc);
+        c0 = Kb/24;
+        c1 = lentheta[i]*lentheta[i]/AREA[i]/AREA[i] * c0;
+        vec_scalar_append(da, c1, i, fx, fy, fz);
+    
+        c1 = lentheta[j]*lentheta[j]/AREA[j]/AREA[j] * c0;
+        vec_scalar_append(db, c1, j, fx, fy, fz);
+    
+        c1 = lentheta[k]*lentheta[k]/AREA[k]/AREA[k] * c0;
+        vec_scalar_append(dc, c1, k, fx, fy, fz);
+    }
 }
 
 real Energy(const real *x, const real *y, const real *z) {
@@ -205,9 +194,9 @@ void Force(const real *x, const real *y, const real *z,
     f_area_force(x, y, z, /**/ fx, fy, fz);
     f_volume_force(x, y, z, /**/ fx, fy, fz);
     f_harmonic_force(x, y, z, /**/ fx, fy, fz);
-    MSG("b: %g %g %g", fx[0], fy[0], fz[0]);
     force(x, y, z, /**/ fx, fy, fz);
-    MSG("a: %g %g %g", fx[0], fy[0], fz[0]);
+    MSG("%g %g %g", fx[0], fy[0], fz[0]);
+    exit(0);
 }
 
 static void write(real *fx, real *fy, real *fz) {
@@ -216,13 +205,13 @@ static void write(real *fx, real *fy, real *fz) {
 }
 
 static void force_ini() {
-  MALLOC(NV, &lentheta);
-  MALLOC(NV, &AREA);
+    MALLOC(NV, &lentheta);
+    MALLOC(NV, &AREA);
 }
 
 static void force_fin() {
-  FREE(lentheta);
-  FREE(AREA);
+    FREE(lentheta);
+    FREE(AREA);
 }
 
 static void energy_ini() {
@@ -253,8 +242,6 @@ int main(int __UNUSED argc, const char *v[]) {
 
     A0 = area(); v0 = volume();
     a0 = A0/NT;   vt = area2volume(A0); e0 = eq_tri_edg(a0);
-    MSG("vt/v0: %g", vt/v0);
-    MSG("area, volume, edg: %g %g %g", a0, vt, e0);
 
     force_ini();
     energy_ini();
@@ -265,22 +252,21 @@ int main(int __UNUSED argc, const char *v[]) {
     f_harmonic_ini(e0, Ke);
     real *queue[] = {XX, YY, ZZ, NULL};
 
-    while (v0 > vt) {
-        v0 -= vt *  0.01;
+    for (;;) {
         f_volume_ini(v0, Kv);
-        min_ini(STEEPEST_DESCENT);
-        for (i = 0; i < 100; i++) {
+        min_ini(VECTOR_BFGS2);
+        MSG("eng: %g", min_energy());
+        for (i = 0; i < 400; i++) {
             min_position(/**/ XX, YY, ZZ);
-            if (i % 1 == 0) {
-                punto_fwrite(NV, queue, stdout);
-                MSG("v/v0 = %g", volume()/v0);
-                MSG("eng: %g", min_energy());
-                off_write(XX, YY, ZZ, "q.off");
-            }
             min_iterate();
         }
+        MSG("a/a0 = %g v/v0 = %g", area()/a0/NT, volume()/v0);
+        punto_fwrite(NV, queue, stdout);
+        off_write(XX, YY, ZZ, "q.off");
         f_volume_fin();
         min_fin();
+        v0 -= vt/10;
+        if (v0 < vt) break;
     }
 
     force(XX, YY, ZZ, fx, fy, fz);
