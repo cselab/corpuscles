e98918842b2d7f1a079aceb0aae4645860d7a2a6
Sergey Litvinov <slitvinov@gmail.com>
2018-09-24 20:16:11 +0200

    initial import of harmonic ref

---
+++ lib/he/f/harmonic_ref.h
@@ -0,0 +1,16 @@
+#define T HeFHarmonicRef
+
+typedef struct He He;
+typedef struct T T;
+
+/* E = K/edg0 * sum_{edg} (edg - edg0)^2 */
+int he_f_harmonic_ref_ini(real e0, real K, He*, T**);
+int he_f_harmonic_ref_fin(T*);
+
+int he_f_harmonic_ref_force(T*, He*,
+                    const real *x, const real *y, const real *z, /**/
+                    real *fx, real *fy, real *fz);
+real he_f_harmonic_ref_energy(T*, He*, const real *x, const real *y, const real *z);
+int he_f_harmonic_ref_e(T*, /**/ real  **v);
+
+#undef T
