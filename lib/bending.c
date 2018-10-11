d45c414f28e6e1d30b83854c186792664bb5a6db
Sergey Litvinov <slitvinov@gmail.com>
2018-10-11 22:26:54 +0200

    compile bending.c

--- lib/bending.c
+++ lib/bending.c
@@ -13,20 +13,22 @@
 struct T { };
 
 int bending_ini(BendingParam param, He *he, /**/ T **pq)  {
-
+    return HE_OK;
 }
 
 int bending_force(T *q, He *he, const real *x, const real *y, const real *z, /**/ real *fx, real *fy, real *fz) {
-
+    return HE_OK;
 }
 
 real bending_energy(T *q, He *he, const real *x, const real *y, const real *z) {
+    return HE_OK;
+}
+
+int bending_energy_ver(T *q, /**/ real **e) {
+    return HE_OK;
 
 }
 
-int bending_energy_ver(T*, /**/ real **e) {
-
-}
-
-int bending_fin(T*) {
+int bending_fin(T *q) {
+    return HE_OK;    
 }
