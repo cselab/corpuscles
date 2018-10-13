a2951c78647049bac66374e0b563c1372b06d6ed
Sergey Litvinov <slitvinov@gmail.com>
2018-10-13 12:03:30 +0200

    wip: bending client

--- example/force/bending/main.c
+++ example/force/bending/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 #include <real.h>
 #include <he/bending.h>
@@ -7,26 +9,74 @@
 #include <he/he.h>
 #include <he/off.h>
 #include <he/memory.h>
+#include <he/err.h>
+#include <he/util.h>
+#include <he/macro.h>
+
+#define FMT_IN   XE_REAL_IN
+
+static const char **argv;
+static char name[4048];
 
 static real *fx, *fy, *fz, *xx, *yy, *zz, *eng;
 static int nv, nt;
 static He *he;
 static Bending *bending;
+static BendingParam param;
+static const char *me = "bending";
+
+static void usg() {
+    fprintf(stderr, "%s kantor/gompper/juelicher/meyer Kb C0, Kad, DA0D < OFF > PUNTO\n", me);
+}
+
+static int eq(const char *a, const char *b) { return util_eq(a, b); }
+static int scl(/**/ real *p) {
+    if (*argv == NULL) {
+        usg();
+        ER("not enough args");
+    }
+    if (sscanf(*argv, FMT_IN, p) != 1)
+        ER("not a number '%s'", *argv);
+    argv++;
+    return HE_OK;
+}
+static int str(/**/ char *p) {
+    if (*argv == NULL) {
+        usg();
+        ER("not enough args");
+    }
+    strncpy(p, *argv, 4048);
+    argv++;
+    return HE_OK;
+}
+static void arg() {
+    if (*argv != NULL && eq(*argv, "-h")) {
+        usg();
+        exit(0);
+    }
+    str(name);
+    scl(&param.Kb); scl(&param.C0); scl(&param.Kad); scl(&param.DA0D);
+}
 
 static void main0() {
-    BendingParam param;
-    real *queue[] = {xx, yy, zz, fx, fy, fz, NULL};
-    param.Kb = 1;
-    bending_ini("gompper", param, he,  &bending);
+    real e;
+    real *queue[] = {xx, yy, zz, fx, fy, fz, eng, NULL};
+    bending_ini(name, param, he,  &bending);
     bending_force(bending, he, xx, yy, zz, /**/ fx, fy, fz);
+    e = bending_energy(bending, he, xx, yy, zz);
+    bending_energy_ver(bending, /**/ &eng);
+    MSG("energy: %g", e);
     punto_fwrite(nv, queue, stdout);
     bending_fin(bending);
 }
 
-int main() {
+int main(int __UNUSED argc, const char *v[]) {
     int *tri;
     const char path[] = "/dev/stdin";
     static HeOff *off;
+    argv = v; argv++;
+    arg();
+    
     he_off_ini(path, &off);
 
     nv = he_off_nv(off);
@@ -35,12 +85,12 @@ int main() {
     he_tri_ini(nv, nt, tri, &he);
 
     MALLOC(nv, &xx); MALLOC(nv, &yy); MALLOC(nv, &zz); 
-    CALLOC(nv, &fx); CALLOC(nv, &fy); CALLOC(nv, &fz); CALLOC(nv, &eng);
+    CALLOC(nv, &fx); CALLOC(nv, &fy); CALLOC(nv, &fz);
 
     he_off_xyz(off, xx, yy, zz);
     main0();
 
-    FREE(xx); FREE(yy); FREE(zz); FREE(eng);
+    FREE(xx); FREE(yy); FREE(zz);
     FREE(fx); FREE(fy); FREE(fz);
 
     he_off_fin(off);
