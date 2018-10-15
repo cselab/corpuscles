6b46f1cd41c27bf3b800ba9ff144a48e1a511d40
Sergey Litvinov <slitvinov@gmail.com>
2018-10-15 13:54:07 +0200

    new coefficeints are zero

--- example/force/fd/main.c
+++ example/force/fd/main.c
@@ -85,9 +85,9 @@ static void main0() {
     int i;
     real e, f[3];
     param.Kb = 1;
-    param.C0 = 2;
-    param.Kad = 1;
-    param.DA0D = 3;
+    param.C0 = 0;
+    param.Kad = 0;
+    param.DA0D = 0;
 
     bending_ini(name, param, he,  &bending);
     bending_force(bending, he, xx, yy, zz, /**/ fx, fy, fz);
