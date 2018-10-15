98314e80a5f05c8414954b6dcc0e2c00851c6466
Sergey Litvinov <slitvinov@gmail.com>
2018-10-15 04:30:40 +0200

    minimize juelicher

--- example/min/visc/main.c
+++ example/min/visc/main.c
@@ -34,7 +34,7 @@ static void zero(int n, real *a) {
 #define FMT_IN   XE_REAL_IN
 
 static real rVolume, Ka, Kga, Kv, Ke;
-static real A0, V0;
+static real A0, V0, e0;
 static const char **argv;
 static char bending[4049];
 static const char *me = "min/visc";
@@ -154,18 +154,33 @@ static real Kin(real *vx, real *vy, real *vz) {
     return s;
 }
 
+static real max_vec(real *fx, real *fy, real *fz) {
+    int i;
+    real c, m;
+    m = 0;
+    for (i = 0; i < NV; i++) {
+        c = sqrt(fx[i]*fx[i] + fy[i]*fy[i] + fz[i]*fz[i]);
+        if (c > m)
+            m = c;
+    }
+    return m;
+}
+
 static void main0(real *vx, real *vy, real *vz,
                   real *fx, real *fy, real *fz) {
     int cnt, i;
-    real dt, mu, rnd;
+    real dt, dt_max, h, mu, rnd;
     real *queue[] = {XX, YY, ZZ, NULL};
-    dt = 1e-3;
-    mu = 20.0;
-    rnd = 0.01;
+    
+    dt_max = 0.1;
+    mu = 100.0;
+    h = 0.01*e0;
 
     zero(NV, vx); zero(NV, vy); zero(NV, vz);
     for (i = 0; /**/ ; i++) {
         Force(XX, YY, ZZ, /**/ fx, fy, fz);
+        dt = fmin(dt_max,  sqrt(h/max_vec(fx, fy, fz)));
+        rnd = 0.001*max_vec(vx, vy, vz);
         if (i % 1500 == 0) {
             do {
                 //equiangulate(&cnt);
@@ -174,12 +189,13 @@ static void main0(real *vx, real *vy, real *vz,
             } while (cnt > 0);
             punto_fwrite(NV, queue, stdout);
             printf("\n");
+            MSG("dt: %g", dt);
             MSG("eng: %g %g", Energy(XX, YY, ZZ), Kin(vx, vy, vz));
             MSG("area, vol: %g, %g", area()/A0, volume()/V0);
             off_write(XX, YY, ZZ, "q.off");
         }
+        jigle(rnd, vx, vy, vz);        
         visc_pair(mu, vx, vy, vz, /**/ fx, fy, fz);
-        jigle(rnd, vx, vy, vz);
         euler(-dt, vx, vy, vz, /**/ XX, YY, ZZ);
         euler( dt, fx, fy, fz, /**/ vx, vy, vz);
     }
@@ -187,6 +203,8 @@ static void main0(real *vx, real *vy, real *vz,
 
 static real sph_volume(real area) { return 0.09403159725795977*pow(area, 1.5); }
 static real target_volume(real area, real v) { return v*sph_volume(area); }
+static real eq_tri_edg(real area) { return 2*sqrt(area)/pow(3, 0.25); }
+
 
 int main(int __UNUSED argc, const char *v[]) {
     real a0;
@@ -202,6 +220,8 @@ int main(int __UNUSED argc, const char *v[]) {
     A0 = area();
     a0 = A0/NT;
     V0 = target_volume(A0, rVolume);
+    e0 = eq_tri_edg(a0);
+    
     MSG("v0/volume(): %g", V0/volume());
     MSG("area, volume, edg: %g %g", A0, V0);
 
@@ -212,7 +232,7 @@ int main(int __UNUSED argc, const char *v[]) {
 
     bending_param.Kb = Kb;
     bending_param.C0 = C0;
-    bending_param.Kad = Kb/(2*pi);
+    bending_param.Kad = 0.0;
     bending_param.DA0D = DA0D;
     f_bending_ini(bending, bending_param);
 
