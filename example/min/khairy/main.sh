d4ec88313d07d0771e721399cf01ef92e364c172
Sergey Litvinov <slitvinov@gmail.com>
2018-10-17 00:21:05 +0200

    use sph as initial conditions

--- example/min/khairy/main.sh
+++ example/min/khairy/main.sh
@@ -5,6 +5,6 @@ end=7500
 for DAD0 in 20 22.5 27.5 30 35 40
 do
     echo 2>&1 '************'  DAD0 = $DAD0 '************'
-    ./main juelicher $rV   3 3 3 0       $Kb $C0 $Kad $DAD0          $end < ../../../data/rbc/laplace/0.off > q
+    ./main juelicher $rV   3 3 3 0       $Kb $C0 $Kad $DAD0          $end < ../../../data/sph/laplace/0.off > q
     mv end.off $DAD0.off
 done
