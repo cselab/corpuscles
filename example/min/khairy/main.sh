cf433e92c33447fd090aec5da2c30fe74180fdd0
Sergey Litvinov <slitvinov@gmail.com>
2018-10-17 00:15:08 +0200

    a bit better

--- example/min/khairy/main.sh
+++ example/min/khairy/main.sh
@@ -1,8 +1,9 @@
 set -eu
 
-for DAD0 in 20 225 27.5 30 35 40
+rV=0.63 Kb=0.1 C0=0 Kad=0.064
+for DAD0 in 20 22.5 27.5 30 35 40
 do
     echo 2>&1 '************'  DAD0 = $DAD0 '************'
-    ./main juelicher 0.63   3 3 3 0       0.1 0 0.064 20          7500 < ../../../data/rbc/laplace/0.off > q
+    ./main juelicher $rV   3 3 3 0       $Kb $C0 $Kad $DAD0          7500 < ../../../data/rbc/laplace/0.off > q
     mv end.off $DAD0.off
 done
