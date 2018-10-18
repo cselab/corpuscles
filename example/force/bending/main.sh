c650023616d673e76df22b9123f359ae37bd3bc5
Sergey Litvinov <slitvinov@gmail.com>
2018-10-18 19:34:51 +0200

    minor

--- example/force/bending/main.sh
+++ example/force/bending/main.sh
@@ -1,10 +1,14 @@
 make
-./main meyer     1 0 0 0  < ../../../data/rbc/laplace/2.off > m
-./main juelicher 1 0 0 0  < ../../../data/rbc/laplace/2.off > j
-./main canham    1 0 0 0  < ../../../data/rbc/laplace/2.off > c
+off=../../../data/rbc/laplace/2.off
+./main meyer     1 0 0 0  <  $off > m
+./main juelicher 1 0 0 0  <  $off > j
+./main canham    1 0 0 0  <  $off > c
 
 gnuplot <<!
 set term png
 set output "i.png"
-plot [][:0.2] "m" u 'r':'fm' t "m", "c" u 'r':'fm' t "c", "j" u 'r':'fm' t "j"
+plot [][:0.2] \
+     "m" u 'r':'fm' t "m", \
+     "c" u 'r':'fm' t "c", \
+     "j" u 'r':'fm' t "j"
 !
