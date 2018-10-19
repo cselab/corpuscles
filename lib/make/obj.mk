55eff37aa70ffb7074ec3268eac4b6bf97227404
Sergey Litvinov <slitvinov@gmail.com>
2018-10-19 09:43:01 +0200

    clone gompper to gompper_xin

--- lib/make/obj.mk
+++ lib/make/obj.mk
@@ -4,16 +4,16 @@ util.o
 O_S = \
 area.s.o bending.s.o ddih.s.o dedg.s.o dih.s.o \
 dtri.s.o edg.s.o equiangulate.s.o f/area.s.o f/canham.s.o \
-f/edg_sq.s.o f/garea.s.o f/gompper.s.o f/gompper_kroll.s.o f/harmonic.s.o \
-f/harmonic_ref.s.o f/juelicher.s.o f/kantor.s.o f/meyer.s.o f/volume.s.o \
-normal.s.o off.s.o punto.s.o real.s.o sum.s.o \
-ten.s.o tri.s.o vec.s.o volume.s.o vtk.s.o \
-x.s.o
+f/edg_sq.s.o f/garea.s.o f/gompper.s.o f/gompper_kroll.s.o f/gompper_xin.s.o \
+f/harmonic.s.o f/harmonic_ref.s.o f/juelicher.s.o f/kantor.s.o f/meyer.s.o \
+f/volume.s.o normal.s.o off.s.o punto.s.o real.s.o \
+sum.s.o ten.s.o tri.s.o vec.s.o volume.s.o \
+vtk.s.o x.s.o
 O_D = \
 area.d.o bending.d.o ddih.d.o dedg.d.o dih.d.o \
 dtri.d.o edg.d.o equiangulate.d.o f/area.d.o f/canham.d.o \
-f/edg_sq.d.o f/garea.d.o f/gompper.d.o f/gompper_kroll.d.o f/harmonic.d.o \
-f/harmonic_ref.d.o f/juelicher.d.o f/kantor.d.o f/meyer.d.o f/volume.d.o \
-normal.d.o off.d.o punto.d.o real.d.o sum.d.o \
-ten.d.o tri.d.o vec.d.o volume.d.o vtk.d.o \
-x.d.o
+f/edg_sq.d.o f/garea.d.o f/gompper.d.o f/gompper_kroll.d.o f/gompper_xin.d.o \
+f/harmonic.d.o f/harmonic_ref.d.o f/juelicher.d.o f/kantor.d.o f/meyer.d.o \
+f/volume.d.o normal.d.o off.d.o punto.d.o real.d.o \
+sum.d.o ten.d.o tri.d.o vec.d.o volume.d.o \
+vtk.d.o x.d.o
