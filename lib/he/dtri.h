ab03325fda3a612db6f582bc6165bae6480591d7
Sergey Litvinov <slitvinov@gmail.com>
2018-10-25 14:55:55 +0200

    minor

--- lib/he/dtri.h
+++ lib/he/dtri.h
@@ -7,4 +7,3 @@ int dtri_norm(const real a[3], const real b[3], const real c[3], /**/ real x[3],
 int dtri_norm_x(const real a[3], const real b[3], const real c[3], /**/ real r[3]);
 int dtri_norm_y(const real a[3], const real b[3], const real c[3], /**/ real r[3]);
 int dtri_norm_z(const real a[3], const real b[3], const real c[3], /**/ real r[3]);
-              
