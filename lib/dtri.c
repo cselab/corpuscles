d907c98c96c094400dd15c09624b785f6b98dbf0
Sergey Litvinov <slitvinov@gmail.com>
2018-10-25 14:46:24 +0200

    minor

--- lib/dtri.c
+++ lib/dtri.c
@@ -68,3 +68,22 @@ int dtri_volume(real a[3], real b[3], real c[3], /**/ real da[3], real db[3], re
     dvolume(z, c, a, /**/ db);
     return HE_OK;
 }
+
+
+int dtri_norm(const real a[3], const real b[3], const real c[3], /**/
+              real x[3], real y[3], real z[3]) {
+    enum {X, Y, Z};
+    real A, n[3], e[3], u[3], C;
+    A = tri_area(a, b, c);
+    vec_minus(a, c, /**/ e);
+    tri_normal(a, b, c, /**/ n);
+    vec_cross(e, n, /**/ u);
+
+    C = 1/(2*A);
+
+    vec_scalar(n, C*u[X], /**/ x);
+    vec_scalar(n, C*u[Y], /**/ y);
+    vec_scalar(n, C*u[Z], /**/ z);    
+    
+    return HE_OK;
+}
