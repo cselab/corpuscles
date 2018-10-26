c21d785a8a149a8d1e95694fe3e5c9863829e97d
Sergey Litvinov <slitvinov@gmail.com>
2018-10-26 18:44:46 +0200

    minor

--- example/min/khairy/meyer/main.sh
+++ example/min/khairy/meyer/main.sh
@@ -1,13 +1,13 @@
 set -eu
 
-rV=0.63 Kb=0.1 C0=0 Kad=0.064
+rV=0.63 Kb=0.1 C0=0 Kad=0.128
 end=2500
 
 if test $# -ne 0
 then
     DAD0="$1"
     echo 2>&1 '************'  DAD0 = $DAD0 '************'
-    ./main juelicher $rV   3 3 3 0       $Kb $C0 $Kad -$DAD0          $end < ../../../data/sph/laplace/0.off > q
+    ./main meyer_xin $rV   3 3 3 0       $Kb $C0 $Kad -$DAD0          $end < ../../../data/sph/laplace/0.off > q
     mv end.off $DAD0.off
 else
     for DAD0 in 20 22.5 25 27.5 28.5 29.25 30 31.25 33.125 32.5 32.8125 33.75 35 40
