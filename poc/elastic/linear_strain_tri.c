14365567631abd319b13c374b0c59f89b7557a59
Sergey Litvinov <slitvinov@gmail.com>
2018-10-28 16:25:15 +0100

    add div by zero check

--- poc/elastic/linear_strain_tri.c
+++ poc/elastic/linear_strain_tri.c
@@ -5,6 +5,7 @@
 #include "he/err.h"
 
 #define SET(key, val) if ((key) != NULL) *(key) = (val)
+#define NOT_ZERO(x) if ((x) == 0) ERR(HE_NUM, "should not be zero");
 static real sq(real x) { return x*x; }
 
 #include "he/linear_strain_tri.h"
