3ca9a838c245b04564ce25b6e77bffeccd285581
Sergey Litvinov <slitvinov@gmail.com>
2018-10-29 16:20:45 +0100

    add constant_strain/3d.h

---
+++ lib/he/constant_strain/3d.h
@@ -0,0 +1,4 @@
+int constant_strain_force(void *param, real (*F1)(void*, real, real), real (*F2)(void*, real, real),
+                          const real a0[3], const real b0[3], const real c0[3],
+                          const real a[3], const real b[3], const real c[3], /**/
+                          real f[3]);
