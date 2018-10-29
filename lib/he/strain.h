90b8aafe949471ea55b781c86bb0055f5cdf6db7
Sergey Litvinov <slitvinov@gmail.com>
2018-10-29 18:22:29 +0100

    wip: Skalak

---
+++ lib/he/strain.h
@@ -0,0 +1,21 @@
+#define T Strain
+#define P StrainParam
+
+typedef struct T T;
+typedef struct P P;
+struct P { real Ka, Ks; };
+
+int strain_ini(const char *name, P, /**/ T**);
+int strain_fin(T*);
+
+int strain_force(T*,
+                 const real a0[3], const real b0[3], const real c0[3],
+                 const real a[3], const real b[3], const real c[3], /**/
+                 real da[3], real db[3], real dc[3]);
+
+real strain_energy(T*,
+                   const real a0[3], const real b0[3], const real c0[3],
+                   const real a[3], const real b[3], const real c[3]);
+
+#undef T
+#undef P
