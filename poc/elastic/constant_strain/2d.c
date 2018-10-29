5f25e0db5dfa573c7d4771129bf8a8d62b116b1b
Sergey Litvinov <slitvinov@gmail.com>
2018-10-29 16:03:40 +0100

    upd name

--- poc/elastic/constant_strain/2d.c
+++ poc/elastic/constant_strain/2d.c
@@ -8,5 +8,5 @@
 #define NOT_ZERO(x) if ((x) == 0) ERR(HE_NUM, "should not be zero");
 static real sq(real x) { return x*x; }
 
-#include "he/constant_strain_2d.h"
-#include "constant_strain_2d.inc"
+#include "he/constant_strain/2d.h"
+#include "constant_strain/2d.inc"
