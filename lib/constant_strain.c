fd4eb96c55a902807f78a8526e55c239f84fa795
Sergey Litvinov <slitvinov@gmail.com>
2018-10-29 18:12:17 +0100

    wip: skalak

--- lib/constant_strain.c
+++ lib/constant_strain.c
@@ -4,6 +4,7 @@
 #include "he/err.h"
 
 #include "he/memory.h"
+#include "he/macro.h"
 #include "he/constant_strain.h"
 
 #define T ConstantStrain
@@ -25,14 +26,14 @@ static real F_skalak(void *p0, real I1, real I2)  {
 
     return (((-2*I2)+sq(I1)+2*I1)*Ks)/12+(sq(I2)*Ka)/12;
 }
-static real F1_skalak(void *p0, real I1, real I2) {
+static real F1_skalak(void *p0, real I1, __UNUSED real I2) {
     P *p;
     real Ks;
     p = (P*)p0;
     Ks = p->Ks;
     return  (I1+1)*Ks/6;
 }
-static real F2_skalak(void *p0, real I1, real I2) {
+static real F2_skalak(void *p0, __UNUSED real I1, real I2) {
     P *p;
     real Ka, Ks;
     p = (P*)p0;
@@ -40,10 +41,14 @@ static real F2_skalak(void *p0, real I1, real I2) {
     return -(Ks-I2*Ka)/6;
 }
 
-int constant_strain_ini(const char *name, P param, /**/ T **pq) {
+int constant_strain_ini(__UNUSED const char *name, P param, /**/ T **pq) {
     T *q;
     MALLOC(1, &q);
 
+    q->param = param;
+    q->F = F_skalak;
+    q->F1 = F1_skalak;
+    q->F2 = F2_skalak;
 
     *pq = q;
     return HE_OK;
