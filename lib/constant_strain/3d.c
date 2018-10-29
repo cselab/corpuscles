6d6f9e09f016c6dc346349e9f8bc2e3f9adbaf6c
Sergey Litvinov <slitvinov@gmail.com>
2018-10-29 16:23:53 +0100

    initial import of 3d.c

--- lib/constant_strain/3d.c
+++ lib/constant_strain/3d.c
@@ -1,4 +1,11 @@
+#include "real.h"
+
+#include "he/err.h"
+
 int constant_strain_force(void *param, real (*F1)(void*, real, real), real (*F2)(void*, real, real),
                           const real a0[3], const real b0[3], const real c0[3],
                           const real a[3], const real b[3], const real c[3], /**/
-                          real da[3], real db[3], real dc[3]);
+                          real da[3], real db[3], real dc[3]) {
+
+    return HE_OK;
+}
--- lib/make/hdr.mk
+++ lib/make/hdr.mk
@@ -1,10 +1,10 @@
 H = \
-he/area.h he/bending.h he/constant_strain/2d.h he/container.h he/ddih.h \
-he/dedg.h he/dih.h he/dtri.h he/edg.h he/equiangulate.h \
-he/err.h he/f/area.h he/f/canham.h he/f/edg_sq.h he/f/garea.h \
-he/f/gompper.h he/f/gompper_kroll.h he/f/gompper_xin.h he/f/harmonic.h he/f/harmonic_ref.h \
-he/f/juelicher.h he/f/kantor.h he/f/meyer.h he/f/meyer_xin.h he/f/volume.h \
-he/hash.h he/he.h he/macro.h he/memory.h he/normal.h \
-he/off.h he/prec/d/real.h he/prec/s/real.h he/punto.h he/read.h \
-he/real.h he/sum.h he/ten.h he/tri.h he/util.h \
-he/vec.h he/volume.h he/vtk.h he/x.h
+he/area.h he/bending.h he/constant_strain/2d.h he/constant_strain/3d.h he/container.h \
+he/ddih.h he/dedg.h he/dih.h he/dtri.h he/edg.h \
+he/equiangulate.h he/err.h he/f/area.h he/f/canham.h he/f/edg_sq.h \
+he/f/garea.h he/f/gompper.h he/f/gompper_kroll.h he/f/gompper_xin.h he/f/harmonic.h \
+he/f/harmonic_ref.h he/f/juelicher.h he/f/kantor.h he/f/meyer.h he/f/meyer_xin.h \
+he/f/volume.h he/hash.h he/he.h he/macro.h he/memory.h \
+he/normal.h he/off.h he/prec/d/real.h he/prec/s/real.h he/punto.h \
+he/read.h he/real.h he/sum.h he/ten.h he/tri.h \
+he/util.h he/vec.h he/volume.h he/vtk.h he/x.h
