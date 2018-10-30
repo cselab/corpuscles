e72e7ef47a868c726c341ede6a86c7b291d737e9
Sergey Litvinov <slitvinov@gmail.com>
2018-10-30 11:47:42 +0100

    wip: testing 2d

--- example/constant_strain/2d/main.c
+++ example/constant_strain/2d/main.c
@@ -14,10 +14,33 @@
 
 #define FMT_IN   XE_REAL_IN
 static const char **argv;
+static const real h = 1e-6;
+static real a[2], b[2], c[2];
+static real v[2], u[2], w[2];
 
 enum {X, Y, Z};
 
-int scl(/**/ real *p) {
+static real  F(__UNUSED void *param, real I1, real I2) { return I1; }
+static real F1(__UNUSED void *param, real I1, real I2) { return 1;  }
+static real F2(__UNUSED void *param, real I1, real I2) { return 0;  }
+
+static int print2(const real a[2], const real b[2]) {
+    enum {X, Y};
+    printf("%.16g %.16g %.16g %.16g\n", a[X], a[Y], b[X], b[Y]);
+    return HE_OK;
+}
+
+static real energy() {
+    real I1, I2, A;
+    constant_strain_2d(NULL, F1, F2,
+                       a[X], a[Y], b[X], b[Y], c[X], c[Y],
+                       v[X], v[Y], u[X], u[Y], w[X], w[Y], /**/
+                       NULL, NULL, NULL, NULL, NULL, NULL,
+                       &I1, &I2, &A);
+    return F(NULL, I1, I2)*A;
+}
+
+static int scl(/**/ real *p) {
     if (*argv == NULL) ER("not enough args");
     if (sscanf(*argv, FMT_IN, p) != 1)
         ER("not a number '%s'", *argv);
@@ -25,24 +48,34 @@ int scl(/**/ real *p) {
     return HE_OK;
 }
 
-int vec2(/**/ real v[2]) {
+static int vec2(/**/ real v[2]) {
     scl(&v[X]);
     scl(&v[Y]);
     return HE_OK;
 }
 
-static real ks = 0, ka = 3;
-static real sq(real x) { return x*x; }
-static real  F(__UNUSED void *param, real I1, real I2) { return (((-2*I2)+sq(I1)+2*I1)*ks)/12+(sq(I2)*ka)/12; }
-static real F1(__UNUSED void *param, real I1, real I2) { return  (I1+1)*ks/6; }
-static real F2(__UNUSED void *param, real I1, real I2) { return -(ks-I2*ka)/6;}
+real fd0(real *p) {
+    real e, eh, v, t;
+    v = *p;
+    e  = energy();
+    *p += h;
+    eh  = energy();
+    return (eh - e)/h;
+}
+
+int fd(real dv[2], real du[2], real dw[2]) {
+    enum {X, Y};
+    dv[X] = fd0(&v[X]); dv[Y] = fd0(&v[Y]);
+    du[X] = fd0(&u[X]); du[Y] = fd0(&u[Y]);
+    dw[X] = fd0(&w[X]); dw[Y] = fd0(&w[Y]);
+    return HE_OK;
+}
 
 int eq(const char *a, const char *b) { return util_eq(a, b); }
 int main(__UNUSED int argc, const char **argv0) {
     const char *op;
-    real a[2], b[2], c[2];
-    real v[2], u[2], w[2];
     real da[2], db[2], dc[2];
+    real ha[2], hb[2], hc[2];
     real I1, I2, A;
     argv = argv0;
     argv++;
@@ -56,14 +89,12 @@ int main(__UNUSED int argc, const char **argv0) {
                            a[X], a[Y], b[X], b[Y], c[X], c[Y],
                            v[X], v[Y], u[X], u[Y], w[X], w[Y], /**/
                            &da[X], &da[Y], &db[X], &db[Y], &dc[X], &dc[Y],
-                           &I1, &I2, &A);
-        printf("A: %g\n", A);
-        printf("eng: %g\n", F(NULL, I1, I2));
-        printf("I12: %g %g\n", I1, I2);
-        printf("%g %.g\n", da[X], da[Y]);
-        printf("%g %.g\n", db[X], db[Y]);
-        printf("%g %.g\n", dc[X], dc[Y]);
-        MSG("cons: %g %g", da[X] + db[X] + dc[X], da[Y] + db[Y] + dc[Y]);
+                           NULL, NULL, NULL);
+        fd(ha, hb, hc);
+        puts("x y fx fy");
+        print2(da, ha);
+        print2(db, hb);
+        print2(dc, hc);
     } else
         ER("unknown operation '%s'", op);
     return 0;
