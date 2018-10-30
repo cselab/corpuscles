06e6aa7d5febbba4763323de0f96d10377024586
bianx <bianx@ethz.ch>
2018-10-30 15:00:06 +0100

    test dihedral

--- example/dih/main.c
+++ example/dih/main.c
@@ -35,6 +35,12 @@ int main(__UNUSED int argc, const char **v) {
         vec_printf(db, "%g");
         vec_printf(dc, "%g");
         vec_printf(dd, "%g");
+	ddih_angle_sup(a, b, c, d, /**/ da, db, dc, dd);
+        vec_printf(da, "%g");
+        vec_printf(db, "%g");
+        vec_printf(dc, "%g");
+        vec_printf(dd, "%g");
+
     } else if (eq(op, "dcos")) {
         vec(a); vec(b); vec(c); vec(d);
         ddih_cos(a, b, c, d, /**/ da, db, dc, dd);
---
+++ example/dih/test_dangle.sh
@@ -0,0 +1,20 @@
+#following coordinates rotate clockwise
+#4th quadrant
+./main dangle -1 0 0  0 -1 0  0 1 0  1 0 0
+./main dangle -1 0 0  0 -1 0  0 1 0  1 0 -0.1
+./main dangle -1 0 0  0 -1 0  0 1 0  0.1 0 -1
+
+#3rd quandrant
+./main dangle -1 0 0  0 -1 0  0 1 0  0 0 -1
+./main dangle -1 0 0  0 -1 0  0 1 0  -0.1 0 -1
+./main dangle -1 0 0  0 -1 0  0 1 0  -1 0 -0.1
+
+#2nd quandrant
+./main dangle -1 0 0  0 -1 0  0 1 0  -1 0 0
+./main dangle -1 0 0  0 -1 0  0 1 0  -1 0 0.1
+./main dangle -1 0 0  0 -1 0  0 1 0  -0.1 0 1
+
+#1st quandrant
+./main dangle -1 0 0  0 -1 0  0 1 0  0 0 1
+./main dangle -1 0 0  0 -1 0  0 1 0  0.1 0 1
+./main dangle -1 0 0  0 -1 0  0 1 0  1 0 0.1
