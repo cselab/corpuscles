833988dd37c0714125cc9463cc1e69a2631fe0cb
bianx <bianx@ethz.ch>
2018-10-30 12:55:03 +0100

    dih.c

--- lib/dih.c
+++ lib/dih.c
@@ -10,19 +10,26 @@
 static const real pi = 3.141592653589793115997964;
   
 real dih_angle(const real a[3], const real b[3], const real c[3], const real d[3]) {
+  real ang, ang_s;
+  
+  ang_sdih_angle(a, b, c, d);
+  ang = pi - ang_s;
+    
+  return ang;
+}
+real dih_angle_supplementary(const real a[3], const real b[3], const real c[3], const real d[3]) {
     real x, y, ang, n[3], k[3], nk[3], e[3];
     tri_normal(b, c, a,   n);
     tri_normal(c, b, d,   k);
     x = vec_dot(n, k);
     vec_cross(n, k,    nk);
     y = vec_abs(nk);
-    ang = pi - atan2(y, x);
+    ang = atan2(y, x);
     vec_minus(c, b, e);
     if (vec_dot(e, nk) < 0)
         ang = - ang;
     return ang;
 }
-
 real dih_cos(const real a[3], const real b[3], const real c[3], const real d[3]) {
     real ang;
     ang = dih_angle(a, b, c, d);
