419a06794753f5f03e2d1d98167bba3dc6256d1c
Sergey Litvinov <slitvinov@gmail.com>
2018-11-01 21:59:48 +0100

    more observation about 3d.c

--- example/tri/main.c
+++ example/tri/main.c
@@ -24,8 +24,8 @@ int scl(/**/ real *p) {
 int eq(const char *a, const char *b) { return util_eq(a, b); }
 int main(__UNUSED int argc, const char **v) {
     const char *op;
-    real a[3], b[3], c[3], d[3], n[3], f[3];
-    real ux, uy, vx, vy, fx, fy;
+    real a[3], b[3], c[3], n[3], ex[3], ey[3];
+    real ux, uy, vx, vy;
     argv = v;
     argv++;
     if (*argv == NULL) ER("mssing OP");
@@ -53,9 +53,10 @@ int main(__UNUSED int argc, const char **v) {
         printf("%.16g %.16g\n", ux, uy);
         printf("%.16g %.16g\n", vx, vy);
     } else if (eq(op, "2to3")) {
-        vec(a); vec(b); vec(c); scl(&fx); scl(&fy);
-        tri_2to3(a, b, c, fx, fy, /**/ f);
-        vec_printf(f, "%.16g");
+        vec(a); vec(b); vec(c);
+        tri_2to3(a, b, c, /**/ ex, ey);
+        vec_printf(ex, "%.16g");
+        vec_printf(ey, "%.16g");        
     } else
         ER("unknown operation '%s'", op);
     return 0;
