4c78628c294a3d6e2475ca4296dd48db1d215135
Sergey Litvinov <slitvinov@gmail.com>
2018-11-04 12:46:27 +0100

    add dvolume

---
+++ lib/f/dvolume.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+
+#include "real.h"
+#include "he/memory.h"
+#include "he/err.h"
+#include "he/he.h"
+#include "he/vec.h"
+#include "he/tri.h"
+#include "he/dtri.h"
+
+#include "he/f/dvolume.h"
+
+#define T HeFDvolume
+
+struct T {
+    int n;
+    real *volume;
+};
+
+static real sum(int n, real *volume) {
+    int t;
+    real v;
+    v = 0;
+    for (t = 0; t < n; t++) v += volume[t];
+    return v;
+}
+int he_f_dvolume_ini(He *he, T **pq) {
+    T *q;
+    int n;
+    MALLOC(1, &q);
+    
+    n = he_nt(he);
+    MALLOC(n, &q->volume);
+    q->n = n;
+
+    *pq = q;
+    return HE_OK;
+}
+
+int he_f_dvolume_fin(T *q) {
+    FREE(q->volume); FREE(q);
+    return HE_OK;
+}
+
+int he_f_dvolume_v(T *q, /**/ real  **pa) {
+    *pa = q->volume;
+    return HE_OK;
+}
+
+static void get_ijk(int t, He *he, /**/ int *pi, int *pj, int *pk) {
+    int h, n, nn, i, j, k;
+    h = he_hdg_tri(he, t);
+    n = he_nxt(he, h);
+    nn = he_nxt(he, n);
+    i = he_ver(he, h); j = he_ver(he, n); k = he_ver(he, nn);
+    *pi = i; *pj = j; *pk = k;
+}
+static void get(int t, He *he,
+                const real *x, const real *y, const real *z, /**/
+                real a[3], real b[3], real c[3]) {
+    int i, j, k;
+    get_ijk(t, he, &i, &j, &k);
+    vec_get(i, x, y, z, /**/ a);
+    vec_get(j, x, y, z, /**/ b);
+    vec_get(k, x, y, z, /**/ c);
+}
+static void compute_volume(He *he, const real *x, const real *y, const real *z, /**/ real *volume) {
+    real a[3], b[3], c[3];
+    int n, t;
+    n = he_nt(he);
+    for (t = 0; t < n; t++) {
+        get(t, he, x, y, z, /**/ a, b, c);
+        volume[t]  = tri_volume(a, b, c);
+    }
+}
+
+static void compute_force(real v,
+                          He *he, const real *x, const real *y, const real *z, /**/
+                          real *fx, real *fy, real *fz) {
+    int n, t, i, j, k;
+    real a[3], b[3], c[3], da[3], db[3], dc[3], coeff;
+    n = he_nt(he);
+    for (t = 0; t < n; t++) {
+        get_ijk(t, he, /**/ &i, &j, &k);
+        vec_get(i, x, y, z, /**/ a);
+        vec_get(j, x, y, z, /**/ b);
+        vec_get(k, x, y, z, /**/ c);
+        dtri_volume(a, b, c, /**/ da, db, dc);
+        vec_append(da, i, /**/ fx, fy, fz);
+        vec_append(db, j, /**/ fx, fy, fz);
+        vec_append(dc, k, /**/ fx, fy, fz);
+    }
+}
+
+int he_f_dvolume_force(T *q, He *he,
+                      const real *x, const real *y, const real *z, /**/
+                      real *fx, real *fy, real *fz) {
+    int n;
+    real *volume, v;
+    n = q->n;
+    volume = q->volume;
+    if (he_nt(he) != n)
+        ERR(HE_INDEX, "he_nt(he)=%d != n = %d", he_nt(he), n);
+    compute_volume(he, x, y, z, /**/ volume);
+    v = sum(n, volume);
+    compute_force(v, he, x, y, z, /**/ fx, fy, fz);
+    return HE_OK;
+}
+
+real he_f_dvolume_energy(T *q, He *he,
+                         const real *x, const real *y, const real *z) {
+    int n;
+    real *volume;
+    n = q->n;
+    volume = q->volume;
+
+    if (he_nt(he) != n)
+        ERR(HE_INDEX, "he_nt(he)=%d != n = %d", he_nt(he), n);
+
+    compute_volume(he, x, y, z, /**/ volume);
+    return sum(n, volume);
+}
--- lib/make/obj.mk
+++ lib/make/obj.mk
@@ -4,18 +4,18 @@ util.o
 O_S = \
 area.s.o bending.s.o ddih.s.o dedg.s.o dih.s.o \
 dtri.s.o edg.s.o equiangulate.s.o f/area.s.o f/canham.s.o \
-f/edg_sq.s.o f/garea.s.o f/gompper.s.o f/gompper_kroll.s.o f/gompper_xin.s.o \
-f/harmonic.s.o f/harmonic_ref.s.o f/juelicher.s.o f/juelicher_xin.s.o f/kantor.s.o \
-f/meyer.s.o f/meyer_xin.s.o f/strain.s.o f/volume.s.o normal.s.o \
-off.s.o punto.s.o real.s.o strain/2d.s.o strain/3d.s.o \
-strain.s.o sum.s.o ten.s.o tri.s.o vec.s.o \
-volume.s.o vtk.s.o x.s.o y.s.o
+f/dvolume.s.o f/edg_sq.s.o f/garea.s.o f/gompper.s.o f/gompper_kroll.s.o \
+f/gompper_xin.s.o f/harmonic.s.o f/harmonic_ref.s.o f/juelicher.s.o f/juelicher_xin.s.o \
+f/kantor.s.o f/meyer.s.o f/meyer_xin.s.o f/strain.s.o f/volume.s.o \
+normal.s.o off.s.o punto.s.o real.s.o strain/2d.s.o \
+strain/3d.s.o strain.s.o sum.s.o ten.s.o tri.s.o \
+vec.s.o volume.s.o vtk.s.o x.s.o y.s.o
 O_D = \
 area.d.o bending.d.o ddih.d.o dedg.d.o dih.d.o \
 dtri.d.o edg.d.o equiangulate.d.o f/area.d.o f/canham.d.o \
-f/edg_sq.d.o f/garea.d.o f/gompper.d.o f/gompper_kroll.d.o f/gompper_xin.d.o \
-f/harmonic.d.o f/harmonic_ref.d.o f/juelicher.d.o f/juelicher_xin.d.o f/kantor.d.o \
-f/meyer.d.o f/meyer_xin.d.o f/strain.d.o f/volume.d.o normal.d.o \
-off.d.o punto.d.o real.d.o strain/2d.d.o strain/3d.d.o \
-strain.d.o sum.d.o ten.d.o tri.d.o vec.d.o \
-volume.d.o vtk.d.o x.d.o y.d.o
+f/dvolume.d.o f/edg_sq.d.o f/garea.d.o f/gompper.d.o f/gompper_kroll.d.o \
+f/gompper_xin.d.o f/harmonic.d.o f/harmonic_ref.d.o f/juelicher.d.o f/juelicher_xin.d.o \
+f/kantor.d.o f/meyer.d.o f/meyer_xin.d.o f/strain.d.o f/volume.d.o \
+normal.d.o off.d.o punto.d.o real.d.o strain/2d.d.o \
+strain/3d.d.o strain.d.o sum.d.o ten.d.o tri.d.o \
+vec.d.o volume.d.o vtk.d.o x.d.o y.d.o
