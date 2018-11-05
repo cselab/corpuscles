fe9073c947f2d824a881e3caf3d4b8a53eae2327
Sergey Litvinov <slitvinov@gmail.com>
2018-11-05 17:02:04 +0100

    use alg_eig

--- lib/orient.c
+++ lib/orient.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "real.h"
+
 #include "he/memory.h"
 #include "he/err.h"
 #include "he/he.h"
@@ -10,6 +11,9 @@
 #include "he/orient.h"
 
 #define T Orient
+
+/* TODO: from alg */
+int alg_eig_vectors(const real data[6], /**/ real e0[9]);
 
 struct T {
     int n;
@@ -38,42 +42,22 @@ int orient_fin(T *q) {
     return HE_OK;
 }
 
-static int inv3x3(const real m[6], /**/ real r[6]) {
-    enum {XX, XY, XZ, YY, YZ, ZZ};
-    real eps = 1e-12;
-    real xx, yy, zz, xy, xz, yz;
-    real yz2, xz2, xy2;
-    real mx, my, mz; /* minors */
-    real d, i; /* determinant and its inverse */
-
-    xx = m[XX]; yy = m[YY]; zz = m[ZZ];
-    xy = m[XY]; xz = m[XZ];
-    yz = m[YZ];
-    yz2 = yz*yz; xz2 = xz*xz; xy2 = xy*xy;
-
-    mx  = yy*zz-yz2;
-    my  = xy*zz-xz*yz;
-    mz  = xy*yz-xz*yy;
-    d   = mz*xz-my*xy+mx*xx;
-    if (d < eps && d > -eps)
-        ERR(HE_NUM, "Matrix is singular, determinant %.16g", d);
-    i   = 1/d;
-
-    r[XX] =  mx*i;
-    r[XY] = -my*i;
-    r[XZ] =  mz*i;
-    r[YY] = i*(xx*zz-xz2);
-    r[YZ] = i*(xy*xz-xx*yz);
-    r[ZZ] = i*(xx*yy-xy2);
+static int transpose(/*io*/ real v[9]) {
+    enum {XX, XY, XZ,
+          YX, YY, YZ,
+          ZX, ZY, ZZ};
+    real t;
+    t = v[XY]; v[XY] = v[YX]; v[YX] = t;
+    t = v[XZ]; v[XZ] = v[ZX]; v[ZX] = t;
+    t = v[YZ]; v[YZ] = v[ZY]; v[ZY] = t;
     return HE_OK;
 }
 
-
 int orient_apply(T *q, /**/ real *xx, real *yy, real *zz) {
-    enum {XX, XY, XZ, YY, YZ, ZZ};    
-    int n, i;
+    enum {XX, XY, XZ, YY, YZ, ZZ};
+    int n, i, j;
     real x0, y0, z0, x, y, z;
-    real m[6], r[6];
+    real m[6], v[3*3];
 
     n = q->n;
 
@@ -85,17 +69,22 @@ int orient_apply(T *q, /**/ real *xx, real *yy, real *zz) {
     m[XX] = m[XY] = m[XZ] = m[YY] = m[YZ] = m[ZZ] = 0;
     for (i = 0; i < n; i++) {
         x = xx[i]; y = yy[i]; z = zz[i];
-	m[XX] += y*y + z*z; m[XY] += -x*y; m[XZ] += -x*z;
+        m[XX] += y*y + z*z; m[XY] += -x*y; m[XZ] += -x*z;
         m[YY] += x*x + z*z; m[YZ] += -y*z;
         m[ZZ] += x*x + y*y;
     }
 
-    inv3x3(m, r);
+    alg_eig_vectors(m, v);
+    transpose(v);
+    MSG("v[1,0]: %g", v[1 + 3*0]);
+    MSG("v[0,1]: %g", v[0 + 3*1]);
+
     for (i = 0; i < n; i++) {
         x = xx[i]; y = yy[i]; z = zz[i];
-        x0 = r[XX]*x + r[XY]*y + r[XZ]*z;
-        y0 = r[XY]*x + r[YY]*y + r[YZ]*z;
-        z0 = r[XZ]*x + r[YZ]*y + r[ZZ]*z;
+        j = 0;
+        x0 = v[j++]*x + v[j++]*y + v[j++]*z;
+        y0 = v[j++]*x + v[j++]*y + v[j++]*z;
+        z0 = v[j++]*x + v[j++]*y + v[j++]*z;
         xx[i] = x0; yy[i] = y0; zz[i] = z0;
     }
 
