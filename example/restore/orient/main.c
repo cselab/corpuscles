5e78e9b39f655a2bd55fb4a36269e680af1611dc
Sergey Litvinov <slitvinov@gmail.com>
2018-11-05 14:40:02 +0100

    initial import of a client for restore_orient

---
+++ example/restore/orient/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+
+#include <real.h>
+#include <he/memory.h>
+#include <he/off.h>
+#include <he/restore.h>
+#include <he/he.h>
+#include <he/y.h>
+
+static int nv;
+static real *x, *y, *z;
+static He *he;
+static Restore *restore;
+
+static void main0() {
+    real *queue[] = {x, y, z, NULL};
+    restore_orient(restore, x, y, z);
+    he_off_he_xyz_fwrite(he, x, y, z, stdout);
+}
+
+int main() {
+    real V;
+    y_ini("/dev/stdin", &he, &x, &y, &z);
+    nv = he_nv(he);
+    V = 5.0;
+    restore_ini(V, he, &restore);
+    main0();
+    restore_fin(restore);
+
+    y_fin(he, x, y, z);
+    return 0;
+}
