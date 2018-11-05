08b2449a9c36e0cbfdf7eb2617799b9cf633ff72
Sergey Litvinov <slitvinov@gmail.com>
2018-11-05 11:00:41 +0100

    wip: restore_volume

--- lib/restore.c
+++ lib/restore.c
@@ -6,7 +6,7 @@
 #include "he/he.h"
 #include "he/vec.h"
 #include "he/sum.h"
-
+#include "he/f/dvolume.h"
 #include "he/restore.h"
 
 #define T Restore
@@ -15,6 +15,7 @@ struct T {
     int n;
     real volume;
     real *fx, *fy, *fz;
+    HeFDvolume *dvolume;
 };
 
 static real sum(int n, real *a) { return he_sum_array(n, a); }
@@ -26,8 +27,10 @@ int restore_ini(real volume, He *he, T **pq) {
 
     n = he_nv(he);
 
-    MALLOC(n, &q->fx); MALLOC(n, &q->fy); MALLOC(n, &q->fz);
     q->n = n;
+    q->volume = volume;
+    MALLOC(n, &q->fx); MALLOC(n, &q->fy); MALLOC(n, &q->fz);
+    he_f_dvolume_ini(he, &q->dvolume);
 
     *pq = q;
     return HE_OK;
@@ -35,11 +38,52 @@ int restore_ini(real volume, He *he, T **pq) {
 
 int restore_fin(T *q) {
     FREE(q->fx); FREE(q->fy); FREE(q->fz);
+    he_f_dvolume_fin(q->dvolume);
     FREE(q);
     return HE_OK;
 }
 
+static real sum_sq(int n, real *x, real *y, real *z) {
+    real v, w;
+    int i;
+    HeSum *sum;
+    he_sum_ini(&sum);
+    for (i = 0; i < n; i++) {
+        w = x[i]*x[i] + y[i]*y[i] + z[i]*z[i];
+        he_sum_add(sum, w);
+    }
+    v = he_sum_get(sum);
+    he_sum_fin(sum);
+    return v;
+}
+
+static void zero(int n, real *a) {
+    int i;
+    for (i = 0; i < n; i++)
+        a[i] = 0;
+}
+
 int restore_volume(T *q, He *he, /**/
                     real *x, real *y, real *z) {
+    int n, status;
+    real volume, volume0, delta, lambda, step;
+    HeFDvolume *dvolume;
+    real *fx, *fy, *fz;
+
+    volume0 = q->volume;
+    dvolume = q->dvolume;
+    fx = q->fx; fy = q->fy; fz = q->fz;
+    n = he_nv(he);
+
+    zero(n, fx); zero(n, fy); zero(n, fx);
+    status = he_f_dvolume_force(dvolume, he, x, y, z, /**/ fx, fy, fz);
+    if (status != HE_OK)
+        ERR(HE_NUM, "he_f_dvolume_force failed");
+    volume = he_f_dvolume_energy(dvolume, he, x, y, z);
+
+    lambda = sum_sq(n, fx, fy, fz);
+    delta = volume - volume0;
+    step = -delta/lambda;
+    
     return HE_OK;
 }
