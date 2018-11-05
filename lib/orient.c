5aed557b3e50a96ee8c9f7d40d173790e7086c38
Sergey Litvinov <slitvinov@gmail.com>
2018-11-05 14:55:41 +0100

    compute moment of intertia

--- lib/orient.c
+++ lib/orient.c
@@ -38,6 +38,24 @@ int orient_fin(T *q) {
     return HE_OK;
 }
 
-int orient_apply(T *q, real *x, real *y, real *z) {
+int orient_apply(T *q, /**/ real *xx, real *yy, real *zz) {
+    int n, i;
+    real xc, yc, zc, x, y, z, XX, XY, XZ, YY, YZ, ZZ;
+
+    n = q->n;
+
+    xc = sum(n, xx)/n; yc = sum(n, yy)/n; zc = sum(n, zz)/n;
+    for (i = 0; i < n; i++) {
+        xx[i] -= xc; yy[i] -= yc; zz[i] -= zc;
+    }
+
+    XX = XY = XZ = YY = YZ = ZZ = 0;
+    for (i = 0; i < n; i++) {
+        x = xx[i]; y = yy[i]; z = zz[i];
+	XX += y*y + z*z; XY += -x*y; XZ += -x*z;
+        YY += x*x + z*z; YZ += -y*z;
+        ZZ += x*x + y*y;
+    }
+
     return HE_OK;
 }
