8e48552e97787c3527d8524c3e89108c90bfa3ee
bianx <bianx@ethz.ch>
2018-11-07 18:51:58 +0100

    Nv5120 runs

---
+++ example/min/helfrich_xin/C0_Nv5120/kantor_nelson_Kb0001/sphere/run_sphere.sh
@@ -0,0 +1,24 @@
+set -eu
+
+Vr=0.65
+Kb=0.001
+C0=0
+Kad=0
+DA0D=0
+end=100000
+
+off=../../../../../../data/sph/laplace/Nt5120.off
+
+if test $# -ne 0
+then
+    Vr="$1"
+    echo '************'  Vr = $Vr '************'
+    #rm inter.dat
+    ../../../main kantor $Vr 3 3 3 0 $Kb $C0 $Kad $DA0D $end < $off > sphere_Vr$Vr.msg
+    mv end.off sphere_Vr$Vr.off
+    mv inter.dat sphere_Vr$Vr.dat
+else
+    for Vr in 0.67 0.66 0.65 0.64 0.63 0.62 0.61 0.6 0.59 0.58 0.57 0.56 0.55
+    do sh run_sphere.sh $Vr
+    done
+fi
