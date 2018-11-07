095a7443ef0dee3c45f3d032179810572554670e
Sergey Litvinov <slitvinov@gmail.com>
2018-11-07 14:47:41 +0100

    simplify

--- example/min/visc/main.c
+++ example/min/visc/main.c
@@ -182,30 +182,9 @@ static real max_vec(real *fx, real *fy, real *fz) {
     return m;
 }
 
-static int restore_volume(real *fx, real *fy, real *fz) {
-    int j, nsub;
-    real v, dv, q, step, f[3];
-    int m;
-
-    v = volume();
-    dv = V0 - v;
-    if (dv/V0 > -EPS && dv/V0 < EPS)
-        return HE_OK;
-    ForceVolume(XX, YY, ZZ, /**/ fx, fy, fz);
-    q = 0;
-    for (m = 0; m < NV; m++) {
-        vec_get(m, fx, fy, fz, /**/ f);
-        q += vec_dot(f, f);
-    }
-    step = -dv/q;
-    euler(step, fx, fy, fz, /**/ XX, YY, ZZ);
-    MSG("v: %g %g %g %g", v, volume(), V0, dv);
-    exit(0);
-}
-
 static void main0(real *vx, real *vy, real *vz,
                   real *fx, real *fy, real *fz) {
-    int cnt, end, i;
+    int cnt, end, i, j, nsub;
     real dt, dt_max, h, mu, rnd;
     real A, V;
     real *queue[] = {XX, YY, ZZ, NULL};
@@ -214,6 +193,7 @@ static void main0(real *vx, real *vy, real *vz,
     mu = 100.0;
     h = 0.01*e0;
     end = 10000;
+    nsub = 100;
     
     zero(NV, vx); zero(NV, vy); zero(NV, vz);
     for (i = 0; i < end ; i++) {
@@ -224,7 +204,13 @@ static void main0(real *vx, real *vy, real *vz,
         visc_pair(mu, vx, vy, vz, /**/ fx, fy, fz);
         euler(-dt, vx, vy, vz, /**/ XX, YY, ZZ);
         euler( dt, fx, fy, fz, /**/ vx, vy, vz);
-        restore_volume(fx, fy, fz);
+
+        for (j = 0; j < nsub; j++) {
+            ForceVolume(XX, YY, ZZ, /**/ fx, fy, fz);
+            visc_pair(mu, vx, vy, vz, /**/ fx, fy, fz);
+            euler(-dt, vx, vy, vz, /**/ XX, YY, ZZ);
+            euler( dt, fx, fy, fz, /**/ vx, vy, vz);
+        }
 
         if (i % 100 == 0) {
 	  do {
--- example/min/visc/main.sh
+++ example/min/visc/main.sh
@@ -3,24 +3,10 @@ set -eu
 . he.util
 pi=3.141592653589793
 
-: ${np=2}
-
-DA0D=0
-off=$HOME/e.off
+off=$HOME/0.off
+ref=$off
 
 v=0.642
-v0=`off.rvolume $off`
-volume=`off.volume $off`
-area=`off.area   $off`
-ta=`ae "3^(2/3)*4^(1/3)*$pi^(1/3)*$volume^(2/3)/$v^(2/3)"`
-scale=`ae "($ta/$area)^(1/2)*$v0^(1/3)"`
+Ka=1 Kga=1 Kb=0 Ks=0 Kad=`ae 2/$pi*$Kb` DA0D=0 C0=0
 
-Ka=1
-Kga=1
-Kb=0
-Kad=`ae 2/$pi*$Kb`
-C0=0
-Ks=0
-
-off.scale $scale $off > ref.off
-./main juelicher $v   $Ka $Kga ref.off linear $Ks 0      $Kb $C0 $Kad $DA0D < $off '>' q
+./main juelicher $v   $Ka $Kga $ref linear $Ks 0      $Kb $C0 $Kad $DA0D < $off '>' q
