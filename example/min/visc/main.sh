e617e09918010fb20fb35191d199ad95278c84ae
Sergey Litvinov <slitvinov@gmail.com>
2018-11-08 22:18:44 +0100

    minor

--- example/min/visc/main.sh
+++ example/min/visc/main.sh
@@ -4,9 +4,9 @@ set -eu
 pi=3.141592653589793
 
 v=0.5
-off=$HOME/he/data/prolate/laplace/Nt1280/prolate_volume_Vr0.8.off
+off=../../../data/prolate/laplace/Nt1280/prolate_volume_Vr0.8.off
 #off=$HOME/q.off
-ref=$HOME/s.off
+ref=$off
 
 Ka=5 Kga=0.5 Ks=0 Kh=0 Kb=0.025 Kad=`ae "0.0/$pi*$Kb"` DA0D=0 C0=0
 
