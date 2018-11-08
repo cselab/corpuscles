0fc0b8f674215f330ce85979fae0e18cd0a61a78
bianx <bianx@ethz.ch>
2018-11-08 14:14:01 +0100

    juelicher NV5120 runs 2nd round

---
+++ example/min/helfrich_xin/C0_Nv5120/juelicher_Kb0001/prolate/run_prolate1.sh
@@ -0,0 +1,24 @@
+set -eu
+
+Vr=0.65
+Kb=0.001
+C0=0
+Kad=0
+DA0D=0
+end=100000
+
+off=../../../../../../data/prolate/laplace/Nt5120/prolate_volume_Vr
+
+if test $# -ne 0
+then
+    Vr="$1"
+    echo '************'  Vr = $Vr '************'
+    #rm inter.dat
+    ../../../main juelicher_xin $Vr 3 3 3 0 $Kb $C0 $Kad $DA0D $end < $off$Vr.off > prolate_Vr$Vr.msg
+    mv end.off prolate_Vr$Vr.off
+    mv inter.dat prolate_Vr$Vr.dat
+else
+    for Vr in 0.5 0.45 0.4 0.35 0.3 0.25 0.2
+    do sh run_prolate.sh $Vr
+    done
+fi
