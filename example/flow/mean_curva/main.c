5c1b0cd6741822bc6fcc212fdaa988529d1aab28
Sergey Litvinov <slitvinov@gmail.com>
2018-11-09 22:50:07 +0100

    laplace

--- example/flow/mean_curva/main.c
+++ example/flow/mean_curva/main.c
@@ -11,14 +11,14 @@
 #include <he/y.h>
 
 static He *he;
-static real *x, *y, *z, *H;
+static real *x, *y, *z, *fx, *fy, *fz;
 static Bending *bending;
 static BendingParam param;
 static int nv;
 
 int main() {
     real eng;
-    const char *name = "juelicher";
+    const char *name = "meyer_xin";
 
     y_ini("/dev/stdin", &he, &x, &y, &z);
     nv = he_nv(he);
@@ -27,11 +27,11 @@ int main() {
     bending_ini(name, param, he,  &bending);
 
     eng = bending_energy(bending, he, x, y, z);
-    bending_curva_mean_ver(bending, &H);
+    bending_laplace_ver(bending, &fx, &fy, &fz);
 
     MSG("eng: %g", eng);
-    MSG("H[0]: %g", H[0]);
-    MSG("H[nv-1]: %g", H[nv-1]);
+    MSG("fx[0]: %g", fx[0]);
+    MSG("fy[nv-1]: %g", fx[nv-1]);
 
     bending_fin(bending);
     y_fin(he, x, y, z);
