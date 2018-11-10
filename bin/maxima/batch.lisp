caf0dc83f8e026308911385369ccadab7971a49e
Sergey Litvinov <slitvinov@gmail.com>
2018-11-10 19:00:13 +0100

    better (bye); add a test

--- bin/maxima/batch.lisp
+++ bin/maxima/batch.lisp
@@ -1,13 +1,24 @@
 ($load "eval_string")
 
 (setq OK 0 FAIL 1)
+
+(defun bye0 (code)
+  #+(or cmu scl clisp) (ext:quit code)
+  #+sbcl               (sb-ext:quit code)
+  #+allegro            (excl:exit code :quiet t)
+  #+(or mcl openmcl)   (ccl:quit code)
+  #+gcl                (system::quit code)
+  #+ecl                (si:quit code)
+  #+lispworks          (lispworks:quit code)
+  #+abcl               (cl-user::quit code)
+  #+kcl                (lisp::bye code)
+  )
+
 (defun get_args (lst)
   (if lst
       (let ((fst (car lst))
 	    (rst (cdr lst)))
 	(if (equal fst "----") rst (get_args rst)))))
-(defun bye0 (code)
-  (ext::quit code))
 
 (setq args (get_args (get-application-args)))
 (setq file (car args)
