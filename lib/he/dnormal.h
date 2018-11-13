5c30c661018d0d7d389600c3d1282bd8476c3a35
Sergey Litvinov <slitvinov@gmail.com>
2018-11-13 21:51:39 +0100

    add dnormal interface

---
+++ lib/he/dnormal.h
@@ -0,0 +1,10 @@
+#define T Dnormal
+
+typedef struct He He;
+typedef struct Ten Ten;
+typedef struct T T;
+
+int dnormal_ini(He*, /**/ T**);
+int dnormal_fin(T*);
+
+int dnormal_apply(T*, /**/ Ten**);
