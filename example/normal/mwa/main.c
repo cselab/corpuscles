1ce3834a84fbe938202fa8878c6c353406af5a93
Sergey Litvinov <slitvinov@gmail.com>
2018-11-13 09:20:34 +0100

    sum

--- example/normal/mwa/main.c
+++ example/normal/mwa/main.c
@@ -6,6 +6,7 @@
 #include <he/normal.h>
 #include <he/memory.h>
 #include <he/punto.h>
+#include <he/sum.h>
 #include <he/y.h>
 
 int main() {
@@ -13,7 +14,8 @@ int main() {
     int n;
     real *x, *y, *z;
     real *nx, *ny, *nz;
-    real A;
+    real sx, sy, sz;
+
     y_ini("/dev/stdin", &he, &x, &y, &z);
     n = he_nv(he);
     MALLOC(n, &nx); MALLOC(n, &ny); MALLOC(n, &nz);
@@ -22,6 +24,14 @@ int main() {
     real *queue[] = {x, y, z, nx, ny, nz, NULL};
     puts("x y z nx ny nz");
     punto_fwrite(n, queue, stdout);
+
+    sx = he_sum_array(n, nx);
+    sy = he_sum_array(n, ny);
+    sz = he_sum_array(n, nz);
+
+    MSG("n[0]: %g %g %g", nx[0], ny[0], nz[0]);
+    MSG("n[n - 1]: %g %g %g", nx[n - 1], ny[n - 1], nz[n - 1]);
+    MSG("sum:  %g %g %g", sx, sy, sz);
 
     FREE(nx); FREE(ny); FREE(nz);
     y_fin(he, x, y, z);
