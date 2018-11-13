18811c1304cdb46c378056889f5e1793875d6ae9
bianx <bianx@ethz.ch>
2018-11-13 22:36:58 +0100

    minor

--- example/min/helfrich_xin/C0_Nt1280/juelicher_Kb0002/stomato_Vr0.4/run_stomato.sh
+++ example/min/helfrich_xin/C0_Nt1280/juelicher_Kb0002/stomato_Vr0.4/run_stomato.sh
@@ -22,7 +22,7 @@ then
     #mv inter.dat prolate_Vr$Vr.dat
     cd ..
 else
-    for Vr in 0.65 0.64 0.63 0.62 0.61 0.6 0.59 0.58 0.57 0.56 0.55 0.54 0.53 0.52 0.51 0.5 0.49 0.48 0.47 0.46 0.45
+    for Vr in 0.65 0.63 0.61 0.59 0.57 0.55 0.53 0.51 0.49 0.47 0.45
     do sh run_stomato.sh $Vr
     done
 fi
