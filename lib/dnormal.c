ce48ad52b9b026fedcd6a2e8476308379bf4597c
Sergey Litvinov <slitvinov@gmail.com>
2018-11-13 22:11:07 +0100

    alloc in dnormal

--- lib/dnormal.c
+++ lib/dnormal.c
@@ -12,24 +12,46 @@
 
 #define T Dnormal
 
+typedef struct Vec Vec;
+struct Vec { real v[3]; };
+
 struct T {
-    int n;
+    int nv, nh;
+    real *ang;
+    Ten *dn, *f;
+    Vec *u, *m, *n;
 };
 
 int dnormal_ini(He *he, /**/ T **pq) {
+    int nv, nh;
     T *q;
     MALLOC(1, &q);
 
+    nv = he_nv(he);
+    nh = he_nh(he);
+
+    MALLOC(nh, &q->u);
+    MALLOC(nh, &q->ang);
+
+    MALLOC(nv, &q->n);
+    MALLOC(nv, &q->dn);
+    MALLOC(nv, &q->m);
+    MALLOC(nv, &q->f);
+
+    q->nv = nv;
+    q->nh = nh;
     *pq = q;
     return HE_OK;
 }
 
 
 int dnormal_fin(T *q) {
+    FREE(q->u); FREE(q->ang); FREE(q->n);
+    FREE(q->dn); FREE(q->m); FREE(q->f);
     FREE(q);
     return HE_OK;
 }
 
-int dnormal_apply(T *q, /**/ Ten **pt) {
+int dnormal_apply(T *q, /**/ Ten **pf) {
 
 }
