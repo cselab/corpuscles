f73ee7b078a13a662aec7dc7b2f6f273e1a533e5
Sergey Litvinov <slitvinov@gmail.com>
2018-11-14 15:09:52 +0100

    add a client of dvec_minus

--- example/dvec/main.c
+++ example/dvec/main.c
@@ -17,7 +17,8 @@ void vec(/**/ real a[3]) { vec_argv(&argv, a); }
 int eq(const char *a, const char *b) { return util_eq(a, b); }
 int main(__UNUSED int argc, const char **v) {
     const char *op;
-    real a[3];
+    real a[3], b[3];
+    Ten Da, Db;
     Ten t;
     argv = v;
     argv++;
@@ -28,6 +29,11 @@ int main(__UNUSED int argc, const char **v) {
         vec(a);
         dvec_norm(a, &t);
         ten_printf(&t, "%g");
+    } else if (eq(op, "minus")) {
+        vec(a); vec(b);
+        dvec_minus(a, b, /**/ &Da, &Db);
+        ten_printf(&Da, "%g");
+        ten_printf(&Db, "%g");
     } else
         ER("unknown operation '%s'", op);
     return 0;
