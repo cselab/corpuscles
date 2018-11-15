58b19c56b0134fa828ff9c2e802a927189eaf832
bianx <bianx@ethz.ch>
2018-11-15 17:14:44 +0100

    meyer stomato reruns

--- example/min/helfrich_xin/C0_Nt1280/meyer_et_al_Kb0002/stomato_Vr0.45/run_stomato.sh
+++ example/min/helfrich_xin/C0_Nt1280/meyer_et_al_Kb0002/stomato_Vr0.45/run_stomato.sh
@@ -7,7 +7,7 @@ Kad=0
 DA0D=0
 end=50000
 
-off=../oblate_Vr0.45.off
+off=../sphere_Vr0.45.off
 
 if test $# -ne 0
 then
@@ -18,9 +18,11 @@ then
     mkdir $Vr
     cd $Vr
     ../../../../main meyer_xin $Vr 3 3 3 0 $Kb $C0 $Kad $DA0D $end < $off > stomato_Vr$Vr.msg &
+    #mv end.off oblate_Vr$Vr.off
+    #mv inter.dat oblate_Vr$Vr.dat
     cd ..
 else
-    for Vr in 0.65 0.64 0.63 0.62 0.61 0.6 0.59 0.58 0.57 0.56 0.55 0.54 0.53 0.52 0.51 0.5 0.49 0.48
+    for Vr in 0.65 0.6 0.55 0.5 0.45 0.4 0.35 0.3 0.25
     do sh run_stomato.sh $Vr
     done
 fi
