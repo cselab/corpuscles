1a822a0609266bc45d39ae89ae4f366ac01051d7
bianx <bianx@ethz.ch>
2018-11-16 22:21:19 +0100

    juelicher fixed area minimization

---
+++ example/min/helfrich_xin_fga/C0_Nt1280/juelicher_Kb0002/prolate/run_prolate.sh
@@ -0,0 +1,24 @@
+set -eu
+
+Vr=0.65
+Kb=0.002
+C0=0
+Kad=0
+DA0D=0
+end=50000
+
+off=../../../../../../data/prolate/laplace/Nt1280/prolate_area_Vr
+
+if test $# -ne 0
+then
+    Vr="$1"
+    echo '************'  Vr = $Vr '************'
+    #rm inter.dat
+    ../../../main juelicher_xin $Vr 3 3 3 0 $Kb $C0 $Kad $DA0D $end < $off$Vr.off > prolate_Vr$Vr.msg
+    mv end.off prolate_Vr$Vr.off
+    mv inter.dat prolate_Vr$Vr.dat
+else
+    for Vr in 0.7 0.65 0.6 0.55 0.5 0.45 0.4 
+    do sh run_prolate.sh $Vr
+    done
+fi
