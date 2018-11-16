c06a70da1e179737c03b49222c754b89e8b3dc7e
bianx <bianx@ethz.ch>
2018-11-16 21:58:24 +0100

    nt20480 for gompper sphere

---
+++ example/min/helfrich_xin/C0_Nt20480/gompper_kroll_Kb00005/sphere/run_sphere.sh
@@ -0,0 +1,24 @@
+set -eu
+
+Vr=0.65
+Kb=0.0005
+C0=0
+Kad=0
+DA0D=0
+end=50000
+
+off=../../../../../../data/sph/laplace/Nt20480.off
+
+if test $# -ne 0
+then
+    Vr="$1"
+    echo '************'  Vr = $Vr '************'
+    #rm inter.dat
+    ../../../main gompper $Vr 3 3 3 0 $Kb $C0 $Kad $DA0D $end < $off > sphere_Vr$Vr.msg
+    mv end.off sphere_Vr$Vr.off
+    mv inter.dat sphere_Vr$Vr.dat
+else
+    for Vr in 0.45 0.4 0.35 0.3 0.25
+    do sh run_sphere.sh $Vr
+    done
+fi
