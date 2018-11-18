b9176aa4dbfad95d9e7eb9508a95c19105e15dea
Sergey Litvinov <slitvinov@gmail.com>
2018-11-18 13:00:24 +0100

    upd read/main.c

--- example/off/read/main.c
+++ example/off/read/main.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <real.h>
+#include <he/macro.h>
 #include <he/err.h>
 #include <he/off.h>
+#include <he/util.h>
 
 static HeOff *read;
 static real *ver;
 static int  nv, nt, *tri;
+
+const char *me = "off/read";
+
+static void usg() {
+    fprintf(stderr, "%s < OFF\n", me);
+    exit(2);
+}
 
 static void ini() {
     he_off_ini("/dev/stdin", &read);
@@ -17,15 +27,20 @@ static void ini() {
 }
 static void fin() { he_off_fin(read); }
 
-int main() {
-    enum {X, Y, Z};
+static void write() {
     int i;
     real x, y, z, *r;
-    ini();
     for (i = 0, r = ver; i < nv; i++) {
         x = *r++; y = *r++; z = *r++;
         printf("%g %g %g\n", x, y, z);
     }
-    
+    MSG("number of vertices: %d",  nv);
+    MSG("number of triangles: %d", nt);
+}
+
+int main(__UNUSED int c, const char **v) {
+    if (*++v != NULL && util_eq(*v, "-h")) usg();
+    ini();
+    write();
     fin();
 }
