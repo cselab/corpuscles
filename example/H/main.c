59bd1d84ea0f02a91152f8a4f54f72456c91889b
Sergey Litvinov <slitvinov@gmail.com>
2018-11-18 16:46:17 +0100

    tested H

--- example/H/main.c
+++ example/H/main.c
@@ -7,23 +7,31 @@
 #include <he/memory.h>
 #include <he/punto.h>
 #include <he/sum.h>
+#include <he/vec.h>
 #include <he/y.h>
 
 int main() {
     He *he;
-    int n;
+    int n, i;
     real *x, *y, *z;
-    real *hh, *area, s;
+    real *hh, *area, *rr, s;
     H *h;
+    real r[3];
 
     y_ini("/dev/stdin", &he, &x, &y, &z);
     n = he_nv(he);
+    MALLOC(n, &rr);
     H_ini(he, /**/ &h);
 
     H_apply(h, he, x, y, z, /**/ &hh, &area);
 
-    real *queue[] = {x, y, z, hh, NULL};
-    puts("x y z H");
+    for (i = 0; i < n; i++) {
+        vec_get(i, x, y, z, /**/ r);        
+        rr[i] = vec_cylindrical_r(r);
+    }
+
+    real *queue[] = {x, y, z, rr, hh, area, NULL};
+    puts("x y z r H area");
     punto_fwrite(n, queue, stdout);
     s = he_sum_array(n, hh);
 
@@ -31,8 +39,8 @@ int main() {
     MSG("h[n - 1]: %g", hh[n - 1]);
     MSG("sum: %g", s);
 
+    FREE(rr);
     H_fin(h);
-
     y_fin(he, x, y, z);
     return HE_OK;
 }
