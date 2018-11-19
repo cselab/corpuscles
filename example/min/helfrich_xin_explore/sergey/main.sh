d9fa58bb8aa7de911f30b11c90be6fe10fe069ff
Sergey Litvinov <slitvinov@gmail.com>
2018-11-19 23:17:53 +0100

    more freq

--- example/min/helfrich_xin_explore/sergey/main.sh
+++ example/min/helfrich_xin_explore/sergey/main.sh
@@ -4,4 +4,5 @@ make
 #off=`he.path`/sph/laplace/Nt1280.off
 off=main.off
 
-./main meyer_xin 0.45 0.1 2 1 0 0.01 0 0 0 20000 200 < $off
+freq=10
+./main meyer_xin 0.45 0.1 2 1 0 0.01 0 0 0 20000 $freq < $off
