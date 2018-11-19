85e537423a2da4c6ae23efe26dc224d586b43b0e
Sergey Litvinov <slitvinov@gmail.com>
2018-11-19 23:50:51 +0100

    never end

--- example/min/helfrich_xin_explore/sergey/main.sh
+++ example/min/helfrich_xin_explore/sergey/main.sh
@@ -3,6 +3,7 @@ set -eu
 make
 #off=`he.path`/sph/laplace/Nt1280.off
 off=0.off
+end=999999999
 
 freq=10000
-./main meyer_xin 0.45 0.1 2 1 0 0.01 0 0 0 20000 $freq < $off
+./main meyer_xin 0.45 0.1 2 1 0 0.01 0 0 0 $end $freq < $off
