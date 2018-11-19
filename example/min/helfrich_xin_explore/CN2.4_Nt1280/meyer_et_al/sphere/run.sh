3c97dba2b84d79c03078c2badca1d8600a0a38c9
bianx <bianx@ethz.ch>
2018-11-19 13:14:23 +0100

    CN2.4

---
+++ example/min/helfrich_xin_explore/CN2.4_Nt1280/meyer_et_al/sphere/run.sh
@@ -0,0 +1,28 @@
+set -eu
+
+Kb=0.1
+C0=-2.4
+Kad=0
+DA0D=0
+end=20000
+freq=200
+
+off=../../../../../../../data/sph/laplace/Nt1280.off
+
+if test $# -ne 0
+then
+    Vr="$1"
+    echo '************'  Vr = $Vr '************'
+    if [ ! -d $Vr ]; then
+	mkdir $Vr
+    fi
+    cd $Vr
+    ../../../../main meyer_xin $Vr 0.1 2 1 0 $Kb $C0 $Kad $DA0D $end $freq < $off > Vr$Vr.msg
+else
+    for i in `seq 0 15`;
+    do
+	Vr=$(echo $i | awk '{print 0.8-$1*0.02}')
+	#echo $Vr
+	bash run.sh $Vr
+    done
+fi
