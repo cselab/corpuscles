8d7b996528711933b5b3f39c957e0b339d1359e2
Sergey Litvinov <slitvinov@gmail.com>
2018-11-20 00:14:33 +0100

    nsub/2

--- example/min/helfrich_xin_explore/sergey/main.c
+++ example/min/helfrich_xin_explore/sergey/main.c
@@ -209,7 +209,7 @@ static void main0(real *vx, real *vy, real *vz,
   mu     = 100.0;
   h      = 0.01*e0;
 
-  nsub = 100;
+  nsub = 10;
 
   zero(NV, vx); zero(NV, vy); zero(NV, vz);
   for (i = 0; i <= end; i++) {
