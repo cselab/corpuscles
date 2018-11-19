915f4282fc14f707c755cbab57100095483e0824
Sergey Litvinov <slitvinov@gmail.com>
2018-11-19 01:33:38 +0100

    compute C)

--- example/min/khairy/c0/main.sh
+++ example/min/khairy/c0/main.sh
@@ -1,7 +1,10 @@
 set -eu
 
-rV=0.56 Ka=0.1 Kga=2 Kv=1 Kb=0.1 C0=2.48 Kad=0 Ke=0 DAD0=0
-end=750000
+. he.util
+
+rV=0.56 Ka=0.1 Kga=2 Kv=1 Ke=0 Kb=0.1 C0=`ae "3*$rV^(1/3)"` Kad=0 DAD0=0
+
+end=999999
 off=../../../../data/sph/laplace/0.off
 
-./main juelicher_xin $rV   $Ka $Kga $Kv  $Ke      $Kb $C0 $Kad $DAD0          $end < $off
+e ./main juelicher_xin $rV   $Ka $Kga $Kv  $Ke      $Kb $C0 $Kad $DAD0          $end '<' $off
