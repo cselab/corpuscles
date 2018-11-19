3882e93612374f393cdade6fc08e315f245263db
Sergey Litvinov <slitvinov@gmail.com>
2018-11-20 00:43:47 +0100

    mu / 100

--- example/min/helfrich_xin_explore/sergey/main.sh
+++ example/min/helfrich_xin_explore/sergey/main.sh
@@ -9,4 +9,4 @@ freq=100
 v=0.45
 ka=1 kga=2 kv=1 ke=0.01 kb=0.01
 
-./main meyer_xin $v $ka $kga $kv $ke $kb 0 0 0 $end $freq < $off
+MIX= ./main meyer_xin $v $ka $kga $kv $ke $kb 0 0 0 $end $freq < $off
