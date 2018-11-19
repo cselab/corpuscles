738383120a7a3822b8bcaf558872e0195207a999
bianx <bianx@ethz.ch>
2018-11-19 16:24:55 +0100

    new C0 runs with kb001

---
+++ example/min/helfrich_xin_explore/C0_Nt1280/kantor_nelson_Kb001/prolate/run.sh
@@ -0,0 +1,28 @@
+set -eu
+
+Kb=0.01
+C0=0
+Kad=0
+DA0D=0
+end=20000
+freq=200
+
+off=../../../../../../../data/prolate/laplace/Nt1280/prolate_area_Vr
+
+if test $# -ne 0
+then
+    Vr="$1"
+    echo '************'  Vr = $Vr '************'
+    if [ ! -d $Vr ]; then
+	mkdir $Vr
+    fi
+    cd $Vr
+    ../../../../main kantor $Vr 0.1 2 1 0 $Kb $C0 $Kad $DA0D $end $freq < $off$Vr.off > Vr$Vr.msg
+else
+    for i in `seq 0 8`;
+    do
+	Vr=$(echo $i | awk '{print 0.6-$1*0.05}')
+	#echo $Vr
+	bash run.sh $Vr
+    done
+fi
