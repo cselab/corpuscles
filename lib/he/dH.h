251dd2fd965316012d78d608f684b7b0d25ea826
Sergey Litvinov <slitvinov@gmail.com>
2018-11-28 09:48:57 +0100

    not tensors in dH

--- lib/he/dH.h
+++ lib/he/dH.h
@@ -1,12 +1,10 @@
 #define T Dh
 
 typedef struct He He;
-typedef struct Ten Ten;
 typedef struct T T;
 
 int dh_ini(He*, /**/ T**);
 int dh_fin(T*);
-
 int dh_apply(T*, He*, const real *x, const real *y, const real *z, /**/ real *fx, real *fy, real *fz);
 
 #undef T
