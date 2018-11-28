940690b6cb0511777d6df0896bfda7ae64b8088e
Sergey Litvinov <slitvinov@gmail.com>
2018-11-28 15:58:37 +0100

    deactivate gompper_xin tests

--- lib/f/gompper_xin.c
+++ lib/f/gompper_xin.c
@@ -31,7 +31,7 @@ static real sq(real x) { return x*x; }
 static real e(real H0, real area, real h) {
     real H;
     H = h/area; /* TODO */
-    return sq(H - H0);
+    return sq(H + H0);
 }
 static real ddh(void *p, real area, real H) { return   4*H/area; }
 static real dda(void *p, real area, real H) { return  -(2*H*H)/(area*area); }
@@ -39,13 +39,12 @@ static void zero(int n, real *a) {
     int i;
     for (i = 0; i < n; i++) a[i] = 0;
 }
-static int scale(int n, real sc, /*io*/ real *a) {
+static int scale(real sc, int n, /*io*/ real *a) {
     int i;
     for (i = 0; i < n; i++)
         a[i] *= sc;
     return HE_OK;
 }
-
 int he_f_gompper_xin_ini(real Kb, real C0, __UNUSED real Kad, __UNUSED real DA0D, He *he, T **pq) {
     T *q;
     int nv;
@@ -71,11 +70,11 @@ int he_f_gompper_xin_fin(T *q) {
 }
 
 static int compute_energy(real H0, int n,
-                           const real *h, const real *area,
+                           const real *area, const real *h,
                            /**/ real *energy) {
     int i;
     for (i = 0; i < n; i++) {
-        energy[i] = area[i]*e(H0, h[i], area[i]);
+        energy[i] = area[i]*e(H0, area[i], h[i]);
     }
     return HE_OK;
 }
@@ -95,9 +94,10 @@ real he_f_gompper_xin_energy(T *q, He *he,
 
     dh_area_h(dh, he, x, y, z);
     dh_area(dh, &area);
-    dh_h(dh, &h);
-
-    compute_energy(H0, nv, h, area, energy);
+    dh_h(dh, &h);    
+    
+    compute_energy(H0, nv, area, h, energy);
+    scale(2*Kb, nv, energy);
 
     return he_sum_array(nv, energy);
 #   undef A
