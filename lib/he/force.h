fb3956b1103040dbd1aac64c782cc340f777c6cb
Sergey Litvinov <slitvinov@gmail.com>
2018-11-29 22:22:25 +0100

    rm force_energy_ver

--- lib/he/force.h
+++ lib/he/force.h
@@ -9,7 +9,6 @@ int force_fin(T*);
 int force_force(T*, He*, const real *x, const real *y, const real *z,
                 /**/ real *fx, real *fy, real *fz);
 real force_energy(T*, He*, const real *x, const real *y, const real *z);
-int force_energy_ver(T*, /**/ real**);
 const char* force_list();
 
 int force_area_ini(const real *param, He*, /**/ T**);
