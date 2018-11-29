ba4c694597c06962dcea0c2e6f2793dc918f3834
Sergey Litvinov <slitvinov@gmail.com>
2018-11-29 16:43:12 +0100

    initial import of lib/force.c

--- lib/he/force.h
+++ lib/he/force.h
@@ -3,13 +3,15 @@
 typedef struct He He;
 typedef struct T T;
 
-int force_ini(const char *name, void *params, He*, /**/ T**);
+int force_ini(const char *name, const real *param, He*, /**/ T**);
 int force_fin(T*);
 
-int force_force(T*, He*, const real *x, const real *y, const real *z, /**/ real *fx, real *fy, real *fz);
+int force_force(T*, He*, const real *x, const real *y, const real *z,
+                /**/ real *fx, real *fy, real *fz);
 real force_energy(T*, He*, const real *x, const real *y, const real *z);
 int force_energy_ver(T*, /**/ real**);
-
 const char* force_list();
+
+int force_area_ini(const real *param, He*, /**/ T**);
 
 #undef T
