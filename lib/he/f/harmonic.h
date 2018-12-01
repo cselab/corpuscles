dc4a5eb58723a4e5eecccd530cb9d82bc0205b78
Sergey Litvinov <slitvinov@gmail.com>
2018-12-01 15:37:04 +0100

    minor

--- lib/he/f/harmonic.h
+++ lib/he/f/harmonic.h
@@ -6,7 +6,6 @@ typedef struct T T;
 /* E = K/edg0 * sum_{edg} (edg - edg0)^2 */
 int he_f_harmonic_ini(real e0, real K, He*, T**);
 int he_f_harmonic_fin(T*);
-
 int he_f_harmonic_force(T*, He*,
                     const real *x, const real *y, const real *z, /**/
                     real *fx, real *fy, real *fz);
