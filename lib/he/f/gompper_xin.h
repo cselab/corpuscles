7a02b961028446241aeaf976c8fca71ab9dd90d3
Sergey Litvinov <slitvinov@gmail.com>
2018-12-01 17:28:06 +0100

    minor

--- lib/he/f/gompper_xin.h
+++ lib/he/f/gompper_xin.h
@@ -5,7 +5,6 @@ typedef struct T T;
 
 int he_f_gompper_xin_ini(real Kb, real C0, real Kad, real DA0D, He*, T**);
 int he_f_gompper_xin_fin(T*);
-
 int he_f_gompper_xin_force(T*, He*,
                       const real *x, const real *y, const real *z, /**/
                       real *fx, real *fy, real *fz);
