69becf2edadbe01dae94bb01916e32f6cb0812ee
Sergey Litvinov <slitvinov@gmail.com>
2018-12-03 14:05:01 +0100

    wip: crash

--- example/min/helfrich_xin_consistency/C0_Nt1280/meyer_et_al_Kb001_Ka1/sphere/run_crash.sh
+++ example/min/helfrich_xin_consistency/C0_Nt1280/meyer_et_al_Kb001_Ka1/sphere/run_crash.sh
@@ -8,7 +8,8 @@ end=200000
 freq=500
 Vr=0.4
 
-off=$(he.path)/sph/laplace/Nt1280.off
+# $(he.path)/sph/laplace/Nt1280.off
+off=start/main.off
 make
 
-./main meyer_xin $Vr 1 2 1 0 $Kb $C0 $Kad $DA0D $end $freq < $off > Vr$Vr.msg
+./voronoi meyer_xin $Vr 10 2 1 0 $Kb $C0 $Kad $DA0D $end $freq < $off > Vr$Vr.msg
