ca518151223e68d37ebc40f65f035bfba7a3e46f
Sergey Litvinov <slitvinov@gmail.com>
2018-12-03 17:50:23 +0100

    use volume_normal

--- example/min/helfrich_xin_consistency/C0_Nt1280/meyer_et_al_Kb001_Ka1/sphere/run_crash.sh
+++ example/min/helfrich_xin_consistency/C0_Nt1280/meyer_et_al_Kb001_Ka1/sphere/run_crash.sh
@@ -5,8 +5,8 @@ Vr=0.4
 freq=500
 end=99999999
 
-off=start/main.off
-#off=$(he.path)/sph/laplace/Nt1280.off
+#off=start/main.off
+off=$(he.path)/sph/laplace/Nt1280.off
 make
 
 he.run ./voronoi meyer_xin $Vr 1 2 1 0 $Kb $C0 $Kad $DA0D $end $freq < $off > Vr$Vr.msg
