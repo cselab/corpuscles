8183831f95c26a68db714b7149a47197dc4c851f
bianx <bianx@ethz.ch>
2018-12-06 13:43:53 +0100

    vr 08 bc run further

---
+++ example/min/helfrich_xin_explore/bc_Nt1280/meyer_xin_Vr08/sphere/run2.sh
@@ -0,0 +1,46 @@
+set -eu
+
+Vr=0.8
+Ka=0.5
+Kga=1
+Kv=1
+Ke=0
+
+Kb=0.001
+C0=0
+Kad=1
+#echo $Kad
+DA0D=0
+D=0.000898798148042
+pi=3.141592653589793115997964
+A=$(echo  $pi | awk '{print $1*4.0}')
+#echo $A
+end=200000
+freq=500
+
+off=$(he.path)/sph/laplace/Nt1280.off
+
+if test $# -ne 0
+then
+    Da="$1"
+    echo '***' Da=$Da '***'
+    
+    DA0=$(echo $pi, $Da | awk '{print 4*$2*$1}')
+    DA0D=$(echo $DA0 | awk '{print $1*2}')
+    #echo $Da, $DA0, $DA0D
+    
+    if [ ! -d $Da ]; then
+	#echo $Da
+	mkdir $Da
+    fi
+    cd $Da
+    ../../../../main meyer_xin $Vr $Ka $Kga $Kv $Ke $Kb $C0 $Kad $DA0D $end $freq < $off > Da$Da.msg
+else
+    for i in `seq 0 5`;
+    do
+	Da=$(echo $i | awk '{print (1.24+$1*0.02)}')
+	#echo $Da
+	bash run2.sh $Da &
+    done
+
+fi
