386f3c9b94a7c1e8cbe9811d3ceed771378b04f7
bianx <bianx@ethz.ch>
2018-12-11 20:48:59 +0100

    c0 nt1280 gompper xin

--- example/min/helfrich_xin_explore/C0_Nt1280/gompper_xin_Kb001_Ka1/sphere/run.sh
+++ example/min/helfrich_xin_explore/C0_Nt1280/gompper_xin_Kb001_Ka1/sphere/run.sh
@@ -4,12 +4,12 @@ Kb=0.01
 C0=0
 Kad=0
 DA0D=0
-end=20000
-freq=200
+end=200000
+freq=500
 
 off=$(he.path)/sph/laplace/Nt1280.off
 
-(cd ../../.. && make clean && make)
+#(cd ../../.. && make clean && make)
 
 if test $# -ne 0
 then
@@ -19,12 +19,11 @@ then
 	mkdir $Vr
     fi
     cd $Vr
-    ../../../../main juelicher_xin $Vr 1 2 1 0 $Kb $C0 $Kad $DA0D $end $freq < $off > Vr$Vr.msg
-    #../../../../main gompper_xin $Vr 1 2 1 0 $Kb $C0 $Kad $DA0D $end $freq < $off > Vr$Vr.msg
+    ../../../../main gompper_xin $Vr 1 2 1 0 $Kb $C0 $Kad $DA0D $end $freq < $off > Vr$Vr.msg
 else
-    for i in `seq 0 8`;
+    for i in `seq 0 5`;
     do
-	Vr=$(echo $i | awk '{print 0.6-$1*0.05}')
+	Vr=$(echo $i | awk '{print 0.45-$1*0.05}')
 	#echo $Vr
 	bash run.sh $Vr
     done
