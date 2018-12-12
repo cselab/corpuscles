0d2d8584832faa28b78f3ac6925215b48c2f886b
bianx <bianx@ethz.ch>
2018-12-12 09:45:28 +0100

    meyer C0

---
+++ example/min/helfrich_xin_explore/C0_Nt1280/meyer_et_al_Kb001_Ka1/prolate/run.sh
@@ -0,0 +1,28 @@
+set -eu
+
+Kb=0.01
+C0=0
+Kad=0
+DA0D=0
+end=50000
+freq=500
+
+off=$(he.path)/prolate/laplace/Nt1280/prolate_area_
+
+if test $# -ne 0
+then
+    Vr="$1"
+    echo '************'  Vr = $Vr '************'
+    if [ ! -d $Vr ]; then
+	mkdir $Vr
+    fi
+    cd $Vr
+    ../../../../main meyer_xin $Vr 1 2 1 0 $Kb $C0 $Kad $DA0D $end $freq < $off$Vr.off > Vr$Vr.msg
+else
+    for i in `seq 0 12`;
+    do
+	Vr=$(echo $i | awk '{print 0.95-$1*0.05}')
+	#echo $Vr
+	bash run1.sh $Vr
+    done
+fi
