abe006e4bc610eb6ba410ca67326474787f4b8d4
bianx <bianx@ethz.ch>
2018-12-12 10:18:49 +0100

    kantor Nt5120

---
+++ example/min/helfrich_xin_explore/C0_Nt5120/kantor_nelson_Kb001_Ka1/sphere/run.sh
@@ -0,0 +1,28 @@
+set -eu
+
+Kb=0.01
+C0=0
+Kad=0
+DA0D=0
+end=50000
+freq=500
+
+off=$(he.path)/sph/laplace/Nt5120.off
+
+if test $# -ne 0
+then
+    Vr="$1"
+    echo '************'  Vr = $Vr '************'
+    if [ ! -d $Vr ]; then
+	mkdir $Vr
+    fi
+    cd $Vr
+    ../../../../main kantor $Vr 1 2 1 0 $Kb $C0 $Kad $DA0D $end $freq < $off > Vr$Vr.msg
+else
+    for i in `seq 0 8`;
+    do
+	Vr=$(echo $i | awk '{print 0.95-$1*0.05}')
+	#echo $Vr
+	bash run.sh $Vr
+    done
+fi
