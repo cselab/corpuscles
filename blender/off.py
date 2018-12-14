89a498f66dac41c3ffd7a1399d8c0c26e4aad823
Sergey Litvinov <slitvinov@gmail.com>
2018-12-14 10:33:20 +0100

    use oogl.off()

--- blender/off.py
+++ blender/off.py
@@ -8,74 +8,7 @@ import mathutils
 sys.path.append(".")
 import oogl
 
-def load(filepath):
-    filepath = os.fsencode(filepath)
-    file = open(filepath, 'r')
-    first_line = file.readline().rstrip()
-    use_colors = (first_line == 'COFF')
-    colors = []
-
-    line = file.readline()
-    while line.isspace() or line[0]=='#':
-        line = file.readline()
-
-    vcount, fcount, ecount = [int(x) for x in line.split()]
-    verts = []
-    facets = []
-    edges = []
-    i=0;
-    while i<vcount:
-        line = file.readline()
-        if line.isspace():
-            continue
-        try:
-             bits = [float(x) for x in line.split()]
-             px = bits[0]
-             py = bits[1]
-             pz = bits[2]
-             if use_colors:
-                 colors.append([float(bits[3]) / 255, float(bits[4]) / 255, float(bits[5]) / 255])
-
-        except ValueError:
-            i=i+1
-            continue
-        verts.append((px, py, pz))
-        i=i+1
-
-    i=0;
-    while i<fcount:
-        line = file.readline()
-        if line.isspace():
-            continue
-        try:
-            splitted  = line.split()
-            ids   = list(map(int, splitted))
-            if len(ids) > 3:
-                facets.append(tuple(ids[1:]))
-            elif len(ids) == 3:
-                edges.append(tuple(ids[1:]))
-        except ValueError:
-            i=i+1
-            continue
-        i=i+1
-
-
-    off_name = bpy.path.display_name_from_filepath(filepath)
-    mesh = bpy.data.meshes.new(name=off_name)
-    mesh.from_pydata(verts,edges,facets)
-
-    mesh.validate()
-    mesh.update()
-
-    if use_colors:
-        color_data = mesh.vertex_colors.new()
-        for i, facet in enumerate(mesh.polygons):
-            for j, vidx in enumerate(facet.vertices):
-                color_data.data[3*i + j].color = colors[vidx]
-
-    return mesh
-
-i = "preved.blend"
+Iblender = "preved.blend"
 Camera = "data/00001.camera"
 Geometry = "data/00001.geometry"
 Oblender = "o.blend"
@@ -87,9 +20,10 @@ print(fov)
 fov /= magic
 fov *= math.pi/180
 
-bpy.ops.wm.open_mainfile(filepath = i)
+bpy.ops.wm.open_mainfile(filepath = Iblender)
 
-mesh = load("data/0.off")
+mesh = oogl.off(Geometry)
+print(mesh)
 
 cam = bpy.data.objects['camera']
 cam.data.angle = fov
