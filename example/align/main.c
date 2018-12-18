c1e2513b0177abdb62dd50e0f8bb7b841ce98994
Sergey Litvinov <slitvinov@gmail.com>
2018-12-18 18:39:44 +0100

    minor

--- example/align/main.c
+++ example/align/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 #include <tgmath.h>
 
 #include <real.h>
 
 #include <he/err.h>
 #include <he/he.h>
+#include <he/macro.h>
 #include <he/memory.h>
 #include <he/off.h>
 #include <he/orient.h>
@@ -12,18 +14,20 @@
 #include <he/vec.h>
 #include <he/y.h>
 
+static const char **argv;
+
 struct Obj {
     int n;
     real *x, *y, *z;
     Ten t;
     He *he;
+    char file[4048];
+    Orient *orient;
 };
 typedef struct Obj Obj;
-static Obj a;
+static Obj a, b;
 
-static Orient *orient;
-
-static int invert(int n, const Ten *t, real *x, real *y, real *z) {
+static int invert(const Ten *t, int n, real *x, real *y, real *z) {
     int i;
     real v[3], u[3];
     for (i = 0; i < n; i++) {
@@ -34,22 +38,45 @@ static int invert(int n, const Ten *t, real *x, real *y, real *z) {
     return HE_OK;
 }
 
-static void main0(void) {
-    orient_apply(orient, a.x, a.y, a.z);
-    orient_transform(orient, &a.t);
-    //ten_printf(&a.t, "%g");
-    invert(a.n, &a.t, a.x, a.y, a.z);
-    off_he_xyz_fwrite(a.he, a.x, a.y, a.z, stdout);
+static int str(/**/ char *p) {
+    if (*argv == NULL)
+        ER("not enough arguments");
+    strncpy(p, *argv, 4047);
+    argv++;
+    return HE_OK;
 }
 
-int main() {
-    y_ini("/dev/stdin", &a.he, &a.x, &a.y, &a.z);
+static int arg() {
+    str(a.file);
+    str(b.file);
+}
+
+int main(__UNUSED int argc, const char **v) {
+    argv = v; argv++;
+    arg();
+
+    y_ini(a.file, &a.he, &a.x, &a.y, &a.z);
     a.n = he_nv(a.he);
-    
-    orient_ini(a.he, &orient);
-    main0();
-    orient_fin(orient);
+
+    y_ini(b.file, &b.he, &b.x, &b.y, &b.z);
+    b.n = he_nv(b.he);
+
+    orient_ini(a.he, &a.orient);
+    orient_ini(b.he, &b.orient);
+
+    orient_apply(a.orient, a.x, a.y, a.z);
+    orient_apply(b.orient, b.x, b.y, b.z);
+
+    orient_transform(a.orient, &a.t);
+    orient_transform(b.orient, &b.t);
+
+    invert(&a.t, b.n, b.x, b.y, b.z);
+    off_he_xyz_fwrite(b.he, b.x, b.y, b.z, stdout);
+
+    orient_fin(a.orient);
+    orient_fin(b.orient);
 
     y_fin(a.he, a.x, a.y, a.z);
+    y_fin(b.he, b.x, b.y, b.z);
     return 0;
 }
