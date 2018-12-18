82a7c70e24c56eb77f24c233032cc7e77238b3e9
Sergey Litvinov <slitvinov@gmail.com>
2018-12-18 18:13:44 +0100

    use Obj

--- example/align/main.c
+++ example/align/main.c
@@ -2,29 +2,37 @@
 #include <tgmath.h>
 
 #include <real.h>
+
+#include <he/he.h>
 #include <he/memory.h>
 #include <he/off.h>
 #include <he/orient.h>
-#include <he/he.h>
+#include <he/ten.h>
 #include <he/y.h>
 
-static int nv;
-static real *x, *y, *z;
-static He *he;
+struct Obj {
+    int nv;
+    real *x, *y, *z;
+    He *he;
+};
+typedef struct Obj Obj;
+static Obj a;
+
 static Orient *orient;
 
 static void main0() {
-    orient_apply(orient, x, y, z);
-    off_he_xyz_fwrite(he, x, y, z, stdout);
+    orient_apply(orient, a.x, a.y, a.z);
+    off_he_xyz_fwrite(a.he, a.x, a.y, a.z, stdout);
 }
 
 int main() {
-    y_ini("/dev/stdin", &he, &x, &y, &z);
-    nv = he_nv(he);
-    orient_ini(he, &orient);
+    y_ini("/dev/stdin", &a.he, &a.x, &a.y, &a.z);
+    a.nv = he_nv(a.he);
+    
+    orient_ini(a.he, &orient);
     main0();
     orient_fin(orient);
 
-    y_fin(he, x, y, z);
+    y_fin(a.he, a.x, a.y, a.z);
     return 0;
 }
