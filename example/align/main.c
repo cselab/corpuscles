007a43d387fb4b802595d79b915206a16ee6adff
Sergey Litvinov <slitvinov@gmail.com>
2018-12-18 18:27:15 +0100

    wip: align

--- example/align/main.c
+++ example/align/main.c
@@ -3,15 +3,17 @@
 
 #include <real.h>
 
+#include <he/err.h>
 #include <he/he.h>
 #include <he/memory.h>
 #include <he/off.h>
 #include <he/orient.h>
 #include <he/ten.h>
+#include <he/vec.h>
 #include <he/y.h>
 
 struct Obj {
-    int nv;
+    int n;
     real *x, *y, *z;
     Ten t;
     He *he;
@@ -21,16 +23,28 @@ static Obj a;
 
 static Orient *orient;
 
-static void main0() {
+static int invert(int n, const Ten *t, real *x, real *y, real *z) {
+    int i;
+    real v[3], u[3];
+    for (i = 0; i < n; i++) {
+        vec_get(i, x, y, z, v);
+        ten_vec(t, v, u);
+        vec_set(u, i, x, y, z);
+    }
+    return HE_OK;
+}
+
+static void main0(void) {
     orient_apply(orient, a.x, a.y, a.z);
     orient_transform(orient, &a.t);
-    ten_printf(&a.t, "%g");
-    //off_he_xyz_fwrite(a.he, a.x, a.y, a.z, stdout);
+    //ten_printf(&a.t, "%g");
+    invert(a.n, &a.t, a.x, a.y, a.z);
+    off_he_xyz_fwrite(a.he, a.x, a.y, a.z, stdout);
 }
 
 int main() {
     y_ini("/dev/stdin", &a.he, &a.x, &a.y, &a.z);
-    a.nv = he_nv(a.he);
+    a.n = he_nv(a.he);
     
     orient_ini(a.he, &orient);
     main0();
