4226fffc57f1c1fd1563980876bee9b01b0d1d83
Sergey Litvinov <slitvinov@gmail.com>
2018-12-21 17:36:27 +0100

    wip: transform

--- lib/transform.c
+++ lib/transform.c
@@ -1,10 +1,62 @@
+#include <stdio.h>
+
 #include "real.h"
 
 #include "he/err.h"
 #include "he/he.h"
+#include "he/tri.h"
+#include "he/sum.h"
+#include "he/vec.h"
+
+struct Sum3 {
+    HeSum *x, *y, *z;
+};
+typedef struct Sum3 Sum3;
+
+static int sum3_ini(Sum3 s) {
+    he_sum_ini(&s.x);
+    he_sum_ini(&s.y);
+    he_sum_ini(&s.z);
+    return HE_OK;
+}
+
+static int sum3_fin(Sum3 s) {
+    he_sum_fin(s.x);
+    he_sum_fin(s.y);
+    he_sum_fin(s.z);
+    return HE_OK;
+}
+
+static int sum3_scalar_add(Sum3 s, real a, real r[3]) {
+    enum {X, Y, Z};
+    he_sum_add(s.x, a*r[X]);
+    he_sum_add(s.y, a*r[Y]);
+    he_sum_add(s.z, a*r[Z]);    
+    return HE_OK;
+}
 
 int transform_centroid(He *he, const real *x, const real *y, const real *z, /**/ real com[3])
 {
+    int t, i, j, k, nt;
+    real a[3], b[3], c[3], area;
+    Sum3 s;
+    
+    nt = he_nt(he);
+    sum3_ini(s);
+
+    for (t = 0; t < nt; t++) {
+        he_tri_ijk(he, t, &i, &j, &k);
+        vec_get(i, x, y, z, /**/ a);
+        vec_get(j, x, y, z, /**/ b);
+        vec_get(k, x, y, z, /**/ c);
+        area = tri_area(a, b, c);
+        
+        sum3_scalar_add(s, area, a);
+        sum3_scalar_add(s, area, b);
+        sum3_scalar_add(s, area, c);
+    }
+
+    sum3_fin(s);
     return HE_OK;
 }
 
