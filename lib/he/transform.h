79d50843b32ffe8e63769877e74036a97ae4b8f1
Sergey Litvinov <slitvinov@gmail.com>
2018-12-21 11:44:05 +0100

    initial import of transform.h

---
+++ lib/he/transform.h
@@ -0,0 +1,5 @@
+int transform_cm(int, /*io*/ real*, real*, real*);
+int transform_rotx(real, int /*io*/ real*, real*, real*);
+int transform_roty(real, int /*io*/ real*, real*, real*);
+int transform_rotz(real, int /*io*/ real*, real*, real*);
+int transform_rot(const real[3], int /*io*/ real*, real*, real*);
