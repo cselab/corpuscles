54b7c45f53a182eea41dad30ebf00c3fd81de333
Sergey Litvinov <slitvinov@gmail.com>
2018-12-21 17:40:35 +0100

    wip: transform_centroid

--- lib/transform.c
+++ lib/transform.c
@@ -10,6 +10,7 @@
 
 struct Sum3 {
     HeSum *x, *y, *z;
+    HeSum *a;
 };
 typedef struct Sum3 Sum3;
 
@@ -17,6 +18,7 @@ static int sum3_ini(Sum3 s) {
     he_sum_ini(&s.x);
     he_sum_ini(&s.y);
     he_sum_ini(&s.z);
+    he_sum_ini(&s.a);
     return HE_OK;
 }
 
@@ -24,6 +26,7 @@ static int sum3_fin(Sum3 s) {
     he_sum_fin(s.x);
     he_sum_fin(s.y);
     he_sum_fin(s.z);
+    he_sum_fin(s.a);
     return HE_OK;
 }
 
@@ -31,7 +34,22 @@ static int sum3_scalar_add(Sum3 s, real a, real r[3]) {
     enum {X, Y, Z};
     he_sum_add(s.x, a*r[X]);
     he_sum_add(s.y, a*r[Y]);
-    he_sum_add(s.z, a*r[Z]);    
+    he_sum_add(s.z, a*r[Z]);
+    he_sum_add(s.a, a);
+    return HE_OK;
+}
+
+static int sum3_get(Sum3 s, /**/ real r[3]) {
+    enum {X, Y, Z};
+    real a;
+    a = he_sum_get(s.a);
+    if (a == 0)
+        ERR(HE_NUM, "a = 0");
+    
+    r[X] = he_sum_get(s.x)/a;
+    r[Y] = he_sum_get(s.y)/a;
+    r[Z] = he_sum_get(s.z)/a;
+
     return HE_OK;
 }
 
@@ -40,7 +58,7 @@ int transform_centroid(He *he, const real *x, const real *y, const real *z, /**/
     int t, i, j, k, nt;
     real a[3], b[3], c[3], area;
     Sum3 s;
-    
+
     nt = he_nt(he);
     sum3_ini(s);
 
@@ -50,7 +68,7 @@ int transform_centroid(He *he, const real *x, const real *y, const real *z, /**/
         vec_get(j, x, y, z, /**/ b);
         vec_get(k, x, y, z, /**/ c);
         area = tri_area(a, b, c);
-        
+
         sum3_scalar_add(s, area, a);
         sum3_scalar_add(s, area, b);
         sum3_scalar_add(s, area, c);
