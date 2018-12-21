ee81a8e63316993760631a37ab585ec598552a81
Sergey Litvinov <slitvinov@gmail.com>
2018-12-21 11:46:06 +0100

    wip: transfrom

--- lib/he/transform.h
+++ lib/he/transform.h
@@ -1,5 +1,16 @@
-int transform_cm(int, /*io*/ real*, real*, real*);
+int transform_com(int, /*io*/ real*, real*, real*);
+
 int transform_rotx(real, int /*io*/ real*, real*, real*);
 int transform_roty(real, int /*io*/ real*, real*, real*);
 int transform_rotz(real, int /*io*/ real*, real*, real*);
 int transform_rot(const real[3], int /*io*/ real*, real*, real*);
+
+int transform_tranx(real, int /*io*/ real*, real*, real*);
+int transform_trany(real, int /*io*/ real*, real*, real*);
+int transform_tranz(real, int /*io*/ real*, real*, real*);
+int transform_tran(const real[3], int /*io*/ real*, real*, real*);
+
+int transform_scalx(real, int /*io*/ real*, real*, real*);
+int transform_scaly(real, int /*io*/ real*, real*, real*);
+int transform_scalz(real, int /*io*/ real*, real*, real*);
+int transform_scal(const real[3], int /*io*/ real*, real*, real*);
