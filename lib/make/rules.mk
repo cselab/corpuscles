09f52d90a70d24eab6bfac8b9ccedd69cc51c26a
Sergey Litvinov <slitvinov@gmail.com>
2018-12-24 13:33:22 +0100

    use -o option for he.m4

--- lib/make/rules.mk
+++ lib/make/rules.mk
@@ -7,7 +7,7 @@ libhe_d.a: $O $(O_D); ar r $@ $O $(O_D) && ranlib $@
 libhe_l.a: $O $(O_L); ar r $@ $O $(O_L) && ranlib $@
 $O : $H $I
 
-%.c:   %.m4; he.m4 -s $< > /tmp/he.$$.c && mv /tmp/he.$$.c $@
+%.c:   %.m4; he.m4 -o $@ $<
 %.o:   %.c; $(CC)                         -I. $(CFLAGS) $(HE_CFLAGS) -c -o $@ $<
 %.s.o: %.c; $(CC) -I$(TRG)/prec/s -Iinc/s -I. $(CFLAGS) $(HE_CFLAGS) -c -o $@ $<
 %.d.o: %.c; $(CC) -I$(TRG)/prec/d -Iinc/d -I. $(CFLAGS) $(HE_CFLAGS) -c -o $@ $<
