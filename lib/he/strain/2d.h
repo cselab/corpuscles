c47d0450f95f6ea39898312b52614d401c5b5323
Sergey Litvinov <slitvinov@gmail.com>
2019-01-04 19:13:00 +0100

    do not need vx and vy

--- lib/he/strain/2d.h
+++ lib/he/strain/2d.h
@@ -1,5 +1,5 @@
 int strain_2d(void *param, real (*F1)(void*, real, real), real (*F2)(void*, real, real),
-              real bx, real by, real cx, real cy, real vx, real vy,
-              real ux, real uy, real wx, real wy, real *dvx, real *dvy,
-              real *dux, real *duy, real *dwx, real *dwy, real *pI1,
-              real *pI2, real *pA);
+              real bx, real by, real cx, real cy, real ux, real uy,
+              real wx, real wy, real *dvx, real *dvy, real *dux,
+              real *duy, real *dwx, real *dwy, real *pI1, real *pI2,
+              real *pA);
