89cc03924ce6b52e4f25bf72d7227d44b1af5bb3
Sergey Litvinov <slitvinov@gmail.com>
2019-01-08 11:55:17 +0100

    minor
    Acked-by: Sergey Litvinov <slitvinov@gmail.com>

--- example/min/dynamic/run.sh
+++ example/min/dynamic/run.sh
@@ -16,18 +16,16 @@ Ke=0
 Kb=$(ae 0.001*$fa)
 C0=0
 Kad=$(ae 2*$Kb/$pi)
-DA0D=0
 D=0.000898798148042
 
 A=$(ae 4*$pi)
 off=data/Nt1280_01.off
 end=1000000
 freq=200
-Da1=0.3
-Da0=$(ae $Da1/100)
-DA0=$(ae "($Da0 - $Kb*$D*$C0/$pi/$Kad)*$A")
 
-DA0D=$(ae $DA0/$D)
+Da=0.3
+Da=$(ae $Da/100)
+DA0D=$(ae "($Da - $Kb*$D*$C0/$pi/$Kad)*$A/$D")
     
 echo ./main juelicher_xin $Vr $Ka $Kga $Kv $Ke $Kb $C0 $Kad $DA0D $end $freq < $off # > msg
 exit
