ec5fc5261419061aff82b42e026a1a0555f3b443
Sergey Litvinov <slitvinov@gmail.com>
2019-01-08 10:58:57 +0100

    minor

--- example/min/dynamic/run.sh
+++ example/min/dynamic/run.sh
@@ -18,20 +18,13 @@ DA0D=0
 D=0.000898798148042
 pi=3.141592653589793115997964
 A=$(echo  $pi | awk '{print $1*4.0}')
-#echo $A
+off=data/Nt1280_01.off
 end=1000000
 freq=200
-
 Da0p=0.3
-
-    Da1=$Da0p
-    Da0=$(echo $Da1 | awk '{print $1/100}')
-    echo '***' Da1=$Da1 '***' Da0 = $Da0 '***'
-
-    DA0=$(echo $Kb, $C0, $Kad, $Da0, $D, $pi, $A | awk '{print ($4 - $1*$5*$2/$6/$3)*$7}')
-    DA0D=$(echo $DA0, $D | awk '{print $1/$2}')
-	  
-    off=data/Nt1280_01.off
-    #off=$(he.path)/prolate/laplace/Nt1280/prolate_area_Vr0.642.off
+Da1=$Da0p
+Da0=$(echo $Da1 | awk '{print $1/100}')
+DA0=$(echo $Kb, $C0, $Kad, $Da0, $D, $pi, $A | awk '{print ($4 - $1*$5*$2/$6/$3)*$7}')
+DA0D=$(echo $DA0, $D | awk '{print $1/$2}')
     
-    ./main juelicher_xin $Vr $Ka $Kga $Kv $Ke $Kb $C0 $Kad $DA0D $end $freq < $off > Da$Da1.msg
+./main juelicher_xin $Vr $Ka $Kga $Kv $Ke $Kb $C0 $Kad $DA0D $end $freq < $off > msg
