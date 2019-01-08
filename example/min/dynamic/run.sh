b7500d3febf8ad91f447d755b1aa1f8c0607ef58
Sergey Litvinov <slitvinov@gmail.com>
2019-01-08 11:07:02 +0100

    use ae

--- example/min/dynamic/run.sh
+++ example/min/dynamic/run.sh
@@ -19,14 +19,14 @@ Kad=$(ae 2*$Kb/$pi)
 DA0D=0
 D=0.000898798148042
 
-A=$(echo  $pi | awk '{print $1*4.0}')
+A=$(ae 4*$pi)
 off=data/Nt1280_01.off
 end=1000000
 freq=200
-Da0p=0.3
-Da1=$Da0p
+Da1=0.3
 Da0=$(echo $Da1 | awk '{print $1/100}')
 DA0=$(echo $Kb, $C0, $Kad, $Da0, $D, $pi, $A | awk '{print ($4 - $1*$5*$2/$6/$3)*$7}')
 DA0D=$(echo $DA0, $D | awk '{print $1/$2}')
     
-./main juelicher_xin $Vr $Ka $Kga $Kv $Ke $Kb $C0 $Kad $DA0D $end $freq < $off > msg
+echo ./main juelicher_xin $Vr $Ka $Kga $Kv $Ke $Kb $C0 $Kad $DA0D $end $freq < $off # > msg
+exit
