c40d57adb0bc5b9bc66673b7fd2014f0dd16d3b7
Sergey Litvinov <slitvinov@gmail.com>
2019-01-09 15:49:58 +0100

    regularize bending' headers

--- lib/he/f/gompper_xin.h
+++ lib/he/f/gompper_xin.h
@@ -5,12 +5,8 @@ typedef struct T T;
 
 int he_f_gompper_xin_ini(real Kb, real C0, real Kad, real DA0D, He*, T**);
 int he_f_gompper_xin_fin(T*);
-int he_f_gompper_xin_force(T*, He*,
-                      const real *x, const real *y, const real *z, /**/
-                      real *fx, real *fy, real *fz);
+int he_f_gompper_xin_force(T*, He*, const real *x, const real *y, const real *z, /**/ real *fx, real *fy, real *fz);
 real he_f_gompper_xin_energy(T*, He*, const real *x, const real *y, const real *z);
-
-
 int he_f_gompper_xin_area_ver(T*, /**/ real**);
 int he_f_gompper_xin_laplace_ver(T*, /**/ /**/ real**, real**, real**);
 int he_f_gompper_xin_norm_ver(T*, /**/ real**, real**, real**);
--- lib/he/f/kantor.h
+++ lib/he/f/kantor.h
@@ -3,17 +3,11 @@
 typedef struct He He;
 typedef struct T T;
 
-/* E = 2 * K * sum_{dih} (1 - cos(ang-ang0)) */
 int he_f_kantor_ini(real Kb, real theta0, He*, T**);
 int he_f_kantor_fin(T*);
-
-int he_f_kantor_force(T*, He*,
-                      const real *x, const real *y, const real *z, /**/
-                      real *fx, real *fy, real *fz);
+int he_f_kantor_force(T*, He*, const real *x, const real *y, const real *z, /**/ real *fx, real *fy, real *fz);
 real he_f_kantor_energy(T*, He*, const real *x, const real *y, const real *z);
-
 int he_f_kantor_energy_ver(T*, /**/ real**);
-
 int he_f_kantor_cos(T*, /**/ real**);
 
 #undef T
