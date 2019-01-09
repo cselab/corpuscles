784f5555b8108d000d0623b19a404b65187dd402
Sergey Litvinov <slitvinov@gmail.com>
2019-01-09 16:18:48 +0100

    regularize forces headers

--- lib/he/f/harmonic.h
+++ lib/he/f/harmonic.h
@@ -6,9 +6,7 @@ typedef struct T T;
 /* E = K/edg0 * sum_{edg} (edg - edg0)^2 */
 int he_f_harmonic_ini(real e0, real K, He*, T**);
 int he_f_harmonic_fin(T*);
-int he_f_harmonic_force(T*, He*,
-                    const real *x, const real *y, const real *z, /**/
-                    real *fx, real *fy, real *fz);
+int he_f_harmonic_force(T*, He*, const real *x, const real *y, const real *z, /**/ real *fx, real *fy, real *fz);
 real he_f_harmonic_energy(T*, He*, const real *x, const real *y, const real *z);
 int he_f_harmonic_e(T*, /**/ real  **v);
 
