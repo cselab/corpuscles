9b17f187cb74dd1540bd8b2685a341d52d0e0cef
Sergey Litvinov <slitvinov@gmail.com>
2019-01-09 17:12:04 +0100

    from kana

--- example/min/dynamic/run.sh
+++ example/min/dynamic/run.sh
@@ -4,12 +4,12 @@ set -eu
 
 make
 
-off=data/Nt1280_01.off
-#off=data/Nt5120_01.off
+#off=data/Nt1280_01.off
+off=data/Nt5120_01.off
 
 Da=0.3
 
-f=1
+f=0.1
 pi=3.141592653589793115997964
 D=0.000898798148042
 A=$(ae 4*$pi)
@@ -17,14 +17,14 @@ Vr=0.642
 Ka=$(ae 0.5*$f)
 Kga=$(ae 1*$f)
 Kv=$(ae 5*$f)
-mu=$(ae 1000*$f)
+mu=$(ae 100000*$f)
 Ke=0
 
 Kb=$(ae 0.001*$f)
 C0=0
 Kad=$(ae 2*$Kb/$pi)
 end=100000000
-freq=$(ae "int(2000/$f)")
+freq=2000
 
 Da=$(ae $Da/100)
 DA0D=$(ae "($Da - $Kb*$D*$C0/$pi/$Kad)*$A/$D")
