9239dc006f39f52a68d6647991c091b93ca204ac
Sergey Litvinov <slitvinov@gmail.com>
2019-01-13 19:18:51 +0100

    minor

--- lib/he/ply.h
+++ lib/he/ply.h
@@ -13,8 +13,6 @@ int ply_nm(T*);
 int ply_he(T*, He**);
 
 int ply_tri(T*, real**);
-int ply_tri(T*, int m, real**);
-
 int ply_x(T*, int m, real**);
 int ply_y(T*, int m, real**);
 int ply_z(T*, int m, real**);
