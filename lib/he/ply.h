8011608b2fd46ac62da9007d0ec9d5d27ee4387b
Sergey Litvinov <slitvinov@gmail.com>
2019-01-13 19:18:01 +0100

    add HePly interface

---
+++ lib/he/ply.h
@@ -0,0 +1,22 @@
+#define T HePly
+
+typedef struct He He;
+typedef struct T T;
+
+int ply_fread(FILE*, T**);
+int ply_fin(T*);
+
+int ply_nv(T*);
+int ply_nt(T*);
+int ply_nm(T*);
+
+int ply_he(T*, He**);
+
+int ply_tri(T*, real**);
+int ply_tri(T*, int m, real**);
+
+int ply_x(T*, int m, real**);
+int ply_y(T*, int m, real**);
+int ply_z(T*, int m, real**);
+
+#undef T
