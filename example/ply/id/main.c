6c88602fff28fa8ef16ccde54bc46ac589a06422
Sergey Litvinov <slitvinov@gmail.com>
2019-01-14 15:35:58 +0100

    initial import of id

---
+++ example/ply/id/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <real.h>
+#include <he/macro.h>
+#include <he/memory.h>
+#include <he/err.h>
+#include <he/ply.h>
+#include <he/util.h>
+
+static Ply *read;
+static const char *me = "ply/id";
+static int nv, nt, nm, m;
+
+static void usg() {
+    fprintf(stderr, "%s < PLY > VTK\n", me);
+    exit(2);
+}
+
+static void ini() {
+    ply_fread(stdin, &read);
+}
+static void fin() { ply_fin(read); }
+
+static int write() {
+    int nm, i;
+    real *color;
+    nm = ply_nm(read);
+    MALLOC(nm, &color);
+    for (i = 0; i < nm; i++)
+        color[i] = i;
+    ply_vtk_bin(read, stdout, NULL, color);
+    FREE(color);
+    return HE_OK;
+}
+
+int main(__UNUSED int c, const char **v) {
+    if (*++v != NULL && util_eq(*v, "-h")) usg();
+    ini();
+    write();
+    fin();
+}
