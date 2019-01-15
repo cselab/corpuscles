a6a5789b6882f556e354b093bd5c24a694ec5255
Sergey Litvinov <slitvinov@gmail.com>
2019-01-15 15:15:45 +0100

    wip: harmonic energy

--- example/force/harmonic_rbc/main.c
+++ example/force/harmonic_rbc/main.c
@@ -7,13 +7,14 @@
 #include <he/err.h>
 #include <he/memory.h>
 #include <he/macro.h>
-#include <he/punto.h>
+#include <he/ply.h>
 #include <he/f/harmonic_ref.h>
 #include <he/he.h>
 #include <he/y.h>
 
-static real *x, *y, *z;
 static HeFHarmonicRef *force;
+static Ply *cell;
+
 static He *he;
 static int nv;
 static const char **argv;
@@ -27,18 +28,44 @@ static int str(/**/ char *p) {
 }
 
 static void main0() {
-    fprintf(stderr, "eng: %g\n", he_f_harmonic_ref_energy(force, he, x, y, z));
+    int i, nm;
+    real *x, *y, *z;
+
+    nm = ply_nm(cell);
+    for (i = 0; i < nm; i++) {
+        ply_x(cell, i, &x);
+        ply_y(cell, i, &y);
+        ply_z(cell, i, &z);
+        fprintf(stderr, "eng: %g\n", he_f_harmonic_ref_energy(force, he, x, y, z));
+    }
+    
 }
 
 int main(int __UNUSED argc, const char *v[]) {
+    int nt;
     real K;
+    FILE *f;
+    real *x, *y, *z;
     
     argv = v; argv++;
     str(off);
     str(ply);
     
-    y_ini("/dev/stdin", &he, &x, &y, &z);
+    y_ini(off, &he, &x, &y, &z);
+    if ((f = fopen(ply, "r")) == NULL)
+        ER("fail to open '%s'", ply);
+    ply_fread(f, &cell);
+    fclose(f);
+
     nv = he_nv(he);
+    nt = he_nt(he);
+
+    if (nv != ply_nv(cell))
+        ER("off_nv=%d != ply_nv=%d", nv, ply_nv(cell));
+
+    if (nt != ply_nt(cell))
+        ER("off_nt=%d != ply_nt=%d", nt, ply_nt(cell));
+    
     K = 1;
     he_f_harmonic_ref_ini(K, x, y, z, he, /**/ &force);
 
