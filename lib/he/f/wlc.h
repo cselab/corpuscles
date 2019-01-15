5696e810f9a181dfad973bba2ef65294c7fda206
Sergey Litvinov <slitvinov@gmail.com>
2019-01-15 15:51:22 +0100

    add wlc

---
+++ lib/he/f/wlc.h
@@ -0,0 +1,12 @@
+#define T HeFWlc
+
+typedef struct He He;
+typedef struct T T;
+
+int he_f_wlc_ini(real K, const real *x, const real *y, const real *z, He*, T**);
+int he_f_wlc_fin(T*);
+int he_f_wlc_force(T*, He*, const real *x, const real *y, const real *z, /**/ real *fx, real *fy, real *fz);
+real he_f_wlc_energy(T*, He*, const real *x, const real *y, const real *z);
+int he_f_wlc_e(T*, /**/ real  **v);
+
+#undef T
