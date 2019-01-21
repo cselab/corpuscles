96abaccdc922ac0754bc5d8dde2b46d91873b74e
Sergey Litvinov <slitvinov@gmail.com>
2019-01-21 10:01:04 +0100

    argv

--- lib/he/f/area.h
+++ lib/he/f/area.h
@@ -5,12 +5,10 @@ typedef struct T T;
 
 /* E = K/area0 * sum_{tri} (area_{tri} - area0)^2 */
 int he_f_area_ini(real a0, real K, He*, T**);
+int he_f_area_argv(const char***, He*, T**);
 int he_f_area_fin(T*);
-
-int he_f_area_force(T*, He*,
-                    const real *x, const real *y, const real *z, /**/
-                    real *fx, real *fy, real *fz);
-real he_f_area_energy(T*, He*, const real *x, const real *y, const real *z);
+int he_f_area_force(T*, He*, const real*, const real*, const real*, /**/ real*, real*, real*);
+real he_f_area_energy(T*, He*, const real*, const real*, const real*);
 
 int he_f_area_a(T*, /**/ real  **a);
 int he_f_area_da(T*, /**/ real **da);
--- lib/he/f/area_voronoi.h
+++ lib/he/f/area_voronoi.h
@@ -4,8 +4,9 @@ typedef struct He He;
 typedef struct T T;
 
 int he_f_area_voronoi_ini(real a0, real K, He*, T**);
+int he_f_area_voronoi_argv(const char***, He*, T**);
 int he_f_area_voronoi_fin(T*);
-int he_f_area_voronoi_force(T*, He*, const real *x, const real *y, const real *z, /**/ real *fx, real *fy, real *fz);
-real he_f_area_voronoi_energy(T*, He*, const real *x, const real *y, const real *z);
+int he_f_area_voronoi_force(T*, He*, const real*, const real*, const real*, /**/ real*, real*, real*);
+real he_f_area_voronoi_energy(T*, He*, const real*, const real*, const real*);
 
 #undef T
