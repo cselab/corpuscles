b444a65f47d3e689a7a3b6aff7f1d5b77a68efea
Sergey Litvinov <slitvinov@gmail.com>
2019-01-21 14:48:23 +0100

    wip: generic force: garea_voronoi

--- lib/f/garea_voronoi.c
+++ lib/f/garea_voronoi.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "real.h"
+#include "he/argv.h"
 #include "he/memory.h"
 #include "he/err.h"
 #include "he/he.h"
@@ -74,7 +75,13 @@ int he_f_garea_voronoi_ini(real A0, real K, He *he, T **pq) {
 }
 
 int he_f_garea_voronoi_argv(char ***p, He *he, T **pq) {
-    return HE_OK;
+    int status;
+    real x, y;
+    if ((status = argv_real(p, &x)) != HE_OK)
+        return status;
+    if ((status = argv_real(p, &y)) != HE_OK)
+        return status;
+    return he_f_garea_voronoi_ini(x, y, he, pq);
 }
 
 int he_f_garea_voronoi_fin(T *q) {
