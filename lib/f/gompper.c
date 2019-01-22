c9e49809a0e599271aea652dde3a48ac050b02dd
Sergey Litvinov <slitvinov@gmail.com>
2019-01-22 10:25:12 +0100

    add argv in gompper.c

--- lib/f/gompper.c
+++ lib/f/gompper.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 #include "real.h"
+
+#include "he/argv.h"
 #include "he/memory.h"
 #include "he/err.h"
 #include "he/he.h"
@@ -104,7 +106,18 @@ int he_f_gompper_ini(real Kb, real C0, real Kad, real DA0D, He *he, T **pq) {
 }
 
 int he_f_gompper_argv(char ***p, He *he, T **pq) {
-    return HE_OK;
+    int status;
+    real Kb, C0, Kad, DA0D;
+    if ((status = argv_real(p, &Kb)) != HE_OK)
+        return status;
+    if ((status = argv_real(p, &C0)) != HE_OK)
+        return status;
+    if ((status = argv_real(p, &Kad)) != HE_OK)
+        return status;
+    if ((status = argv_real(p, &DA0D)) != HE_OK)
+        return status;
+
+    return he_f_gompper_ini(Kb, C0, Kad, DA0D, he, pq);
 }
 
 int he_f_gompper_fin(T *q) {
