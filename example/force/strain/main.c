475cb237b956dfac2bfb620f9635c36a18e2f53b
Sergey Litvinov <slitvinov@gmail.com>
2019-01-25 16:08:52 +0100

    area.h

--- example/force/strain/main.c
+++ example/force/strain/main.c
@@ -4,6 +4,7 @@
 #include <string.h>
 
 #include <real.h>
+#include <he/area.h>
 #include <he/memory.h>
 #include <he/punto.h>
 #include <he/macro.h>
@@ -15,27 +16,27 @@
 
 static const real pi = 3.141592653589793115997964;
 static char **argv;
-static real *xx, *yy, *zz, *fm, *fx, *fy, *fz;
+static real *x, *y, *z, *fm, *fx, *fy, *fz, *area;
 static int nv, nt;
 static HeFStrain *strain;
 static He *he;
 static real h = 1e-6;
 
 
-static real energy() {
-    return he_f_strain_energy(strain, he, xx, yy, zz);
-}
+static real energy() { return he_f_strain_energy(strain, he, x, y, z); }
 
 int main0() {
     int i;
     real *eng, e, eh, tmp;
     he_f_strain_argv(&argv, he, &strain);
-    
-    he_f_strain_force(strain, he, xx, yy, zz, /**/ fx, fy, fz);
-    e = he_f_strain_energy(strain, he, xx, yy, zz);
+
+    he_f_strain_force(strain, he, x, y, z, /**/ fx, fy, fz);
+    e = he_f_strain_energy(strain, he, x, y, z);
     he_f_strain_energy_ver(strain, &eng);
-    real *queue[] = {xx, yy, zz, fx, fy, fz, eng, NULL};
-    puts("x y z fx fy fz eng");
+    he_area_ver(he, x, y, z, /**/ area);
+
+    real *queue[] = {x, y, z, fx, fy, fz, eng, area, NULL};
+    puts("x y z fx fy fz eng area");
     punto_fwrite(nv, queue, stdout);
     he_f_strain_fin(strain);
 
@@ -44,16 +45,17 @@ int main0() {
 
 int main(int __UNUSED argc, char *v[]) {
     argv = v; argv++;
-    
-    y_ini("/dev/stdin", /**/ &he, &xx, &yy, &zz);
+
+    y_ini("/dev/stdin", /**/ &he, &x, &y, &z);
     nv = he_nv(he);
 
     CALLOC(nv, &fm); CALLOC(nv, &fx);  CALLOC(nv, &fy); CALLOC(nv, &fz);
+    MALLOC(nv, &area);
 
     main0();
 
-    FREE(fm); FREE(fx); FREE(fy); FREE(fz);
+    FREE(fm); FREE(fx); FREE(fy); FREE(fz); FREE(area);
 
-    y_fin(he, xx, yy, zz);
+    y_fin(he, x, y, z);
     return 0;
 }
