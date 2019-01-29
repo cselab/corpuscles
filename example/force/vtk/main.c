ef88a87e14fa3c8b6d64c099de1e2e878719d74f
Sergey Litvinov <slitvinov@gmail.com>
2019-01-29 11:14:44 +0100

    dump area0

--- example/force/vtk/main.c
+++ example/force/vtk/main.c
@@ -14,7 +14,6 @@
 #include <he/argv.h>
 #include <he/macro.h>
 #include <he/vec.h>
-#include <he/fd.h>
 #include <he/y.h>
 
 #define FMT_IN   HE_REAL_IN
@@ -25,7 +24,6 @@ static char name[1024];
 
 static real *fx, *fy, *fz, *fm, *x, *y, *z, *rr, *area, *area0;
 static real *x0, *y0, *z0;
-static real *gx, *gy, *gz;
 static int nv, nt;
 static He *he, *he0;
 static Force *force;
@@ -45,7 +43,6 @@ static void main0() {
     real e, r[3], f[3];
 
     force_force(force, he, x, y, z, /**/ fx, fy, fz);
-    fd(force, he, delta, x, y, z, /**/ gx, gy, gz);
     e = force_energy(force, he, x, y, z);
     he_area_ver(he, x, y, z, /**/ area);
     he_area_ver(he0, x0, y0, z0, /**/ area0);
@@ -53,7 +50,6 @@ static void main0() {
     MSG("name: %s", force_name(force));
     MSG("energy: %g", e);
     MSG("f0: %g %g %g", fx[0], fy[0], fz[0]);
-    MSG("g0: %g %g %g", gx[0], gy[0], gz[0]);
 
     for (i = 0; i < nv; i++) {
         vec_get(i, x, y, z, /**/ r);
@@ -62,8 +58,8 @@ static void main0() {
         fm[i] = vec_abs(f);
     }
 
-    const char *na[] = {"fm", "fx", "fy", "fz", "area", "area0", "gx", "gy", "gz", NULL};
-    const real *sc[] = {fm, fx, fy, fz, area, area0, gx, gy, gz, NULL};
+    const char *na[] = {"fm", "fx", "fy", "fz", "area", "area0", NULL};
+    const real *sc[] = {fm, fx, fy, fz, area, area0, NULL};
     vtk_fwrite(he, x, y, z, sc, na, stdout);
     force_fin(force);
 }
@@ -82,13 +78,11 @@ int main(int __UNUSED argc, char *argv[]) {
 
     MALLOC(nv, &rr); MALLOC(nv, &fm); MALLOC(nv, &area); MALLOC(nv, &area0);
     CALLOC(nv, &fx); CALLOC(nv, &fy); CALLOC(nv, &fz);
-    MALLOC(nv, &gx); MALLOC(nv, &gy); MALLOC(nv, &gz);
 
     main0();
 
     FREE(rr); FREE(fm); FREE(area); FREE(area0);
     FREE(fx); FREE(fy); FREE(fz);
-    FREE(gx); FREE(gy); FREE(gz);
 
     y_fin(he, x, y, z);
     y_fin(he0, x0, y0, z0);    
