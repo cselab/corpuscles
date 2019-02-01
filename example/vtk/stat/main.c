a882f64e9dae4a87988f41fc5b72da310e7f0d7b
Sergey Litvinov <slitvinov@gmail.com>
2019-02-01 15:51:27 +0100

    dump area

--- example/vtk/stat/main.c
+++ example/vtk/stat/main.c
@@ -6,47 +6,25 @@
 #include <real.h>
 #include <he/area.h>
 #include <he/memory.h>
-#include <he/punto.h>
 #include <he/macro.h>
 #include <he/y.h>
 #include <he/he.h>
 #include <he/err.h>
-#include <he/strain.h>
 #include <he/vtk.h>
-#include <he/f/strain.h>
 
 static const real pi = 3.141592653589793115997964;
-static char **argv;
-static real *x, *y, *z, *fm, *fx, *fy, *fz, *area, *area0;
-static real *x0, *y0, *z0;
+static real *x, *y, *z, *area;
 static int nv, nt;
-static HeFStrain *strain;
-static He *he, *he0;
-
-static real energy() { return he_f_strain_energy(strain, he, x, y, z); }
+static He *he;
+char **argv;
 
 int main0() {
-    real *eng, e, *al, *be;
-    he_f_strain_argv(&argv, he, &strain);
-
-    he_f_strain_force(strain, he, x, y, z, /**/ fx, fy, fz);
-    e = he_f_strain_energy(strain, he, x, y, z);
-    he_f_strain_energy_ver(strain, &eng);
-
-    he_f_strain_invariants(strain, he, x, y, z, &al, &be);
-
     he_area_ver(he, x, y, z, /**/ area);
-    he_area_ver(he0, x0, y0, z0, /**/ area0);
 
-    const real *sc[] = {x, y, z, fx, fy, fz, eng, area, area0, al, be, NULL};
-    const char *na[] = {"x", "y", "z", "fx", "fy", "fz", "eng", "area", "area0", "al", "be", NULL};
+    const real *sc[] = {area, NULL};
+    const char *na[] = {"area", NULL};
 
-    puts("x y z fx fy fz eng area area0 al be");
-
-    vtk_write(he, x, y, z, sc, na, "o.vtk");
-    punto_fwrite(nv, sc, stdout);
-
-    he_f_strain_fin(strain);
+    vtk_fwrite(he, x, y, z, sc, na, stdout);
 
     return HE_OK;
 }
@@ -55,20 +33,14 @@ int main(int __UNUSED argc, char *v[]) {
     argv = v; argv++;
 
     y_ini("/dev/stdin", /**/ &he, &x, &y, &z);
-    y_ini(*argv, /**/ &he0, &x0, &y0, &z0);
 
     nv = he_nv(he);
 
-    CALLOC(nv, &fm); CALLOC(nv, &fx);  CALLOC(nv, &fy); CALLOC(nv, &fz);
-    MALLOC(nv, &area); MALLOC(nv, &area0);
+    MALLOC(nv, &area);
 
     main0();
 
-    FREE(fm); FREE(fx); FREE(fy); FREE(fz);
-    FREE(area); FREE(area0);
-
+    FREE(area);
     y_fin(he, x, y, z);
-    y_fin(he0, x0, y0, z0);
-
     return 0;
 }
