c4156242a99a075444e7789fe0b3fb895163a481
Sergey Litvinov <slitvinov@gmail.com>
2019-02-01 10:29:19 +0100

    minor

--- lib/he/vtk.h
+++ lib/he/vtk.h
@@ -1,5 +1,3 @@
 typedef struct He He;
-int vtk_fwrite(He *he, const real *x, const real *y, const real *z, const real *scalars[], const char *names[], /**/
-                  FILE *f);
-int vtk_write(He *he, const real *x, const real *y, const real *z, const real *scalars[], const char *names[], /**/
-                 const char *path);
+int vtk_fwrite(He*, const real*, const real*, const real*, const real *scalars[], const char *names[], /**/ FILE*);
+int vtk_write(He*, const real*, const real*, const real*, const real *scalars[], const char *names[], /**/ const char*);
