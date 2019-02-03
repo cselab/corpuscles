9f0db94aabfa9fcf8d364e41888d6d754d5ea4bc
Sergey Litvinov <slitvinov@gmail.com>
2019-02-03 22:51:02 +0100

    initial import of write/xyz

---
+++ example/off/write/xyz/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include <real.h>
+#include <he/err.h>
+#include <he/off.h>
+
+#include <he/y.h>
+
+static HeOff *off;
+static real *x, *y, *z;
+static He *he;
+
+int main() {
+    y_ini("/dev/stdin", &he, &x, &y, &z);
+
+    y_fin(he, x, y, z);
+}
