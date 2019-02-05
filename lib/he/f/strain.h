5bc28b17e760506001cb4bf96e9690163a30c45d
Sergey Litvinov <slitvinov@gmail.com>
2019-02-05 10:34:20 +0100

    minor

--- lib/he/f/strain.h
+++ lib/he/f/strain.h
@@ -11,7 +11,6 @@ int he_f_strain_fin(T*);
 int he_f_strain_force(T*, /*unused*/ He*, const real*, const real*, const real*, /**/ real*, real*, real*);
 real he_f_strain_energy(T*, /*unused*/ He*, const real*, const real*, const real*);
 int he_f_strain_energy_ver(T*, /**/ real**);
-
 int he_f_strain_invariants(T*, const real*, const real*, const real*, /**/ real**, real**);
 int he_f_strain_invariants_tri(T*, const real*, const real*, const real*, /**/ real**, real**);
 
