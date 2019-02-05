47b707e24f03410e3d9ca24eba9214192f5c107d
Sergey Litvinov <slitvinov@gmail.com>
2019-02-05 15:08:03 +0100

    add an env variable AREA to switch ref and current

--- lib/strain/2d.c
+++ lib/strain/2d.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 #include "real.h"
 #include "he/err.h"
@@ -8,8 +9,22 @@
 #define NOT_ZERO(x) if ((x) == 0) ERR(HE_NUM, "should not be zero");
 
 static real sq(real x) { return x*x; }
-static real select(real x, real y) {
-    return x;
+static real select(real ref, real cur) {
+    enum {UNSET, REF, CUR};
+    static int f = UNSET;
+    if (f == UNSET) {
+        if (!getenv("AREA")) {
+            f = REF;
+            MSG("area: reference");
+        } else {
+            f = CUR;
+            MSG("area: current");
+        }
+    }
+    if (f == REF)
+        return ref;
+    else
+        return cur;
 }
 
 #include "he/strain/2d.h"
