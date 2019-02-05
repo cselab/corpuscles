24034d32c98ae653a49b8a89f946bc55bf30d9fb
Sergey Litvinov <slitvinov@gmail.com>
2019-02-05 14:59:45 +0100

    use emepty select

--- lib/strain/2d.c
+++ lib/strain/2d.c
@@ -6,7 +6,11 @@
 
 #define SET(key, val) if ((key) != NULL) *(key) = (val)
 #define NOT_ZERO(x) if ((x) == 0) ERR(HE_NUM, "should not be zero");
+
 static real sq(real x) { return x*x; }
+static real select(real x, real y) {
+    return x;
+}
 
 #include "he/strain/2d.h"
 #include "strain/2d.inc"
