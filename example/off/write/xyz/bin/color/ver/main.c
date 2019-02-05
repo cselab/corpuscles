3e420c8e5d88a08ec0402331d5a6f328739e73f6
Sergey Litvinov <slitvinov@gmail.com>
2019-02-05 20:59:18 +0100

    add simple client for boff_lh_ver_fwrite

--- example/off/write/xyz/bin/color/ver/main.c
+++ example/off/write/xyz/bin/color/ver/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 #include <real.h>
-#include <he/area.h>
 #include <he/err.h>
 #include <he/off.h>
 #include <he/he.h>
@@ -10,23 +9,14 @@
 #include <he/y.h>
 
 static HeOff *off;
-static int nt;
 static real *x, *y, *z;
+static real lo, hi;
 static He *he;
-static real lo, hi, *a;
 
 int main() {
-    lo = 0; hi = 0.005;
+    lo = -5; hi = 5;
     y_ini("/dev/stdin", &he, &x, &y, &z);
-
-    nt = he_nt(he);
-    CALLOC(nt, &a);
-
-    he_area_tri(he, x, y, z, a);
-    //boff_lh_tri_fwrite(he, x, y, z, lo, hi, a, stdout);
-    boff_tri_fwrite(he, x, y, z, a, stdout);
+    boff_lh_ver_fwrite(he, x, y, z, lo, hi, x, stdout);
     y_fin(he, x, y, z);
-
-    FREE(a);
     return 0;
 }
