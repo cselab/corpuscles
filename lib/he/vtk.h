c149908dbc4f3999c8989172989f38a47cf9db13
Sergey Litvinov <slitvinov@gmail.com>
2019-02-05 10:26:39 +0100

    initial import of vtk_tri_(f)write

--- lib/he/vtk.h
+++ lib/he/vtk.h
@@ -1,3 +1,5 @@
 typedef struct He He;
-int vtk_fwrite(He*, const real*, const real*, const real*, const real *scalars[], const char *names[], /**/ FILE*);
-int vtk_write(He*, const real*, const real*, const real*, const real *scalars[], const char *names[], /**/ const char*);
+int vtk_fwrite(He*, const real*, const real*, const real*, const real**, const char**, /**/ FILE*);
+int vtk_write(He*, const real*, const real*, const real*, const real**, const char**, /**/ const char*);
+int vtk_tri_fwrite(He*, const real*, const real*, const real*, const real**, const char**, /**/ FILE*);
+int vtk_tri_write(He*, const real*, const real*, const real*, const real**, const char**, /**/ const char*);
