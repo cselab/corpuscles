6175272371bb6c2ec1c3e00ee6be083fa5f8d8f4
Sergey Litvinov <slitvinov@gmail.com>
2019-02-06 10:47:10 +0100

    add query loop

--- bin/q/main.c
+++ bin/q/main.c
@@ -38,26 +38,31 @@ static int q_area(void) {
 static const char *Name[] = {"x", "area"};
 static int (*Func[])(void) = {q_x, q_area};
 
+int eputs(const char *s) {
+    fputs(s, stderr);
+    fputs("\n", stderr);
+    return HE_OK;
+}
+
 int main(__UNUSED int c, char **v) {
     char q[1024];
+    int n, i;
+
     argv = v;
     argv++;
     if (!argv[0] || util_eq(argv[0], "-h"))
         usg();
     argv_str(&argv, q);
 
+    n = sizeof(Name)/sizeof(Name[0]);
+    for (i = 0; i < n; i++) {
+        if (util_eq(q, Name[i]))
+            return Func[i]();
+    }
 
-    lo = 0; hi = 0.005;
-    y_ini("/dev/stdin", &he, &x, &y, &z);
-
-    nt = he_nt(he);
-    CALLOC(nt, &a);
-
-    he_area_tri(he, x, y, z, a);
-    //boff_lh_tri_fwrite(he, x, y, z, lo, hi, a, stdout);
-    boff_tri_fwrite(he, x, y, z, a, stdout);
-    y_fin(he, x, y, z);
-
-    FREE(a);
-    return 0;
+    MSG("unknown query: '%s'", q);
+    eputs("possible values are:");
+    for (i = 0; i < n; i++)
+        eputs(Name[i]);
+    return 1;
 }
