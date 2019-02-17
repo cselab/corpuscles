e02f31c96a94b52f0d63901c48ac56520c8d550c
Sergey Litvinov <slitvinov@gmail.com>
2019-02-17 13:15:03 +0100

    wip: dtri_beta

--- lib/he/dtri.h
+++ lib/he/dtri.h
@@ -11,3 +11,4 @@ int dtri_normal(const real a[3], const real b[3], const real c[3], /**/ Ten *x, 
 int dtri_edg_area(const real a[3], const real b[3], const real[3],  /**/ real[3], real[3], real[3]);
     
 int dtri_alpha(const real[3], const real[3], const real[3], const real u[3], const real v[3], const real w[3], /**/ real[3], real[3], real[3]);
+int dtri_beta(const real[3], const real[3], const real[3], const real u[3], const real v[3], const real w[3], /**/ real[3], real[3], real[3]);
