01fc1e5cf0a303846169c427f2c99ee27b0771bf
Sergey Litvinov <slitvinov@gmail.com>
2019-02-18 15:02:10 +0100

    add dtri_lim_* functions

--- lib/he/dtri.h
+++ lib/he/dtri.h
@@ -13,4 +13,6 @@ int dtri_edg_area(const real a[3], const real b[3], const real[3],  /**/ real[3]
 int dtri_alpha(const real[3], const real[3], const real[3], const real u[3], const real v[3], const real w[3], /**/ real[3], real[3], real[3]);
 int dtri_beta(const real[3], const real[3], const real[3], const real u[3], const real v[3], const real w[3], /**/ real[3], real[3], real[3]);
 
-int dtri_lim_area(real Ka, real a3, real a4, const real[3], const real[3], const real[3], const real[3], const real[3], const real[3], real[3], real[3], real[3]);
+int dtri_lim_area(real Ka, real a3, real a4, const real[3], const real[3], const real[3], const real[3], const real[3], const real[3], /**/ real[3], real[3], real[3]);
+int dtri_lim_shear(real mu, real b1, real b2, const real[3], const real[3], const real[3], const real[3], const real[3], const real[3], /**/ real[3], real[3], real[3]);
+int dtri_lim(real Ka, real a3, real a4, real mu, real b1, real b2, const real[3], const real[3], const real[3], const real[3], const real[3], const real[3], /**/ real[3], real[3], real[3]);
