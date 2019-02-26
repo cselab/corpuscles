d6417aa3c5125299f29995ecddf2353d36b4aacd
Sergey Litvinov <slitvinov@gmail.com>
2019-02-26 11:04:28 +0100

    use c.m4 and h.m4

--- lib/make/rules.mk
+++ lib/make/rules.mk
@@ -7,7 +7,9 @@ libhe_d.a: $O $(O_D); ar r $@ $O $(O_D) && ranlib $@
 libhe_l.a: $O $(O_L); ar r $@ $O $(O_L) && ranlib $@
 $O : $H $I
 
-%.c:   %.m4; he.m4 -s -o $@ $<
+.c.m4.c:; he.m4 -s -o $@ $<
+.h.m4.h:; he.m4 -s -o $@ $<
+
 %.o:   %.c; $(CC)                         -I. $(CFLAGS) $(HE_CFLAGS) -c -o $@ $<
 %.s.o: %.c; $(CC) -I$(TRG)/prec/s -Iinc/s -I. $(CFLAGS) $(HE_CFLAGS) -c -o $@ $<
 %.d.o: %.c; $(CC) -I$(TRG)/prec/d -Iinc/d -I. $(CFLAGS) $(HE_CFLAGS) -c -o $@ $<
@@ -17,3 +19,5 @@ clean:; rm -f $O $(O_S) $(O_D) $L
 test:
 
 .PHONY: clean install test
+
+.SUFFIXES: .c.m4 .h.m4
