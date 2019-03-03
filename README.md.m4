dcebec0cda751de7c529652bbd1f026b2ba408a8
Sergey Litvinov <slitvinov@gmail.com>
2019-03-03 16:03:09 +0100

    minor

--- README.md.m4
+++ README.md.m4
@@ -1,7 +1,7 @@
 include(`co.m4')dnl
 h_define(`r_define',
 `h_define($1, `h_changequote()h_changequote(`,')`'$2`'h_changequote(`<<',`>>')')')dnl
-r_define(`r_header', `[$1](lib/he/$1)')dnl
+r_define(`r_header', `[$1](lib/co/$1)')dnl
 r_define(`r_file', `h_cmd(cat $1)')dnl
 r_define(`r_cmd', `h_cmd($1)')dnl
 r_define(`r_dir', `[$1]($1/)')dnl
--- emacs/cc.el
+++ emacs/cc.el
@@ -1,9 +1,9 @@
-(defun he/c-indent ()
+(defun co/c-indent ()
   (setq c-basic-offset 4
 	indent-tabs-mode nil
 	c-default-style '((java-mode . "java")
 			  (awk-mode  . "awk")
 			  (c-mode    . "k&r")
 			  (cc-mode   . "k&r"))))
-(add-hook 'c-mode-hook   'he/c-indent)
+(add-hook 'c-mode-hook   'co/c-indent)
 (add-to-list 'auto-mode-alist '("\\.h\\'"  . c-mode))
