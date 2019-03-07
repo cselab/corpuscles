354007a779df5b1a653482dc8e3d846985b985f4
Sergey Litvinov <slitvinov@gmail.com>
2019-03-07 18:08:37 +0100

    add a clinet for matrix

--- example/matrix/main.c
+++ example/matrix/main.c
@@ -3,19 +3,45 @@
 #include <real.h>
 #include <co/err.h>
 #include <co/matrix.h>
+#include <co/macro.h>
+#include <co/util.h>
 
 #define FMT CO_REAL_OUT
+static const char **argv;
 
-int main(void) {
+static int eq(const char *a, const char *b) { return util_eq(a, b); }
+int main(__UNUSED int argc, const char **v0) {
+    const char *op;
     int M, N, N0, K;
-    real *a, *b, *c;
+    real *a, *b, c[999];
 
-    matrix_fread(stdin, &M,  &N, &a);
-    matrix_fread(stdin, &N0, &K, &b);
-    if (N0 != N) ER("N0=%d != N=%d", N0, N);
-    matrix_ini(M, K, &c);
-    matrix_mult_nn(M, N, K, a, b, c);
+    argv = v0;
+    argv++;
+    if (*argv == NULL)
+        ER("mssing OP");
+    op = *argv++;
+    if (eq(op, "nn")) {
+        matrix_fread(stdin, &M,  &N, &a);
+        matrix_fread(stdin, &N0, &K, &b);
+        if (N0 != N) ER("N0=%d != N=%d", N0, N);
+        matrix_mult_nn(M, N, K, a, b, c);
+    } else if (eq(op, "nt")) {
+        matrix_fread(stdin, &M,  &N, &a);
+        matrix_fread(stdin, &K, &N0, &b);
+        if (N0 != N) ER("N0=%d != N=%d", N0, N);
+        matrix_mult_nt(M, N, K, a, b, c);
+    } else if (eq(op, "tn")) {
+        matrix_fread(stdin, &N,  &M, &a);
+        matrix_fread(stdin, &N0, &K, &b);
+        if (N0 != N) ER("N0=%d != N=%d", N0, N);
+        matrix_mult_tn(M, N, K, a, b, c);
+    } else if (eq(op, "tt")) {
+        matrix_fread(stdin, &N,  &M, &a);
+        matrix_fread(stdin, &K, &N0, &b);
+        if (N0 != N) ER("N0=%d != N=%d", N0, N);
+        matrix_mult_nn(M, N, K, a, b, c);
+    } else
+        ER("unknown operation '%s'", op);
     matrix_fwrite(M, K, c, stdout);
-    matrix_free(a); matrix_free(b); matrix_free(c);
-
+    matrix_free(a); matrix_free(b);
 }
