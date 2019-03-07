0b39d47211ddcb75b871c00860076f2fd7c48e24
Sergey Litvinov <slitvinov@gmail.com>
2019-03-07 15:27:43 +0100

    up matrix.c

--- lib/matrix.c
+++ lib/matrix.c
@@ -2,6 +2,6 @@
 #include "co/err.h"
 #include "co/matrix.h"
 
-int matrix_mult_nn(int M, int N, int K, const real *a, const real *b, const real *c) {
+int matrix_mult_nn(int M, int N, int K, const real *a, const real *b, /**/ real *c) {
     return CO_OK;
 }
