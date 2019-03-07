9669e55e801006ca412b91f5d5404f8de03fdcba
Sergey Litvinov <slitvinov@gmail.com>
2019-03-07 17:49:30 +0100

    wip: matrix_*

--- lib/co/matrix.h
+++ lib/co/matrix.h
@@ -1,8 +1,11 @@
 int matrix_zero(int, int, real*);
 
-int matrix_mult_nn(int, int, int, const real*, const real*, real*);
-int matrix_mult_nt(int, int, int, const real*, const real*, real*);
-int matrix_mult_tn(int, int, int, const real*, const real*, real*);
-int matrix_mult_tt(int, int, int, const real*, const real*, real*);
+int matrix_mult_nn(int, int, int, const real*, const real*, /**/ real*);
+int matrix_mult_nt(int, int, int, const real*, const real*, /**/ real*);
+int matrix_mult_tn(int, int, int, const real*, const real*, /**/ real*);
+int matrix_mult_tt(int, int, int, const real*, const real*, /**/ real*);
 
 int matrix_fwrite(int, int, const real*, FILE*);
+int matrix_fread(FILE*, /**/ int*, int*, real**);
+int matrix_ini(int, int, real**);
+int matrix_free(real*);
