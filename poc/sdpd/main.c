a5d88f4db6bf526398da41b4033bfd81dff9f7d5
Sergey Litvinov <slitvinov@gmail.com>
2019-03-20 15:38:43 +0100

    wip: alist

--- poc/sdpd/main.c
+++ poc/sdpd/main.c
@@ -8,7 +8,8 @@
 #define T Ilist
 typedef struct T T;
 
-enum { ILIST_END = -1 };
+enum {
+	ILIST_END = -1 };
 int ilist_ini(T**);
 int ilist_fin(T*);
 int ilist_push(T*, int);
@@ -20,7 +21,8 @@ struct T { /* size, head, tail */
 	int s;
 	int *h, *t;
 };
-enum { SIZE = 28, END = ILIST_END };
+enum {
+	SIZE = 28, END = ILIST_END };
 int ilist_ini(T **pq)
 {
 	T *q;
@@ -44,7 +46,7 @@ int ilist_fin(T *q)
 int ilist_push(T *q, int x)
 {
 	int n;
-	int s, *h, *t, *cp;
+	int s, *h, *t;
 
 	s = q->s;
 	h = q->h;
@@ -52,10 +54,10 @@ int ilist_push(T *q, int x)
 	n = t - h;
 	if (n + 1 >= s) {
 		s *= 2;
-                REALLOC(s, &h);
+		REALLOC(s, &h);
 		t = h + n;
 	}
-	*t = x; 
+	*t = x;
 	t++;
 	*t = END;
 	q->s = s;
@@ -87,7 +89,8 @@ int ilist_reset(T *q)
 	return CO_OK;
 }
 
-int ilist_fwrite(FILE *f, T *q) {
+int ilist_fwrite(FILE *f, T *q)
+{
 	int n, *a;
 
 	ilist_head(q, &a);
@@ -96,6 +99,7 @@ int ilist_fwrite(FILE *f, T *q) {
 	while (*a != ILIST_END) {
 		fprintf(f,  "%d\n", *(a++));
 	}
+	return CO_OK;
 }
 
 #undef T
@@ -111,6 +115,51 @@ int alist_push(T*, int, int);
 int alist_head(T*, int);
 int alist_len(T*, int);
 
+struct T {
+	int n;
+	Ilist **a;
+};
+
+int alist_int(int n, T **pq)
+{
+	T *q;
+	int i;
+	Ilist **a;
+
+	MALLOC(1, &q);
+	if (n <= 0)
+		ERR(CO_INDEX, "n=%d <= 0", n);
+	MALLOC(n,  &a);
+	for (i = 0; i < n; i++)
+		ilist_ini(&a[i]);
+	q->n = n;
+	q -> a = a;
+	*pq = q;
+	return CO_OK;
+}
+
+int alist_fin(T *q)
+{
+	int n, i;
+	Ilist **a;
+	a = q->a;
+	for (i = 0; i < n; i++)
+		ilist_fin(a[i]);
+	FREE(a);
+	FREE(q);
+	return CO_OK;
+}
+
+int alist_reset(T *q) {
+	int n, i;
+	Ilist **a;
+	n = q->n;
+	a = q->a;
+	for (i = 0; i < n; i++)
+		ilist_reset(a[i]);
+	return CO_OK;
+}
+
 int main(void)
 {
 	int i;
@@ -119,13 +168,11 @@ int main(void)
 
 	for (i = 0; i < 20; i++)
 		ilist_push(a, i);
-        ilist_reset(a);
-        ilist_push(a, 10);
+	ilist_reset(a);
+	ilist_push(a, 10);
 	fprintf(stderr, "size: %d\n", a->s);
 	ilist_fwrite(stdout, a);
 
 	ilist_fin(a);
 	return 0;
 }
-
-
