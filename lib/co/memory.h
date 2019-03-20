9a5b4740ce838f7488f0d9165034d631488e6169
Sergey Litvinov <slitvinov@gmail.com>
2019-03-20 13:24:58 +0100

    missing ;

--- lib/co/memory.h
+++ lib/co/memory.h
@@ -3,9 +3,8 @@
 #define CALLOC(n, pp) co_calloc((n)*sizeof(**(pp)), (void**)(pp))
 #define FREE(p) co_free(p)
 #define MEMCPY(n, src, dest) co_memcpy((void*)(dest), (const void*)(src), (n)*sizeof(*(dest)))
-
 int co_malloc(int, void**);
 int co_realloc(int, void**);
-int co_calloc(int, void**)
+int co_calloc(int, void**);
 int co_free(void *ptr);
 int co_memcpy(void *dest, const void *src, size_t n);
