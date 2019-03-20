2b48f9f5bcde24a309542d66d807beadcef62cee
Sergey Litvinov <slitvinov@gmail.com>
2019-03-20 13:08:32 +0100

    add realloc to memory.h

--- lib/co/memory.h
+++ lib/co/memory.h
@@ -1,9 +1,11 @@
 #define MALLOC(n, pp) he_malloc((n)*sizeof(**(pp)), (void**)(pp))
+#define REALLOC(n, pp) he_realloc((n)*sizeoff(**(pp)), (void**)(pp))
 #define CALLOC(n, pp) he_calloc((n)*sizeof(**(pp)), (void**)(pp))
 #define FREE(p) he_free(p)
 #define MEMCPY(n, src, dest) he_memcpy((void*)(dest), (const void*)(src), (n)*sizeof(*(dest)))
 
 int he_malloc(int, void**);
+int he_realloc(int, void**);
 int he_calloc(int, void**);
 int he_free(void *ptr);
 int he_memcpy(void *dest, const void *src, size_t n);
--- lib/memory.c
+++ lib/memory.c
@@ -1,35 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "co/err.h"
 #include "co/memory.h"
 
-int he_malloc(int size, /**/ void **pq) {
-    void *q;
-    q = malloc(size);
-    if (q == NULL) ERR(CO_MEMORY, "malloc failed for size=%ld", size);
-    *pq = q;
-    return CO_OK;
+int he_malloc(int size, /**/ void **pq)
+{
+	void *q;
+	q = malloc(size);
+	if (q == NULL) ERR(CO_MEMORY, "malloc failed for size=%ld", size);
+	*pq = q;
+	return CO_OK;
 }
 
-int he_calloc(int size, /**/ void **pq) {
-    void *q;
-    q = calloc(1, size);
-    if (q == NULL) ERR(CO_MEMORY, "calloc failed for size=%ld", size);
-    *pq = q;
-    return CO_OK;
+int he_realloc(int size, /**/ void **pa)
+{
+	void *a, *b;
+	a = *pa;
+	b = realloc(a, size);
+	if (b == NULL)
+		ERR(CO_MEMORY, "remalloc failed for size=%ld", size);
+	*pa = b;
+	return CO_OK;
 }
 
-int he_free(void *q) {
-    free(q);
-    q = NULL;
-    return CO_OK;
+
+int he_calloc(int size, /**/ void **pq)
+{
+	void *q;
+	q = calloc(1, size);
+	if (q == NULL) ERR(CO_MEMORY, "calloc failed for size=%ld", size);
+	*pq = q;
+	return CO_OK;
 }
 
-int he_memcpy(void *dest, const void *src, size_t n) {
-    if (dest == NULL) ERR(CO_MEMORY, "dest == NULL");
-    if (src == NULL) ERR(CO_MEMORY, "src == NULL");
-    memcpy(dest, src, n);
-    return CO_OK;
+int he_free(void *q)
+{
+	free(q);
+	q = NULL;
+	return CO_OK;
 }
+
+int he_memcpy(void *dest, const void *src, size_t n)
+{
+	if (dest == NULL) ERR(CO_MEMORY, "dest == NULL");
+	if (src == NULL) ERR(CO_MEMORY, "src == NULL");
+	memcpy(dest, src, n);
+	return CO_OK;
+}
+
+
+
