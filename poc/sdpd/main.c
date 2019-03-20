b940cb50d03a146eb69d0224afe5ca0b2b5a7639
Sergey Litvinov <slitvinov@gmail.com>
2019-03-20 11:35:08 +0100

    wip: ilist

--- poc/sdpd/main.c
+++ poc/sdpd/main.c
@@ -5,10 +5,19 @@
 
 #pragma	src	"$HOME/co/lib"
 
+#define T Ilist
+
+typedef struct T T;
+struct T {
+};
+
+int ilist_ini(T**);
+int ilist_fin(T*);
+int ilist_push(T*, int);
+int ilist_size(T*);
+int ilist_head(T*, int*);
+
 int main(void)
 {
-MALLOC(1, fd);
 	printf("hello a_list\n");
 }
-
-
