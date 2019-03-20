68ee073ed1dd3b3e01f337efa6d1ff259608fc5e
Sergey Litvinov <slitvinov@gmail.com>
2019-03-20 13:16:52 +0100

    change memory.h

--- lib/co/memory.h
+++ lib/co/memory.h
@@ -1,11 +1,11 @@
-#define MALLOC(n, pp) he_malloc((n)*sizeof(**(pp)), (void**)(pp))
-#define REALLOC(n, pp) he_realloc((n)*sizeoff(**(pp)), (void**)(pp))
-#define CALLOC(n, pp) he_calloc((n)*sizeof(**(pp)), (void**)(pp))
-#define FREE(p) he_free(p)
-#define MEMCPY(n, src, dest) he_memcpy((void*)(dest), (const void*)(src), (n)*sizeof(*(dest)))
+#define MALLOC(n, pp) co_malloc((n)*sizeof(**(pp)), (void**)(pp))
+#define REALLOC(n, pp) co_realloc((n)*sizeoff(**(pp)), (void**)(pp))
+#define CALLOC(n, pp) co_calloc((n)*sizeof(**(pp)), (void**)(pp))
+#define FREE(p) co_free(p)
+#define MEMCPY(n, src, dest) co_memcpy((void*)(dest), (const void*)(src), (n)*sizeof(*(dest)))
 
-int he_malloc(int, void**);
-int he_realloc(int, void**);
-int he_calloc(int, void**);
-int he_free(void *ptr);
-int he_memcpy(void *dest, const void *src, size_t n);
+int co_malloc(int, void**);
+int co_realloc(int, void**);
+int co_calloc(int, void**)
+int co_free(void *ptr);
+int co_memcpy(void *dest, const void *src, size_t n);
