f5b8be1e34956aead6237a58042f5b4a821f6ee9
Sergey Litvinov <slitvinov@gmail.com>
2019-03-21 12:41:09 +0100

    Initial import of clients for cell lists

---
+++ example/list/i.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#include <real.h>
+#include <co/array.h>
+#include <co/he.h>
+#include <co/y.h>
+
+#define FMT CO_REAL_OUT
+
+int main(void) {
+    real *x, *y, *z;
+    real hi, lo;
+    He *he;
+    int nv;
+
+    y_inif(stdin, &he, &x, &y, &z);
+    nv = he_nv(he);
+    hi = array_max(nv, x);
+    lo = array_min(nv, x);
+
+    printf("number of vertices is %d\n", nv);
+    printf(FMT " " FMT "\n", hi, lo);
+    y_fin(he, x, y, z);
+}
