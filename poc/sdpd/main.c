697d0a93499de8247cc56e839e98bf52b5c2fa69
Sergey Litvinov <slitvinov@gmail.com>
2019-03-28 14:36:24 +0100

    poc: sdpd/main.c uses cell2 from lib/

--- poc/sdpd/main.c
+++ poc/sdpd/main.c
@@ -7,26 +7,18 @@
 
 #include <co/cell2.h>
 
-int
-main(void)
+enum {X, Y};
+static int n;
+static int nx = 20;
+static int ny = 20;
+static real *x, *y;
+real lo[2], hi[2], size;
+
+static int
+ini(void)
 {
-	enum {X, Y};
-	Cell2 *cell;
-	int nx, ny, n;
-	real lo[2], hi[2], size;
-	real *x, *y, x0, y0;
-	int i, j, k, *a;
-
-	lo[X] = -0.5; lo[Y] = 1.0;
-	hi[X] = 2.0; hi[Y] =2.0;
-	size = 0.2;
-
-	nx = ny = 20;
-	n = nx * ny;
-	MALLOC(n, &x);
-	MALLOC(n, &y);
-	cell2_pp_ini(lo, hi, size, &cell);
-
+	real x0, y0;
+	int i, j, k;
 	k = 0;
 	for (i = 0; i < nx; i++)
 		for (j = 0; j < ny; j++) {
@@ -36,6 +28,27 @@ main(void)
 			y[k] = y0;
 			k++;
 	}
+	return CO_OK;
+}
+
+int
+main(void)
+{
+	Cell2 *cell;
+
+	int i, j, k, *a;
+
+	lo[X] = -0.5; lo[Y] = 1.0;
+	hi[X] = 2.0; hi[Y] =2.0;
+	size = 0.2;
+	
+	n = nx * ny;
+	MALLOC(n, &x);
+	MALLOC(n, &y);
+	ini();
+	cell2_pp_ini(lo, hi, size, &cell);
+
+
 	cell2_push(cell, n, x, y);
 	cell2_push(cell, n, x, y);
 
