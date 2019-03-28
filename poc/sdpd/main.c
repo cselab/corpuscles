7cc408a73e484c9deca7d74e9987476bcd7e7fb3
Sergey Litvinov <slitvinov@gmail.com>
2019-03-28 10:47:29 +0100

    wip: sdpd/main.c

--- poc/sdpd/main.c
+++ poc/sdpd/main.c
@@ -7,28 +7,91 @@
 
 #include <co/list/c.h>
 
-#define T Cell
-typedef struct Cell Cell;
-int cell_pp_ini(const real lo[], const real hi[], real size, T**);
-int cell_push(T*, const real[], const real[], const real[], int);
-int cell_head(T*, const real[2],  int**);
-int cell_len(T*, const real[]);
-int cell_reset(T*);
-int cell_fin(T*);
+#define T Cell2
+typedef struct Cell2 Cell2;
+int cell2_pp_ini(const real lo[], const real hi[], real size, T**);
+int cell2_push(T*, const real[], const real[], int);
+int cell2_head(T*, real,  real, int**);
+int cell2_len(T*, real, real);
+int cell2_reset(T*);
+int cell2_fin(T*);
+
+struct T
+{
+	int size;
+	real lo[2], hi[3];
+};
+
+int cell2_pp_ini(const real lo[2], const real hi[3], real size, T **pq)
+{
+	enum {X, Y};
+	int x, y;
+	T *q;
+	real lx, ly;
+	Clist *clist;
+
+	MALLOC(1, &q);
+
+	lx = hi[X] - lo[X];
+	ly = hi[Y] - lo[Y];
+
+	if (lx < 0)
+		ERR(CO_INDEX, "lx < 0");
+	if (ly < 0)
+		ERR(CO_INDEX, "ly < 0");
+
+	x= lx/size;
+	if (x * size < lx) x++;
+	
+	y = ly/size;
+	if (y * size < ly) y++;
+
+	clist_gen_pp(x, y, &clist);
+
+	q->size = size;
+	q->lo[X] = lo[X];
+	q->lo[Y] = lo[Y];
+
+	q->hi[X] = hi[X];
+	q->hi[Y] = hi[Y];
+
+	*pq = q;
+	return CO_OK;
+}
+
+int cell2_fin(T *q)
+{
+	FREE(q);
+	return CO_OK;
+}
 
 int main(void)
 {
-	Clist *clist;
-	int n;
+	enum {X, Y};
+	Cell2 *cell;
+	int nx, ny, n;
+	real lo[2], hi[2], size;
+	real *x, *y, x0, y0;
 
-	n = 3;
-	clist_gen_np(n, n, &clist);
-	clist_push(clist, 0, 100);
-	clist_push(clist, 1, 101);
-	clist_push(clist, 2, 102);
-	clist_push(clist, 3, 103);
-	clist_fwrite(stdout, clist);
-	clist_fin(clist);
+	lo[X] = lo[Y] = 0;
+	hi[X] = hi[Y] = 1;
+	size = 0.2;
 
-	return 0;
+	nx = ny = 5;
+	n = nx * ny;
+	MALLOC(n, &x);
+	MALLOC(n, &y);
+
+	k = 0;
+	for (i = 0; i < nx; i++)
+		for (j = 0; j < ny; j++) {
+			x0 = lo[X] + (hi[X] - lo[X])*(i + 0.5)/nx;
+			y0 = lo[Y] + (hi[Y] - lo[Y])*(j + 0.5)/ny;
+			x[k] = x0;
+			y[k] = y0;
+			k++;
+	}
+
+	cell2_pp_ini(lo, hi, size, &cell);
+	cell2_fin(cell);
 }
