14fee0f3f026f10583cba01455a05f66e0a438a3
Sergey Litvinov <slitvinov@gmail.com>
2019-03-28 13:58:50 +0100

    sdpd: add cell2_wrap

--- poc/sdpd/main.c
+++ poc/sdpd/main.c
@@ -9,22 +9,25 @@
 
 #define T Cell2
 typedef struct Cell2 Cell2;
-int cell2_pp_ini(const real lo[], const real hi[], real size, T**);
-int cell2_push(T*, int, const real[], const real[]);
+int cell2_pp_ini(const real lo2[], const real hi[2], real size, T**);
+int cell2_push(T*, int, const real*, const real*);
 int cell2_parts(T*, real, real, int**);
 int cell2_len(T*, real, real);
+int cell2_wrap(T*, int, real*, real*);
 int cell2_fin(T*);
 
+enum {X, Y};
 struct T
 {
 	int ny;
 	real lo[2], hi[3], size;
 	Clist *clist;
+	int (*wrp)(T*q, real*, real*);
 };
 
-int cell2_pp_ini(const real lo[2], const real hi[3], real size, T **pq)
+static int
+ini(const real lo[2], const real hi[3], real size, int (*gen)(int, int, Clist**), T **pq)
 {
-	enum {X, Y};
 	int nx, ny;
 	T *q;
 	real lx, ly;
@@ -41,7 +44,7 @@ int cell2_pp_ini(const real lo[2], const real hi[3], real size, T **pq)
 	ny = ly/size;
 	if (ny * size < ly) ny++;
 
-	clist_gen_pp(nx, ny, &clist);
+	gen(nx, ny, &clist);
 
 	q->size = size;
 	q->lo[X] = lo[X];
@@ -57,16 +60,50 @@ int cell2_pp_ini(const real lo[2], const real hi[3], real size, T **pq)
 	return CO_OK;
 }
 
-int cell2_fin(T *q)
+
+static
+int wrp_pp(T *q, real *x, real *y)
+{
+	real *lo, *hi;
+	lo = q->lo;
+	hi = q->hi;
+	if (*x > hi[X]) *x -= (hi[X] - lo[X]);
+	if (*x < lo[X]) *x += (hi[X] - lo[X]);
+
+	if (*y > hi[Y]) *y -= (hi[Y] - lo[Y]);
+	if (*y < lo[Y]) *y += (hi[Y] - lo[Y]);
+	return CO_OK;
+}
+
+int
+cell2_pp_ini(const real lo[2], const real hi[3], real size, T **pq)
+{
+	int status;
+	status = ini(lo, hi, size, clist_gen_pp, pq);
+	(*pq)->wrp = wrp_pp;
+	return status;
+}
+
+int
+cell2_nn_ini(const real lo[2], const real hi[3], real size, T **pq)
+{
+	int status;
+	status = ini(lo, hi, size, clist_gen_nn, pq);
+	(*pq)->wrp = NULL;
+	return status;
+}
+
+int
+cell2_fin(T *q)
 {
 	clist_fin(q->clist);
 	FREE(q);
 	return CO_OK;
 }
 
-static int map(T *q, real x, real y, int *i, int *j)
+static int
+map(T *q, real x, real y, int *i, int *j)
 {
-	enum {X, Y};
 	real *lo, *hi, size;
 	
 	size = q->size;
@@ -82,7 +119,8 @@ static int map(T *q, real x, real y, int *i, int *j)
 	return CO_OK;
 }
 
-int cell2_push(T *q, int n, const real *x, const real *y)
+int
+cell2_push(T *q, int n, const real *x, const real *y)
 {
 	int i, j, k;
 	int ny;
@@ -96,7 +134,8 @@ int cell2_push(T *q, int n, const real *x, const real *y)
 	return CO_OK;
 }
 
-int cell2_parts(T *q, real x, real y, int **a)
+int
+cell2_parts(T *q, real x, real y, int **a)
 {
 	int i, j, ny;
 	ny = q->ny;
@@ -104,7 +143,28 @@ int cell2_parts(T *q, real x, real y, int **a)
 	return clist_parts(q->clist, i*ny + j, a);
 }
 
-int main(void)
+int
+cell2_len(T *q, real x, real y)
+{
+	int i, j, ny;
+	ny = q->ny;
+	map(q, x, y, &i, &j);
+	return clist_len(q->clist, i*ny + j);
+}
+
+int
+cell2_wrap(T *q, int n, real *x, real *y)
+{
+	int i;
+	if (q->wrp == NULL)
+		return CO_OK;
+	for (i = 0; i < n; i++)
+		q->wrp(q, &x[i], &y[i]);
+	return CO_OK;
+}
+
+int
+main(void)
 {
 	enum {X, Y};
 	Cell2 *cell;
@@ -113,11 +173,11 @@ int main(void)
 	real *x, *y, x0, y0;
 	int i, j, k, *a;
 
-	lo[X] = lo[Y] = 0;
-	hi[X] = hi[Y] = 1;
+	lo[X] = lo[Y] = -0.5;
+	hi[X] = hi[Y] = 0.5;
 	size = 0.2;
 
-	nx = ny = 5;
+	nx = ny = 10;
 	n = nx * ny;
 	MALLOC(n, &x);
 	MALLOC(n, &y);
@@ -135,13 +195,15 @@ int main(void)
 	cell2_push(cell, n, x, y);
 	cell2_push(cell, n, x, y);
 
-	for (i = 0; i < n; i++) {
+	for (i = 0; i < 2; i++) {
 		cell2_parts(cell, x[i], y[i], &a);
 		while ( (j = *a++) != -1) {
 			if (j == i) continue;
-			printf("%d %d\n", i, j);
+			printf("%g %g %g %g\n", x[i], y[i], x[j]  - x[i], y[j] - y[i]);
 		}
 	}
-
 	cell2_fin(cell);
+
+	FREE(x);
+	FREE(y);
 }
