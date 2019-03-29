5607e65736095b4340c15f917edaa158293474e1
Sergey Litvinov <slitvinov@gmail.com>
2019-03-29 10:48:38 +0100

    initial import of quintic, wendland, and yang kernels

---
+++ lib/kernel.c
@@ -0,0 +1,12 @@
+#include <math.h>
+#include <stdio.h>
+
+#include "tri/err.h"
+#include "tri/memory.h"
+#include "tri/kernel.h"
+
+#define pi (3.141592653589793)
+#include "kernel/quintic.h"
+#include "kernel/wendland6.h"
+#include "kernel/yang.h"
+#include "kernel/main.h"
