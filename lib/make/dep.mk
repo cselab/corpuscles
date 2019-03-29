893f1b46e1cc765d793512cecb977153c9088a26
Sergey Litvinov <slitvinov@gmail.com>
2019-03-29 10:52:29 +0100

    upd dep.mk

--- lib/make/dep.mk
+++ lib/make/dep.mk
@@ -2,3 +2,8 @@ real.s.o: inc/s/real.inc
 real.d.o: inc/d/real.inc
 
 constant_strain/2d.c: constant_strain/2d.inc
+kernal.c: \
+kernal/quintic.inc\
+kernal/wendland6.inc\
+kernel/yang.inc\
+kernal/main.inc
