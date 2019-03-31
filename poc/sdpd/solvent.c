0527ca5eb9adf3fa9758b04f24c8c81890681905
Sergey Litvinov <slitvinov@gmail.com>
2019-03-31 20:04:10 +0200

    run morris

--- poc/sdpd/solvent.c
+++ poc/sdpd/solvent.c
@@ -28,23 +28,18 @@
 		r = sqrt(rsq);
 #define END } }
 
-enum {
-	X, Y};
+enum {X, Y};
 static int n;
 #define nx  (20)
 #define ny  (20)
 static const real c = 10.0;
 static const real mu = 1.0;
-static const real size = 2.0/nx;
-static const real g[] = {5, 5};
+static const real size = 1.5/nx;
+static const real g = 0.0;
 static const real mass = 1.0/(nx*ny);
-static const real R = 0.2;
-static const real dt = 0.00025;
 static real lo[2], hi[2];
 static AlgRng *rng;
 static Kernel *kernel;
-static real *x, *y, *vx, *vy, *fx, *fy, *rho, *p;
-static Cell2 *cell;
 
 static int
 grid(real *x, real *y)
@@ -62,7 +57,7 @@ grid(real *x, real *y)
 			x[k] = x0 + alg_rng_uniform(rng, a, b);
 			y[k] = y0 + alg_rng_uniform(rng, a, b);
 			k++;
-		}
+	}
 	return CO_OK;
 }
 
@@ -96,31 +91,11 @@ euler_step(real dt, int n, const real *vx, const real *vy, real *x, real *y)
 }
 
 static int
-euler_step_fun(real dt, int (*f)(real, real), int n, const real *vx, const real *vy, real *xx, real *yy)
+body_force(int n, const real *x, __UNUSED const real *y, real *fx, __UNUSED real *fy)
 {
 	int i;
-	for (i = 0; i < n; i++) {
-		if (f(x[i], y[i])) continue;
-		xx[i] += dt*vx[i];
-		yy[i] += dt*vy[i];
-	}
-	return CO_OK;
-}
-
-static int
-circle(real x, real y)
-{
-	return x*x + y*y < R*R;
-}
-
-static int
-body_force(int n, __UNUSED const real *x, __UNUSED const real *y, real *fx,  real *fy)
-{
-	int i;
-	for (i = 0; i < n; i++) {
-		fx[i] += g[X];
-		fy[i] += g[Y];
-	}
+	for (i = 0; i < n; i++)
+		fx[i] += y[i] > 0 ? g : -g;
 	return CO_OK;
 }
 
@@ -130,73 +105,24 @@ eq_state(real rho)
 	return c*c*rho;
 }
 
-static int
-force(void)
-{
-	int i, j, k, t, *a;
-	real xi, yi, xj, yj, xr, yr, rsq, r, w, dw;
-	real coeff;
-
-	array_zero(n, fx);
-	array_zero(n, fy);
-	array_zero(n, rho);
-	body_force(n, x, y, fx, fy);
-	cell2_wrap(cell, n, x, y);
-	cell2_push(cell, n, x, y);
-	BEGIN {
-		w = kernel_w(kernel, size, r);
-		rho[i] += mass*w;
-	} END
-	
-	 for (i = 0; i < n; i++)
-		p[i] = eq_state(rho[i]);
-
-	BEGIN {
-		if (j == i) continue;
-		dw = kernel_dw(kernel, size, r);
-		coeff = p[i]/(rho[i]*rho[i]) + p[j]/(rho[j]*rho[j]);
-		coeff *= mass*dw/r;
-		fx[i]  -= coeff * (xi - xj);
-		fy[i]  -= coeff * (yi - yj);
-
-		coeff = 1/(rho[i]*rho[j]);
-		coeff *= 2*mass*mu*dw/r;
-		fx[i]  += coeff * (vx[i] - vx[j]);
-		fy[i]  += coeff * (vy[i] - vy[j]);
-	} END	
-	return CO_OK;
-}
-
-int
-dump(int t)
-{
-	static int First = 1;
-	if (t % 100 == 0) {
-		if (First) First = 0;
-		else printf("\n");
-		const real *q[] = {
-			x, y, vx, vy, rho, NULL
-		};
-		punto_fwrite(n, q, stdout);
-		MSG("rho[0] = %g", rho[0]);
-	}
-	return CO_OK;
-}
-
 int
 main(void)
 {
-	int t;
+	int First;
+	real *x, *y, *vx, *vy, *fx, *fy, *rho, *p;
+	Cell2 *cell;
+
+	int i, j, k, t, *a;
+	real xi, yi, xj, yj, xr, yr, rsq, r, w, dw;
+	real dt, coeff;
 
 	alg_rng_ini(&rng);
 	kernel_ini(KERNEL_2D, KERNEL_YANG, &kernel);
 
-	lo[X] = -0.5; 
-	hi[X] = 0.5;
-	lo[Y] = -0.5; 
-	hi[Y] =0.5;
+	lo[X] = -0.5; hi[X] = 0.5;
+	lo[Y] = -0.5; hi[Y] =0.5;
 	n = nx * ny;
-
+		
 	MALLOC(n, &x);
 	MALLOC(n, &y);
 	CALLOC(n, &vx);
@@ -208,20 +134,44 @@ main(void)
 	ini(x, y);
 	cell2_pp_ini(lo, hi, size, &cell);
 
+	dt = 0.0002; First = 1;
 	for (t = 0; t < 1000; t ++) {
-		force();
-		euler_step(dt,  n, vx, vy, x, y);
-		euler_step(dt,  n, fx, fy, vx, vy);
-		dump(t);
-	}
+		array_zero(n, fx);
+		array_zero(n, fy);
+		array_zero(n, rho);
+		body_force(n, x, y, fx, fy);
+		cell2_wrap(cell, n, x, y);
+		cell2_push(cell, n, x, y);
+		BEGIN {
+			w = kernel_w(kernel, size, r);
+			rho[i] += mass*w;
+		} END
+		
+		for (i = 0; i < n; i++)
+			p[i] = eq_state(rho[i]);
 
-	array_zero(n, vx);
-	array_zero(n, vy);
-	for (/**/; t < 20000; t ++) {
-		force();
-		euler_step_fun(dt, circle, n, vx, vy, x, y);
-		euler_step_fun(dt, circle, n, fx, fy, vx, vy);
-		dump(t);
+		BEGIN {
+			if (j == i) continue;
+			dw = kernel_dw(kernel, size, r);
+			coeff = p[i]/(rho[i]*rho[i]) + p[j]/(rho[j]*rho[j]);
+			coeff *= mass*dw/r;
+			fx[i]  -= coeff * (xi - xj);
+			fy[i]  -= coeff * (yi - yj);
+
+			coeff = 1/(rho[i]*rho[j]);
+			coeff *= 2*mass*mu*dw/r;
+			fx[i]  += coeff * (vx[i] - vx[j]);
+			fy[i]  += coeff * (vy[i] - vy[j]);
+		} END
+
+		euler_step(dt, n, vx, vy, x, y);
+		euler_step(dt, n, fx, fy, vx, vy);
+		if (t % 100 == 0) {
+			if (First) First = 0; else printf("\n");		
+			const real *q[] = {x, y, rho, NULL};
+			punto_fwrite(n, q, stdout);
+			MSG("rho[0] = %g", rho[0]);
+		}
 	}
 
 	cell2_fin(cell);
