d108ff49275ad277e62b697bc4fe3c2f0ba33424
Sergey Litvinov <slitvinov@gmail.com>
2019-04-01 18:12:24 +0200

    add poc/predicates

---
+++ poc/predicates/predicates.h
@@ -0,0 +1,4 @@
+double orient2d(double*, double*, double*);
+double orient3d(double*, double*, double*, double*);
+double incircle(double*, double*, double*, double*);
+double  insphere(double*, double*, double*, double*, double*);
