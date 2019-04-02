f383ac66804835943c73d99d083f4501350bd85a
Sergey Litvinov <slitvinov@gmail.com>
2019-04-02 10:21:55 +0200

    add predict client

--- lib/predicate.c
+++ lib/predicate.c
@@ -4,29 +4,37 @@
 
 #include "predicate/main.inc"
 
-enum {X, Y, Z};
+enum {
+	X, Y, Z
+};
 
-#define CP(q, Q) \
+#define C3(q, Q) \
 	do { \
 		Q[X] = q[X]; \
 		Q[Y] = q[Y]; \
 		Q[Z] = q[Z]; \
 	} while (0)
 
+#define C2(q, Q) \
+	do { \
+		Q[X] = q[X]; \
+		Q[Y] = q[Y]; \
+	} while (0)
+
 int
 predicate_ini(void)
 {
-	
+	exactinit();
 	return CO_OK;
 }
 
 real
 predicate_orient2d(real *a, real *b, real *c)
 {
-	REAL A[3], B[3], C[3];
-	CP(a, A);
-	CP(b, B);
-	CP(c, C);
+	REAL A[2], B[2], C[2];
+	C2(a, A);
+	C2(b, B);
+	C2(c, C);
 	return orient2d(A, B, C);
 }
 
@@ -35,33 +43,33 @@ real
 predicate_orient3d(real *a, real *b, real *c, real *d)
 {
 	REAL A[3], B[3], C[3], D[3];
-	CP(a, A);
-	CP(b, B);
-	CP(c, C);
-	CP(d, D);
+	C3(a, A);
+	C3(b, B);
+	C3(c, C);
+	C3(d, D);
 	return orient3d(A, B, C, D);
 }
 
 real
 predicate_incircle(real *a, real *b, real *c, real *d)
 {
-	REAL A[3], B[3], C[3], D[3];
-	CP(a, A);
-	CP(b, B);
-	CP(c, C);
-	CP(d, D);
-	return incircle(A, B, C, D);	
+	REAL A[2], B[2], C[2], D[2];
+	C2(a, A);
+	C2(b, B);
+	C2(c, C);
+	C2(d, D);
+	return incircle(A, B, C, D);
 }
 
 real
 predicate_insphere(real *a, real *b, real *c, real *d, real *e)
 {
 	REAL A[3], B[3], C[3], D[3], E[3];
-	CP(a, A);
-	CP(b, B);
-	CP(c, C);
-	CP(d, D);
-	CP(e, E);
-	return insphere(A, B, C, D, E);	
+	C3(a, A);
+	C3(b, B);
+	C3(c, C);
+	C3(d, D);
+	C3(e, E);
+	return insphere(A, B, C, D, E);
 }
 
