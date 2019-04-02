671a310a13a0ad88586bd47989dff47c12038214
Sergey Litvinov <slitvinov@gmail.com>
2019-04-02 12:55:26 +0200

    finish a client for ray

--- example/predicate/ray.c
+++ example/predicate/ray.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <real.h>
+
 #include <co/predicate.h>
+#include <co/vec.h>
 
 #define FMT CO_REAL_OUT
 
@@ -10,16 +12,25 @@ enum
 };
 
 int
-main(int argc, char **argv)
+main(int argc, const char *v[])
 {
-	real a[3], b[3], c[3], x;
+	real a[3], b[3], c[3], d[3], e[3], x;
 	predicate_ini();
 
-	a[X] = a[Y] = 0;
-	b[X] = b[Y] = 1;
-	c[X] = 1;
-	c[Y] = 0.9;
-	x = predicate_orient2d(a, b, c);
-	printf(FMT "\n", x);
+	v++;
+	vec_argv(&v, a);
+	vec_argv(&v, b);
+	vec_argv(&v, c);
+	vec_argv(&v, d);
+	vec_argv(&v, e);
+
+	printf("%d\n", predicate_ray(d, e, a, b, c));
 	return 0;
 }
+
+
+/*
+
+./ray 0 0 0    1 0 0    0 1 0      0.5 0.3 1    0.5 0.2 1
+
+*/
