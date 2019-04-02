2b2e8989c86f0404a76dde2feeba79444a0f61c3
Sergey Litvinov <slitvinov@gmail.com>
2019-04-02 11:05:11 +0200

    add orient_sas

--- lib/predicate.c
+++ lib/predicate.c
@@ -73,3 +73,127 @@ predicate_insphere(real *a, real *b, real *c, real *d, real *e)
 	return insphere(A, B, C, D, E);
 }
 
+#define SIGN(x) ((x) > 0 ? 1 : -1)
+#define ORIENT1D(a,b) ((a) > (b) ? 1 : (a) < (b) ? -1 : 0)
+int
+predicate_orient3d_sas(real a[3], real b[3], real c[3], real d[3])
+{
+	/* TODO: points should be sorted */
+	real A[2], B[2], C[2], o;
+	int sign;
+
+	o = predicate_orient3d(a, b, c, d);
+	if (o != 0)
+		return SIGN(o);
+	else {
+		/* epsilon^1/8 */
+		A[X] = b[X];
+		A[Y] = b[Y];
+		B[X] = c[X];
+		B[Y] = c[Y];
+		C[X] = d[X];
+		C[Y] = d[Y];
+		o = predicate_orient2d(A, B, C);
+		if (o != 0)
+			return SIGN (o)*sign;
+
+		/* epsilon^1/4 */
+		A[X] = b[X];
+		A[Y] = b[Z];
+		B[X] = c[X];
+		B[Y] = c[Z];
+		C[X] = d[X];
+		C[Y] = d[Z];
+		o = predicate_orient2d(A, B, C);
+		if (o != 0)
+			return - SIGN (o)*sign;
+
+		/* epsilon^1/2 */
+		A[X] = b[Y];
+		A[Y] = b[Z];
+		B[X] = c[Y];
+		B[Y] = c[Z];
+		C[X] = d[Y];
+		C[Y] = d[Z];
+		o = predicate_orient2d(A, B, C);
+		if (o != 0)
+			return SIGN (o)*sign;
+
+		/* epsilon */
+		A[X] = a[X];
+		A[Y] = a[Y];
+		B[X] = c[X];
+		B[Y] = c[Y];
+		C[X] = d[X];
+		C[Y] = d[Y];
+		o = predicate_orient2d(A, B, C);
+		if (o != 0)
+			return - SIGN (o)*sign;
+
+		/* epsilon^5/4 */
+		o = ORIENT1D (c[X], d[X]);
+		if (o != 0)
+			return SIGN (o)*sign;
+
+		/* epsilon^3/2 */
+		o = ORIENT1D (c[Y], d[Y]);
+		if (o != 0)
+			return - SIGN (o)*sign;
+
+		/* epsilon^2 */
+		A[X] = a[X];
+		A[Y] = a[Z];
+		B[X] = c[X];
+		B[Y] = c[Z];
+		C[X] = d[X];
+		C[Y] = d[Z];
+		o = predicate_orient2d(A, B, C);
+		if (o != 0)
+			return SIGN (o)*sign;
+
+		/* epsilon^5/2 */
+		o = ORIENT1D (c[Z], d[Z]);
+		if (o != 0)
+			return SIGN (o)*sign;
+
+		/* epsilon^4 */
+		A[X] = a[Y];
+		A[Y] = a[Z];
+		B[X] = c[Y];
+		B[Y] = c[Z];
+		C[X] = d[Y];
+		C[Y] = d[Z];
+		o = predicate_orient2d(A, B, C);
+		if (o != 0)
+			return - SIGN (o)*sign;
+
+		/* epsilon^8 */
+		A[X] = a[X];
+		A[Y] = a[Y];
+		B[X] = b[X];
+		B[Y] = b[Y];
+		C[X] = d[X];
+		C[Y] = d[Y];
+		o = predicate_orient2d(A, B, C);
+		if (o != 0)
+			return SIGN (o)*sign;
+
+		/* epsilon^33/4 */
+		o = ORIENT1D (b[X], d[X]);
+		if (o != 0)
+			return - SIGN (o)*sign;
+
+		/* epsilon^17/2 */
+		o = ORIENT1D (b[Y], d[Y]);
+		if (o != 0)
+			return SIGN (o)*sign;
+
+		/* epsilon^10 */
+		o = ORIENT1D (a[X], d[X]);
+		if (o != 0)
+			return SIGN (o)*sign;
+
+		/* epsilon^21/2 */
+		return sign;
+	}
+}
