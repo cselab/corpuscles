29aefb77aa5e6e2dd19bf5bfa8fe75d9bbc15614
Sergey Litvinov <slitvinov@gmail.com>
2019-04-10 15:55:59 +0200

    initial import of cons2

---
+++ example/integral/cons2.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <tgmath.h>
+#include <real.h>
+
+#include <co/arg.h>
+#include <co/err.h>
+#include <co/integral/sph_plane.h>
+
+#define FMT CO_REAL_OUT
+
+const real R = 1.0;
+
+static real
+sq(real x)
+{
+	return x*x;
+}
+
+static real
+wc(real r)
+{
+	return 1 - r/R;
+}
+
+static real
+E(real x, real y, real z, void *p)
+{
+	real r;
+	r = sqrt(sq(x) + sq(y) + sq(z));
+	return -wc(r)*z/r;
+}
+
+char *argv0;
+int
+main(int argc, char **argv)
+{
+	real d, lo, hi, res;
+	int i, n;
+	SphPlane *integ;
+	sph_plane_ini(R, &integ);
+
+	n = 200;
+	lo = 0; 
+	hi = 1;
+	for (i = 0; i < n; i++) {
+		d = lo + (hi - lo)/(n - 1)*i;
+		sph_plane_apply(integ, d, E, NULL, &res);
+		printf(FMT "\n", res);
+	}
+	sph_plane_fin(integ);
+}
