8718860818e5c44b36e95dfedd672d113daeb92f
Sergey Litvinov <slitvinov@gmail.com>
2019-04-11 10:30:08 +0200

    visc2 uses library

--- example/integral/visc2.c
+++ example/integral/visc2.c
@@ -3,14 +3,12 @@
 #include <tgmath.h>
 
 #include <real.h>
-#include <co/arg.h>
 #include <co/err.h>
 #include <co/memory.h>
 #include <co/macro.h>
 #include <co/vec.h>
-#include <co/integral/sph_plane.h>
-#include <alg/spline.h>
 #include <co/kernel.h>
+#include <co/pre/visc.h>
 
 #define FMT CO_REAL_OUT
 
@@ -20,130 +18,6 @@
 	r, normal, point return Force
 
 */
-
-#define T PreVisc
-static const int n = 20;
-static const int type =  STEFFEN;
-static const real eps = 1e-8;
-
-typedef struct T T;
-struct T
-{
-	real R;
-	AlgSpline *s;
-};
-
-typedef struct Eparam Eparam;
-struct Eparam
-{
-	real (*F)(real, void*);
-	void *param;
-	real beta, R, d;
-};
-
-static real
-E(real x, real y, real z, void *param)
-{
-	real beta, R, r, d;
-	Eparam *p;
-	p = param;
-	beta = p->beta;
-	R = p->R;
-	r = sqrt(x*x + y*y + z*z);
-	d = d/R < beta ? R*beta : d;
-	return p->F(r, p->param)*(1 - z/d);
-}
-
-int
-pre_visc_ini(real R, real beta, real (*F)(real, void*), void *param, T  **pq)
-{
-	T *q;
-	real *x, *y, res, volume, d;
-	int i;
-	Eparam p;
-
-	MALLOC(1, &q);
-	SphPlane *integ;
-	sph_plane_ini(R, &integ);
-	MALLOC(n, &x);
-	MALLOC(n, &y);
-	p.F = F;
-	p.param = param;
-	p.beta = beta;
-	p.R = R;
-	for (i = 0; i < n; i++) {
-		d = R/(n - 1)*i;
-		sph_plane_apply(integ, d, E, &p, &res);
-		sph_plane_volume(integ, d, &volume);
-		x[i] = d;
-		if (volume > eps)
-			y[i] = res/volume;
-		else
-			y[i] = 0;
-		MSG(FMT " " FMT, x[i], y[i]);
-	}
-	alg_spline_ini(n, x, y, type, &q->s);
-	sph_plane_fin(integ);
-	FREE(x);
-	FREE(y);
-
-	q->R = R;
-	*pq = q;
-	return CO_OK;
-}
-
-typedef struct Fparam Fparam;
-struct Fparam
-{
-	Kernel *k;
-	real size;
-};
-static real
-F(real r, void *param)
-{
-	Fparam *p;
-	p = param;
-	return kernel_dwr(p->k, p->size, r);
-}
-
-int
-pre_visc_kernel_ini(real R, real beta, Kernel *kernel, T **pq)
-{
-	Fparam fparam;
-	fparam.k = kernel;
-	fparam.size = R;
-	return pre_visc_ini(R, beta, F, &fparam, pq);
-}
-
-int
-pre_visc_apply(T *q, real r[3], real point[3], real n[3], /**/ real f[3])
-{
-	real p[3], d, f0;
-
-	if (fabs(vec_abs(n) - 1) > eps)
-		ERR(CO_NUM, "vec_abs(n) != 1");
-
-	vec_minus(r, point, p);
-	d = vec_project_scalar(p, n);
-	MSG("d: " FMT " " FMT, d, q->R);
-	if (d < 0)
-		d = 0;
-	if (d > q->R)
-		f0 = 0;
-	else
-		f0 = alg_spline_apply(q->s, d);
-	MSG("f0: " FMT, f0);
-	vec_scalar(n, f0, f);
-	return CO_OK;
-}
-
-int
-pre_visc_fin(T *q)
-{
-	alg_spline_fin(q->s);
-	FREE(q);
-	return CO_OK;
-}
 
 int
 main(void)
