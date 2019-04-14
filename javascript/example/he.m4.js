4bb04ea09eb760ba1e8ea85e2ea797146acc2eaa
Sergey Litvinov <slitvinov@gmail.com>
2019-04-14 15:19:14 +0200

    wip: he

--- javascript/example/he.m4.js
+++ javascript/example/he.m4.js
@@ -1,13 +1,25 @@
 include(`mh.m4')dnl
 #!/usr/bin/env node
+'use strict';
 mh_include(off.js)dnl
 mh_include(he.js)dnl
 
+
 function he_ini(f)
 {
-	var off, nv, nt, ne, nh, tt
+	var off, nv, nt, tt
+	off = off_read(f)
+	nv = off_nv(off)
+	nt = off_nt(off)
+	tt  = off_tri(off)
+	return he_ini0(nv, nt, tt)
+}
+
+function he_ini0(nv, nt, tt)
+{
+	var nv, nt, ne, nh, tt
 	var hdg
-	var hdg_ver, hdg_tri, nxt, flp, ver, tri, edg
+	var hdg_ver, hdg_edg, hdg_tri, nxt, flp, ver, tri, edg
 	var t, e, i, j, k, u, v, w, h, f
 
 	function g(i, j)
@@ -33,11 +45,6 @@ function he_ini(f)
 		} else
 			flp[h] = -1
 	}
-
-	off = off_read(f)
-	nv = off_nv(off)
-	nt = off_nt(off)
-	tt  = off_tri(off)
 
 	nh = 3*nt
 	hdg_ver = new Array(nv)
@@ -89,6 +96,197 @@ function he_ini(f)
 		    'hdg_ver': hdg_ver, 'hdg_edg': hdg_edg, 'hdg_tri': hdg_tri}
 }
 
-var he
+function he_nv(he)
+{
+	if (!('hdg_ver' in he))
+		throw new Error("not a he object")
+	return he.hdg_ver.length
+}
+
+function he_nt(he)
+{
+	if (!('hdg_tri' in he))
+		throw new Error("not a he object")
+	return he.hdg_tri.length
+}
+
+function he_ne(he)
+{
+	if (!('hdg_edg' in he))
+		throw new Error("not a he object")
+	return he.hdg_edg.length
+}
+
+function he_nh(he)
+{
+	if (!('ver' in he))
+		throw new Error("not a he object")
+	return he.ver.length
+}
+
+function he_tri(he, h)
+{
+	var nh
+	if (!('tri' in he))
+		throw new Error("not a he object")
+	nh = he_nh(he)
+	if (!(h in he.tri))
+		throw new Error("h = " + h+ " nh = " + nh)
+	return he.tri[h]
+}
+
+function he_hdg_tri(he, t)
+{
+	var nt
+	if (!('hdg_tri' in he))
+		throw new Error("not a he object")
+	nt = he_nt(he)
+	if (!(t in he.hdg_tri))
+		throw new Error("t = " + t + " nt = " + nt)
+	return he.hdg_tri[t]
+}
+
+function he_hdg_edg(he, e)
+{
+	var ne
+	if (!('hdg_edg' in he))
+		throw new Error("not a he object")
+	ne = he_ne(he)
+	if (!(e in he.hdg_edg))
+		throw new Error("e = " + e + " ne = " + ne)
+	return he.hdg_edg[e]
+}
+
+function he_nxt(he, h)
+{
+	var nh
+	if (!('nxt' in he))
+		throw new Error("not a he object")
+	nh = he_nh(he)
+	if (!(h in he.nxt))
+		throw new Error("h = " + h + " nh = " + nh)
+	return he.nxt[h]
+}
+
+function he_ver(he, h)
+{
+	var nh
+	if (!('ver' in he))
+		throw new Error("not a he object")
+	nh = he_nh(he)
+	if (!(h in he.ver))
+		throw new Error("h = " + h + " nh = " + nh)
+	return he.ver[h]
+}
+
+function he_edg(he, h)
+{
+	var nh
+	if (!('edg' in he))
+		throw new Error("not a he object")
+	nh = he_nh(he)
+	if (!(h in he.edg))
+		throw new Error("h = " + h + " nh = " + nh)
+	return he.edg[h]
+}
+
+function he_tri_ijk(he, t)
+{
+	function hdg_tri(t)
+	{
+		return he_hdg_tri(he, t)
+	}
+	function nxt(h)
+	{
+		return he_nxt(he, h)
+	}
+	function ver(h)
+	{
+		return he_ver(he, h)
+	}
+	var u, v, w, i, j, k
+	u = hdg_tri(t)
+	v = nxt(u)
+	w = nxt(v)
+	i = ver(u)
+	j = ver(v)
+	k = ver(w)
+	return [i, j, k]
+}
+
+function rank(he, ans)
+{
+	function hdg_tri(t)
+	{
+		return he_hdg_tri(he, t)
+	}
+	function nxt(h)
+	{
+		return he_nxt(he, h)
+	}
+	function ver(h)
+	{
+		return he_ver(he, h)
+	}
+
+	var nt, t, u, v, w, i, j, k
+	nt = he_nt(he)
+	nv = he_nv(he)
+	for (i = 0; i < nv; i++)
+		ans[i] = 0
+
+	for (t = 0; t < nt; t++) {
+		u = hdg_tri(t)
+		v = nxt(u)
+		w = nxt(v)
+		i = ver(u)
+		j = ver(v)
+		k = ver(w)
+		ans[i] += 1
+		ans[j] += 1
+		ans[k] += 1
+	}
+}
+
+function rank2(he, ans)
+{
+	function hdg_edg(e)
+	{
+		return he_hdg_edg(he, e)
+	}
+	function nxt(h)
+	{
+		return he_nxt(he, h)
+	}
+	function ver(h)
+	{
+		return he_ver(he, h)
+	}
+
+	var ne, e, u, v, i, j
+	ne = he_ne(he)
+	nv = he_nv(he)
+	for (i = 0; i < nv; i++)
+		ans[i] = 0
+
+	for (e = 0; e < ne; e++) {
+		u = hdg_edg(e)
+		v = nxt(u)
+		i = ver(u)
+		j = ver(v)
+		ans[i] += 1
+		ans[j] += 1
+	}
+}
+
+var he, r, nv, i
 he = he_ini("/dev/stdin")
-console.log(he)
+nv = he_nv(he)
+
+r = new Array(nv)
+rank2(he, r)
+
+//for (i = 0; i < nv; i++)
+//	console.log(r[i])
+
+console.log(he_tri_ijk(he, 10))
