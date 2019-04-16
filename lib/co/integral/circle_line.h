489e8f94eda576ee2d075a527877822dbab808d0
Sergey Litvinov <slitvinov@gmail.com>
2019-04-16 09:05:41 +0200

    minor

--- lib/co/integral/circle_line.h
+++ lib/co/integral/circle_line.h
@@ -3,7 +3,7 @@
 typedef struct T T;
 int circle_line_ini(real d, T**);
 int circle_line_fin(T*);
-int circle_line_apply(T*, real r, real (*)(real x, real y, real z, void*), void*, /**/ real*);
+int circle_line_apply(T*, real r, real (*)(real x, real y, void*), void*, /**/ real*);
 int circle_line_volume(T*, real d, /**/ real*);
 
 #undef T
