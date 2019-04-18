0a3cc51cf0c994923ea1e187ef2b69308df04364
Sergey Litvinov <slitvinov@gmail.com>
2019-04-18 17:41:28 +0200

    minor

--- poc/javascript/matrix.js
+++ poc/javascript/matrix.js
@@ -48,9 +48,9 @@ function matrix_create()
 function matrix_translate(out, a, v) {
 	var x = v[0], y = v[1], z = v[2]
 
-	out[12] = a[0] * x + a[4] * y + a[8] * z + a[12]
-	out[13] = a[1] * x + a[5] * y + a[9] * z + a[13]
-	out[14] = a[2] * x + a[6] * y + a[10] * z + a[14]
-	out[15] = a[3] * x + a[7] * y + a[11] * z + a[15]
+	out[12] = a[0]*x + a[4]*y + a[8]*z + a[12]
+	out[13] = a[1]*x + a[5]*y + a[9]*z + a[13]
+	out[14] = a[2]*x + a[6]*y + a[10]*z + a[14]
+	out[15] = a[3]*x + a[7]*y + a[11]*z + a[15]
 	return out
 }
