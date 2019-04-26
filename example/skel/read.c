94d51fc9e8f502a2d89144af66bb0cfdd182f113
Sergey Litvinov <slitvinov@gmail.com>
2019-04-26 10:34:12 +0200

    wip: skeleton

--- example/skel/read.c
+++ example/skel/read.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <tgmath.h>
-#include <real.h>
 
+#include <real.h>
 #include <co/arc.h>
 #include <co/err.h>
+#include <co/memory.h>
+#include <real.h>
 
 #define T Skel
 typedef struct T T;
@@ -19,6 +21,61 @@ int skel_ne(T*);
 int skel_bnd(T*, int);
 int skel_edg_ij(T*, int, /**/  int*, int*);
 #undef T
+
+#define T Skel
+struct T
+{
+	int nv, Close;
+};
+
+int
+skel_close_ini(int nv, T **pq)
+{
+	T *q;
+	
+	if (nv < 0)
+		ERR(CO_INDEX, "nv=%d < 0", nv);
+	MALLOC(1, &q);
+	q->nv = nv;
+	q->Close = 1;
+	*pq = q;
+	return CO_OK;
+}
+
+int
+skel_open_ini(int nv, T **pq)
+{
+	T *q;	
+	if (nv < 0)
+		ERR(CO_INDEX, "nv=%d < 0", nv);
+	MALLOC(1, &q);
+	q->nv = nv;
+	q->Close = 0;
+	*pq = q;
+	return CO_OK;
+}
+
+int
+skel_fin(T *q)
+{
+	FREE(q);
+	return CO_OK;
+}
+
+int
+skel_xy_fin(real *x, real *y, T *q)
+{
+	FREE(x);
+	FREE(y);
+	FREE(q);
+	return CO_OK;
+}
+
+int
+skel_write(T *q, const real *x, const real *y, FILE *f)
+{
+	 
+}
 
 #define PI (3.141592653589793)
 int
