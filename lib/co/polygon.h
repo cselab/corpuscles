21ef385fc1025de5114d65cc6d856fab6c8d0735
Sergey Litvinov <slitvinov@gmail.com>
2019-04-30 11:11:44 +0200

    minor

--- lib/co/polygon.h
+++ lib/co/polygon.h
@@ -6,7 +6,7 @@ typedef struct Skel Skel;
 int polygon_ini(real lo, real hi, real size, T**);
 int polygon_fin(T*);
 int polygon_update(T*, Skel*, const real *x, const real *y);
-int polygon_inside(T*, real, rea);
+int polygon_inside(T*, real, real);
 int polygon_inside_fast(T*, real, real);
 
 #undef T
