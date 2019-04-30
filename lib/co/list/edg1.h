7fcf51f8bd874998361e9fcefcc47bee52307e55
Sergey Litvinov <slitvinov@gmail.com>
2019-04-30 09:54:02 +0200

    initial import of edg1

---
+++ lib/co/list/edg1.h
@@ -0,0 +1,10 @@
+#define T Edg1List
+
+typedef struct T T;
+typedef struct Skel Skel;
+int edg1list_ini(const real lo, const real hi, real size, T**);
+int edg1list_fin(T*);
+int edg1list_push(T*, Skel*, const real*);
+int edg1list_tris(T*, real, int**);
+
+#undef T
