e5a353017cc3bc24e658011c48d977f9b37a4146
Sergey Litvinov <slitvinov@gmail.com>
2019-04-30 11:11:10 +0200

    initial import of polygon

---
+++ lib/co/polygon.h
@@ -0,0 +1,12 @@
+#define T Polygon
+
+typedef struct T T;
+typedef struct Skel Skel;
+
+int polygon_ini(real lo, real hi, real size, T**);
+int polygon_fin(T*);
+int polygon_update(T*, Skel*, const real *x, const real *y);
+int polygon_inside(T*, real, rea);
+int polygon_inside_fast(T*, real, real);
+
+#undef T
