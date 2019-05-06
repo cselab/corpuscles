cd7fb89b0c38bc39d660996c59b17b741e427abf
Sergey Litvinov <slitvinov@gmail.com>
2019-05-06 16:45:26 +0200

    minor

--- lib/co/fd.h
+++ lib/co/fd.h
@@ -2,5 +2,4 @@ typedef struct He He;
 typedef struct Force Force;
 
 /* finite difference */
-int fd(Force*, He*, real delta, real *x, real *y, real *z,
-       /**/ real *fx, real *fy, real *fz);
+int fd(Force*, He*, real delta, real *x, real *y, real *z, /**/ real *fx, real *fy, real *fz);
