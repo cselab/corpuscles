42980dc59565e3aa76ebdfe6e3eeca00efd4fb39
Sergey Litvinov <slitvinov@gmail.com>
2019-05-06 10:53:55 +0200

    minor

--- lib/dedg2.c
+++ lib/dedg2.c
@@ -6,16 +6,20 @@
 
 #include "co/dedg2.h"
 
-int dedg2_abs(const real a[2], const real b[2], /**/ real da[2], real db[2]) {
-    vec2_minus(b, a,   db);
-    vec2_normalize(db);
-    vec2_negative(db, da);
-    return CO_OK;
+int
+dedg2_abs(const real a[2], const real b[2], real da[2], real db[2])
+{
+	vec2_minus(b, a,   db);
+	vec2_normalize(db);
+	vec2_negative(db, da);
+	return CO_OK;
 }
 
-int dedg2_sq(const real a[2], const real b[2], /**/ real da[2], real db[2]) {
-    vec2_minus(b, a,   db);
-    vec2_scale(2,  db);
-    vec2_negative(db,    da);
-    return CO_OK;
+int
+dedg2_sq(const real a[2], const real b[2], real da[2], real db[2])
+{
+	vec2_minus(b, a,   db);
+	vec2_scale(2,  db);
+	vec2_negative(db,    da);
+	return CO_OK;
 }
