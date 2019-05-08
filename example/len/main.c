ff899319cf1e87eba5a9fa1b088ef002738c4e8f
Sergey Litvinov <slitvinov@gmail.com>
2019-05-08 17:48:04 +0200

    add example/len

---
+++ example/len/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <tgmath.h>
+#include <real.h>
+#include <co/array.h>
+#include <co/err.h>
+#include <co/len.h>
+#include <co/skel.h>
+#include <co/memory.h>
+
+#define FMT CO_REAL_OUT
+
+int
+main(void)
+{
+	Skel *q;
+	real *x, *y, *ver, *edg;
+	int nv, ne;
+
+	skel_read(stdin, &x, &y, &q);
+	nv = skel_nv(q);
+	ne = skel_ne(q);
+	MALLOC(nv, &ver);
+	MALLOC(ne, &edg);
+
+	len_ver(q, x, y, ver);
+	len_edg(q, x, y, edg);
+
+	MSG("len: " FMT, len(q, x, y));
+	MSG("ver: " FMT " " FMT " " FMT, array_min(nv, ver), array_mean(nv, ver), array_max(nv, ver));
+	MSG("edg: " FMT " " FMT " " FMT, array_min(ne, edg), array_mean(ne, edg), array_max(ne, edg));
+
+	FREE(ver);
+	FREE(edg);
+	skel_xy_fin(x, y, q);
+}
---
+++ lib/co/f2/len.h
@@ -0,0 +1,13 @@
+#define T F2Len
+
+typedef struct Skel Skel;
+typedef struct T T;
+
+int f2_len_ini(FILE*, real Ka, real a3, real a4, T**);
+int f2_len_argv(char***, Skel*, T**);
+int f2_len_fin(T*);
+
+real f2_len_energy(T*, Skel*, const real*, const real*);
+int f2_len_force(T*, Skel*, const real*, const real*, real*, real*);
+
+#undef T
