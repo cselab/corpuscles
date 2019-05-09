0d4e161b6d115cca4a3738e0e8a23f954996a8b0
Sergey Litvinov <slitvinov@gmail.com>
2019-05-09 11:39:14 +0200

    add example/edg2

--- example/edg2/main.c
+++ example/edg2/main.c
@@ -3,11 +3,13 @@
 #include <tgmath.h>
 
 #include <real.h>
-#include <co/dedg2.h>
+#include <co/edg2.h>
 #include <co/err.h>
 #include <co/macro.h>
 #include <co/util.h>
 #include <co/vec2.h>
+
+#define FMT CO_REAL_OUT
 
 static const char **argv;
 
@@ -26,7 +28,7 @@ eq(const char *a, const char *b)
 int main(__UNUSED int argc, const char **v)
 {
 	const char *op;
-	real a[2], b[2], p[2], q[2], dp[2], dq[2], da[2], db[2];
+	real a[2], b[2], da[2], db[2], p[2], q[2];
 	argv = v;
 	argv++;
 	if (*argv == NULL) ER("mssing OP");
@@ -35,23 +37,33 @@ int main(__UNUSED int argc, const char **v)
 	if (eq(op, "abs")) {
 		vec(a);
 		vec(b);
-		dedg2_abs(a, b, da, db);
-		vec2_fprintf(da, stdout);
-		vec2_fprintf(db, stdout);
+		printf(FMT "\n", edg2_abs(a, b));
 	} else if (eq(op, "sq")) {
 		vec(a);
 		vec(b);
-		dedg2_sq(a, b, da, db);
-		vec2_fprintf(da, stdout);
-		vec2_fprintf(db, stdout);
+		printf("%g\n", edg2_sq(a, b));
 	} else if (eq(op, "alpha")) {
 		vec(a);
 		vec(b);
 		vec(p);
 		vec(q);
-		dedg2_alpha(a, b, p, q, dp, dq);
-		vec2_fprintf(dp, stdout);
-		vec2_fprintf(dq, stdout);
+		printf("%g\n", edg2_alpha(a, b, p, q));
+	} else if (eq(op, "point_distance2")) {
+		vec(a);
+		vec(b);
+		vec(p);
+		printf(FMT "\n", edg2_point_distance2(a, b, p));
+	} else if (eq(op, "point_distance")) {
+		vec(a);
+		vec(b);
+		vec(p);
+		printf(FMT "\n", edg2_point_distance(a, b, p));
+	} else if (eq(op, "point_closest")) {
+		vec(a);
+		vec(b);
+		vec(p);
+		edg2_point_closest(a, b, p, q);
+		vec2_fprintf(q, stdout);
 	} else
 		ER("unknown operation '%s'", op);
 	return 0;
