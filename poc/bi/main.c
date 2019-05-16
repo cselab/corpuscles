0a2d9dc318e835931c8bb32d675934b35fd57c69
Sergey Litvinov <slitvinov@gmail.com>
2019-05-16 16:15:57 +0200

    wip bi

--- poc/bi/main.c
+++ poc/bi/main.c
@@ -19,19 +19,21 @@ main(void)
 	real *sigma;
 	real *kx, *ky;
 	real *Oxx, *Oxy, *Oyy;
-	real*Ax, *Ay;
+	real*Ax, *Ay, *res;
+	real xx, xy, yy;
+	int n, i, j;
 
-	int n;
 	skel_read(stdin, &x, &y, &skel);
 	n = skel_nv(skel);
 
-	MALLOC2(n, &vx, &vy);
-	MALLOC2(n, &ux, &uy);
+	CALLOC2(n, &vx, &vy);
+	CALLOC2(n, &ux, &uy);
 	MALLOC(n, &sigma);
 	CALLOC2(n, &kx, &ky);
+	CALLOC(n, &res);
 	matrix_ini(n, n, &Oxx);
 	matrix_ini(n, n, &Oxy);
-	matrix_ini(n, n, &Oyy)
+	matrix_ini(n, n, &Oyy);
 	matrix_ini(n, n, &Ax);
 	matrix_ini(n, n, &Ay);
 
@@ -39,8 +41,23 @@ main(void)
 	array_one(n, sigma);
 	matrix_array_append_n(n, n, Ax, sigma, kx);
 	matrix_array_append_n(n, n, Ay, sigma, ky);
-
-	matrix_fwrite(n, 1, ky, stdout);
+	oseen2(skel, x, y, Oxx, Oxy, Oyy);
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j ++) {
+			xx = matrix_get(n, n, i, j, Oxx);
+			xy = matrix_get(n, n, i, j, Oxy);
+			yy = matrix_get(n, n, i, j, Oyy);
+			vx[i] += xx*kx[j] + xy*ky[j];
+			vy[i] += xy*kx[j] + yy*ky[j];
+		}
+	}
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++) {
+			xx = matrix_get(n, n, i, j, Ax);
+			yy = matrix_get(n, n, i, j, Ay);
+			res[i] += xx*vx[j] + yy*vy[j];
+		}
+	matrix_fwrite(n, 1, res, stdout);
 
 	FREE2(vx, vy);
 	FREE2(ux, uy);
@@ -51,6 +68,7 @@ main(void)
 	matrix_fin(Oyy);
 	matrix_fin(Ax);
 	matrix_fin(Ay);
+	FREE(res);
 
 	MSG("n %d", n);
 	skel_xy_fin(x, y, skel);
