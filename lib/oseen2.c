d7e87a991546b9e79327870be86090184d6305bf
Sergey Litvinov <slitvinov@gmail.com>
2019-05-16 15:58:44 +0200

    wip: oseen2

--- lib/oseen2.c
+++ lib/oseen2.c
@@ -1,22 +1,53 @@
 #include <tgmath.h>
 #include <stdio.h>
 #include "real.h"
-#include "co/array.h"
-#include "co/edg2.h"
 #include "co/err.h"
 
 #include "co/skel.h"
 #include "co/oseen2.h"
 #include "co/vec2.h"
+#include "co/matrix.h"
+
+static const real r0 = 0.0;
+
+static int
+oseen(const real a[2], const real b[2], real *xx, real *xy, real *yy)
+{
+	enum {X, Y};
+	real d[2], r, r2,l;
+
+	vec2_minus(a, b, d);
+	r = vec2_abs(d);
+	if (r == 0)
+		ERR(CO_NUM, "r == 0");
+	r2 = r*r;
+	l = log(r/r0);
+	*xx = -l + d[X]*d[X]/r2;
+	*xy = d[X]*d[Y]/r2;
+	*yy = -l + d[Y]*d[Y]/r2;
+	return CO_OK;
+}
 
 int
 oseen2(Skel *skel, const real *x, const real *y, real *xx, real *xy, real *yy)
 {
 	int n, i, j;
+	real a[2], b[2], oxx,oxy, oyy;
 	n = skel_nv(skel);
 
-	for (i = 0; i < n; i++)
-		for (j = 0; j < n; j++)
-			
+	for (i = 0; i < n; i++) {
+		vec2_get(i, x, y, a);
+		matrix_set(n, n, i, i, 0, xx);
+		matrix_set(n, n, i, i, 0, xy);
+		matrix_set(n, n, i, i, 0, yy);
+		for (j = 0; j < n; j++) {
+			if (i == j) continue;
+			vec2_get(j, x, y, b);
+			oseen(a, b, &oxx, &oxy, &oyy);
+			matrix_set(n, n, i, j, oxx, xx);
+			matrix_set(n, n, i, j, oxy, xy);
+			matrix_set(n, n, i, j, oyy, yy);
+		}
+	}			
 	return CO_OK;
 }
