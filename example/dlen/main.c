67ede7c5f1f799a1943734a614cd01c67036707b
Sergey Litvinov <slitvinov@gmail.com>
2019-05-21 15:22:45 +0200

    compute Ax^2 + Ay^2

--- example/dlen/main.c
+++ example/dlen/main.c
@@ -16,18 +16,29 @@ main(void)
 {
 	Skel *skel;
 	real *x, *y;
-	real *Ax, *Ay;
-	int n;
+	real *Ax, *Ay, *S;
+	real ax, ay, s;
+	int n, i, j;
 
 	skel_read(stdin, &x, &y, &skel);
 	n = skel_nv(skel);
 	MSG("len: " FMT, len(skel, x, y));
 
+	matrix_ini(n , n, &S);
 	matrix_ini(n, n, &Ax);
 	matrix_ini(n, n, &Ay);
 	dlen_ver(skel, x, y, Ax, Ay);
+
+	for (i = 0; i < n; i++)
+		for (j = 0;j < n; j++) {
+		ax = matrix_get(n, n, i, j, Ax);
+		ay = matrix_get(n, n, i, j, Ay);
+		s = ax*ax + ay*ay;
+		matrix_set(n, n, i, j, s, S);
+	}
 	
-	matrix_fwrite(n, n, Ay, stdout);
+	matrix_fwrite(n, n, S, stdout);
+	matrix_fin(S);
 	matrix_fin(Ax);
 	matrix_fin(Ay);
 	skel_xy_fin(x, y, skel);
