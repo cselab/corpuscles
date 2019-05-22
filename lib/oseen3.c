14fcdd3d2c1530c8bbf6657f2dea24556f4769f3
Sergey Litvinov <slitvinov@gmail.com>
2019-05-22 17:23:18 +0200

    wip: oseen3

--- lib/oseen3.c
+++ lib/oseen3.c
@@ -7,7 +7,7 @@
 #include "co/memory.h"
 #include "co/oseen3.h"
 #include "co/skel.h"
-#include "co/vec2.h"
+#include "co/vec.h"
 
 #define T Oseen3
 static const real pi = 3.141592653589793115997964;
@@ -17,34 +17,39 @@ struct T
 };
 
 static int
-oseen(__UNUSED real e, const real a[2], const real b[2], real *xx, real *xy, real *yy)
+oseen(__UNUSED real e, const real a[3], const real b[3],
+	real *xx, real *xy, real *xz, real *yy, real *yz, real *zz)
 {
 	enum
 	{
-		X, Y
+		X, Y, Z
 	};
-	real d[2], r, r2,l;
+	real d[3], r, r3,l;
 
-	vec2_minus(a, b, d);
-	r = vec2_abs(d);
+	vec_minus(a, b, d);
+	r = vec_abs(d);
 	if (r == 0)
 		ERR(CO_NUM, "r == 0");
-	r2 = r*r;
-	l = log(r);
-	*xx = -l + d[X]*d[X]/r2;
-	*xy = d[X]*d[Y]/r2;
-	*yy = -l + d[Y]*d[Y]/r2;
+	r3 = r*r*r;
+	l = 1/r;
+	*xx = l + d[X]*d[X]/r3;
+	*yy = l + d[Y]*d[Y]/r3;
+	*zz = l + d[Z]*d[Z]/r3;
+
+	*xy = d[X]*d[Y]/r3;
+	*xz = d[X]*d[Z]/r3;
+	*yz = d[Y]*d[Z]/r3;
 	return CO_OK;
 }
 
 static int
-oseen0(real e, real *xx, real *xy, real *yy)
+oseen0(real e, real *xx, real *xy, real *xz, real *yy, real *yz, real *zz)
 {
 	real l;
-	l = log(2*e) - 3/2;
-	*xx = -l;
-	*xy = 0;
-	*yy = -l;
+
+	l = 2/e;
+	*xx = *yy = *zz = l;
+	*xy = *xz = *yz = 0;
 	return CO_OK;
 }
 
@@ -52,6 +57,7 @@ int
 oseen3_ini(real e, T **pq)
 {
 	T *q;
+
 	MALLOC(1, &q);
 	q->e = e;
 	*pq = q;
@@ -70,28 +76,37 @@ oseen3_apply(T *q, Skel *skel, const real *x, const real *y, const real *z,
 	real *oxx, real *oxy, real *oxz, real *oyy, real *oyz, real *ozz)
 {
 	int n, i, j;
-	real e, s, a[2], b[2], xx, xy, yy;
+	real e, s, a[3], b[3], xx, xy, xz, yy, yz, zz;
 
 	n = skel_nv(skel);
 	e = q->e;
 	for (i = 0; i < n; i++) {
-		vec2_get(i, x, y, a);
-		oseen0(e, &xx, &xy, &yy);
+		vec_get(i, x, y, z, a);
+		oseen0(e, &xx, &xy, &xz, &yy, &yz, &zz);
 		matrix_set(n, n, i, i, xx, oxx);
 		matrix_set(n, n, i, i, xy, oxy);
+		matrix_set(n, n, i, i, xz, oxz);
 		matrix_set(n, n, i, i, yy, oyy);
+		matrix_set(n, n, i, i, yz, oyz);
+		matrix_set(n, n, i, i, zz, ozz);
 		for (j = 0; j < n; j++) {
 			if (i == j) continue;
-			vec2_get(j, x, y, b);
-			oseen(e, a, b, &xx, &xy, &yy);
+			vec_get(j, x, y, z, b);
+			oseen(e, a, b, &xx, &xy, &xz, &yy, &yz, &zz);
 			matrix_set(n, n, i, j, xx, oxx);
 			matrix_set(n, n, i, j, xy, oxy);
+			matrix_set(n, n, i, j, xz, oxz);
 			matrix_set(n, n, i, j, yy, oyy);
+			matrix_set(n, n, i, j, yz, oyz);
+			matrix_set(n, n, i, j, zz, ozz);
 		}
 	}
-	s = 1/(4*pi);
+	s = 1/(8*pi);
 	matrix_scale(n, n, s, oxx);
 	matrix_scale(n, n, s, oxy);
+	matrix_scale(n, n, s, oxz);
 	matrix_scale(n, n, s, oyy);
+	matrix_scale(n, n, s, oyz);
+	matrix_scale(n, n, s, ozz);
 	return CO_OK;
 }
