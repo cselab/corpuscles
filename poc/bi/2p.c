b3e181f29a061cfc725b1069aa2f6af6d80dbd91
Sergey Litvinov <slitvinov@gmail.com>
2019-05-29 16:45:15 +0200

    minor

--- poc/bi/2p.c
+++ poc/bi/2p.c
@@ -175,7 +175,7 @@ main(__UNUSED int argc, char **argv)
 		get_pressure();
 		sprintf(file, "%05d.vtk", k);
 		f = fopen(file, "w");
-		vtk2_fwrite1(vtk, vx, "pressure", f);
+		vtk2_fwrite1(vtk, pressure, "pressure", f);
 		fclose(f);
 		k++;
 	}
@@ -198,10 +198,11 @@ main(__UNUSED int argc, char **argv)
 Put
 
 git clean -fdxq
+
 m clean lint
 A=0.8835572001943658
 f=data/100.skel
-valgrind ./2p len $f 50 0.05 0.05 bend_sc 0.01 40 < $f
+./2p len $f 50 0.05 0.05 bend_sc 0.01 40 < $f
 
 co.geomview -f 38 -a /u/a *0.off
 vi.visit -o *.vtk
