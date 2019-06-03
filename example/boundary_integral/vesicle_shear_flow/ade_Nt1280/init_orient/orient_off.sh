d8fb94160136a7e8bfff27d64cdde6785d67fd90
bianx <bianx@ethz.ch>
2019-06-03 13:57:20 +0200

    update

---
+++ example/boundary_integral/vesicle_shear_flow/ade_Nt1280/init_orient/orient_off.sh
@@ -0,0 +1,20 @@
+end=200000.off
+Da1=0.143
+for i in `seq 0 9`;
+do
+    v=$(echo $i | awk '{print (0.5+$i*0.05)}')
+    echo "orient v="$v
+    orient < ../init/init_v${v}_Da1${Da1}.off > init_${v}_Da1${Da1}.off
+done
+
+v=0.52
+echo "orient v="$v
+orient < ../init/init_v${v}_Da1${Da1}.off > init_${v}_Da1${Da1}.off
+
+v=0.642
+echo "orient v="$v
+orient < ../init/init_v${v}_Da1${Da1}.off > init_${v}_Da1${Da1}.off
+
+v=0.98
+echo "orient v="$v
+orient < ../init/init_v${v}_Da1${Da1}.off > init_${v}_Da1${Da1}.off
