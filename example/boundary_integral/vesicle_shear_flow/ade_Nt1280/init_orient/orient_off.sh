4a81a8651deeefedeec9561b4e6dd8d40e77d90e
Xin Bian <bianx@eu-login-06-ng.euler.ethz.ch>
2019-06-03 15:06:40 +0200

    update

--- example/boundary_integral/vesicle_shear_flow/ade_Nt1280/init_orient/orient_off.sh
+++ example/boundary_integral/vesicle_shear_flow/ade_Nt1280/init_orient/orient_off.sh
@@ -4,17 +4,17 @@ for i in `seq 0 9`;
 do
     v=$(echo $i | awk '{print (0.5+$i*0.05)}')
     echo "orient v="$v
-    orient < ../init/init_v${v}_Da1${Da1}.off > init_${v}_Da1${Da1}.off
+    orient < ../init/init_v${v}_Da1${Da1}.off > init_v${v}_Da1${Da1}.off
 done
 
 v=0.52
 echo "orient v="$v
-orient < ../init/init_v${v}_Da1${Da1}.off > init_${v}_Da1${Da1}.off
+orient < ../init/init_v${v}_Da1${Da1}.off > init_v${v}_Da1${Da1}.off
 
 v=0.642
 echo "orient v="$v
-orient < ../init/init_v${v}_Da1${Da1}.off > init_${v}_Da1${Da1}.off
+orient < ../init/init_v${v}_Da1${Da1}.off > init_v${v}_Da1${Da1}.off
 
 v=0.98
 echo "orient v="$v
-orient < ../init/init_v${v}_Da1${Da1}.off > init_${v}_Da1${Da1}.off
+orient < ../init/init_v${v}_Da1${Da1}.off > init_v${v}_Da1${Da1}.off
