c4049fa694dd9d31fc28bb89baf818b4485323dd
bianx <bianx@ethz.ch>
2019-06-14 14:05:52 +0200

    update xcolor

--- bin/xcolor/main.c
+++ bin/xcolor/main.c
@@ -8,25 +8,38 @@
 #include <co/util.h>
 #include <co/off.h>
 #include <co/y.h>
+#include <co/memory.h>
 
 static const char *me = "co.xcolor";
+
+#define FMT_IN CO_REAL_IN
+
+static int scl(char **v, /**/ real *p) {
+    if (*v == NULL) ER("not enough args");
+    if (sscanf(*v, FMT_IN, p) != 1)
+        ER("not a number '%s'", *v);
+    return CO_OK;
+}
 
 static void
 usg(void)
 {
-	fprintf(stderr, "%s A.off B.off > C.off\n", me);
-	fprintf(stderr, "color vertices in B acording to x coordinate in A\n");
+	fprintf(stderr, "%s A.off lo hi B.off > C.off\n", me);
+	fprintf(stderr, "color vertices in B acording to (x-x_min)/(x_max-x_min) coordinate in A\n");
 	exit(2);
 }
 
-int
-main(int c, char **a)
+int main(int argc, char **a)
 {
 	int status;
-	real *x, *y, *z;
+	real *x, *y, *z, *c;
 	real *u, *v, *w;
 	He *p, *q;
-	err_set_ignore();
+	int n;
+	real min, max, d;
+	real lo, hi;
+
+       	err_set_ignore();
 	a++;
 	if (*a == NULL)
 		ER("not enougth arguments");
@@ -36,13 +49,40 @@ main(int c, char **a)
 	status = y_ini(*a, &q, &u, &v, &w);
 	if (status != CO_OK)
 		ER("not an off file '%s'", a[0]);
+
+	lo=0.0;
+	hi=1.0;
+	  
+	scl(++a, &lo);
+	scl(++a, &hi);
+
 	a++;
 	if (*a == NULL)
 		ER("not enougth arguments");
 	status = y_ini(*a, &p, &x, &y, &z);
 	if (status != CO_OK)
 		ER("not an off file '%s'", a[0]);
-	boff_ver_fwrite(p, x, y, z, u, stdout);
+
+
+	n = he_nv(q);
+	CALLOC(n, &c);
+	min=1000.0;
+	max=-1000.0;
+	
+	for (int i=0;i<n;i++){
+	  if (u[i] < min ){
+	    min=u[i]; }
+	  else if ( u[i]> max ) {
+	    max=u[i];
+	  }
+	}
+	d=max-min;
+	for (int i=0;i<n;i++){
+	  c[i]=(u[i]-min)/d;
+	}
+
+	//boff_ver_fwrite(p, x, y, z, c, stdout);
+	boff_lh_ver_fwrite(p, x, y, z, lo, hi, c, stdout);
 	y_fin(p, x, y, z);
 	y_fin(q, u, v, w);
 }
