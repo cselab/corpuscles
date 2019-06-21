6aa1a6585d52d3ef3af47d8e04e63c191a4325bd
bianx <bianx@ethz.ch>
2019-06-21 13:51:46 +0200

    min Nt320 tilt

---
+++ example/boundary_integral/sde_rho_shear_flow/min_Nt320_tilt/run.sh
@@ -0,0 +1,147 @@
+set -eu
+
+pi=3.141592653589793115997964
+A=12.5663706144
+V=2.68919777043
+#v=0.641998677173
+v=0.642
+R=1
+
+Nt=320
+Kc=20000
+Kga=$Kc
+Kv=$Kc
+
+Kb=1.0
+C0=0.0
+#alpha=$(echo $pi | gawk '{print 2.0/$1}')
+#Kad=$(echo $Kb $alpha | gawk '{print $1*$2}')
+alpha=0
+Kad=0
+D=0.000898798148042
+
+mub=139.260575205
+Ka=$(echo $mub | awk '{print $1*2}')
+a3=-2
+a4=8
+b1=0.7
+b2=0.75
+
+rho=1.0
+eta=645.928652122
+start=0
+end=1000000
+freq_out=500
+freq_stat=50
+
+#number of threads
+NT=1
+export CO_ARG="-W 24:00"
+     
+if test $# -ne 0
+then
+
+    gamdot="$1"
+    dt="$2"
+    Kc="$3"
+    
+    Kga=$Kc
+    Kv=$Kc
+    
+    Re=$(echo $gamdot $R $rho $eta | awk '{print $1*$2*$3/$4}')
+    echo Re=$Re
+    
+    in_file=../init_tilt/init_v${v}.off
+    ref_file=../ref/ref_v0.95.off
+    
+    echo "input file:" $in_file
+    echo "ref file  :" $ref_file
+
+    V=$(echo $A, $v | awk '{print 0.09403159725795977*$1**1.5*$2}')
+    echo "A, V, v   :" $A, $V, $v
+    
+    area=$(echo $A, $Nt | awk '{print $1/$2}')
+    edge=$(echo $area | awk '{print 2*sqrt($1/sqrt(3.0))}')
+    CFL=0.25
+    dt_gam=$(echo $CFL $gamdot | awk '{print $1/$2}')
+    echo "area, edge, dt_gam:" $area, $edge, $dt_gam
+
+    DA0D=0
+    
+    gam=$(printf "%.4f" $gamdot)
+    echo "gam="$gam
+    if [ ! -d g${gam}_dt${dt}_Kc${Kc} ]; then
+
+	mkdir g${gam}_dt${dt}_Kc${Kc}
+	
+    fi
+    
+    cd g${gam}_dt${dt}_Kc${Kc}
+    
+    co.run ../../main garea $A $Kga volume $V $Kv juelicher_xin $Kb $C0 $Kad $DA0D strain $ref_file lim $Ka $mub $a3 $a4 $b1 $b2 $R $D $rho $eta $gamdot $dt $start $end $freq_out $freq_stat '<' $in_file
+   
+    
+else
+
+    gamdot=0.00143923833018
+    dt=0.05
+    Kc=20000
+    bash run.sh $gamdot $dt $Kc
+
+    gamdot=0.00287847666036
+    dt=0.05
+    Kc=20000
+    bash run.sh $gamdot $dt $Kc
+
+    gamdot=0.0071961916509
+    dt=0.02
+    Kc=20000
+    bash run.sh $gamdot $dt $Kc
+
+    gamdot=0.0143923833018
+    dt=0.01
+    Kc=20000
+    bash run.sh $gamdot $dt $Kc
+
+    gamdot=0.0287847666036
+    dt=0.005
+    Kc=20000
+    bash run.sh $gamdot $dt $Kc
+
+    gamdot=0.071961916509
+    dt=0.002
+    Kc=20000
+    bash run.sh $gamdot $dt $Kc
+
+    gamdot=0.143923833018
+    dt=0.001
+    Kc=20000
+    bash run.sh $gamdot $dt $Kc
+
+    gamdot=0.287847666036
+    dt=0.0005
+    Kc=40000
+    bash run.sh $gamdot $dt $Kc
+
+    gamdot=0.71961916509
+    dt=0.0002
+    Kc=100000
+    bash run.sh $gamdot $dt $Kc
+    
+    gamdot=1.43923833018
+    dt=0.0001
+    Kc=200000
+    bash run.sh $gamdot $dt $Kc
+
+    gamdot=2.87847666036
+    dt=0.00005
+    Kc=500000
+    bash run.sh $gamdot $dt $Kc
+
+    gamdot=7.1961916509
+    dt=0.00002
+    Kc=1000000
+    bash run.sh $gamdot $dt $Kc
+
+fi
+    
