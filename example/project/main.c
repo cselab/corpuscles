a36584a616fff8ae24863a088de9109f15fe1029
Sergey Litvinov <slitvinov@gmail.com>
2019-09-05 16:22:15 +0200

    example/project: work for off file

--- example/project/main.c
+++ example/project/main.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <tgmath.h>
+#include <real.h>
+#include <co/y.h>
+#include <co/he.h>
+#include <co/vec.h>
+#include <co/off.h>
 
 static double a = 0.54353, b = 0.121435, c = -0.561365;
 static double pi = 3.141592653589793;
 
-struct Point
-{
+struct Point {
     double x, y;
 };
 
 static double
 F(double q)
 {
-    return c*pow(q, 5)+b*pow(q, 3)+a*q;
+    return c * pow(q, 5) + b * pow(q, 3) + a * q;
 }
 
 static int
@@ -24,7 +28,8 @@ q2xy(double q, double *x, double *y)
 }
 
 static double
-dist2(double q, void *p) {
+dist2(double q, void *p)
+{
     struct Point *point;
     double x, y, dx, dy;
 
@@ -32,20 +37,22 @@ dist2(double q, void *p) {
     q2xy(q, &x, &y);
     dx = point->x - x;
     dy = point->y - y;
-    return dx*dx + dy*dy;
+    return dx * dx + dy * dy;
 }
 
 static double
-min(double x, double y, double *px, double *py) {
+min(double x, double y, double *px, double *py)
+{
     double lo, hi, q, h, d;
     double p, m;
     int NegX, NegY;
     struct Point point;
     int i, n;
+
     lo = 0;
-    hi = pi/2;
+    hi = pi / 2;
     n = 1000;
-    h = (hi - lo)/(n - 1);
+    h = (hi - lo) / (n - 1);
 
     NegX = NegY = 0;
     if (x < 0) {
@@ -57,13 +64,13 @@ min(double x, double y, double *px, double *py) {
 	NegY = 1;
 	y = -y;
     }
-    
+
     point.x = x;
     point.y = y;
     p = 0;
     m = 1e42;
     for (i = 0; i < n; i++) {
-	q = lo + i*h;
+	q = lo + i * h;
 	d = dist2(q, &point);
 	if (d < m) {
 	    m = d;
@@ -78,42 +85,42 @@ min(double x, double y, double *px, double *py) {
 
     *px = x;
     *py = y;
-    
+
     return 0;
 }
 
-int min3(const double r[3], /**/ double s[3]) {
-    enum {X, Y, Z};
+int
+min3(const double r[3], /**/ double s[3])
+{
+    enum { X, Y, Z };
     double x, y, p, u, v;
-    x = sqrt(r[X]*r[X] + r[Y]*r[Y]);
-    y = r[Y];
+
+    x = sqrt(r[X] * r[X] + r[Y] * r[Y]);
+    y = r[Z];
     p = atan2(r[Y], r[X]);
-
     min(x, y, &u, &v);
-
+    s[X] = u * cos(p);
+    s[Y] = u * sin(p);
     s[Z] = v;
-    s[X] = u*cos(p);
-    s[Y] = u*sin(p);
-
     return 0;
 }
 
 int
 main()
 {
-    double lo, hi, q, x, y, x0, y0;
-    int i, n;
-    lo = 0;
-    hi = pi/2;
-    n = 20;
+    enum { X, Y, Z };
+    double *x, *y, *z, r[3], s[3];
+    He *he;
+    int i, nv;
 
-    for (i = 0; i < n; i++) {
-	q = lo + i*(hi - lo)/(n - 1);
-	q2xy(q, &x, &y);
-	printf("%g %g\n", x, y);
+    y_inif(stdin, &he, &x, &y, &z);
+
+    nv = he_nv(he);
+    for (i = 0; i < nv; i++) {
+	vec_get(i, x, y, z, r);
+	min3(r, s);
+	vec_set(s, i, x, y, z);
     }
-
-    min(x = 0.5, y = 0.0, &x0, &y0);
-    printf("%g %g\n", x, y);
-    printf("%g %g\n", x0, y0);    
+    off_he_xyz_fwrite(he, x, y, z, stdout);
+    y_fin(he, x, y, z);
 }
