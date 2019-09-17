36c343a42fd3d37eb9fe4d73a606c674b32d8e39
Sergey Litvinov <slitvinov@gmail.com>
2019-09-17 10:26:02 +0200

    add rotation matrix

---
+++ javascript/example/data/README.md
@@ -0,0 +1,1 @@
+co.geomview -OO -t 0 0 0 -r 70.4456 -0.881623 -12.2513 -f 26.598  data/rbc.off
--- javascript/example/load.js
+++ javascript/example/load.js
@@ -4,50 +4,70 @@ loader.load('data/rbc.off', load, progress, err)
 
 function load(d)
 {
-	var X = 0, Y = 1, Z = 2
-	var nt, t, tt, i, j, k, off, ver, pos, v
-	var geometry, scene, material, mesh
-	var camera, scene, renderer
+        var X = 0, Y = 1, Z = 2
+        var nt, nv, t, tt, i, j, k, off, ver, pos, v
+        var x, y, z, ve, t
+        var geometry, scene, material, mesh
+        var camera, scene, renderer
+        var XX = 0, XY = 1, XZ = 2,
+            YX = 4, YY = 5, YZ = 6,
+            ZX = 8, ZY = 9, ZZ = 10
 
-	off = off_string_ini(d)
-	nt = off_nt(off)
-	tt = off_tri(off)
-	ver = off_ver(off)
-	
-	pos = Array(9*nt)
+        off = off_string_ini(d)
+        nt = off_nt(off)
+        nv = off_nv(off)
+        tt = off_tri(off)
+        ver = off_ver(off)
 
-	for (t = v = 0; t < nt; t++) {
-		i = tt[t][0]
-		j = tt[t][1]
-		k = tt[t][2]
-		
-		pos[v++] = ver[i][X]
-		pos[v++] = ver[i][Y]
-		pos[v++] = ver[i][Z]
+        t = [0.97711056,    0.21217471,  -0.015386616,             0,
+             -0.085192636,    0.32400259,   -0.94221258,             0,
+             -0.19492838,    0.92195672,    0.33466208,             0,
+             0,             0,             0,             1]
+        for (i = 0; i < nv; i++) {
+            ve = ver[i]
+            x = ve[X]
+            y = ve[Y]
+            z = ve[Z]
+            ve[X] = t[XX]*x + t[YX]*y + t[ZX]*z
+            ve[Y] = t[XY]*x + t[YY]*y + t[ZY]*z
+            ve[Z] = t[XZ]*x + t[YZ]*y + t[ZZ]*z
+        }
 
-		pos[v++] = ver[j][X]
-		pos[v++] = ver[j][Y]
-		pos[v++] = ver[j][Z]
+        pos = Array(9*nt)
+        for (t = v = 0; t < nt; t++) {
+                i = tt[t][0]
+                j = tt[t][1]
+                k = tt[t][2]
 
-		pos[v++] = ver[k][X]
-		pos[v++] = ver[k][Y]
-		pos[v++] = ver[k][Z]
-	}
+                pos[v++] = ver[i][X]
+                pos[v++] = ver[i][Y]
+                pos[v++] = ver[i][Z]
 
-	geometry = new THREE.BufferGeometry()
-	geometry.addAttribute( 'position', new THREE.Float32BufferAttribute(pos, 3) )
+                pos[v++] = ver[j][X]
+                pos[v++] = ver[j][Y]
+                pos[v++] = ver[j][Z]
 
-	scene = new THREE.Scene()
-	material = new THREE.MeshBasicMaterial({color: 0x00ffff})
-	mesh = new THREE.Mesh(geometry, material)
-	scene.add(mesh)
-	renderer = new THREE.WebGLRenderer( { antialias: true } )
-	renderer.setSize(window.innerWidth, window.innerHeight )
-	document.body.appendChild( renderer.domElement )
+                pos[v++] = ver[k][X]
+                pos[v++] = ver[k][Y]
+                pos[v++] = ver[k][Z]
+        }
 
-	camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.01, 10 )
-	camera.position.z = 5
-	renderer.render( scene, camera )
+        geometry = new THREE.BufferGeometry()
+        geometry.addAttribute( 'position', new THREE.Float32BufferAttribute(pos, 3) )
+
+        scene = new THREE.Scene()
+        material = new THREE.MeshBasicMaterial({color: 0x00ffff})
+        material.wireframe = true
+
+        mesh = new THREE.Mesh(geometry, material)
+        scene.add(mesh)
+        renderer = new THREE.WebGLRenderer( { antialias: true } )
+        renderer.setSize(window.innerWidth, window.innerHeight )
+        document.body.appendChild( renderer.domElement )
+
+        camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.01, 10 )
+        camera.position.z = 3
+        renderer.render(scene, camera)
 }
 
 function progress(xhr)
@@ -56,5 +76,5 @@ function progress(xhr)
 
 function err(e)
 {
-	console.log('error reading off file')
+        console.log('error reading off file')
 }
