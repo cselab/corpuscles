c4b9a2f858a69b7e075040bd9becc18f5d69dd2c
Sergey Litvinov <slitvinov@gmail.com>
2019-09-18 15:54:36 +0200

    minor

--- poc/distmesh/off_write.m
+++ poc/distmesh/off_write.m
@@ -1,6 +1,11 @@
 function err = off_write(f, p, t)
   nv = size(p, 1);
   nt = size(t, 1);
+  if f == -1
+    warning('off_write: f is not valid file id');
+    err = 1;
+    return;
+  end
   fprintf(f, 'OFF\n');
   fprintf(f, '%d %d 0\n', nv, nt);
   for i = 1:nv
@@ -9,4 +14,6 @@ function err = off_write(f, p, t)
   for i = 1:nt
     fprintf(f, '3 %d %d %d\n', t(i, :) - 1);
   end
+  err = 1;
+  return;
 end
