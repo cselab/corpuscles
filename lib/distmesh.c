4a78935f56c9d8319f1ff0f72fa9d0f328803490
Sergey Litvinov <slitvinov@gmail.com>
2019-09-19 10:41:43 +0200

    distemesh: add t2n

--- lib/distmesh.c
+++ lib/distmesh.c
@@ -3,14 +3,47 @@
 #include "co/err.h"
 #include "co/distmesh.h"
 
-int
-distmesh_t2n(struct He *he, /**/ int *out)
+static int
+count(He *he, int i)
 {
+    int t, j, cnt;
+    if (he_bnd(he, i))
+	return -1;
+    i = he_flp(he, i);
+    t = he_tri(he, i);
+    j = he_hdg_tri(he, t);
+    j = he_nxt(he, j);
+    j = he_nxt(he, j);
+
+    for (cnt = 0; i != j; cnt++)
+	j = he_nxt(he, j);
+
+    return cnt;
+}
+
+int
+distmesh_t2n(He *he, /**/ int *out)
+{
+    int nt, t, i, j, k;
+
+    nt = he_nt(he);
+    for (t = 0; t < nt; t++) {
+	i = he_hdg_tri(he, t);
+	j = he_nxt(he, i);
+	k = he_nxt(he, j);
+
+	i = count(he, i);
+	j = count(he, j);
+	k = count(he, k);
+	*out++ = k; /* to much matlab distmesh order */
+	*out++ = i;
+	*out++ = j;
+    }
     return CO_OK;
 }
 
 int
-distmesh_t2t(struct He *he, /**/ int *out)
+distmesh_t2t(He *he, /**/ int *out)
 {
     return CO_OK;
 }
