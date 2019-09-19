86299192a074371cb629db67552433a659b950ae
Sergey Litvinov <slitvinov@gmail.com>
2019-09-19 10:51:21 +0200

    use t2n from the library

--- example/he/tri/t2n.c
+++ example/he/tri/t2n.c
@@ -1,47 +1,29 @@
 #include <stdio.h>
 
 #include <real.h>
+#include <co/distmesh.h>
 #include <co/err.h>
-#include <co/y.h>
 #include <co/he.h>
-
-static int
-count(He *he, int i)
-{
-    int t, j, cnt;
-    t = he_tri(he, i);
-    j = he_hdg_tri(he, t);
-    j = he_nxt(he, j);
-    j = he_nxt(he, j);
-
-    for (cnt = 0; i != j; cnt++)
-	j = he_nxt(he, j);
-
-    return cnt;
-}
+#include <co/memory.h>
+#include <co/y.h>
 
 int
 main() {
 	He      *he;
 	real *x, *y, *z;
-	int i, j, k, t, nt;
+	int i, j, k, m, t, nt;
+	int *t2n;
 	y_inif(stdin, &he, &x, &y, &z);
-
 	nt = he_nt(he);
-	for (t = 0; t < nt; t++) {
-	    i = he_hdg_tri(he, t);
-	    j = he_nxt(he, i);
-	    k = he_nxt(he, j);
-
-	    i = he_flp(he, i);
-	    j = he_flp(he, j);
-	    k = he_flp(he, k);
-
-	    i = count(he, i);
-	    j = count(he, j);
-	    k = count(he, k);
-	    printf("%d %d %d\n", k, i, j);
+	MALLOC(3*nt, &t2n);
+	distmesh_t2n(he, t2n);
+	for (t = m = 0; t < nt; t++) {
+	    i = t2n[m++];
+	    j = t2n[m++];
+	    k = t2n[m++];
+	    printf("%d %d %d\n", i, j, k);
 	}
+	FREE(t2n);
 	y_fin(he, x, y, z);
 	return CO_OK;
 }
