6af5f1613faf3cb1ad7f2b2d1b6e0bc7e867e919
Sergey Litvinov <slitvinov@gmail.com>
2019-09-23 19:26:23 +0200

    add i_matrix_scale

--- lib/co/i/matrix.h
+++ lib/co/i/matrix.h
@@ -10,3 +10,11 @@ i_matrix_set(int M, int N, int m, int n, real s, real *a)
 	a[N*m + n] = s;
 	return CO_OK;
 }
+
+static int
+i_matrix_scale(int M, int N, real s, real *a) {
+    int i;
+    for (i = 0; i < N*M; i++)
+        a[i] *= s;
+    return CO_OK;
+}
