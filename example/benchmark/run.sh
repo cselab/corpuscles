21729c5ca88f1f63e77ce3776e68b5710b93c5e4
Sergey Litvinov <slitvinov@gmail.com>
2019-09-24 17:47:01 +0200

    run level one

--- example/benchmark/run.sh
+++ example/benchmark/run.sh
@@ -2,16 +2,16 @@
 
 make main
 
-la=0.5
+la=5
 start=0
-end=200
+end=10
 freq=1000
 #valgrind --tool=callgrind --quiet \
 ./main \
        garea 12.5663706144 1000000 \
        volume 2.6892 1000000 \
        juelicher_xin 1.0 0.0 0.63662 19.9933 \
-       strain ref/main.off lim 278.521 139.260575205 -2 8 0.7 0.75 \
+       strain ref/1.off lim 278.521 139.260575205 -2 8 0.7 0.75 \
        1 0.000898798148042 1.0 645.928652122 $la \
        7.1961916509 0.00002 \
-       $start $end $freq 50 < init/main.off
+       $start $end $freq 50 < init/1.off
