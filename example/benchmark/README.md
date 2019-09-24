95fd511ea0ef30e5ccc1a926fdc9d1d067f2a81b
Sergey Litvinov <slitvinov@gmail.com>
2019-09-24 20:35:33 +0200

    fast

--- example/benchmark/README.md
+++ example/benchmark/README.md
@@ -6,5 +6,8 @@ real	0m25.333s
 real	0m29.367s
 real	0m24.985s
 
+-Ofast
+real	0m19.665s
+
 openmp:
 real	0m21.226s
--- example/benchmark/run.sh
+++ example/benchmark/run.sh
@@ -6,12 +6,12 @@ la=5
 start=0
 end=15
 freq=1000
-#valgrind --tool=callgrind --quiet \
-./main \
+#VAL='--tool=callgrind --quiet' \
+co.run ./main \
        garea 12.5663706144 1000000 \
        volume 2.6892 1000000 \
        juelicher_xin 1.0 0.0 0.63662 19.9933 \
        strain ref/1.off lim 278.521 139.260575205 -2 8 0.7 0.75 \
        1 0.000898798148042 1.0 645.928652122 $la \
        7.1961916509 0.00002 \
-       $start $end $freq 50 < init/1.off
+       $start $end $freq 50 '<' init/1.off
