d4dd12194a9c9d920f213580c218a208847a4d33
Sergey Litvinov <slitvinov@gmail.com>
2019-09-24 15:33:31 +0200

    use he_T

--- lib/normal.c
+++ lib/normal.c
@@ -12,18 +12,12 @@
 #include "co/normal.h"
 
 static real pi = 3.141592653589793115997964;
-
-static int get3(const real *x, const real *y, const real *z,
-                int i, int j, int k,  /**/
-                real a[3], real b[3], real c[3]) {
-    vec_get(i, x, y, z, /**/ a);
-    vec_get(j, x, y, z, /**/ b);
-    vec_get(k, x, y, z, /**/ c);
-    return CO_OK;
-}
-int normal_mwa(He *he, const real *x, const real *y, const real *z, /**/
-                  real *nx, real *ny, real *nz) {
+int
+normal_mwa(He *he, const real *x, const real *y, const real *z, /**/
+	   real *nx, real *ny, real *nz)
+{
     enum {X, Y, Z};
+    int *t0, *t1, *t2;
     int t, nt;
     int i, j, k, nv;
     real a[3], b[3], c[3], u[3], u0[3];
@@ -31,23 +25,27 @@ int normal_mwa(He *he, const real *x, const real *y, const real *z, /**/
 
     nt = he_nt(he);
     nv = he_nv(he);
+    he_T(he, &t0, &t1, &t2);
+
     array_zero3(nv, nx, ny, nz);
     for ( t = 0; t < nt; t++ ) {
-        he_tri_ijk(he, t, &i, &j, &k);
-        get3(x, y, z, i, j, k, a, b, c);
-        theta_a = i_tri_angle(c, a, b);
-        theta_b = i_tri_angle(a, b, c);
+	i = t0[t];
+	j = t1[t];
+	k = t2[t];
+	i_vec_get3(i, j, k, x, y, z, a, b, c);
+	theta_a = i_tri_angle(c, a, b);
+	theta_b = i_tri_angle(a, b, c);
 	theta_c = pi - theta_a - theta_b;
-        i_tri_normal(a, b, c, u);
-        vec_scalar_append(u, theta_a, i, nx, ny, nz);
-        vec_scalar_append(u, theta_b, j, nx, ny, nz);
-        vec_scalar_append(u, theta_c, k, nx, ny, nz);
+	i_tri_normal(a, b, c, u);
+	vec_scalar_append(u, theta_a, i, nx, ny, nz);
+	vec_scalar_append(u, theta_b, j, nx, ny, nz);
+	vec_scalar_append(u, theta_c, k, nx, ny, nz);
     }
 
     for (i = 0; i < nv; i++) {
-        vec_get(i, nx, ny, nz, /**/ u);
-        vec_norm(u, /**/ u0);
-        vec_set(u0, i, /**/ nx, ny, nz);
+	vec_get(i, nx, ny, nz, /**/ u);
+	vec_norm(u, /**/ u0);
+	vec_set(u0, i, /**/ nx, ny, nz);
     }
     return CO_OK;
 }
