be59abb1808bcc29debec132a8d251279eed0ba7
Sergey Litvinov <slitvinov@gmail.com>
2019-09-25 11:09:34 +0200

    wip: stfmm3

--- poc/stfmm3d/lib/main.f90
+++ poc/stfmm3d/lib/main.f90
@@ -1,2 +1,20 @@
-function stfmm3_dpartself()
-end function stfmm3_dpartself
+subroutine stfmm3_dpartself(&
+     ier,iprec,nparts,source,&
+     ifsingle,sigma_sl,ifdouble,sigma_dl,sigma_dv,&
+     ifpot,pot,pre,ifgrad,grad)bind(c)
+
+  use iso_c_binding
+  implicit real *8 (a-h,o-z)
+
+  real *8 source(3,nparts)
+  real *8 sigma_sl(3,nparts)
+  real *8 sigma_dl(3,nparts),sigma_dv(3,nparts)
+  real *8 pot(3,nparts),pre(nparts),train(3,3,nparts)
+  integer nparts,ntargs
+
+  call stfmm3dpartself(&
+       ier,iprec,nparts,source,&
+       ifsingle,sigma_sl,ifdouble,sigma_dl,sigma_dv,&
+       ifpot,pot,pre,ifgrad,grad)
+
+end subroutine stfmm3_dpartself
