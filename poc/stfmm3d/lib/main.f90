cbc8d8429f4aef7757ad4b1b51304168e2033614
Sergey Litvinov <slitvinov@gmail.com>
2019-09-25 15:20:03 +0200

    stfmm3d: dump pre

--- poc/stfmm3d/lib/main.f90
+++ poc/stfmm3d/lib/main.f90
@@ -47,3 +47,30 @@ subroutine stfmm3_dparttarg(&
        ntargs,target,ifpottarg,pottarg,pretarg,&
        ifgradtarg,gradtarg)
 end subroutine stfmm3_dparttarg
+
+subroutine stfmm3_dpartdirect(&
+     nparts,source,&
+     ifsingle,sigma_sl,ifdouble,sigma_dl,sigma_dv,&
+     ifpot,pot,pre,ifgrad,grad,&
+     ntargs,target,ifpottarg,pottarg,pretarg,&
+     ifgradtarg,gradtarg) &
+     bind(c)
+  use iso_c_binding
+  implicit none
+  real(c_double), intent(out) :: &
+       pot(3,nparts),pre(nparts),grad(3,3,nparts),&
+       pottarg(3,nparts), pretarg(nparts), gradtarg(3,3,nparts)
+  real(c_double), intent(in) :: &
+       sigma_dl(3,nparts),sigma_dv(3,nparts),&
+       sigma_sl(3,nparts), source(3,nparts),&
+       target(3,nparts)
+  integer(c_int), intent(in), value :: &
+       ifsingle, ifdouble, ifpot, ifgrad,&
+       nparts, ntargs, ifpottarg, ifgradtarg
+  call st3dpartdirect(&
+       nparts,source,&
+       ifsingle,sigma_sl,ifdouble,sigma_dl,sigma_dv,&
+       ifpot,pot,pre,ifgrad,grad,&
+       ntargs,target,ifpottarg,pottarg,pretarg,&
+       ifgradtarg,gradtarg)
+end subroutine stfmm3_dpartdirect
