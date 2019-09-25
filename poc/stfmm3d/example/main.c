021340ca75acf94d33ec9d5f44ba5b788c240f63
Sergey Litvinov <slitvinov@gmail.com>
2019-09-25 14:18:45 +0200

    wip: stfmm3d

--- poc/stfmm3d/example/main.c
+++ poc/stfmm3d/example/main.c
@@ -1,13 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void stfmm3_dpartself(int *ier, int iprec, int nparts, const double *source,
+		      int ifsingle, const double *sigma_sl,
+		      int ifdouble, const double *sigma_dl, double *sigma_dv,
+		      int ifvel, double *vel, double *pre, int ifgrad, double *grad);
 
 int
 main()
 {
+    int i, j;
     double *pre;
-    double **pot, grad, sigma_dl, sigma_dv, sigma_sl, source;
-    int *ier;
-    int iprec, ifsingle, ifdouble, ifpot, ifgrad, nparts;
-    stfmm3_dpartself(ier,iprec,nparts,source,
-		     ifsingle,sigma_sl,ifdouble,sigma_dl,sigma_dv,
-		     ifpot,pot,pre,ifgrad,grad);
+    double *vel, *grad, *sigma_dl, *sigma_dv, *sigma_sl, *source;
+    int ier;
+    int iprec, ifsingle, ifdouble, ifvel, ifgrad, nparts;
+
+    iprec = 3; /* < 0.5e-9 */
+    nparts = 4;
+    source = malloc(3*nparts*sizeof(*source));
+    sigma_sl = malloc(3*nparts*sizeof(*sigma_sl));
+    vel = malloc(3*nparts*sizeof(*sigma_sl));
+    pre = malloc(3*nparts*sizeof(*sigma_sl));
+
+    ifsingle = 1;
+    ifdouble = 0;
+    sigma_dl = sigma_dv = NULL;
+    ifvel = 1;
+    ifgrad = 0;
+
+    for (i = j = 0; i < nparts; i++) {
+	source[j++] = 1 * (i + 1);
+	source[j++] = 2 * (i + 1);
+	source[j++] = 3 * (i + 1);
+    }
+
+    for (i = j = 0; i < nparts; i++) {
+	sigma_sl[j++] = 1 * (i + 1);
+	sigma_sl[j++] = 2 * (i + 1);
+	sigma_sl[j++] = 3 * (i + 1);
+    }
+
+    stfmm3_dpartself(&ier, iprec, nparts, source,
+		     ifsingle, sigma_sl, ifdouble, sigma_dl, sigma_dv,
+		     ifvel, vel, pre, ifgrad, grad);
+
+    for (i = j = 0; i < nparts; i++) {
+	printf("%g ", vel[j++]);
+	printf("%g ", vel[j++]);
+	printf("%g\n", vel[j++]);
+    }
+
+    free(source);
+    free(sigma_sl);
+    free(vel);
+    free(pre);
 }
--- poc/stfmm3d/lib/main.f90
+++ poc/stfmm3d/lib/main.f90
@@ -7,12 +7,19 @@ subroutine stfmm3_dpartself(&
   implicit none
 
   real(c_double), intent(out) :: &
-       pot(3,nparts),pre(nparts),grad(3,3,nparts), &
+       pot(3,nparts),pre(nparts),grad(3,3,nparts)
+  real(c_double), intent(in) :: &
        sigma_dl(3,nparts),sigma_dv(3,nparts), sigma_sl(3,nparts), source(3,nparts)
   integer(c_int), intent(out) ::  ier
   integer(c_int), intent(in), value :: &
        iprec, ifsingle, ifdouble, ifpot, ifgrad, &
        nparts
+
+  print *, "nparts = ", nparts
+  print *, "ifsingle = ", ifsingle
+  print *, "sigma_sl = ", sigma_sl(:, 4)
+  print *, "ifgrad = ", ifgrad
+
   call stfmm3dpartself(&
        ier,iprec,nparts,source,&
        ifsingle,sigma_sl,ifdouble,sigma_dl,sigma_dv,&
