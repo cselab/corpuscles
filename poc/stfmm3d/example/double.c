65b2e9e4eb86802dced7889d7b02ca20eed13a7c
Sergey Litvinov <slitvinov@gmail.com>
2019-09-26 16:45:57 +0200

    fmm3: add a client for fmm_double

---
+++ poc/stfmm3d/example/double.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <real.h>
+#include <co/err.h>
+#include <co/he.h>
+#include <co/memory.h>
+#include <co/normal.h>
+#include <co/y.h>
+#include <fmm3.h>
+
+#define FMT   CO_REAL_OUT
+
+int
+main()
+{
+    int i, n;
+    FMM3 *q;
+    double *x, *y, *z, *ux, *uy, *uz, *nx, *ny, *nz, *vx, *vy, *vz;
+
+    He *he;
+    y_inif(stdin, &he, &x, &y, &z);
+    n = he_nv(he);
+    MALLOC3(n, &ux, &uy, &uz);
+    MALLOC3(n, &nx, &ny, &nz);
+    CALLOC3(n, &vx, &vy, &vz);
+
+    fmm3_ini(n, &q);
+    normal_mwa(he, x, y, z, nx, ny, nz);
+
+    for (i = 0; i < n; i++) {
+	ux[i] = 1;
+	uy[i] = 1;
+	uz[i] = 1;
+    }
+    fmm3_double(q, x, y, z, ux, uy, uz, nx, ny, nz, /**/ vx, vy, vz);
+
+    for (i = 0; i < 2; i++)
+	MSG(FMT " " FMT " " FMT, vx[i], vy[i], vz[i]);
+
+    y_fin(he, x, y, z);
+    FREE3(vx, vy, vz);
+    FREE3(ux, uy, uz);
+    FREE3(nx, ny, nz);    
+    fmm3_fin(q);
+}
