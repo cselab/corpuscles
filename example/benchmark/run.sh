cb674427e1a0b42ad68e91ba30bb6821f444d882
Sergey Litvinov <slitvinov@gmail.com>
2019-09-29 00:37:40 +0200

    gamdot /= 2

--- example/benchmark/run.sh
+++ example/benchmark/run.sh
@@ -2,7 +2,9 @@
 
 make main
 
-la=5
+#gamdot=645.928652122
+gamdot=322.964326061
+lambda=5
 start=0
 end=10000000
 freq=1000
@@ -14,6 +16,6 @@ co.run ./main \
        volume 2.6892 1000000 \
        juelicher_xin 1.0 0.0 0.63662 19.9933 \
        strain ref/0.off lim 278.521 139.260575205 -2 8 0.7 0.75 \
-       1 0.000898798148042 1.0 645.928652122 $la \
+       1 0.000898798148042 1.0 $gamdot $lambda \
        7.1961916509 \
        $dt $start $end $freq 50 '<' init/0.off
