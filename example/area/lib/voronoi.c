e5980d44b47e99f45957e36ee468977542092304
Sergey Litvinov <slitvinov@gmail.com>
2019-10-03 19:00:56 +0200

    test area_voronoi

---
+++ example/area/lib/voronoi.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <tgmath.h>
+
+#include <real.h>
+#include <co/err.h>
+#include <co/he.h>
+#include <co/area.h>
+#include <co/i/vec.h>
+#include <co/i/tri.h>
+#include <co/i/area.h>
+#include <co/memory.h>
+#include <co/punto.h>
+#include <co/y.h>
+
+int main() {
+    He *he;
+    int n;
+    real *x, *y, *z;
+    real A;
+    real *area;
+
+    y_inif(stdin, &he, &x, &y, &z);
+
+    n = he_nv(he);
+    MALLOC(n, &area);
+
+    A = he_area(he, x, y, z);
+    area_ver_voronoi(he, x, y, z, /**/ area);
+
+    MSG("area: " CO_REAL_OUT, A);
+
+    const real *queue[] = {area, NULL};
+    puts("area");
+    punto_fwrite(n, queue, stdout);
+
+    FREE(area);
+    y_fin(he, x, y, z);
+    return CO_OK;
+}
