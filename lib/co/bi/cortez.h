2d8c8998793aa33539d1c8b43380bd4a681ea002
Sergey Litvinov <slitvinov@gmail.com>
2019-10-07 13:20:49 +0200

    add cortez.h

---
+++ lib/co/bi/cortez.h
@@ -0,0 +1,16 @@
+#define T BeCortez
+
+typedef struct He He;
+typedef struct T T;
+
+int bi_cortez_ini(real *eps, He*, /**/ T**);
+int bi_cortez_fin(T*);
+int bi_cortez_single(T*, He*, real alpha,
+	      const real *x, const real *y, const real *z,
+	      const real *fx, const real *fy, const real *fz,
+	      /*io*/, real*, real*, real*);
+
+int bi_cortez_double(T*, He*, real alpha,
+	      const real *x, const real *y, const real *z,
+	      const real *vx, const real *vy, const real *vz,
+	      /*io*/, real*, real*, real*);
