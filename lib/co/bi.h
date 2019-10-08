257551c705e2b812a392aa508d7f22421a0a9da2
Sergey Litvinov <slitvinov@gmail.com>
2019-10-08 09:30:57 +0200

    minor

--- lib/co/bi.h
+++ lib/co/bi.h
@@ -3,10 +3,10 @@
 typedef struct He He;
 typedef struct T T;
 
-int bi_argv(const char *name, char***, He*, /**/ T**);
+int bi_argv(const char *name, char***, He*, T**);
 int bi_fin(T*);
-int bi_single(T*, He*, real alpha, const real *x, const real *y, const real *z, const real *fx, const real *fy, const real *fz, /*io*/ real*, real*, real*);
-int bi_double(T*, He*, real alpha, const real *x, const real *y, const real *z, const real *vx, const real *vy, const real *vz, /*io*/ real*, real*, real*);
+int bi_single(T*, He*, real alpha, const real *x, const real *y, const real *z, const real *fx, const real *fy, const real *fz, real*, real*, real*);
+int bi_double(T*, He*, real alpha, const real *x, const real *y, const real *z, const real *vx, const real *vy, const real *vz, real*, real*, real*);
 const char* bi_name(T*);
 int bi_good(const char*);
 const char* bi_list();
