5863b578ba259afc3d8045c16132f534d261a70e
Sergey Litvinov <slitvinov@gmail.com>
2019-10-08 14:36:00 +0200

    needs fm.conf

--- lib/make/rules.mk
+++ lib/make/rules.mk
@@ -10,10 +10,10 @@ lib$(P)_l.a: $O $(O_L); ar r $@ $O $(O_L) && ranlib $@
 
 .c.m4.c:; co.m4 -s -o $@ $<
 .h.m4.h:; co.m4 -s -o $@ $<
-.c.o:;   $(CC)                         -I. $(CFLAGS) $(CO_CFLAGS) `alg.conf --cflags d` -c -o $@ $<
-.c.s.o:; $(CC) -I$P/prec/s -Iinc/s -I. $(CFLAGS) $(CO_CFLAGS)  `alg.conf --cflags s` -c -o $@ $<
-.c.d.o:; $(CC) -I$P/prec/d -Iinc/d -I. $(CFLAGS) $(CO_CFLAGS) `alg.conf --cflags d`  -c -o $@ $<
-.c.l.o:; $(CC) -I$P/prec/l -Iinc/l -I. $(CFLAGS) $(CO_CFLAGS) `alg.conf --cflags l` -c -o $@ $<
+.c.o:;   $(CC)                     -I. $(CFLAGS) $(CO_CFLAGS) $(LIB_CFLAGS_D) -c -o $@ $<
+.c.s.o:; $(CC) -I$P/prec/s -Iinc/s -I. $(CFLAGS) $(CO_CFLAGS) $(LIB_CFLAGS_S) -c -o $@ $<
+.c.d.o:; $(CC) -I$P/prec/d -Iinc/d -I. $(CFLAGS) $(CO_CFLAGS) $(LIB_CFLAGS_D) -c -o $@ $<
+.c.l.o:; $(CC) -I$P/prec/l -Iinc/l -I. $(CFLAGS) $(CO_CFLAGS) $(LIB_CFLAGS_L) -c -o $@ $<
 
 clean:; rm -f $O $(O_S) $(O_D) $(O_L) $L
 test:
