2ecab2c74977b6bffa8209f8c0aa0a33e1f50a00
Sergey Litvinov <slitvinov@gmail.com>
2019-10-11 11:31:12 +0200

    minor

--- lib/co/i/matrix.h
+++ lib/co/i/matrix.h
@@ -1,14 +1,12 @@
 static real
 i_matrix_get(int M, int N, int m, int n, const real *a)
 {
-    USED(M);
     return a[N*m + n];
 }
 
 static int
 i_matrix_set(int M, int N, int m, int n, real s, real *a)
 {
-    USED(M);
     a[N*m + n] = s;
     return CO_OK;
 }
