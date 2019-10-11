8465d6cf779219f6165e1bd6c2ac3c3072ed6d40
Sergey Litvinov <slitvinov@gmail.com>
2019-10-11 15:34:26 +0200

    minor

--- example/vesicle/run.sh
+++ example/vesicle/run.sh
@@ -11,19 +11,21 @@ dt=0.05
 eta=645.928652122
 rho=1
 D=0.000898798148042
-A=12.5663706144
 
-init=init/0.off a=0.0392699 reg=0.0301148
+A=12.3165
+V=3.6678
+
+init=init/0.off a=0.0393 reg=0.0301148
 #init=init/1.off a=0.0098174770425 reg=0.0150574
 #init=init/2.off a=0.00245436875 reg=1
 
 make
 co.run ./main \
        garea $A 1000 \
-       volume 3.76991 5000 \
+       volume $V 5000 \
        area $a 500 \
        juelicher_xin 1 0 0 0 \
-       cortez_fm \
+       cortez_zero \
        1 $D $rho $eta $lambda $gdot $dt \
        $start $end $freq 200 \
        '<' $init
