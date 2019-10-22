1ac40a4a2eb72257c0bf3f775ed17492b5e8a8fb
bianx <bianx@ethz.ch>
2019-10-22 11:11:05 +0200

    update

---
+++ example/boundary_integral/sde_rho_eta_shear_flow/ade_Nt1280_orient/run_cont1.sh
@@ -0,0 +1,169 @@
+set -eu
+
+pi=3.141592653589793115997964
+A=12.5663706144
+V=2.68919777043
+#v=0.641998677173
+v=0.642
+R=1
+
+Nt=1280
+Kc0=20000
+
+Kb=1.0
+C0=0.0
+alpha=$(echo $pi | gawk '{print 2.0/$1}')
+Kad=$(echo $Kb $alpha | gawk '{print $1*$2}')
+D=0.000898798148042
+
+mub=139.260575205
+Ka=$(echo $mub | awk '{print $1*2}')
+a3=-2
+a4=8
+b1=0.7
+b2=0.75
+
+Da1=0.143
+
+rho=1.0
+eta=645.928652122
+start=0
+end=9000000
+freq_out=100
+freq_stat=10
+
+
+lambda=1.0
+gamdot0=0.00143923833018
+gamdot1=0.00144
+dt0=0.01
+tscale=10
+
+#number of threads
+NT=1
+export CO_ARG="-W 120:00"
+
+
+if test $# -ne 0
+then
+
+    num="$1"
+    Kc="$2"
+    
+    Kga=$Kc
+    Kv=$Kc
+    
+    V=$(echo $A, $v | awk '{print 0.09403159725795977*$1**1.5*$2}')
+    echo "A, V, v   :" $A, $V, $v
+
+    Da0=$(echo $Da1 | awk '{print $1/100}')
+    DA0=$(echo $Kb, $C0, $Kad, $Da0, $D, $pi, $A | awk '{print $4*$7}')
+    DA0D=$(echo $DA0, $D | awk '{print $1/$2}')
+
+    ref_file=../ref/ref_v0.95.off
+    
+    Da1=$(printf "%.3f" $Da1)
+    #echo "Da1="$Da1
+    lam=$(printf "%.4f" $lambda)
+    #echo "lam="$lam
+
+    gamdot=$(echo $gamdot0, $num | awk '{print $1*$2}')
+    gam=$gamdot
+    gam=$(printf "%.4f" $gam)
+    #echo "gam="$gam
+    gam1=$(echo $gamdot1, $num | awk '{print $1*$2}')
+    gam1=$(printf "%.4f" $gam1)
+    #echo "gam1="$gam1
+
+    dt=$(echo $dt0, $num, $tscale | awk '{print $1*$3/$2}')
+    #echo "dt="$dt
+    ddt=$(printf "%.4f" $dt)
+    #echo "ddt="$ddt
+
+    
+    if [ -d  ../Da${Da1}_lam${lam}_g${gam1}_dt${dt}_Kc${Kc} ]; then
+	gam=$gam1;
+    fi
+
+    if [ -d  ../Da${Da1}_lam${lam}_g${gam1}_dt${ddt}_Kc${Kc} ]; then
+	gam=$gam1;
+	dt=$ddt;
+    fi
+    
+    if [ -d  ../Da${Da1}_lam${lam}_g${gam}_dt${ddt}_Kc${Kc} ]; then
+	dt=$ddt;
+    fi
+    
+    #echo "gam="$gam
+    #echo "dt="$dt
+
+    folder="Da${Da1}_lam${lam}_g${gam}_dt${dt}_Kc${Kc}"
+    
+    if [ ! -d ${folder}_cont ]; then
+	mkdir ${folder}_cont
+    fi    
+    cd ${folder}_cont
+
+    in_file=$(ls ../${folder}/*.off | tail -n 1)
+
+    fname=`basename $in_file`
+    fnum=${fname%.*}
+    start=$(echo $fnum | sed 's/^0*//')
+
+    echo "folder:  "$folder
+    echo "in_file: "$in_file
+    echo "start: "$start
+    
+    co.run ../../main garea $A $Kga volume $V $Kv juelicher_xin $Kb $C0 $Kad $DA0D strain $ref_file lim $Ka $mub $a3 $a4 $b1 $b2 $R $D $rho $eta $lambda $gamdot $dt $start $end $freq_out $freq_stat '<' $in_file
+
+else
+    
+    num=1
+    Kc=$Kc0
+    bash run_cont1.sh $num $Kc
+
+    num=2
+    Kc=$Kc0
+    bash run_cont1.sh $num $Kc
+
+    num=5
+    Kc=$Kc0
+    bash run_cont1.sh $num $Kc
+
+    num=10
+    Kc=$Kc0
+    bash run_cont1.sh $num $Kc
+    
+    num=20
+    Kc=$Kc0
+    bash run_cont1.sh $num $Kc
+
+    num=50
+    Kc=$Kc0
+    bash run_cont1.sh $num $Kc
+
+    num=100
+    Kc=$Kc0
+    bash run_cont1.sh $num $Kc
+
+    num=200
+    Kc=40000
+    bash run_cont1.sh $num $Kc
+
+    num=500
+    Kc=100000
+    bash run_cont1.sh $num $Kc
+
+    num=1000
+    Kc=200000
+    bash run_cont1.sh $num $Kc
+
+    num=2000
+    Kc=500000
+    bash run_cont1.sh $num $Kc
+
+    num=5000
+    Kc=1000000
+    bash run_cont1.sh $num $Kc
+
+fi
