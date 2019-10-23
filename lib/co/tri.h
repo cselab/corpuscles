8bf8c9328b8808be8f2e850d03467427a059fe73
Sergey Litvinov <slitvinov@gmail.com>
2019-10-23 10:39:43 +0200

    no tri_dih

--- lib/co/tri.h
+++ lib/co/tri.h
@@ -1,7 +1,6 @@
 real tri_angle(const real[3], const real b[3], const real[3]);
 real tri_area(const real[3], const real[3], const real[3]);
 real tri_cot(const real[3], const real b[3], const real[3]);
-real tri_dih(const real[3], const real[3], const real[3], const real[3]);
 int tri_edg(const real[3], const real[3], const real[3], /**/ real[3],
             real[3], real[3]);
 real tri_volume(const real[3], const real[3], const real[3]);
