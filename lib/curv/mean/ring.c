776181f3f7c48ad60f4811cbadc8f4b883e0a67c
Sergey Litvinov <slitvinov@gmail.com>
2019-10-23 15:56:48 +0200

    flip H in ring

--- lib/curv/mean/ring.c
+++ lib/curv/mean/ring.c
@@ -25,7 +25,6 @@ int
 curv_mean_ring_ini(He * he, T **pq)
 {
     T *q;
-    int status;
     int nv;
 
     nv = he_nv(he);
@@ -50,30 +49,19 @@ int
 curv_mean_ring_apply(T * q, He * he, const real * x, const real * y, const real * z,
 	/**/ real * ans)
 {
-    real a[3], b[3], c[3], d[3], A, *area;
-    int i, j, k, l, t, nt, ne, nv;
-    int e;
-    real len, theta, coeff;
+    int nv, i;
+    int n, *rring;
+    real *C, *xyz;
     
     nv = q->nv;
     if (nv != he_nv(he))
 	ERR(CO_NUM, "nv=%d != he_nv(he)=%d", nv, he_nv(he));
-
-    int status, n, *rring;
-    real *C, *xyz;
     for (i = 0; i < nv; i++) {
-	status = he_ring(he, i, &n, &rring);
+	he_ring(he, i, &n, &rring);
 	ring_xyz(q->ring, i, rring, x, y, z, &xyz);
 	ring_C(q->ring, i, rring, x, y, z, &C);
+	ans[i] = ring_H(n, xyz, C);
     }
     
     return CO_OK;
 }
-
-int
-curv_mean_ring_area(T * q, /**/ real ** ans)
-{
-    *ans = q->area;
-    return CO_OK;
-}
-	
