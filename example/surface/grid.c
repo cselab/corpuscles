bd6864c5ac9c673e6bbe8f5f97e3cc5c912db86a
Sergey Litvinov <slitvinov@gmail.com>
2019-10-29 16:20:13 +0100

    indent

--- example/surface/grid.c
+++ example/surface/grid.c
@@ -9,65 +9,64 @@
 
 #define FMT CO_REAL_OUT
 
-enum
-{
-	X, Y, Z
+enum {
+  X, Y, Z
 };
 
 int
 main(int argc, const char **argv)
 {
-	Surface *surface;
-	He *he;
-	real *x, *y, *z, *u, *v, *w, *color;
+  Surface *surface;
+  He *he;
+  real *x, *y, *z, *u, *v, *w, *color;
 
-	int m, n, i, j, k, l;
-	real lo, hi, dx, x0, y0, z0;
-	real clo[3] = {-2, -2, -2};
-	real chi[3] = {2, 2, 2};
-	real size = 0.2;
+  int m, n, i, j, k, l;
+  real lo, hi, dx, x0, y0, z0;
+  real clo[3] = { -2, -2, -2 };
+  real chi[3] = { 2, 2, 2 };
+  real size = 0.2;
 
-	m  = 50;
-	lo = -1;
-	hi =  1;
-	n = m*m*m;
-	MALLOC(n, &u);
-	MALLOC(n, &v);
-	MALLOC(n, &w);
-	CALLOC(n, &color);
+  m = 50;
+  lo = -1;
+  hi = 1;
+  n = m * m * m;
+  MALLOC(n, &u);
+  MALLOC(n, &v);
+  MALLOC(n, &w);
+  CALLOC(n, &color);
 
-	y_inif(stdin, &he, &x, &y, &z);
-	surface_ini(clo, chi, size, &surface);
-	surface_update(surface, he, x, y, z);
+  y_inif(stdin, &he, &x, &y, &z);
+  surface_ini(clo, chi, size, &surface);
+  surface_update(surface, he, x, y, z);
 
-	dx = (hi - lo)/(m - 1);
-	for (l = i = 0; i <m ; i++) {
-		x0 = lo + dx*i;
-		for (j = 0; j < m; j++) {
-			y0 = lo + dx*j;
-			for (k = 0; k < m; k++) {
-				z0 = lo + dx*k;
-				u[l] = x0;
-				v[l] = y0;
-				w[l]= z0;
-				color[l] = surface_inside_fast(surface, u[l], v[l], w[l]);
-				l++;
-			}
-		}
-	}
+  dx = (hi - lo) / (m - 1);
+  for (l = i = 0; i < m; i++) {
+    x0 = lo + dx * i;
+    for (j = 0; j < m; j++) {
+      y0 = lo + dx * j;
+      for (k = 0; k < m; k++) {
+        z0 = lo + dx * k;
+        u[l] = x0;
+        v[l] = y0;
+        w[l] = z0;
+        color[l] = surface_inside_fast(surface, u[l], v[l], w[l]);
+        l++;
+      }
+    }
+  }
 
-	const real *q[] = {
-		u, v, w, color, NULL
-	};
-	punto_fwrite(n, q, stdout);
+  const real *q[] = {
+    u, v, w, color, NULL
+  };
+  punto_fwrite(n, q, stdout);
 
-	FREE(u); 
-	FREE(v); 
-	FREE(w); 
-	FREE(color);
-	y_fin(he, x, y, z);
-	surface_fin(surface);
-	return 0;
+  FREE(u);
+  FREE(v);
+  FREE(w);
+  FREE(color);
+  y_fin(he, x, y, z);
+  surface_fin(surface);
+  return 0;
 }
 
 /*
