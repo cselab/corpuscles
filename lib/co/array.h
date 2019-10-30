c1b5265cf417589f44524e5176fc48dc2e31ccc3
Sergey Litvinov <slitvinov@gmail.com>
2019-10-30 12:44:59 +0100

    remove duplicates

--- lib/co/array.h
+++ lib/co/array.h
@@ -1,36 +1,25 @@
-int array_axpy3(int, real, const real[], const real[], const real[],
-                /**/ real[], real[], real[]);
+int array_axpy3(int, real, const real[], const real[], const real[], /**/ real[], real[], real[]);
 int array_axpy(int, real, const real[], /**/ real[]);
-int array_copy3(int, const real[], const real[], const real[], real[],
-                real[], real[]);
+int array_copy3(int, const real[], const real[], const real[], real[], real[], real[]);
 int array_copy(int, const real[], real[]);
 int array_fwrite(FILE *, int, const real[]);
 int array_fwrite(FILE *, int, const real[]);
+int array_multiply3(int, const real[], real[], real[], real[]);
+int array_multiply(int, const real[], real[]);
 int array_negative(int, const real[], real[]);
 int array_neg(int, real[]);
 int array_one(int, real[]);
-int array_one(int, real[]);
 int array_scale3(int, real, real[], real[], real[]);
 int array_scale(int, real, real[]);
-int array_multiply(int, const real[], real[]);
-int array_multiply3(int, const real[], real[], real[], real[]);
 int array_zero3(int, real[], real[], real[]);
 int array_zero(int, real[]);
-int array_zero(int, real[]);
-real array_diff3(int, const real[], const real[], const real[],
-                 const real[], const real[], const real[]);
+real array_diff3(int, const real[], const real[], const real[], const real[], const real[], const real[]);
 real array_diff(int, const real[], const real[]);
 real array_dot(int, const real[], const real[]);
-real array_dot(int, const real[], const real[]);
-real array_l2_3d(int, const real[], const real[], const real[],
-                 const real[], const real[], const real[]);
-real array_l2(int, const real[], const real[]);
+real array_l2_3d(int, const real[], const real[], const real[], const real[], const real[], const real[]);
 real array_l2(int, const real[], const real[]);
 real array_max(int, const real[]);
-real array_max(int, const real[]);
 real array_mean(int, const real[]);
-real array_mean(int, const real[]);
-real array_min(int, const real[]);
 real array_min(int, const real[]);
 real array_msq_3d(int, const real[], const real[], const real[]);
 real array_msq(int, const real[]);
