60e6ec31977389db04024208e699245a157fb322
Sergey Litvinov <slitvinov@gmail.com>
2019-11-03 16:56:58 +0000

    add -v option

--- bin/eigenvector/main.c
+++ bin/eigenvector/main.c
@@ -1,45 +1,83 @@
 #include <stdio.h>
-#include <tgmath.h>
+#include <stdlib.h>
 #include <real.h>
 #include <co/eigen.h>
 #include <co/err.h>
 #include <co/he.h>
 #include <co/macro.h>
-#include <co/memory.h>
+#include <co/vec.h>
 #include <co/y.h>
+#define FMT CO_REAL_OUT
+const char me[] = "co.eigenvector";
 
-static int nv;
-static real *x, *y, *z;
-static real vec[3 * 3];
-static He *he;
-static Eigen *eigen;
+static void
+usg(void)
+{
+    fprintf(stderr, "%s [-sptv] < C.off\n", me);
+    fprintf(stderr, "  -t   use triangles\n", me);
+    fprintf(stderr, "  -s   use surface\n", me);
+    fprintf(stderr, "  -p   use points (default)\n", me);
+    fprintf(stderr, "  -v   print eigenvalues (default: eigenvector)\n", me);
+    exit(2);
+}
 
 static int
 eigen_vector_point(Eigen * q, He * he, real * x, real * y, real * z,
-                   real * v)
+		   real * v)
 {
     USED(he);
     return eigen_vector(q, x, y, z, v);
 }
 
 int
-main(int argc, char **argv)
+main(__UNUSED int argc, char **argv)
 {
-    enum { POINT, SURFACE, TRI };
-    int (*Alg)(Eigen *, He *, real *, real *, real *, real *);
+    enum { VECTOR, VALUE };
+    int Report;
+    real *x, *y, *z, vec[3 * 3], val[3];
+    He *he;
+    Eigen *eigen;
 
-    y_inif(stdin, &he, &x, &y, &z);
-    nv = he_nv(he);
-    eigen_ini(he, &eigen);
+    int (*Alg)(Eigen *, He *, real *, real *, real *, real *);
+    const char *arg;
 
     argv++;
     Alg = eigen_vector_point;
-    if (argv[0] && argv[0][0] == '-' && argv[0][1] == 's')
-        Alg = eigen_vector_surface;
-    if (argv[0] && argv[0][0] == '-' && argv[0][1] == 't')
-        Alg = eigen_vector_tri;
+    Report = VECTOR;
+    while (*argv != NULL) {
+	arg = argv++[0];
+	if (arg[0] != '-')
+	    break;
+	switch (arg[1]) {
+	case 'h':
+	    usg();
+	    break;
+	case 's':
+	    Alg = eigen_vector_surface;
+	    break;
+	case 'p':
+	    Alg = eigen_vector_point;
+	    break;
+	case 't':
+	    Alg = eigen_vector_tri;
+	    break;
+	case 'v':
+	    Report = VALUE;
+	    break;
+	default:
+	    ER("%s: unknown option '%s'", me, arg);
+	}
+    }
+
+    y_inif(stdin, &he, &x, &y, &z);
+    eigen_ini(he, &eigen);
     Alg(eigen, he, x, y, z, vec);
-    printf("%.16g %.16g %.16g\n", vec[0], vec[3], vec[6]);
+    eigen_values(eigen, val);
+
+    if (Report == VECTOR)
+	printf(FMT " " FMT " " FMT "\n", vec[0], vec[3], vec[6]);
+    else
+	vec_printf(val, FMT);
     eigen_fin(eigen);
     y_fin(he, x, y, z);
     return 0;
