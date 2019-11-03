2111ad8505e5b566fad19d79126954cb36a14eb3
Sergey Litvinov <slitvinov@gmail.com>
2019-11-03 11:24:20 +0000

    off: add read_bin

--- lib/off.c
+++ lib/off.c
@@ -30,25 +30,18 @@ struct T {
   int nv, nt;
 };
 
-int
-off_inif(FILE * f, T ** pq)
+
+#define NXT() if (util_comment_fgets(line, f) == NULL)  \
+	ERR(CO_IO, "unexpected EOF")
+static int
+read_text(T *q, FILE * f)
 {
-  T *q;
   char line[SIZE];
   int i, nv, nt;
   int *t0, *t1, *t2, cnt, np;
   real *ver, *x, *y, *z;
   int *tri;
-
-  MALLOC(1, &q);
-  if (f == NULL)
-    ERR(CO_IO, "fail to read");
-
-#define NXT() if (util_comment_fgets(line, f) == NULL)  \
-	ERR(CO_IO, "unexpected EOF")
-  NXT();
-  if (!util_eq(line, "OFF"))
-    ERR(CO_IO, "expecting 'OFF' got '%s'", line);
+  
   NXT();
   cnt = sscanf(line, "%d %d %*d", &nv, &nt);
   if (cnt != 2)
@@ -83,11 +76,77 @@ off_inif(FILE * f, T ** pq)
   }
   q->nv = nv;
   q->nt = nt;
-  *pq = q;
-  return CO_OK;
-#undef NXT
 }
 
+static int
+read_binary(T *q, FILE * f)
+{
+  char line[SIZE];
+  int i, nv, nt;
+  int *t0, *t1, *t2, cnt, np;
+  real *ver, *x, *y, *z;
+  int *tri;
+  
+  NXT();
+  cnt = sscanf(line, "%d %d %*d", &nv, &nt);
+  if (cnt != 2)
+    ERR(CO_IO, "fail to parse: '%s'", line);
+  if (3 * nt < nv)
+    ERR(CO_IO, "3*(nt=%d)   <   nv=%d", nt, nv);
+
+  MALLOC(3 * nv, &q->ver);
+  ver = q->ver;
+  MALLOC(3 * nt, &q->tri);
+  tri = q->tri;
+  for (i = 0; i < nv; i++) {
+    NXT();
+    x = ver++;
+    y = ver++;
+    z = ver++;
+    cnt = sscanf(line, FMT " " FMT " " FMT, x, y, z);
+    if (cnt != 3)
+      ERR(CO_IO, "wrong vertex line '%s'", line);
+  }
+
+  for (i = 0; i < nt; i++) {
+    NXT();
+    t0 = tri++;
+    t1 = tri++;
+    t2 = tri++;
+    cnt = sscanf(line, "%d %d %d %d", &np, t0, t1, t2);
+    if (cnt != 4)
+      ERR(CO_IO, "wrong triangle line '%s'", line);
+    if (np != 3)
+      ERR(CO_IO, "not a triangle '%s'", line);
+  }
+  q->nv = nv;
+  q->nt = nt;
+}
+
+int
+off_inif(FILE * f, T ** pq)
+{
+  T *q;
+  char line[SIZE];
+  int status;
+
+  MALLOC(1, &q);
+  if (f == NULL)
+    ERR(CO_IO, "fail to read");
+  NXT();
+  if (util_eq(line, "OFF"))
+    status = read_text(q, f);
+  else if (util_eq(line, "COFF"))
+    status = read_binary(q, f);
+  else
+    ERR(CO_IO, "expecting 'OFF' got '%s'", line);
+  if (status != CO_OK)
+    ERR(CO_IO, "read_xt failed");
+  *pq = q;
+  return CO_OK;
+}
+
+#undef NXT
 int
 off_ini(const char *path, T ** pq)
 {
