c752dd0deb24ead44303d1bfee3269d47fb7c6e2
Sergey Litvinov <slitvinov@gmail.com>
2019-11-07 17:08:33 +0100

    ini

---
+++ example/list/ini.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <co/err.h>
+#include <co/list/a.h>
+#include <co/list/c.h>
+
+int
+main(void)
+{
+    Clist *clist;
+    int n;
+
+    n = 3;
+    clist_gen_nn(n, n, &clist);
+    clist_fin(clist);
+
+    return 0;
+}
