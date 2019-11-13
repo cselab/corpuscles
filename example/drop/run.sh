ea3137d631c973ca9965b96e35c4a586280f695b
Sergey Litvinov <slitvinov@gmail.com>
2019-11-13 20:20:19 +0000

    eta=1, sigma=1

--- example/drop/run.sh
+++ example/drop/run.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 
-set -eu
+. co.util
 
-gdot=0.1
-lambda=1
+Ca=1.5
+area=12.5065
+gdot=`ae $Ca*$area`
+lambda=3.6
 start=0
 end=1000000
 freq=100
-dt=0.01
-eta=645.928652122
+dt=0.001
+eta=1
 rho=1
 
 A=12.3165
 V=3.6678
-sigma=10
+sigma=1
 
 #init=`co.path`/sph/icosa/Nt320.off
 init=`co.path`/sph/icosa/Nt1280.off
-#init=init/1.off a=0.009622265625
-#init=init/2.off a=0.00245436875
 
 make
 co.run ./main \
-       area_diff 1 \
+       area_diff 0.1 \
        garea_sq $sigma \
        cortez_zero \
        $rho $eta $lambda $gdot $dt \
