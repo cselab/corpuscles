d000d7decc7af552e4f755bda17214803efffd04
Sergey Litvinov <slitvinov@gmail.com>
2019-11-13 22:33:02 +0000

    up

--- example/drop/run.sh
+++ example/drop/run.sh
@@ -2,10 +2,11 @@
 
 . co.util
 
-#lambda=0.08 Ca=0.4
+lambda=0.08 Ca=0.4
 #lambda=1 Ca=0.38
 #lambda=3.6 Ca=1.5
-lambda=6.4 Ca=2.5
+#lambda=6.4 Ca=2.5
+
 area=12.5065
 gdot=`ae $Ca*$area`
 start=0
@@ -24,7 +25,7 @@ init=`co.path`/sph/icosa/Nt1280.off
 
 make
 co.run ./main \
-       area_diff 0.1 \
+       area_diff 0.02 \
        garea_sq $sigma \
        cortez_zero \
        $rho $eta $lambda $gdot $dt \
