2b0442fd9e1e0023bcf434f19259802e16a110f8
Sergey Litvinov <slitvinov@gmail.com>
2019-11-14 12:30:25 +0100

    use correct Ca

--- example/drop/run.sh
+++ example/drop/run.sh
@@ -8,7 +8,7 @@ lambda=1 Ca=0.38
 #lambda=6.4 Ca=2.5
 
 area=12.5065
-gdot=`ae $Ca*$area`
+gdot=$Ca
 start=0
 end=1000000
 freq=100
@@ -26,7 +26,7 @@ init=`co.path`/sph/icosa/Nt1280.off
 make
 co.run ./main \
        area_diff 0.1 \
-       garea_sq $sigma \
+       garea_zero $sigma \
        cortez_zero \
        $rho $eta $lambda $gdot $dt \
        $start $end $freq 200 \
