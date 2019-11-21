53028dd96de9aaab19a1604a2144fac05cf713db
Sergey Litvinov <slitvinov@gmail.com>
2019-11-21 17:44:30 +0100

    add two

---
+++ example/two/run.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+. co.util
+
+prog=run.sh
+if test $# -eq 0
+then err 'missing input file'
+fi
+i=$1; shift
+V=2.6892
+kv=20000
+A=12.5663706144
+kga=20000
+R=1
+D=0.000898798148042
+rho=1
+eta=645.928652122
+lambda=5
+gamdot=0.576
+start=0
+end=100
+freq_out=1
+freq_stat=100
+dt=`ae 0.01*$gamdot`
+
+make
+co.run ./main volume $V $kv \
+       garea $A $kga \
+       juelicher_xin 1.0 0.0 0.63662 19.9933 \
+       strain ref/ref_v0.95.off lim 278.521 139.260575205 -2 8 0.7 0.75 \
+       cortez_zero \
+       $R $D $rho $eta $lambda $gamdot $dt $start $end $freq_out $freq_stat \
+       '<' "$i"
