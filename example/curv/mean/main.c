4e7ce674108bae58ac14d794c94117fd692142e7
Sergey Litvinov <slitvinov@gmail.com>
2019-11-28 16:38:49 +0100

    add curv/mean/ring.c

--- example/curv/mean/main.c
+++ example/curv/mean/main.c
@@ -12,94 +12,133 @@
 
 real *tx, *ty, *tz, *ang, *vx, *vy, *vz;
 
-static void get3(int i, int j, int k, /**/
-          real a[3], real b[3], real c[3]) {
+static void
+get3(int i, int j, int k, /**/ real a[3], real b[3], real c[3])
+{
     vec_get(i, XX, YY, ZZ, a);
     vec_get(j, XX, YY, ZZ, b);
     vec_get(k, XX, YY, ZZ, c);
 }
 
-static void get_edg(int i, int j, real r[3]) {
+static void
+get_edg(int i, int j, real r[3])
+{
     real a[3], b[3];
+
     vec_get(i, XX, YY, ZZ, a);
     vec_get(j, XX, YY, ZZ, b);
     vec_minus(a, b, r);
 }
 
-static void mesh_T(real *cot, /**/ real *H) {
+static void
+mesh_T(real * cot, /**/ real * H)
+{
     int h, f;
     real T;
+
     for (h = 0; h < NH; h++) {
-        T  = cot[h];
+        T = cot[h];
         if ((f = flp(h)) != -1)
             T += cot[f];
         H[h] = T;
     }
 }
 
-static void mesh_cot(real *H) {
+static void
+mesh_cot(real * H)
+{
     int h, n, nn;
     int i, j, k;
     real a[3], b[3], c[3];
+
     for (h = 0; h < NH; h++) {
-        n = nxt(h); nn = nxt(n);
-        i = ver(h); j = ver(n); k = ver(nn);
+        n = nxt(h);
+        nn = nxt(n);
+        i = ver(h);
+        j = ver(n);
+        k = ver(nn);
         get3(i, j, k, /**/ a, b, c);
         H[h] = tri_cot(a, b, c);
     }
 }
 
-static void mesh_l2(real *H) {
+static void
+mesh_l2(real * H)
+{
     int h, n;
     int i, j;
     real r[3];
+
     for (h = 0; h < NH; h++) {
         n = nxt(h);
-        i = ver(h); j = ver(n);
+        i = ver(h);
+        j = ver(n);
         get_edg(i, j, /**/ r);
         H[h] = vec_dot(r, r);
     }
 }
 
-static void mesh_voronoi(real *T, real *l2, /**/ real *V) {
+static void
+mesh_voronoi(real * T, real * l2, /**/ real * V)
+{
     int h, i;
+
     for (h = 0; h < NH; h++) {
         i = ver(h);
-        V[i] += T[h]*l2[h]/4;
+        V[i] += T[h] * l2[h] / 4;
     }
 }
 
-static void mesh_laplace(real *V0, real *T, real *A, /**/ real *V1) {
+static void
+mesh_laplace(real * V0, real * T, real * A, /**/ real * V1)
+{
     int h, n, i, j;
+
     for (h = 0; h < NH; h++) {
         n = nxt(h);
-        i = ver(h); j = ver(n);
-        V1[i] += T[h]*(V0[i] - V0[j])/2;
+        i = ver(h);
+        j = ver(n);
+        V1[i] += T[h] * (V0[i] - V0[j]) / 2;
     }
     for (i = 0; i < NV; i++)
         V1[i] /= A[i];
 }
 
-static real sum(int n, real *a) {
+static real
+sum(int n, real * a)
+{
     int i;
     real s;
+
     for (i = 0, s = 0; i < n; i++)
         s += a[i];
     return s;
 }
 
-static void main0() {
+static void
+main0()
+{
     RZERO(NH, &ang);
-    RZERO(NH, &tx); RZERO(NH, &ty); RZERO(NH, &tz); /* tri normal */
-    
-    RZERO(NV, &vx); RZERO(NV, &vy); RZERO(NV, &vz); /* ver normal */
+    RZERO(NH, &tx);
+    RZERO(NH, &ty);
+    RZERO(NH, &tz);             /* tri normal */
+
+    RZERO(NV, &vx);
+    RZERO(NV, &vy);
+    RZERO(NV, &vz);             /* ver normal */
 
     FREE(ang);
-    FREE(tx); FREE(ty); FREE(tz);
-    FREE(vx); FREE(vy); FREE(vz);
+    FREE(tx);
+    FREE(ty);
+    FREE(tz);
+    FREE(vx);
+    FREE(vy);
+    FREE(vz);
 }
 
-int main() {
+int
+main()
+{
     ini("/dev/stdin");
     main0();
     fin();
--- example/curv/mean/ring.c
+++ example/curv/mean/ring.c
@@ -3,7 +3,7 @@
 
 #include <real.h>
 #include <co/array.h>
-#include <co/curv/mean/juelicher.h>
+#include <co/curv/mean/ring.h>
 #include <co/err.h>
 #include <co/he.h>
 #include <co/memory.h>
@@ -17,23 +17,22 @@ int
 main(void)
 {
     int nv;
-    real *mean, *area, *x, *y, *z;
+    real *mean, *x, *y, *z;
     He *he;
-    CurvMeanJuelicher *q;
+    CurvMeanRing *q;
     int t, nt, i, j, k;
     real *tri;
 
     y_inif(stdin, &he, &x, &y, &z);
     nv = he_nv(he);
-    curv_mean_juelicher_ini(he, &q);
+    curv_mean_ring_ini(he, &q);
     MALLOC(nv, &mean);
-    curv_mean_juelicher_apply(q, he, x, y, z, mean);
-    curv_mean_juelicher_area(q, &area);
-    const real *queue[] = { x, y, z, area, mean, NULL };
-    puts("x y z area mean");
+    curv_mean_ring_apply(q, he, x, y, z, mean);
+    const real *queue[] = { x, y, z, mean, NULL };
+    puts("x y z mean");
     punto_fwrite(nv, queue, stdout);
     MSG(FMT " " FMT, array_min(nv, mean), array_max(nv, mean));
-    curv_mean_juelicher_fin(q);
+    curv_mean_ring_fin(q);
     y_fin(he, x, y, z);
     FREE(mean);
 }
