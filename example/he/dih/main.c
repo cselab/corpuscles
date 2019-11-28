7beaa282632672f56cefc1e8ef7b5166c1b02982
Sergey Litvinov <slitvinov@gmail.com>
2019-11-28 16:28:26 +0100

    add example/he/dih/ijkl

--- example/he/dih/main.c
+++ example/he/dih/main.c
@@ -6,16 +6,17 @@
 #include <co/he.h>
 
 int
-main() {
-	He      *he;
-	real *x, *y, *z;
-	int *q, *u, *v, *w, ne, i;
+main()
+{
+    He *he;
+    real *x, *y, *z;
+    int *q, *u, *v, *w, ne, i;
 
-	y_inif(stdin, &he, &x, &y, &z);
-	ne = he_ne(he);
-	he_D(he, &q, &u, &v, &w);
-	for (i = 0; i < ne; i++)
-		printf("%d %d %d %d\n", q[i], u[i], v[i], w[i]);
-	y_fin(he, x, y, z);
-	return CO_OK;
+    y_inif(stdin, &he, &x, &y, &z);
+    ne = he_ne(he);
+    he_D(he, &q, &u, &v, &w);
+    for (i = 0; i < ne; i++)
+        printf("%d %d %d %d\n", q[i], u[i], v[i], w[i]);
+    y_fin(he, x, y, z);
+    return CO_OK;
 }
