bac73a32dc5cc43c0adb57694347e541dfd08ec4
Sergey Litvinov <slitvinov@gmail.com>
2019-12-02 19:42:27 +0100

    initial import of povray export

---
+++ lib/co/povray.h
@@ -0,0 +1,2 @@
+typedef struct He He;
+int povray_mesh2(He *, const real *, const real *, const real *, FILE *);
---
+++ lib/povray.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "real.h"
+#include "co/array.h"
+#include "co/err.h"
+#include "co/he.h"
+
+#include "co/povray.h"
+
+int
+povray_mesh2(He * he, const real * x, const real * y, const real * z, FILE *f)
+{
+  return CO_OK;
+}
