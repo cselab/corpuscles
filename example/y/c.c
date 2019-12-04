01d66ce9384a98c48aa2b8ae25dce77fb0aec7c3
Sergey Litvinov <slitvinov@gmail.com>
2019-12-04 10:11:06 +0100

    add c.c: dump he as a static array

--- example/y/c.c
+++ example/y/c.c
@@ -10,6 +10,9 @@
 #include <co/y.h>
 
 const char *me = "y/c";
+#define TAB "  "
+#define Prefix "ico_"
+#define FMT CO_REAL_OUT
 static void
 usg()
 {
@@ -17,19 +20,44 @@ usg()
     exit(2);
 }
 
+#define COORD(x)						\
+    do {							\
+	printf("static double " Prefix #x "[%d] = {\\\n", nv);	\
+	for (i = 0; i < nv; i++)				\
+	    printf(TAB FMT ",\\\n", x[i]);			\
+	puts("};");						\
+    } while (0);						\
+
+#define INDEX(x)						\
+    do {							\
+	printf("static int " Prefix #x "[%d] = {\\\n", nt);	\
+	for (i = 0; i < nt; i++)				\
+	    printf(TAB "%d" ",\\\n", x[i]);			\
+	puts("};");						\
+    } while (0);						\
+
+
 int
 main(int argc, char **argv)
 {
     He *he;
     real *x, *y, *z;
     int *t0, *t1, *t2;
-    int nv, nt;
+    int nv, nt, i;
 
     USED(argc);
     USED(argv);
     y_inif(stdin, &he, &x, &y, &z);
-
+    nv = he_nv(he);
+    nt = he_nt(he);
     he_T(he, &t0, &t1, &t2);
-    
+    printf("static int " Prefix "nv = %d;\n", nv);
+    printf("static int " Prefix "nt = %d;\n", nt);
+    COORD(x);
+    COORD(y);
+    COORD(z);
+    INDEX(t0);
+    INDEX(t1);
+    INDEX(t2);
     y_fin(he, x, y, z);
 }
