33d8984cd9dadff705d2b693e625af9571f4d52b
Sergey Litvinov <slitvinov@gmail.com>
2019-12-13 13:19:19 +0100

    middle: loop subdivision which put vert in a center

--- example/loop/middle.c
+++ example/loop/middle.c
@@ -5,6 +5,7 @@
 #include <co/he.h>
 #include <co/macro.h>
 #include <co/memory.h>
+#include <co/off.h>
 #include <co/vec.h>
 #include <co/tri.h>
 #include <co/y.h>
@@ -17,13 +18,14 @@ usg()
     exit(2);
 }
 
-static int t2edg(He *he, int t, int *, int *, int *);
-static int t2ver(He *he, int t, int *, int *, int *);
+static int t2edg(He * he, int t, int *, int *, int *);
+static int t2ver(He * he, int t, int *, int *, int *);
 
 int
 main()
 {
     He *he;
+    He *he0;
     int i;
     int j;
     int k;
@@ -43,9 +45,9 @@ main()
     real *y0;
     real *z;
     real *z0;
-    int ei;
-    int ej;
-    int ek;
+    int eij;
+    int ejk;
+    int eki;
     int i0;
 
     y_inif(stdin, &he, &x, &y, &z);
@@ -53,12 +55,12 @@ main()
     nv = he_nv(he);
     nt = he_nt(he);
     nv0 = nv + ne;
-    nt0 = 4*nt;
+    nt0 = 4 * nt;
     MALLOC(nv0, &x0);
     MALLOC(nv0, &y0);
     MALLOC(nv0, &z0);
-    MALLOC(3*nt0, &tri);
-    
+    MALLOC(3 * nt0, &tri);
+
 
     real a[3];
     real b[3];
@@ -66,67 +68,67 @@ main()
     real ab[3];
     real bc[3];
     real ca[3];
+
 #define ADD(i, j, k) tri[i0++] = (i), tri[i0++] = (j), tri[i0++] = (k)
-    for (i0 = t = 0; t < nt; t++)
-    {
-	t2ver(he, t, &i, &j, &k);
-	t2edg(he, t, &ei, &ej, &ek);
-	ij = nv + ei;
-	jk = nv + ej;
-	ki = nv + ek;
-	MSG("%d %d %d", ij, jk, ki);
-	ADD(i, ij, ki);
-	ADD(ij, j, jk);
-	ADD(ij, jk, ki);
-	ADD(ki, jk, k);
-	vec_get3(i, j, k, x, y, z, a, b, c);
-	tri_edg_center(a, b, c, ab, bc, ca);
-	vec_set(a, i, x0, y0, z0);
-	vec_set(b, j, x0, y0, z0);
-	vec_set(c, k, x0, y0, z0);
-	vec_set(ab, ij, x0, y0, z0);
-	vec_set(bc, jk, x0, y0, z0);
-	vec_set(ca, ki, x0, y0, z0);
+    for (i0 = t = 0; t < nt; t++) {
+        t2ver(he, t, &i, &j, &k);
+        t2edg(he, t, &eij, &ejk, &eki);
+        ij = nv + eij;
+        jk = nv + ejk;
+        ki = nv + eki;
+        ADD(i, ij, ki);
+        ADD(ij, j, jk);
+        ADD(ij, jk, ki);
+        ADD(ki, jk, k);
+        vec_get3(i, j, k, x, y, z, a, b, c);
+        tri_edg_center(a, b, c, bc, ca, ab);
+        vec_set(a, i, x0, y0, z0);
+        vec_set(b, j, x0, y0, z0);
+        vec_set(c, k, x0, y0, z0);
+        vec_set(ab, ij, x0, y0, z0);
+        vec_set(bc, jk, x0, y0, z0);
+        vec_set(ca, ki, x0, y0, z0);
     }
-    if (i0 != 3*nt0)
-	ER("i0=%d != nt0=%d", i0, nt0);
+    if (i0 != 3 * nt0)
+        ER("i0=%d != nt0=%d", i0, nt0);
+    he_tri_ini(nv0, nt0, tri, &he0);
+    if (off_he_xyz_fwrite(he0, x0, y0, z0, stdout) != CO_OK)
+        ER("off_he_xyz_fwrite failed");
 
     FREE(x0);
     FREE(y0);
     FREE(z0);
     FREE(tri);
+    he_fin(he0);
     y_fin(he, x, y, z);
 }
 
 static int
-t2edg(He *he, int t, int *i, int *j, int *k)
+t2edg(He * he, int t, int *i, int *j, int *k)
 {
     int h, n, nn;
-    
+
     h = he_hdg_tri(he, t);
     n = he_nxt(he, h);
     nn = he_nxt(he, n);
-
     *i = he_edg(he, h);
     *j = he_edg(he, n);
     *k = he_edg(he, nn);
-    
+
     return CO_OK;
 }
 
 static int
-t2ver(He *he, int t, int *i, int *j, int *k)
+t2ver(He * he, int t, int *i, int *j, int *k)
 {
     int h, n, nn;
-    
+
     h = he_hdg_tri(he, t);
     n = he_nxt(he, h);
     nn = he_nxt(he, n);
-
     *i = he_ver(he, h);
     *j = he_ver(he, n);
     *k = he_ver(he, nn);
-    
+
     return CO_OK;
 }
-
