eebf5c95d64f9d47d1befb3391980995a7f6eca4
Sergey Litvinov <slitvinov@gmail.com>
2019-12-13 19:13:58 +0100

    clean up sqrt3

--- bin/sqrt3/main.c
+++ bin/sqrt3/main.c
@@ -11,7 +11,7 @@
 #include <co/tri.h>
 #include <co/y.h>
 
-static const char *me = "sqrt3/spline";
+static const char *me = "sqrt3";
 static void
 usg()
 {
@@ -19,6 +19,8 @@ usg()
     exit(2);
 }
 
+static int t2edg(He * he, int t, int *, int *, int *);
+static int t2ver(He * he, int t, int *, int *, int *);
 static const real pi = 3.141592653589793;
 
 int
@@ -27,20 +29,32 @@ main(int argc, char **argv)
     USED(argc);
     He *he;
     He *he0;
+    int e;
+    int eij;
+    int ejk;
+    int eki;
     int i;
     int i0;
+    int ij;
     int j;
+    int jk;
     int k;
+    int ki;
+    int ne;
     int nt;
     int nt0;
     int nv;
     int nv0;
+    int t;
     int *tri;
     real alpha;
     real beta;
     real a[3];
+    real ab[3];
     real b[3];
+    real bc[3];
     real c[3];
+    real ca[3];
     real *x;
     real *x0;
     real *y;
@@ -59,6 +73,7 @@ main(int argc, char **argv)
             ER("unknown option '%s'", argv[0]);
         }
     y_inif(stdin, &he, &x, &y, &z);
+    ne = he_ne(he);
     nv = he_nv(he);
     nt = he_nt(he);
     nv0 = nv + nt;
@@ -69,10 +84,12 @@ main(int argc, char **argv)
     MALLOC(3 * nt0, &tri);
 #define ADD(i, j, k) tri[i0++] = (i), tri[i0++] = (j), tri[i0++] = (k)
     int v;
+    int v0;
     int iring;
     real g[3];
 
     i0 = 0;
+    v0 = nv;
     for (v = 0; v < nv; v++) {
         he_tri_ring(he, v, &nring, &ring);
         for (iring = 0; ring[iring + 1] != -1; iring++) {
@@ -113,3 +130,32 @@ main(int argc, char **argv)
     y_fin(he, x, y, z);
 }
 
+static int
+t2edg(He * he, int t, int *i, int *j, int *k)
+{
+    int h, n, nn;
+
+    h = he_hdg_tri(he, t);
+    n = he_nxt(he, h);
+    nn = he_nxt(he, n);
+    *i = he_edg(he, h);
+    *j = he_edg(he, n);
+    *k = he_edg(he, nn);
+
+    return CO_OK;
+}
+
+static int
+t2ver(He * he, int t, int *i, int *j, int *k)
+{
+    int h, n, nn;
+
+    h = he_hdg_tri(he, t);
+    n = he_nxt(he, h);
+    nn = he_nxt(he, n);
+    *i = he_ver(he, h);
+    *j = he_ver(he, n);
+    *k = he_ver(he, nn);
+
+    return CO_OK;
+}
