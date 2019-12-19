d3d157a4a81f5f0edc18f7977edbfb5be845e805
Sergey Litvinov <slitvinov@gmail.com>
2019-12-19 18:27:35 +0100

    indent

--- lib/co/vtk.h
+++ lib/co/vtk.h
@@ -1,5 +1,5 @@
 typedef struct He He;
-enum {VTK_INT, VTK_REAL};
+enum { VTK_INT, VTK_REAL };
 int vtk_fwrite(He *, const real *, const real *, const real *,
                const real **, const char **, /**/ FILE *);
 int vtk_tri_fwrite(He *, const real *, const real *, const real *,
