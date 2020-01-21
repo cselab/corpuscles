f0a95d9eab05215ec5eea5ac9f0457fbd5999861
Sergey Litvinov <slitvinov@gmail.com>
2020-01-21 18:15:36 +0100

    add s0()

--- lib/green/3wall.c
+++ lib/green/3wall.c
@@ -57,7 +57,7 @@ green3_wall_fin(T * q)
 #define SET(i, j, s, K) i_matrix_set(n, n, i, j, s, t->d[K])
 int
 green3_wall_apply(T * q, He * he, const real * x, const real * y,
-                  const real * z, struct Tensor3x3 *t)
+		  const real * z, struct Tensor3x3 *t)
 {
     int n, i;
     real s;
@@ -69,17 +69,17 @@ green3_wall_apply(T * q, He * he, const real * x, const real * y,
     tensor3x3_zero(n, t);
 #pragma omp parallel for
     for (i = 0; i < n; i++) {
-        real a[3], b[3], xx, xy, xz, yy, yz, zz;
-        int j;
+	real a[3], b[3];
+	int j;
 
-        i_vec_get(i, x, y, z, a);
-        for (j = 0; j < n; j++) {
-            if (i == j)
-                continue;
-            i_vec_get(j, x, y, z, b);
-            if (green3_wall_s(q, a, b, &t0) != CO_OK)
-                ERR(CO_NUM, "ossen failed (i=%d, j=%d)", i, j);
-        }
+	i_vec_get(i, x, y, z, a);
+	for (j = 0; j < n; j++) {
+	    if (i == j)
+		continue;
+	    i_vec_get(j, x, y, z, b);
+	    if (green3_wall_s(q, a, b, &t0) != CO_OK)
+		ERR(CO_NUM, "ossen failed (i=%d, j=%d)", i, j);
+	}
     }
     s = 1 / (8 * pi);
     tensor3x3_scale(n, s, t);
@@ -95,15 +95,48 @@ rad(real x, real y, real z)
     eps = 1e-32;
     r = x * x + y * y + z * z;
     if (r < eps)
-        ERR(CO_NUM, "r < eps");
+	ERR(CO_NUM, "r < eps");
     return sqrt(r);
+}
+
+static int
+s0(T * q, const real a[3], Ten * t0)
+{
+    enum {
+	X, Y, Z
+    };
+    real w;
+    real *t;
+    real r1;
+    real r13;
+    real r15;
+    real w2;
+    real zw;
+
+    t = t0->t;
+    w = q->w - a[Z];
+    zw = - 2 * w;
+    r1 = 1 / fabs(zw);
+    r13 = r1 * r1 * r1;
+    r15 = r1 * r1 * r1 * r1 * r1;
+    w2 = w * w;
+    t[XX] = (-2*r13*w2)-r1;
+    t[YY] = t[XX];
+    t[ZZ] = (-2*w2*(12*r15*w2-r13))-4*r13*w2-r1;
+    t[XY] = 0;
+    t[XZ] = 0;
+    t[ZX] = 0;
+    t[YX] = 0;
+    t[YZ] = 0;
+    t[ZY] = 0;
+    return CO_OK;
 }
 
 int
 green3_wall_s(T * q, const real a[3], const real b[3], Ten * t0)
 {
     enum {
-        X, Y, Z
+	X, Y, Z
     };
     real d[3];
     real w;
@@ -143,41 +176,41 @@ green3_wall_s(T * q, const real a[3], const real b[3], Ten * t0)
     zw2 = zw * zw;
 
     t[XX] =
-        2 * w * (r13 - 3 * r15 * x2) * zw + 2 * w2 * (r13 - 3 * r15 * x2) -
-        r13 * x2 + r03 * x2 - r1 + r0;
+	2 * w * (r13 - 3 * r15 * x2) * zw + 2 * w2 * (r13 - 3 * r15 * x2) -
+	r13 * x2 + r03 * x2 - r1 + r0;
     t[YY] =
-        2 * w * (r13 - 3 * r15 * y2) * zw + 2 * w2 * (r13 - 3 * r15 * y2) -
-        r13 * y2 + r03 * y2 - r1 + r0;
+	2 * w * (r13 - 3 * r15 * y2) * zw + 2 * w2 * (r13 - 3 * r15 * y2) -
+	r13 * y2 + r03 * y2 - r1 + r0;
     t[ZZ] =
-        2 * w * zw * (3 * r15 * zw2 - r13) + 2 * w2 * (3 * r15 * zw2 -
-                                                       r13) - r13 * zw2 +
-        r03 * z2 - r1 + r0;
+	2 * w * zw * (3 * r15 * zw2 - r13) + 2 * w2 * (3 * r15 * zw2 -
+						       r13) - r13 * zw2 +
+	r03 * z2 - r1 + r0;
     t[XY] =
-        (-6 * r15 * w * x * y * zw) - 6 * r15 * w2 * x * y - r13 * x * y +
-        r03 * x * y;
+	(-6 * r15 * w * x * y * zw) - 6 * r15 * w2 * x * y - r13 * x * y +
+	r03 * x * y;
     t[XZ] =
-        2 * w * (3 * r15 * x * zw2 - r13 * x) + 6 * r15 * w2 * x * zw -
-        r13 * x * zw + r03 * x * z;
+	2 * w * (3 * r15 * x * zw2 - r13 * x) + 6 * r15 * w2 * x * zw -
+	r13 * x * zw + r03 * x * z;
     t[ZX] =
-        2 * w * ((-3 * r15 * x * zw2) - r13 * x) - 6 * r15 * w2 * x * zw -
-        r13 * x * zw + r03 * x * z;
+	2 * w * ((-3 * r15 * x * zw2) - r13 * x) - 6 * r15 * w2 * x * zw -
+	r13 * x * zw + r03 * x * z;
     t[YX] = t[XY];
     t[YZ] =
-        2 * w * (3 * r15 * y * zw2 - r13 * y) + 6 * r15 * w2 * y * zw -
-        r13 * y * zw + r03 * y * z;
+	2 * w * (3 * r15 * y * zw2 - r13 * y) + 6 * r15 * w2 * y * zw -
+	r13 * y * zw + r03 * y * z;
     t[ZY] =
-        2 * w * ((-3 * r15 * y * zw2) - r13 * y) - 6 * r15 * w2 * y * zw -
-        r13 * y * zw + r03 * y * z;
+	2 * w * ((-3 * r15 * y * zw2) - r13 * y) - 6 * r15 * w2 * y * zw -
+	r13 * y * zw + r03 * y * z;
     return CO_OK;
 }
 
 int
 green3_wall_t(T * q, const real a[3], const real n[3], const real b[3],
-              real * xx, real * xy, real * xz, real * yy, real * yz,
-              real * zz)
+	      real * xx, real * xy, real * xz, real * yy, real * yz,
+	      real * zz)
 {
     enum {
-        X, Y, Z
+	X, Y, Z
     };
     real d[3], r, p, l;
 
@@ -186,7 +219,7 @@ green3_wall_t(T * q, const real a[3], const real n[3], const real b[3],
     r = i_vec_abs(d);
     p = i_vec_dot(d, n);
     if (r == 0)
-        ERR(CO_NUM, "r == 0");
+	ERR(CO_NUM, "r == 0");
     l = p / (r * r * r * r * r);
     *xx = d[X] * d[X] * l;
     *xy = d[X] * d[Y] * l;
