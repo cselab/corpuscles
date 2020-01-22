b932e5b663189954e12be7730cb75dbcb99fb4c2
Sergey Litvinov <slitvinov@gmail.com>
2020-01-22 13:02:41 +0100

    add bi_wall

--- lib/bi/wall.c
+++ lib/bi/wall.c
@@ -8,33 +8,35 @@
 #include "co/i/matrix.h"
 #include "co/macro.h"
 #include "co/memory.h"
-#include "co/oseen/3zero.h"
+#include "co/green/3wall.h"
+#include "co/tensor3x3.h"
 #include "co/tensor.h"
 
 #define T BiWall
 struct T {
     BiSelfCircle *self;
-    Oseen3Zero *oseen;
-    struct Tensor O, K;
+    Green3Wall *green;
+    struct Tensor3x3 O;
+    struct Tensor3x3 K;
     int KReady;
 };
 
 int
-bi_wall_ini(He * he, /**/ T ** pq)
+bi_wall_ini(real w, He * he, /**/ T ** pq)
 {
     T *q;
     int status, n;
 
     MALLOC(1, &q);
-    status = oseen3_zero_ini(he, &q->oseen);
+    status = green3_wall_ini(he, w, &q->green);
     if (status != CO_OK)
-        ERR(CO_MEMORY, "oseen3_zero_ini failed");
+        ERR(CO_MEMORY, "green3_wall_ini failed");
     status = bi_self_circle_ini(he, &q->self);
     if (status != CO_OK)
         ERR(CO_MEMORY, "bi_self_circle_ini failed");
     n = he_nv(he);
-    tensor_ini(n, &q->O);
-    tensor_ini(n, &q->K);
+    tensor3x3_ini(n, &q->O);
+    tensor3x3_ini(n, &q->K);
     q->KReady = 0;
     *pq = q;
     return CO_OK;
@@ -43,34 +45,36 @@ bi_wall_ini(He * he, /**/ T ** pq)
 int
 bi_wall_argv(char ***p, He * he, /**/ T ** pq)
 {
-    USED(p);
-    return bi_wall_ini(he, pq);
+    int status;
+    real x;
+
+    if ((status = argv_real(p, &x)) != CO_OK)
+        return status;
+    return bi_wall_ini(x, he, pq);
 }
 
 int
 bi_wall_fin(T * q)
 {
-    oseen3_zero_fin(q->oseen);
+    green3_wall_fin(q->green);
     bi_self_circle_fin(q->self);
-    tensor_fin(&q->O);
-    tensor_fin(&q->K);
+    tensor3x3_fin(&q->O);
+    tensor3x3_fin(&q->K);
     FREE(q);
     return CO_OK;
 }
 
 int
 bi_wall_update(T * q, He * he, const real * x, const real * y,
-                      const real * z)
+               const real * z)
 {
-    struct Tensor *O;
+    struct Tensor3x3 *O;
     int status;
 
     O = &q->O;
-    status =
-        oseen3_zero_apply(q->oseen, he, x, y, z, O->xx, O->xy, O->xz,
-                          O->yy, O->yz, O->zz);
+    status = green3_wall_apply(q->green, he, x, y, z, O);
     if (status != CO_OK)
-        ERR(CO_NUM, "oseen3_zero_apply failed");
+        ERR(CO_NUM, "green3_wall_apply failed");
     status = bi_self_circle_update(q->self, he, x, y, z);
     if (status != CO_OK)
         ERR(CO_NUM, "bi_self_circle_update failed");
@@ -80,16 +84,13 @@ bi_wall_update(T * q, He * he, const real * x, const real * y,
 
 int
 bi_wall_single(T * q, He * he, real al,
-                      const real * x, const real * y, const real * z,
-                      const real * fx, const real * fy, const real * fz,
-                      /*io */ real * ux, real * uy, real * uz)
+               const real * x, const real * y, const real * z,
+               const real * fx, const real * fy, const real * fz,
+               /*io */ real * ux, real * uy, real * uz)
 {
-    struct Tensor *O;
-    int n, status;
-
-    USED(x);
-    USED(y);
-    USED(z);
+    struct Tensor3x3 *O;
+    int n;
+    int status;
 
     O = &q->O;
     n = he_nv(he);
@@ -98,30 +99,27 @@ bi_wall_single(T * q, He * he, real al,
                               uz);
     if (status != CO_OK)
         ERR(CO_NUM, "bi_self_circle_single failed");
-    tensor_vector(n, al, fx, fy, fz, O, ux, uy, uz);
+    tensor3x3_vector(n, al, fx, fy, fz, O, ux, uy, uz);
     return CO_OK;
 }
 
 int
 bi_wall_double(T * q, He * he, real al,
-                      const real * x, const real * y, const real * z,
-                      const real * ux, const real * uy, const real * uz,
-                      /*io */ real * wx, real * wy, real * wz)
+               const real * x, const real * y, const real * z,
+               const real * ux, const real * uy, const real * uz,
+               /*io */ real * wx, real * wy, real * wz)
 {
     int n, status;
-    struct Tensor *K;
+    struct Tensor3x3 *K;
 
-    USED(x);
-    USED(y);
-    USED(z);
+    ERR(CO_NUM, "not implimented");
 
     K = &q->K;
     if (q->KReady == 0) {
-        status =
-            oseen3_zero_stresslet(q->oseen, he, x, y, z, K->xx, K->xy,
-                                  K->xz, K->yy, K->yz, K->zz);
+        /* status =
+           green3_wall_stresslet(q->green, he, x, y, z, K); */
         if (status != CO_OK)
-            ERR(CO_NUM, "oseen3_zero_stresslet failed");
+            ERR(CO_NUM, "green3_wall_stresslet failed");
         q->KReady = 1;
     }
     n = he_nv(he);
@@ -129,28 +127,30 @@ bi_wall_double(T * q, He * he, real al,
                           wz);
     if (status != CO_OK)
         ERR(CO_NUM, "bi_self_circle_double failed");
-    tensor_vector(n, al, ux, uy, uz, K, wx, wy, wz);
+    tensor3x3_vector(n, al, ux, uy, uz, K, wx, wy, wz);
     return CO_OK;
 }
 
 int
 bi_wall_single_velocity(T * q, He * he,
-                               const real * x, const real * y,
-                               const real * z, const real * fx,
-                               const real * fy, const real * fz,
-                               const real r[3], /**/ real v[3])
+                        const real * x, const real * y,
+                        const real * z, const real * fx,
+                        const real * fy, const real * fz,
+                        const real r[3], /**/ real v[3])
 {
-    return oseen3_zero_single_velocity(q->oseen, he, x, y, z, fx, fy, fz,
-                                       r, v);
+    ERR(CO_NUM, "not implimented");
+//    return green3_wall_single_velocity(q->green, he, x, y, z, fx, fy, fz,
+//                                       r, v);
 }
 
 int
 bi_wall_double_velocity(T * q, He * he,
-                               const real * x, const real * y,
-                               const real * z, const real * ux,
-                               const real * uy, const real * uz,
-                               const real r[3], /**/ real v[3])
+                        const real * x, const real * y,
+                        const real * z, const real * ux,
+                        const real * uy, const real * uz,
+                        const real r[3], /**/ real v[3])
 {
-    return oseen3_zero_double_velocity(q->oseen, he, x, y, z, ux, uy, uz,
-                                       r, v);
+    ERR(CO_NUM, "not implimented");
+//    return green3_wall_double_velocity(q->green, he, x, y, z, ux, uy, uz,
+//                                       r, v);
 }
