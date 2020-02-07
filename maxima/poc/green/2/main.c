2eeb9e03e519ab199d638bf9783f1be24b726eae
Sergey Litvinov <slitvinov@gmail.com>
2020-02-07 22:41:40 +0100

    minor

--- maxima/poc/green/2/main.c
+++ maxima/poc/green/2/main.c
@@ -15,7 +15,7 @@ static char me[] = "tensor";
 static void
 usg()
 {
-    fprintf(stderr, "%s -x float -y float -z float -0 float -W float\n",
+    fprintf(stderr, "%s -x float -y float -z float -0 float -w float\n",
             me);
     exit(1);
 }
