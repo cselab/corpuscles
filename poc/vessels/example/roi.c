321e4da7f193be81dba1d0be9eebfe01302a8afe
Sergey Litvinov <slitvinov@gmail.com>
2020-03-01 15:47:17 +0100

    minor

--- poc/vessels/example/roi.c
+++ poc/vessels/example/roi.c
@@ -8,7 +8,8 @@
 #include <co/memory.h>
 
 const char *me = "vtk";
-static const real spacing[] = {1.6, 1.6, 1.0};
+static const real spacing[] = { 1.6, 1.6, 1.0 };
+
 static void
 usg(void)
 {
@@ -21,83 +22,89 @@ main(int argc, char **argv)
 {
     enum { X, Y, Z };
     int dircount;
+    int hi[3];
     int i;
     int j;
     int k;
     int l;
     int length;
-    int n;
-    int size[3];
     int lo[3];
-    int hi[3];
+    int n;
+    int Rflag;
     int roi[3];
+    int size[3];
     int width;
     real *data;
-    unsigned short *inptr;
-    int x;
     real origin[3];
     tdata_t buf;
-    tsize_t scanline;
     TIFF *tif;
-    int Rflag;
+    tsize_t scanline;
+    unsigned short *inptr;
 
     USED(argc);
     Rflag = 0;
     while (*++argv != NULL && argv[0][0] == '-')
-	switch (argv[0][1]) {
-	case 'h':
-	    usg();
-	    break;
-	case 'r':
-	    if (argv[1] == NULL || argv[2] == NULL || argv[3] == NULL ||
-		argv[4] == NULL || argv[5] == NULL || argv[6] == NULL) {
-		fprintf(stderr, "%s: -r needs six arguments\n", me);
-		exit(2);
-	    }
-	    lo[X] = atoi(*++argv);
-	    lo[Y] = atoi(*++argv);
-	    lo[Z] = atoi(*++argv);
-	    hi[X] = atoi(*++argv);
-	    hi[Y] = atoi(*++argv);
-	    hi[Z] = atoi(*++argv);
-	    Rflag = 1;
-	    break;
-	default:
-	    fprintf(stderr, "%s: unknown option '%s'\n", me, argv[0]);
-	    exit(1);
-	}
+        switch (argv[0][1]) {
+        case 'h':
+            usg();
+            break;
+        case 'r':
+            if (argv[1] == NULL || argv[2] == NULL || argv[3] == NULL ||
+                argv[4] == NULL || argv[5] == NULL || argv[6] == NULL) {
+                fprintf(stderr, "%s: -r needs six arguments\n", me);
+                exit(2);
+            }
+            lo[X] = atoi(*++argv);
+            lo[Y] = atoi(*++argv);
+            lo[Z] = atoi(*++argv);
+            hi[X] = atoi(*++argv);
+            hi[Y] = atoi(*++argv);
+            hi[Z] = atoi(*++argv);
+            Rflag = 1;
+            break;
+        default:
+            fprintf(stderr, "%s: unknown option '%s'\n", me, argv[0]);
+            exit(1);
+        }
     if (argv[0] == NULL) {
-	fprintf(stderr, "%s: missing an argument\n", me);
-	exit(2);
+        fprintf(stderr, "%s: missing an argument\n", me);
+        exit(2);
     }
 
     if (Rflag == 0) {
-	fprintf(stderr, "%s: -r is not set\n", me);
-	exit(2);	
+        fprintf(stderr, "%s: -r is not set\n", me);
+        exit(2);
     }
-
     if (lo[X] >= hi[X]) {
-	fprintf(stderr, "%s: lo[X]=%d >= hi[X]=%d\n", me, lo[X], hi[X]);
-	exit(2);		
+        fprintf(stderr, "%s: lo[X]=%d >= hi[X]=%d\n", me, lo[X], hi[X]);
+        exit(2);
+    }
+    if (lo[Y] >= hi[Y]) {
+        fprintf(stderr, "%s: lo[Y]=%d >= hi[Y]=%d\n", me, lo[Y], hi[Y]);
+        exit(2);
+    }
+    if (lo[Z] >= hi[Z]) {
+        fprintf(stderr, "%s: lo[Z]=%d >= hi[Z]=%d\n", me, lo[Z], hi[Z]);
+        exit(2);
     }
 
     if ((tif = TIFFOpen(argv[0], "r")) == NULL) {
-	fprintf(stderr, "%s: fail to topen %s\n", me, argv[0]);
-	exit(2);
+        fprintf(stderr, "%s: fail to topen %s\n", me, argv[0]);
+        exit(2);
     }
 
     dircount = 0;
     do
-	dircount++;
+        dircount++;
     while (TIFFReadDirectory(tif));
 
     if (TIFFGetField(tif, TIFFTAG_IMAGEWIDTH, &width) != 1) {
-	fprintf(stderr, "%s: TIFFGetField failed\n", me);
-	exit(2);
+        fprintf(stderr, "%s: TIFFGetField failed\n", me);
+        exit(2);
     };
     if (TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &length) != 1) {
-	fprintf(stderr, "%s: TIFFGetField failed\n", me);
-	exit(2);
+        fprintf(stderr, "%s: TIFFGetField failed\n", me);
+        exit(2);
     };
     size[X] = width;
     size[Y] = length;
@@ -115,34 +122,37 @@ main(int argc, char **argv)
     MALLOC(n, &data);
     scanline = TIFFScanlineSize(tif);
     if ((buf = _TIFFmalloc(scanline)) == NULL) {
-	fprintf(stderr, "%s: _TIFFmalloc failed failed\n", me);
-	exit(2);
+        fprintf(stderr, "%s: _TIFFmalloc failed failed\n", me);
+        exit(2);
     }
     l = 0;
     for (k = 0; k < size[Z]; k++) {
-	if (lo[Z] > k || k >= hi[Z]) continue;
-	if (TIFFSetDirectory(tif, k) != 1) {
-	    fprintf(stderr, "%s: TIFFSetDirectory(k=%d) failed\n", me, k);
-	    exit(2);
-	}
-	for (j = 0; j < size[Y]; j++) {
-	    if (lo[Y] > j || j >= hi[Y]) continue;
-	    if (TIFFReadScanline(tif, buf, j, 0) != 1) {
-		fprintf(stderr, "%s: TIFFReadScanline failed\n", me);
-		exit(2);
-	    }
-	    inptr = buf;
-	    for (i = 0; i < size[X]; i++) {
-		if (lo[X] > i || i >= hi[X]) continue;
-		data[l++] = inptr[i];
-	    }
-	}
+        if (lo[Z] > k || k >= hi[Z])
+            continue;
+        if (TIFFSetDirectory(tif, k) != 1) {
+            fprintf(stderr, "%s: TIFFSetDirectory(k=%d) failed\n", me, k);
+            exit(2);
+        }
+        for (j = 0; j < size[Y]; j++) {
+            if (lo[Y] > j || j >= hi[Y])
+                continue;
+            if (TIFFReadScanline(tif, buf, j, 0) != 1) {
+                fprintf(stderr, "%s: TIFFReadScanline failed\n", me);
+                exit(2);
+            }
+            inptr = buf;
+            for (i = 0; i < size[X]; i++) {
+                if (lo[X] > i || i >= hi[X])
+                    continue;
+                data[l++] = inptr[i];
+            }
+        }
     }
     assert(l == n);
     _TIFFfree(buf);
     if (vtk_grid_write(stdout, roi, origin, spacing, data) != CO_OK) {
-	fprintf(stderr, "%s: vtk_grid_write failed\n", me);
-	exit(2);
+        fprintf(stderr, "%s: vtk_grid_write failed\n", me);
+        exit(2);
     }
     FREE(data);
     TIFFClose(tif);
