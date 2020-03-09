137c37f5ff022c3bf1eb26b639193136c9e00a9a
Sergey Litvinov <slitvinov@gmail.com>
2020-03-09 09:51:32 +0100

    indent

--- lib/co/subst.h
+++ lib/co/subst.h
@@ -11,9 +11,9 @@ typedef struct BI BI;
 enum { SUBST_TOL, SUBST_ITER, SUBST_FAIL };
 int subst_ini(int n, real alpha, real tol, int iter_max, T **);
 int subst_fin(T *);
-int subst_apply(T *, He *, BI *, const real *, const real *,
-                const real *, const real *, const real *,
-                const real *, real *, real *, real *);
+int subst_apply(T *, He *, BI *, const real * x, const real * y,
+                const real * z, const real * ux, const real * uy,
+                const real * uz, real *, real *, real *);
 int subst_apply_initial(T *, He *, BI *, const real *, const real *,
                         const real * ux, const real *, const real *,
                         const real * ix, const real *, const real *,
--- lib/co/vtk.h
+++ lib/co/vtk.h
@@ -1,12 +1,14 @@
 typedef struct He He;
 enum { VTK_INT, VTK_REAL };
 int vtk_write(He *, const real *, const real *, const real *,
-	      const real **, const char **, /**/ FILE *);
+              const real **, const char **, /**/ FILE *);
 int vtk_tri_write(He *, const real *, const real *, const real *,
-		  const real **, const char **, /**/ FILE *);
+                  const real **, const char **, /**/ FILE *);
 int vtk_tri_int_write(He *, const real *, const real *, const real *,
-		      const int **, const char **, /**/ FILE *);
+                      const int **, const char **, /**/ FILE *);
 int vtk_tri_int_read(FILE *, const char *[], /**/ He **, real **, real **,
-		     real **, int **[]);
-int vtk_grid_write(FILE *, const int size[3], const real origin[3], const real spacing[3], const real *);
-int vtk_grid_read(FILE *, /**/ int size[3], real origin[3], real spacing[3], real **);
+                     real **, int **[]);
+int vtk_grid_write(FILE *, const int size[3], const real origin[3],
+                   const real spacing[3], const real *);
+int vtk_grid_read(FILE *, /**/ int size[3], real origin[3],
+                  real spacing[3], real **);
