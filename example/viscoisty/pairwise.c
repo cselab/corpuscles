bd3076d1b3443f3ce59a26a73194cf5f44849e04
Sergey Litvinov <lisergey@debian>
2020-03-23 13:09:42 +0100

    example/viscoisty: wip

--- example/viscoisty/pairwise.c
+++ example/viscoisty/pairwise.c
@@ -30,17 +30,29 @@ main(int argc, char **argv)
     real *z;
     int fd;
     long pid;
-    char path[SIZE];
+    char path_in[SIZE];
+    char path_out[SIZE];
+    char command[SIZE];
+    const char *cmd;
     FILE *f;
     int i;
     int nv;
 
     err_set_ignore();
     USED(argc);
+    cmd = NULL;
     while (*++argv != NULL && argv[0][0] == '-')
         switch (argv[0][1]) {
         case 'h':
             usg();
+            break;
+        case 'c':
+            argv++;
+            if (argv[0] == NULL) {
+                fprintf(stderr, "%s: -c needs an argumen'%s'\n", me);
+                exit(2);
+            }
+            cmd = argv[0];
             break;
         default:
             fprintf(stderr, "%s: unknown option '%s'\n", me, argv[0]);
@@ -55,15 +67,37 @@ main(int argc, char **argv)
         fprintf(stderr, "%s: y_inif failed\n", me);
         exit(2);
     }
-    snprintf(path, SIZE, "/tmp/pairwise.%ld", getpid());
-    if ((f = fopen(path, "w")) == NULL) {
-        fprintf(stderr, "%s: fail to open '%s'\n", me, path);
+    if (cmd == NULL) {
+        fprintf(stderr, "%s: option -c is not set\n", me);
+        exit(2);
+    }
+
+    snprintf(path_in, SIZE, "/tmp/pairwise.in.%ld", getpid());
+    snprintf(path_out, SIZE, "/tmp/pairwise.out.%ld", getpid());
+
+    if ((f = fopen(path_in, "w")) == NULL) {
+        fprintf(stderr, "%s: fail to open '%s'\n", me, path_in);
         exit(2);
     }
     nv = he_nv(he);
     for (i = 0; i < nv; i++)
-        fprintf(f, FMT3, x[i], y[i], z[i]);
-    fclose(f);
-    fprintf(stderr, "%s\n", path);
+        if (fprintf(f, FMT3, x[i], y[i], z[i]) < 0) {
+            fprintf(stderr, "%s: fprintf(3) failed for '%s'\n", me,
+                    path_in);
+            exit(2);
+        }
+
+    if (fclose(f) != 0) {
+        fprintf(stderr, "%s: fclose(3) failed for '%s'\n", me, path_in);
+        exit(2);
+    }
+
+    snprintf(command, SIZE, "<%s %s >%s", path_in, cmd, path_out);
+    if (system(command) != 0) {
+        fprintf(stderr, "%s: command '%s' failed\n", me, command);
+        exit(2);
+    }
+
+    fprintf(stderr, "%s\n", command);
     y_fin(he, x, y, z);
 }
