38137c3e4a618ecd69a13fc101aa5259251fd0b5
Sergey Litvinov <lisergey@debian>
2020-03-23 15:20:45 +0100

    example/viscosity: call viscosity_pairwise_ini

--- example/viscosity/pairwise.c
+++ example/viscosity/pairwise.c
@@ -9,6 +9,7 @@
 #include <co/he.h>
 #include <co/macro.h>
 #include <co/memory.h>
+#include <co/punto.h>
 #include <co/viscosity/pairwise.h>
 #include <co/y.h>
 
@@ -49,73 +50,79 @@ main(int argc, char **argv)
     real *vx;
     real *vy;
     real *vz;
+    real *fx;
+    real *fy;
+    real *fz;
     real v[3];
+    ViscosityPairwise *viscosity;
 
     err_set_ignore();
     USED(argc);
     cmd = NULL;
     while (*++argv != NULL && argv[0][0] == '-')
-        switch (argv[0][1]) {
-        case 'h':
-            usg();
-            break;
-        case 'c':
-            argv++;
-            if (argv[0] == NULL) {
-                fprintf(stderr, "%s: -c needs an argumen\n", me);
-                exit(2);
-            }
-            cmd = argv[0];
-            break;
-        default:
-            fprintf(stderr, "%s: unknown option '%s'\n", me, argv[0]);
-            exit(2);
-            break;
-        }
+	switch (argv[0][1]) {
+	case 'h':
+	    usg();
+	    break;
+	case 'c':
+	    argv++;
+	    if (argv[0] == NULL) {
+		fprintf(stderr, "%s: -c needs an argumen\n", me);
+		exit(2);
+	    }
+	    cmd = argv[0];
+	    break;
+	default:
+	    fprintf(stderr, "%s: unknown option '%s'\n", me, argv[0]);
+	    exit(2);
+	    break;
+	}
     if (argv[0] != NULL) {
-        fprintf(stderr, "%s: unknown argument '%s'\n", me, argv[0]);
-        exit(2);
+	fprintf(stderr, "%s: unknown argument '%s'\n", me, argv[0]);
+	exit(2);
     }
     if (y_inif(stdin, &he, &x, &y, &z) != 0) {
-        fprintf(stderr, "%s: y_inif failed\n", me);
-        exit(2);
+	fprintf(stderr, "%s: y_inif failed\n", me);
+	exit(2);
     }
     if (cmd == NULL) {
-        fprintf(stderr, "%s: option -c is not set\n", me);
-        exit(2);
+	fprintf(stderr, "%s: option -c is not set\n", me);
+	exit(2);
     }
     nv = he_nv(he);
     MALLOC3(nv, &vx, &vy, &vz);
+    CALLOC3(nv, &fx, &fy, &fz);
+    viscosity_pairwise_ini(1.0, he, &viscosity);
 
     snprintf(path_in, SIZE, "/tmp/pairwise.in.%d", getpid());
     snprintf(path_out, SIZE, "/tmp/pairwise.out.%d", getpid());
     if (signal(SIGINT, cleanup) == SIG_ERR) {
-        fprintf(stderr, "%s: signal(3) failed", me);
-        exit(2);
+	fprintf(stderr, "%s: signal(3) failed", me);
+	exit(2);
     }
 
     if ((f = fopen(path_in, "w")) == NULL) {
-        fprintf(stderr, "%s: fail to open '%s'\n", me, path_in);
-        exit(2);
+	fprintf(stderr, "%s: fail to open '%s'\n", me, path_in);
+	exit(2);
     }
     for (i = 0; i < nv; i++)
-        if (fprintf(f, OUT3, x[i], y[i], z[i]) < 0) {
-            fprintf(stderr, "%s: fprintf(3) failed for '%s'\n", me,
-                    path_in);
-            exit(2);
-        }
+	if (fprintf(f, OUT3, x[i], y[i], z[i]) < 0) {
+	    fprintf(stderr, "%s: fprintf(3) failed for '%s'\n", me,
+		    path_in);
+	    exit(2);
+	}
     if (fclose(f) != 0) {
-        fprintf(stderr, "%s: fclose(3) failed for '%s'\n", me, path_in);
-        exit(2);
+	fprintf(stderr, "%s: fclose(3) failed for '%s'\n", me, path_in);
+	exit(2);
     }
     snprintf(command, 4 * SIZE, "<%s %s >%s", path_in, cmd, path_out);
     if (system(command) != 0) {
-        fprintf(stderr, "%s: command '%s' failed\n", me, command);
-        exit(2);
+	fprintf(stderr, "%s: command '%s' failed\n", me, command);
+	exit(2);
     }
     if ((f = fopen(path_out, "r")) == NULL) {
-        fprintf(stderr, "%s: fail to open '%s'\n", me, path_out);
-        exit(2);
+	fprintf(stderr, "%s: fail to open '%s'\n", me, path_out);
+	exit(2);
     }
 
     i = 0;
@@ -132,7 +139,7 @@ main(int argc, char **argv)
 	}
 	if (sscanf(line, IN, &v[dim]) != 1) {
 	  fprintf(stderr, "%s: fail to parse'%s'\n", me, line);
-	  exit(2);	  
+	  exit(2);
 	}
 	dim++;
       } while (strtok(NULL, delimeter) != NULL);
@@ -141,14 +148,22 @@ main(int argc, char **argv)
       vz[i] = v[Z];
       i++;
     }
-    
     if (fclose(f) != 0) {
-        fprintf(stderr, "%s: fclose(3) failed for '%s'\n", me, path_out);
-        exit(2);
+	fprintf(stderr, "%s: fclose(3) failed for '%s'\n", me, path_out);
+	exit(2);
     }
     cleanup0();
+    viscosity_pairwise_force(viscosity, he, x, y, z, vx, vy, vz, fx, fy, fz);
     y_fin(he, x, y, z);
+    viscosity_pairwise_fin(viscosity);
+
+
+    const real *queue[] = {x, y, z, vx, vy, vz, fx, fy, fz, NULL};
+    printf("x y z vx vy vz fx fy fz\n");
+    punto_fwrite(nv, queue, stdout);
+    
     FREE3(vx, vy, vz);
+    FREE3(fx, fy, fz);
 }
 
 static void
