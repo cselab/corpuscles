bf99e954d2a9325e27b0e801f0b500eabf6b5d1c
Sergey Litvinov <slitvinov@gmail.com>
2020-11-09 13:30:42 +0100

    add just kb

--- poc/eng/run.sh
+++ poc/eng/run.sh
@@ -21,5 +21,6 @@ Da0=$(ae $Da1/100)
 DA0=$(ae $Da0*$A)
 DA0D=$(ae $DA0/$D)
 
+co.eng juelicher_xin $Kb 0 0 0 < ini.off
 co.eng juelicher_xin $Kb $C0 $Kad $DA0D < ini.off
 co.eng strain ref.off lim $Ka $mub $a3 $a4 $b1 $b2 < ini.off
