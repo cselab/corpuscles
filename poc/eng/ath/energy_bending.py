b987c607110304696a33cf39d9373ee6a756a668
Athena Economides <eceva@ethz.ch>
2020-12-10 15:11:25 +0100

    Add poc/eng/ath energy computations

---
+++ poc/eng/ath/energy_bending.py
@@ -0,0 +1,324 @@
+import math
+import sys
+import numpy as np
+import trimesh
+import argparse
+import matplotlib.pyplot as plt
+
+from color_by_inimesh import export_colored_faces
+
+
+def vec_cross(a, b):
+    X=0; Y=1; Z=2
+    c = np.empty(np.shape(a))
+    c[X] = a[Y] * b[Z] - b[Y] * a[Z];
+    c[Y] = b[X] * a[Z] - a[X] * b[Z];
+    c[Z] = a[X] * b[Y] - b[X] * a[Y];
+    return c;
+
+
+def single_angle_between(v1, v2):
+    x = np.dot(v1, v2)
+    n = vec_cross(v1, v2)
+    y = np.sqrt(np.dot(n, n))
+    return math.atan2(y, x)
+
+
+def angle_between(vec1, vec2):
+    angles = np.zeros(len(vec1))
+    for i, v1, v2 in zip(range(len(vec1)), vec1, vec2):
+        angles[i] = single_angle_between(v1, v2)
+    return angles
+
+
+def single_compute_magnitude(v):
+    return np.sqrt( np.dot(v, v) )
+
+
+def compute_magnitude(vec):
+    mag = np.zeros(len(vec))
+    for i, v in enumerate(vec):
+        mag[i] = single_compute_magnitude(v)
+    return mag
+
+
+def tri_normal(a, b, c):
+    """
+    from:
+    tri_normal(const real a[3], const real b[3], const real c[3],  /**/ real e[3])
+    {
+        real u[3], v[3], n[3];
+        vec_minus(b, a, u);
+        vec_minus(c, a, v);
+        vec_cross(u, v, n);
+        vec_norm(n, e);
+    }
+ 
+    """
+    u = b - a
+    v = c - a
+    n = vec_cross(u, v)
+    InI = single_compute_magnitude(n)
+    return (n/InI)
+
+
+
+def kantor(thetas):
+    """
+    Eq. 17 in Bian2020
+    len(thetas) = N_edges
+    """
+    import math
+    c = math.sqrt(3.0) / 3.0
+    E = 16.0*np.pi/3.0  +  2.0 * c * np.sum(1.0-np.cos(thetas))
+    return E
+
+
+
+def dih_angle_sup(a, b, c, d):
+    """
+    Based on `dih_angle_sup':
+     repo: https://github.com/cselab/corpuscles
+     file: corpuscles/lib/f/juelicher_xin.c
+     date: 16 Nov 2020
+
+ 27 dih_angle_sup(const real a[3], const real b[3], const real c[3],
+ 28               const real d[3])
+ 29 {
+ 30     /*supplementary angle of dihedral */
+ 31     real x, y, ang, n[3], k[3], nk[3], e[3];
+ 32
+ 33     tri_normal(b, c, a, n);
+ 34     tri_normal(c, b, d, k);
+ 35     x = vec_dot(n, k);
+ 36     vec_cross(n, k, nk);
+ 37     y = vec_abs(nk);
+ 38     ang = atan2(y, x);
+ 39     vec_minus(c, b, e);
+ 40     if (vec_dot(e, nk) < 0)
+ 41         ang = -ang;
+ 42     return ang;
+ 43 }
+    """
+
+    n = tri_normal(b, c, a)
+    k = tri_normal(c, b, d)
+    x = np.dot(n, k)
+    nk = vec_cross(n, k)
+    y = single_compute_magnitude(nk)
+    ang = math.atan2(y, x)
+    e = c - b
+
+    if (np.dot(e, nk) < 0):
+        ang = -ang
+
+    return ang
+ 
+
+
+def compute_juelicher(mesh):
+    """
+    bc: common edge
+    triangle 0: (b, c, a)
+    triangle 1: (c, b, d)
+            c ---- d
+           / \    /
+          /   \  /
+         a ---- b 
+    """
+
+    Nv = len(mesh.vertices)
+    Hk = np.zeros(Nv)
+    Ak = np.zeros(Nv)
+
+    vertices = mesh.vertices
+
+
+    # 1/2: Area per vertex
+    At = mesh.area_faces
+    for k, (iv0, iv1, iv2) in enumerate(mesh.faces):
+        area = At[k]
+        Ak[iv0] += area
+        Ak[iv1] += area
+        Ak[iv2] += area
+    Ak = Ak / 3.0
+
+
+    # 2/2: Local curvature per vertex
+    adjacent_faces = mesh.face_adjacency   # Pairs of faces which share an edge
+    faces = mesh.faces
+    for k, (iv0, iv1) in enumerate(mesh.edges_unique):
+
+        # indices of adjacent faces
+        if0 = adjacent_faces[k,0]
+        if1 = adjacent_faces[k,1]
+        face0 = faces[if0]
+        face1 = faces[if1]
+
+        # vertex indices for triangles in dihedral
+        m1 = iv0
+        m2 = iv1
+        for _iv in face0:
+            if (_iv != m1) and (_iv != m2):
+                ia = _iv
+        for _iv in face1:
+            if (_iv != m1) and (_iv != m2):
+                idd = _iv
+
+        # face0
+        if   (face0 == np.asarray([ia, m1, m2])).all(): # abc
+            ib=m1; ic=m2
+        elif (face0 == np.asarray([ia, m2, m1])).all():
+            ib=m2; ic=m1
+        elif (face0 == np.asarray([m1, ia, m2])).all(): # cab
+            ib=m2; ic=m1
+        elif (face0 == np.asarray([m2, ia, m1])).all():
+            ib=m1; ic=m2
+        elif (face0 == np.asarray([m1, m2, ia])).all(): # bca
+            ib=m1; ic=m2
+        elif (face0 == np.asarray([m2, m1, ia])).all():
+            ib=m2; ic=m1
+        else:
+            print("Error!")
+            sys.exit()
+            assert(0)
+
+        # face1
+        if   (face1 == np.asarray([idd, m1, m2])).all(): # dcb
+            ib_=m2; ic_=m1
+        elif (face1 == np.asarray([idd, m2, m1])).all():
+            ib_=m1; ic_=m2
+        elif (face1 == np.asarray([m1, idd, m2])).all(): # bdc
+            ib_=m1; ic_=m2
+        elif (face1 == np.asarray([m2, idd, m1])).all():
+            ib_=m2; ic_=m1
+        elif (face1 == np.asarray([m1, m2, idd])).all(): # cbd
+            ib_=m2; ic_=m1
+        elif (face1 == np.asarray([m2, m1, idd])).all():
+            ib_=m1; ic_=m2
+        else:
+            print("Error!")
+            sys.exit()
+            assert(0)
+
+
+        if ib!=ib_ or ic!=ic_:
+            assert(0)
+
+            _t_ = ia
+            ia = idd
+            idd = _t_
+
+            # face0
+            if   (face0 == np.asarray([ia, m1, m2])).all(): # abc
+                ib=m1; ic=m2
+            elif (face0 == np.asarray([ia, m2, m1])).all():
+                ib=m2; ic=m1
+            elif (face0 == np.asarray([m1, ia, m2])).all(): # cab
+                ib=m2; ic=m1
+            elif (face0 == np.asarray([m2, ia, m1])).all():
+                ib=m1; ic=m2
+            elif (face0 == np.asarray([m1, m2, ia])).all(): # bca
+                ib=m1; ic=m2
+            elif (face0 == np.asarray([m2, m1, ia])).all():
+                ib=m2; ic=m1
+            else:
+                print("Error!")
+                sys.exit()
+                assert(0)
+
+            # face1
+            if   (face1 == np.asarray([idd, m1, m2])).all(): # dcb
+                ib_=m2; ic_=m1
+            elif (face1 == np.asarray([idd, m2, m1])).all():
+                ib_=m1; ic_=m2
+            elif (face1 == np.asarray([m1, idd, m2])).all(): # bdc
+                ib_=m1; ic_=m2
+            elif (face1 == np.asarray([m2, idd, m1])).all():
+                ib_=m2; ic_=m1
+            elif (face1 == np.asarray([m1, m2, idd])).all(): # cbd
+                ib_=m2; ic_=m1
+            elif (face1 == np.asarray([m2, m1, idd])).all():
+                ib_=m1; ic_=m2
+            else:
+                print("Error!")
+                sys.exit()
+                assert(0)
+
+        if ib!=ib_ or ic!=ic_:
+            assert(0)
+
+
+        # vertices of triangles
+        a = vertices[ia]
+        b = vertices[ib]
+        c = vertices[ic]
+        d = vertices[idd]
+
+        # angle between triangles
+        thj = dih_angle_sup(a, b, c, d)
+
+        # length of common edge between triangles
+        e = c - b
+        lk = single_compute_magnitude(e)
+
+        # edge curvature
+        cur = lk * thj
+
+        # accumulate curvature on vertices
+        Hk[iv0] += cur
+        Hk[iv1] += cur
+
+    Hk = 0.25 * Hk / Ak
+
+    return Hk, Ak
+
+
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--f', type=str, required=True, nargs='+', help="mesh files")
+
+    parser.add_argument('--kb',         type=float, default=2.e-19, help="bending modulus")
+    parser.add_argument('--m0bar',      type=float, default=10,     help="dimensionless effective spontaneous curvature")
+    parser.add_argument('--kade',       type=float, default=1.27e-19, help="area difference modulus")
+    parser.add_argument('--A0',         type=float, default=140e-12, help="area of rbc")
+    parser.add_argument('--L_UNIT',     type=float, default=5.5629843151e-7, help="length unit, m")
+    args = parser.parse_args()
+
+
+    for i, file_ in enumerate(args.f):
+        print(file_)
+
+        mesh = trimesh.load(file_, process=False)
+        Nv = len(mesh.vertices)
+
+
+        # Minimum energy - Kantor
+        Eb_kantor = args.kb * kantor(mesh.face_adjacency_angles)
+        print("Eb_local (Kantor):", Eb_kantor)
+
+
+        # local curvature and area (per vertex)
+        Hk, Ak = compute_juelicher(mesh)
+        M1 = np.sum(Hk      * Ak)  *  args.L_UNIT
+        M2 = np.sum(Hk * Hk * Ak)
+        M12 = M1*M1
+
+
+        # Minimum energy - Juelicher
+        Eb_local = 2.0 * args.kb * M2
+        print("Eb_local (Juelicher):", Eb_local)
+
+
+        # ADE energy. Ref: X.Bian, CMEA, 2020
+        RA = np.sqrt(args.A0/4.0/np.pi)
+        DA0D = 2.0 * args.m0bar * RA
+        len_theta = 4. * Hk * Ak * args.L_UNIT
+        len_theta_tot = np.sum( len_theta )
+        scurv = (len_theta_tot / 2 - DA0D) / args.A0;
+        Eb_ade = np.pi * args.kade * args.A0 * scurv * scurv / 2.0;
+        print("Eb_ade (Juelicher):", Eb_ade)
+
---
+++ poc/eng/ath/energy_strain.py
@@ -0,0 +1,146 @@
+import math
+import numpy as np
+import trimesh
+import argparse
+import matplotlib.pyplot as plt
+
+
+def single_angle_between(v1, v2):
+    x = np.dot(v1, v2)
+    n = np.cross(v1, v2)
+    y = np.sqrt(np.dot(n, n))
+    return math.atan2(y, x)
+
+
+def angle_between(vec1, vec2):
+    angles = np.zeros(len(vec1))
+    for i, v1, v2 in zip(range(len(vec1)), vec1, vec2):
+        angles[i] = single_angle_between(v1, v2)
+    return angles
+
+
+def single_compute_magnitude(v):
+    return np.sqrt( v.dot(v) )
+
+
+def compute_magnitude(vec):
+    mag = np.zeros(len(vec))
+    for i, v in enumerate(vec):
+        mag[i] = single_compute_magnitude(v)
+    return mag
+
+
+
+def compute_invariants(DAi, DA0i, tri, tri0):
+
+    alpha = DAi/DA0i - 1
+
+    # vertex points
+    v1  = tri [:,0]
+    v2  = tri [:,1]
+    v3  = tri [:,2]
+    w1  = tri0[:,0]
+    w2  = tri0[:,1]
+    w3  = tri0[:,2]
+
+    # vectors on triangle edge
+    l   = v2 - v1
+    lp  = v3 - v1
+    l0  = w2 - w1
+    lp0 = w3 - w1
+
+    Ml   = compute_magnitude(l)
+    Ml0  = compute_magnitude(l0)
+    Mlp  = compute_magnitude(lp)
+    Mlp0 = compute_magnitude(lp0)
+
+    a = Ml / Ml0
+
+    phi  = angle_between(l,  lp)
+    phi0 = angle_between(l0, lp0)
+
+    b = 1./np.sin(phi0) * ( Mlp/Mlp0*np.cos(phi) - Ml/Ml0*np.cos(phi0) )
+
+    c = Mlp/Mlp0 * np.sin(phi)/np.sin(phi0)
+
+    a2 = a*a
+    b2 = b*b
+    c2 = c*c
+    a2c = 2.*a*c
+    beta = 1./a2c * ( a2 + b2 + c2 - a2c )
+
+    return alpha, beta
+
+
+def compute_Eshear(b1, b2, ai, bi):
+    ai2 = ai  * ai
+    ai3 = ai2 * ai
+    ai4 = ai2 * ai2
+    bi2 = bi  * bi
+    return bi  + b1*ai*bi + b2*bi2
+
+
+def compute_Earea(a3, a4, ai):
+    ai2 = ai  * ai
+    ai3 = ai2 * ai
+    ai4 = ai2 * ai2
+    return 0.5*(ai2 + a3*ai3 + a4*ai4)
+
+
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ref',        type=str,   required=True,  help="stress-free mesh file")
+    parser.add_argument('--f',      type=str,   required=True,  nargs='+', help="rbc mesh files")
+
+    parser.add_argument('--L_UNIT',     type=float, default=5.5629843151e-7, help="length unit, m")
+    parser.add_argument('--mu',         type=float, default=2.5e-6, help="shear modulus")
+    parser.add_argument('--b1',         type=float, default=0.7,    help="non-linear shear coefficients")
+    parser.add_argument('--b2',         type=float, default=0.75,   help="non-linear shear coefficients")
+    parser.add_argument('--ka',         type=float, default=5.0e-6, help="stretch modulus")
+    parser.add_argument('--a3',         type=float, default=-2,     help="non-linear stretch coefficients")
+    parser.add_argument('--a4',         type=float, default=8,      help="non-linear stretch coefficients")
+    parser.add_argument('--kb',         type=float, default=2.e-19, help="bending modulus")
+    parser.add_argument('--m0bar',      type=float, default=10,     help="dimensionless effective spontaneous curvature")
+    parser.add_argument('--kade',       type=float, default=1.27e-19, help="area difference modulus")
+    parser.add_argument('--A0',         type=float, default=140e-12, help="area of rbc")
+    args = parser.parse_args()
+    
+    
+    ref = trimesh.load(args.ref, process=False)
+    triangles_ref = ref.triangles
+    DA0i = trimesh.triangles.area(triangles=triangles_ref)
+    Area_ref = ref.area
+    RA = np.sqrt(Area_ref/4/np.pi)
+    
+    Nv = len(ref.vertices)
+    triangle_list = ref.vertex_faces  # face indices that correspond to each vertex
+    
+    
+
+    for i, file_ in enumerate(args.f):
+        print(file_)
+
+        # Mesh info:
+        mesh = trimesh.load(file_, process=False)
+        triangles_mesh = mesh.triangles
+        DAi = trimesh.triangles.area(triangles=triangles_mesh)
+        Area_mesh = mesh.area
+
+        assert(Area_ref<=1.005*Area_mesh and Area_ref>=0.995*Area_mesh)
+        assert( np.sum(np.abs(ref.faces - mesh.faces)) == 0 )  # check vertex ordering is the same! 
+
+
+        # strain energy of membrane skeleton (MS), LWM
+        ai, bi = compute_invariants(DAi, DA0i, triangles_mesh, triangles_ref)
+
+        Estretch = (args.L_UNIT**2) * args.ka * np.sum( DA0i * compute_Earea(args.a3, args.a4, ai) )   # sum over all triangles
+        Eshear   = (args.L_UNIT**2) * args.mu * np.sum( DA0i * compute_Eshear(args.b1, args.b2, ai, bi) )
+        Ems      = Estretch + Eshear
+
+        print("Ems (total): %e" % Ems)
+        print("Estretch:    %e" % Estretch)
+        print("Eshear:      %e" % Eshear)
+
