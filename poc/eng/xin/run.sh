0ebcabda23ee78165e3886a27b60688d2cf8ca5b
Athena Economides <eceva@ethz.ch>
2020-12-10 16:02:50 +0100

    use physical values

--- poc/eng/xin/run.sh
+++ poc/eng/xin/run.sh
@@ -7,26 +7,22 @@ ref=$1; shift
 ini=$1; shift
 
 pi=3.141592653589793115997964
-A=12.5663706144
+LUNIT=3.3377e-6
 a3=-2
 a4=8
 b1=0.7
 b2=0.75
 C0=0.0
-D=0.000898798148042
-Da1=0.143
-Kb=1.0
-mub=139.260575205
+m0bar=10
+Kb=2.e-19
+mub=2.5e-6
+
+Ka=$(ae 2*$mub)
 
 alpha=$(ae 2.0/$pi)
 Kad=$(ae $Kb*$alpha)
-Ka=$(ae 2*$mub)
-
-Kad=$(ae $Kb*$alpha)
-Da0=$(ae $Da1/100)
-DA0=$(ae $Da0*$A)
-DA0D=$(ae $DA0/$D)
+DA0D=$(ae 2*$m0bar)
 
 co.eng juelicher_xin $Kb 0 0 0 < "$ini"
 co.eng juelicher_xin $Kb $C0 $Kad $DA0D < "$ini"
-co.eng strain "$ref" lim $Ka $mub $a3 $a4 $b1 $b2 < "$ini"
+co.eng strain "$ref" lim $Ka $mub $a3 $a4 $b1 $b2 < "$ini"  | awk -v LU=$LUNIT '{print $1 * LU*LU}'
