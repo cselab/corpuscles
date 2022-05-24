e64bcdda6502bfac65038e023cd1adad191605c8
Sergey Litvinov <slitvinov@gmail.com>
2022-05-24 16:44:19 -0400

    bin/2vtk: add -h

--- bin/2vtk/main.c
+++ bin/2vtk/main.c
@@ -2,15 +2,32 @@
 #include <math.h>
 #include <real.h>
 #include <co/err.h>
-#include <co/macro.h>
 #include <co/vtk.h>
 #include <co/y.h>
+
+static const char *me = "co.2vtk";
+static void
+usg(void)
+{
+    fprintf(stderr, "%s < OFF > VTK\n", me);
+    exit(1);
+}
 
 int
 main(int argc, char **argv)
 {
-    static real *x, *y, *z;
-    static He *he;
+    real *x, *y, *z;
+    He *he;
+
+    while (*++argv != NULL && argv[0][0] == '-')
+      switch (argv[0][1]) {
+      case 'h':
+	usg();
+	break;
+      default:
+	fprintf(stderr, "%s: unknown option '%s'\n", me, argv[0]);
+	exit(1);
+      }
 
     y_inif(stdin, &he, &x, &y, &z);
     vtk_write(he, x, y, z, NULL, NULL, stdout);
