ccbb3fb1bd4bb71d1ce9c1aa77404f18c9e2ee31
Sergey Litvinov <slitvinov@gmail.com>
2022-05-24 17:23:37 -0400

    bin/ply2off: update

--- bin/ply2off/main.c
+++ bin/ply2off/main.c
@@ -2,48 +2,46 @@
 #include <stdlib.h>
 
 #include <real.h>
-#include <co/macro.h>
 #include <co/err.h>
-#include <co/ply.h>
 #include <co/off.h>
-#include <co/util.h>
+#include <co/ply.h>
 
-static const char *me = "ply2off";
-static real *x, *y, *z;
-static He *he;
-
+static const char *me = "co.ply2off";
 static void
 usg(void)
 {
     fprintf(stderr, "%s < PLY > OFF\n", me);
-    exit(2);
-}
-
-static void
-ini()
-{
-    ply_he_ini(stdin, &he, &x, &y, &z);
-}
-
-static void
-fin()
-{
-    ply_he_fin(he, x, y, z);
-}
-
-static int
-write()
-{
-    off_he_xyz_fwrite(he, x, y, z, stdout);
-    return CO_OK;
+    exit(1);
 }
 
 int
-main(__UNUSED int c, const char **v)
+main(int argc, const char **argv)
 {
-    if (*++v != NULL && util_eq(*v, "-h"))
-        usg();
-    ini();
-    write();
-    fin();
+    real *x;
+    real *y;
+    real *z;
+    He *he;
+
+    while (*++argv != NULL && argv[0][0] == '-')
+        switch (argv[0][1]) {
+        case 'h':
+            usg();
+            break;
+        default:
+            fprintf(stderr, "%s: unknown option '%s'\n", me, argv[0]);
+            exit(1);
+        }
+
+    err_set_ignore();
+    if (ply_he_ini(stdin, &he, &x, &y, &z) != CO_OK) {
+      fprintf(stderr, "%s: fail to read ply file\n", me);
+      exit(1);
+    }
+
+    if (off_he_xyz_fwrite(he, x, y, z, stdout) != CO_OK) {
+      fprintf(stderr, "%s: fail to write OFF file\n", me);
+      exit(1);
+    }
+    
+    ply_he_fin(he, x, y, z);
 }
