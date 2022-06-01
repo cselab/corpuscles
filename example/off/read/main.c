a0522881a35b37a8e90c12f461f673c67fa7d933
Sergey Litvinov <slitvinov@gmail.com>
2022-06-01 18:45:46 -0400

    simplify off/read/main.c

--- example/off/read/main.c
+++ example/off/read/main.c
@@ -5,10 +5,10 @@
 #include <co/he.h>
 #include <co/y.h>
 
-#define FMT CO_REAL_OUT
-
-int main(void) {
-  real *x, *y, *z, hi;
+int
+main(void)
+{
+    real *x, *y, *z, hi;
     He *he;
     int nv, nt;
 
@@ -16,9 +16,8 @@ int main(void) {
     nv = he_nv(he);
     nt = he_nt(he);
     hi = array_max(nv, x);
-
-    printf("number of vertices is %d\n", nv);
-    printf("number of triangles is %d\n", nt);
-    printf("maximum x coordinate: " FMT "\n", hi);
+    printf("number of vertices is %d\n"
+           "number of triangles is %d\n"
+           "maximum x coordinate is " CO_REAL_OUT "\n", nv, nt, hi);
     y_fin(he, x, y, z);
 }
