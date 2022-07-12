da50b3683b41a6b61b4bd5623ac3c34a0d2da1de
Sergey Litvinov <slitvinov@gmail.com>
2022-07-12 13:31:05 +0200

    install.sh: fail on error

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-(cd tool && make)
-(cd pkgconfig && make)
-(cd m4/lib && make)
-(cd lib && make install)
+(cd tool && make) &&
+(cd pkgconfig && make) &&
+(cd m4/lib && make) &&
+(cd lib && make install) &&
 for i in bin/*/
 do (cd $i; make install)
 done
