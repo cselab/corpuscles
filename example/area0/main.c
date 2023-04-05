8cb3e4f6f64f067a8128600b7ca5ca3b1415c613
Sergey Litvinov <slitvinov@gmail.com>
2023-04-05 19:53:59 -0400

    update area0/main.c

--- example/area0/main.c
+++ example/area0/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-
 #include <real.h>
-#include <co/err.h>
-#include <co/x.h>
+#include <co/area.h>
+#include <co/y.h>
 
 int main() {
-    ini("/dev/stdin");
-    printf("%g\n", area());
-    fin();
-    return CO_OK;
+  He *he;
+  real *x, *y, *z;
+  y_inif(stdin, &he, &x, &y, &z);
+  printf(CO_REAL_OUT "\n", he_area(he, x, y, z));
+  y_fin(he, x, y, z);
+  return 0;
 }
